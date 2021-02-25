0b97bb69b6651ce0088744fb961096276f5cd87c
LiuChiaChi <709153940@qq.com>
2021-02-25 14:08:06 +0000

    update version info

--- paddlenlp/__init__.py
+++ paddlenlp/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '2.0.0rc2'
+__version__ = '2.0.0rc3'
 
 from . import data
 from . import datasets
