f675ab7e607e9eb0878d7100679844f881a6e620
Steffy-zxf <zhangxuefei@baidu.com>
2021-03-02 16:17:33 +0800

    update Copy License

--- examples/information_extraction/knowledge_extraction/re_official_evaluation.py
+++ examples/information_extraction/knowledge_extraction/re_official_evaluation.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 ########################################################
-# Copyright (c) 2019, Baidu Inc. All rights reserved.
+# Copyright (c) 2021, Baidu Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
