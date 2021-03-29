31abec9c0b716130a9bb9f51b92d5109c80beeb8
zhoushunjie <zhoushunjie@baidu.com>
2021-03-29 06:36:07 +0000

    allow run coverage for module

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -1,7 +1,8 @@
 MODULE=paddlenlp
-if [ "$#" -gt 0 ];
-then  MODULE=$1
+IFS=','
+if [ "$#" -gt 0 ]; then
+MODULE=$*
 fi
-coverage run --source $MODULE -m unittest discover
+coverage run --source "$MODULE" -m unittest discover
 coverage report -m
 coverage html
