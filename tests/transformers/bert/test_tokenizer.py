02cb6173fd4ce87e2e34fa825ed1b80b0679ada6
zhoushunjie <zhoushunjie@baidu.com>
2021-04-07 09:05:11 +0000

    add expensive decorator to decorate test case

--- tests/transformers/bert/test_tokenizer.py
+++ tests/transformers/bert/test_tokenizer.py
@@ -11,3 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import numpy as np
+import os
+import unittest
+from paddlenlp.transformers import BertTokenizer
+
+from common_test import CpuCommonTest
+import util
