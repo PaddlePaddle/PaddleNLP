c937f307d93d868af32dd0ac5bde64267b9dba10
kinghuin <kinghuin_chull@163.com>
2021-04-16 23:00:37 +0800

    rm waybill hapi (#268)
    * rm waybill happi
    
    * optimize waybill

--- examples/information_extraction/waybill_ie/model.py
+++ examples/information_extraction/waybill_ie/model.py
@@ -17,6 +17,40 @@ from paddlenlp.transformers import ErniePretrainedModel
 from paddlenlp.layers.crf import LinearChainCrf, ViterbiDecoder, LinearChainCrfLoss
 
 
+class BiGRUWithCRF(nn.Layer):
+    def __init__(self,
+                 emb_size,
+                 hidden_size,
+                 word_num,
+                 label_num,
+                 use_w2v_emb=False):
+        super(BiGRUWithCRF, self).__init__()
+        if use_w2v_emb:
+            self.word_emb = TokenEmbedding(
+                extended_vocab_path='./data/word.dic', unknown_token='OOV')
+        else:
+            self.word_emb = nn.Embedding(word_num, emb_size)
+        self.gru = nn.GRU(emb_size,
+                          hidden_size,
+                          num_layers=2,
+                          direction='bidirectional')
+        self.fc = nn.Linear(hidden_size * 2, label_num + 2)  # BOS EOS
+        self.crf = LinearChainCrf(label_num)
+        self.crf_loss = LinearChainCrfLoss(self.crf)
+        self.viterbi_decoder = ViterbiDecoder(self.crf.transitions)
+
+    def forward(self, inputs, lengths, labels=None):
+        embs = self.word_emb(inputs)
+        output, _ = self.gru(embs)
+        emission = self.fc(output)
+        if labels is not None:
+            loss = self.crf_loss(emission, lengths, labels)
+            return loss
+        else:
+            _, prediction = self.viterbi_decoder(emission, lengths)
+            return prediction
+
+
 class ErnieCrfForTokenClassification(nn.Layer):
     def __init__(self, ernie, crf_lr=100):
         super().__init__()
