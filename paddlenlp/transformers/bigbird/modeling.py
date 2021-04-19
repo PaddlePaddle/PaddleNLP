5428bb09a3b6e285799bd2fe8b8c6bc42fc61cb0
zhoushunjie <zhoushunjie@baidu.com>
2021-04-19 09:14:51 +0000

    fix embedding doc

--- paddlenlp/transformers/bigbird/modeling.py
+++ paddlenlp/transformers/bigbird/modeling.py
@@ -414,42 +414,42 @@ class BigBirdModel(BigBirdPretrainedModel):
             rand_mask_idx_list (`list`, optional):
                 A list which contains some tensors used in bigbird random block.
 
-            Returns:
-                A tuple of shape (``encoder_output``, ``pooled_output``)
+        Returns:
+            A tuple of shape (``encoder_output``, ``pooled_output``)
 
-            Examples:
-                .. code-block::
-                    
-                    import paddle
-                    from paddlenlp.transformers import BigBirdModel, BigBirdTokenizer
-                    from paddlenlp.transformers import create_bigbird_rand_mask_idx_list
+        Examples:
+            .. code-block::
+                
+                import paddle
+                from paddlenlp.transformers import BigBirdModel, BigBirdTokenizer
+                from paddlenlp.transformers import create_bigbird_rand_mask_idx_list
 
-                    tokenizer = BigBirdTokenizer.from_pretrained('bigbird-base-uncased')
-                    model = BigBirdModel.from_pretrained('bigbird-base-uncased')
-                    config = model.config
-                    max_seq_len = 512
-                    input_ids = tokenizer.convert_tokens_to_ids(
-                        tokenizer(
-                            "This is a docudrama story on the Lindy Chamberlain case and a look at "
-                            "its impact on Australian society It especially looks at the problem of "
-                            "innuendo gossip and expectation when dealing with reallife dramasbr br "
-                            "One issue the story deals with is the way it is expected people will all "
-                            "give the same emotional response to similar situations Not everyone goes "
-                            "into wild melodramatic hysterics to every major crisis Just because the "
-                            "characters in the movies and on TV act in a certain way is no reason to "
-                            "expect real people to do so"
-                        ))
-                    input_ids.extend([0] * (max_seq_len - len(input_ids)))
-                    seq_len = len(input_ids)
-                    input_ids = paddle.to_tensor([input_ids])
-                    rand_mask_idx_list = create_bigbird_rand_mask_idx_list(
-                        config["num_layers"], seq_len, seq_len, config["nhead"],
-                        config["block_size"], config["window_size"], config["num_global_blocks"],
-                        config["num_rand_blocks"], config["seed"])
-                    rand_mask_idx_list = [
-                        paddle.to_tensor(rand_mask_idx) for rand_mask_idx in rand_mask_idx_list
-                    ]
-                    output = model(input_ids, rand_mask_idx_list=rand_mask_idx_list)
+                tokenizer = BigBirdTokenizer.from_pretrained('bigbird-base-uncased')
+                model = BigBirdModel.from_pretrained('bigbird-base-uncased')
+                config = model.config
+                max_seq_len = 512
+                input_ids = tokenizer.convert_tokens_to_ids(
+                    tokenizer(
+                        "This is a docudrama story on the Lindy Chamberlain case and a look at "
+                        "its impact on Australian society It especially looks at the problem of "
+                        "innuendo gossip and expectation when dealing with reallife dramasbr br "
+                        "One issue the story deals with is the way it is expected people will all "
+                        "give the same emotional response to similar situations Not everyone goes "
+                        "into wild melodramatic hysterics to every major crisis Just because the "
+                        "characters in the movies and on TV act in a certain way is no reason to "
+                        "expect real people to do so"
+                    ))
+                input_ids.extend([0] * (max_seq_len - len(input_ids)))
+                seq_len = len(input_ids)
+                input_ids = paddle.to_tensor([input_ids])
+                rand_mask_idx_list = create_bigbird_rand_mask_idx_list(
+                    config["num_layers"], seq_len, seq_len, config["nhead"],
+                    config["block_size"], config["window_size"], config["num_global_blocks"],
+                    config["num_rand_blocks"], config["seed"])
+                rand_mask_idx_list = [
+                    paddle.to_tensor(rand_mask_idx) for rand_mask_idx in rand_mask_idx_list
+                ]
+                output = model(input_ids, rand_mask_idx_list=rand_mask_idx_list)
         """
         embedding_output = self.embeddings(input_ids, token_type_ids)
         attention_mask_list, query_mask, key_mask = self._process_mask(
@@ -766,7 +766,7 @@ class BigBirdPretrainingCriterion(paddle.nn.Layer):
                 The masked token labels. Shape: [mask_token_num, 1]
             next_sentence_labels (`Tensor`):
                 The labels of NSP tasks. Shape: [batch_size, 1]
-            masked_lm_scale (`Tensor`| int):
+            masked_lm_scale (`Tensor` or `int`):
                 The scale of masked tokens.
             masked_lm_weights (`Tensor`):
                 The weight of masked tokens. Shape: [mask_token_num, 1]
