a300a3b3e1bbae904c7863ce341a71a4870109d3
FrostML <380185688@qq.com>
2021-04-23 08:47:36 +0000

    refine benchmark.sh

--- examples/machine_translation/transformer/deploy/benchmark.sh
+++ examples/machine_translation/transformer/deploy/benchmark.sh
@@ -5,24 +5,24 @@ log_path="output_pipeline"
 for batch_size in "1" "2" "4"; do
     python python/inference.py \
         --config="../configs/transformer.base.yaml" \
-        --model_dir=${model_dir} \
-        --batch_size=${batch_size} \
+        --model-dir=${model_dir} \
+        --batch-size=${batch_size} \
         --profile > ${log_path}/transformer_base_cpu_nomkl_bs${batch_size}_inference.log 2>&1
 
     for threads in "1" "6"; do
         python python/inference.py \
             --config="../configs/transformer.base.yaml" \
-            --model_dir=${model_dir} \
-            --use_mkl \
+            --model-dir=${model_dir} \
+            --use-mkl \
             --threads=${threads} \
-            --batch_size=${batch_size} \
+            --batch-size=${batch_size} \
             --profile > ${log_path}/transformer_base_cpu_mkl_threads${threads}_bs${batch_size}_inference.log 2>&1 
     done
 
     python python/inference.py \
         --config="../configs/transformer.base.yaml" \
-        --model_dir=${model_dir} \
-        --use_gpu \
-        --batch_size=${batch_size} \
+        --model-dir=${model_dir} \
+        --use-gpu \
+        --batch-size=${batch_size} \
         --profile > tee ${log_path}/transformer_base_gpu_bs${batch_size}_inference.log 2>&1 
 done
