b5a041b8d4a823895d48b5091802968921b501c4
LiuChiaChi <709153940@qq.com>
2021-04-27 14:39:04 +0000

    delete code copied from base class, and call it directly

--- paddlenlp/transformers/distilbert/tokenizer.py
+++ paddlenlp/transformers/distilbert/tokenizer.py
@@ -55,69 +55,8 @@ class DistilBertTokenizer(BertTokenizer):
                  return_length=False,
                  return_overflowing_tokens=False,
                  return_special_tokens_mask=False):
-        """
-            Main method to tokenize and prepare for the model one or several sequence(s) or one or several pair(s) of
-            sequences. This method will call `self.encode()` or `self.batch_encode()` depending on input format and  
-            `is_split_into_words` argument.
-            Args:
-                text (:obj:`str`, :obj:`List[str]`, :obj:`List[List[str]]`):
-                    The sequence or batch of sequences to be encoded. Each sequence can be a string or a list of strings
-                    (pretokenized string). If the sequences are provided as list of strings (pretokenized), you must set
-                    :obj:`is_split_into_words=True` (to lift the ambiguity with a batch of sequences).
-                text_pair (:obj:`str`, :obj:`List[str]`, :obj:`List[List[str]]`):
-                    The sequence or batch of sequences to be encoded. Each sequence can be a string or a list of strings
-                    (pretokenized string). If the sequences are provided as list of strings (pretokenized), you must set
-                    :obj:`is_split_into_words=True` (to lift the ambiguity with a batch of sequences).
-            """
-        # Input type checking for clearer error
-        assert isinstance(text, str) or (
-            isinstance(text, (list, tuple)) and (len(text) == 0 or (
-                isinstance(text[0], str) or
-                (isinstance(text[0], (list, tuple)) and
-                 (len(text[0]) == 0 or isinstance(text[0][0], str)))))
-        ), ("text input must of type `str` (single example), `List[str]` (batch or single pretokenized example) "
-            "or `List[List[str]]` (batch of pretokenized examples).")
-
-        assert (text_pair is None or isinstance(text_pair, str) or (
-            isinstance(text_pair, (list, tuple)) and (len(text_pair) == 0 or (
-                isinstance(text_pair[0], str) or
-                (isinstance(text_pair[0], (list, tuple)) and
-                 (len(text_pair[0]) == 0 or isinstance(text_pair[0][0], str)))))
-        )), (
-            "text_pair input must of type `str` (single example), `List[str]` (batch or single pretokenized example) "
-            "or `List[List[str]]` (batch of pretokenized examples).")
-
-        is_batched = bool(
-            (not is_split_into_words and isinstance(text, (list, tuple))) or
-            (is_split_into_words and isinstance(text, (list, tuple)) and
-             text and isinstance(text[0], (list, tuple))))
-
-        if is_batched:
-            batch_text_or_text_pairs = list(zip(
-                text, text_pair)) if text_pair is not None else text
-            return self.batch_encode(
-                batch_text_or_text_pairs=batch_text_or_text_pairs,
-                max_seq_len=max_seq_len,
-                stride=stride,
-                is_split_into_words=is_split_into_words,
-                pad_to_max_seq_len=pad_to_max_seq_len,
-                truncation_strategy="longest_first",
-                return_position_ids=return_position_ids,
-                return_token_type_ids=return_token_type_ids,
-                return_attention_mask=return_attention_mask,
-                return_length=return_length,
-                return_overflowing_tokens=return_overflowing_tokens,
-                return_special_tokens_mask=return_special_tokens_mask)
-        else:
-            return self.encode(
-                text=text,
-                text_pair=text_pair,
-                max_seq_len=max_seq_len,
-                pad_to_max_seq_len=pad_to_max_seq_len,
-                truncation_strategy="longest_first",
-                return_position_ids=return_position_ids,
-                return_token_type_ids=return_token_type_ids,
-                return_attention_mask=return_attention_mask,
-                return_length=return_length,
-                return_overflowing_tokens=return_overflowing_tokens,
-                return_special_tokens_mask=return_special_tokens_mask)
+        return super(DistilBertTokenizer, self).__call__(
+            text, text_pair, max_seq_len, stride, is_split_into_words,
+            pad_to_max_seq_len, truncation_strategy, return_position_ids,
+            return_token_type_ids, return_attention_mask, return_length,
+            return_overflowing_tokens, return_special_tokens_mask)
