da101fdcec48cf8de364f175b04a4cdf5b2038ad
Zhong Hui <zhonghui.net@gmail.com>
2021-05-06 18:38:27 +0800

    [GPT] delete some code.

--- paddlenlp/transformers/gpt/modeling.py
+++ paddlenlp/transformers/gpt/modeling.py
@@ -288,7 +288,6 @@ class TransformerDecoder(nn.Layer):
                                         use_cache=use_cache,
                                         cache=cache[i])
                 new_caches.append(new_cache)
-            # paddle.static.Print(output, message=f'checkpoints: {i} layers.')
             self.checkpoints.append(output.name)
 
         if self.norm is not None:
@@ -408,62 +407,6 @@ class TransformerDecoderLayer(nn.Layer):
         incremental_cache = self.self_attn.gen_cache(
             memory, type=self.self_attn.Cache)
         return incremental_cache
-
-
-class GPTEmbeddingsStatic:
-    """
-    Include embeddings from word, position and token_type embeddings
-    """
-
-    def __init__(self,
-                 vocab_size,
-                 hidden_size=768,
-                 hidden_dropout_prob=0.1,
-                 max_position_embeddings=512,
-                 type_vocab_size=16,
-                 initializer_range=0.02,
-                 topo=None):
-        super(GPTEmbeddingsStatic, self).__init__()
-        self.vocab_size = vocab_size
-        self.hidden_size = hidden_size
-        self.hidden_dropout_prob = hidden_dropout_prob
-        self.max_position_embeddings = max_position_embeddings
-        self.type_vocab_size = type_vocab_size
-        self.initializer_range = initializer_range
-
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-
-    def forward(self, input_ids, position_ids=None):
-
-        input_embedings = paddle.fluid.layers.embedding(
-            paddle.fluid.layers.unsqueeze(input_ids, -1),
-            (self.vocab_size, self.hidden_size),
-            param_attr=paddle.ParamAttr(
-                name="word_embeddings",
-                initializer=nn.initializer.Normal(
-                    mean=0.0, std=self.initializer_range)))
-
-        position_embeddings = paddle.fluid.layers.embedding(
-            paddle.fluid.layers.unsqueeze(position_ids, -1),
-            (self.max_position_embeddings, self.hidden_size),
-            param_attr=paddle.ParamAttr(
-                name="pos_embeddings",
-                initializer=nn.initializer.Normal(
-                    mean=0.0, std=self.initializer_range)))
-
-        from collections import namedtuple
-        LayerEmbedding = namedtuple('LayerEmbedding', ['weight'])
-        self.word_embeddings = LayerEmbedding(
-            weight=paddle.fluid.default_main_program().global_block().var(
-                "word_embeddings"))
-        # paddle.static.Print(self.word_embeddings.weight, message="embedding table")
-
-        embeddings = input_embedings + position_embeddings
-        embeddings = paddle.fluid.layers.dropout(embeddings,
-                                                 self.hidden_dropout_prob)
-
-        return embeddings
 
 
 class GPTEmbeddings(nn.Layer):
