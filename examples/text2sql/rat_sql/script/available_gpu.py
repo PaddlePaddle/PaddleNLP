afc0f0beb6e065910238956d540d1b7352ef2713
kinghuin <kinghuin_chull@163.com>
2021-05-13 11:53:08 +0800

    add license

--- examples/text2sql/rat_sql/script/available_gpu.py
+++ examples/text2sql/rat_sql/script/available_gpu.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-##########################################################
-# Copyright (c) 2019 Baidu.com, Inc. All Rights Reserved #
-##########################################################
-"""print available_gpu id, using nvgpu
-
-Filname: available_gpu.py
-Authors: ZhangAo(@baidu.com)
-Date: 2019-12-05 15:51:32
-"""
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import sys
 import os
