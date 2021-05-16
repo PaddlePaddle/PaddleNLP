ef76e08ebc1cd32c061ff5e1761c256189ff49c3
Zhong Hui <zhonghui.net@gmail.com>
2021-05-16 20:42:05 +0800

    ERNIE, add readme.

--- examples/language_model/ernie/pretraining_args.py
+++ examples/language_model/ernie/pretraining_args.py
@@ -30,8 +30,7 @@ def define_args():
     # Model Args
     parser.add_argument(
         '--ernie_config_file', type=str, default='./config/ernie_config.json')
-    parser.add_argument(
-        '--vocab_file', type=str, default='./config/30k-clean.vocab.albert')
+    parser.add_argument('--vocab_file', type=str, default='./config/vocab.txt')
     parser.add_argument('--init_checkpoint', type=str, default="")
     parser.add_argument('--max_seq_len', type=int, default=512)
     parser.add_argument('--preln', type=str2bool, default=False)
