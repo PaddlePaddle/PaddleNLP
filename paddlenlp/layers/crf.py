2563a9d1eb75a459a356c147a86dc04661b6c44a
kinghuin <kinghuin_chull@163.com>
2021-05-18 00:29:26 +0800

    optimize crf, tcn and erniectm docstring (#291)
    * optimize crf, tcn and erniectm docstring
    
    * optimize doc
    
    * optimize doc
    
    * optimize doc
    
    Co-authored-by: Zeyu Chen <chenzeyu01@baidu.com>

--- paddlenlp/layers/crf.py
+++ paddlenlp/layers/crf.py
@@ -28,11 +28,13 @@ class LinearChainCrf(nn.Layer):
     See https://repository.upenn.edu/cgi/viewcontent.cgi?article=1162&context=cis_papers for reference.
 
     Args:
-        batch_size (int): The batch size.
-        num_labels (int): The label number.
-        crf_lr (float): The crf layer learning rate.
-        with_start_stop_tag (bool): If set to True, the start tag and stop tag will be considered, the transitions params will be a tensor with shape `[num_labels+2, num_labels+2]`.
-                                Else, the transitions params will be a tensor with shape `[num_labels, num_labels]`.
+        num_labels (`int`): 
+            The label number.
+        crf_lr (`float`, optional): 
+            The crf layer learning rate. Defaults to ``0.1``.
+        with_start_stop_tag (`bool`, optional): 
+            If set to True, the start tag and stop tag will be considered, the transitions params will be a tensor with a shape of `[num_labels+2, num_labels+2]`.
+            Else, the transitions params will be a tensor with a shape of `[num_labels, num_labels]`.
     """
 
     def __init__(self, num_labels, crf_lr=0.1, with_start_stop_tag=True):
@@ -95,17 +97,17 @@ class LinearChainCrf(nn.Layer):
             F(2) & = log\\sum_{y1}\\sum_{y2} exp(p(y_1) + T([START], y1) + p(y_2) + T(y_1,y_2)) \\\\
             & = log\\sum_{y2} exp(F(1) + p(y_2) + T(y_1,y_2))
 
-        .. math::
-            F(...) = ...
-
-        A recursive formula.
+        Further, We can get F(n) is a recursive formula with F(n-1).
 
         Args:
-            inputs (Tensor): The input tensor with shape `[batch_size, sequence_length, num_tags]`.
-            lengths (Tensor): The input length with shape `[batch_size]`.
+            inputs (`Tensor`): 
+                The input predicted tensor. Its dtype is float32 and has a shape of `[batch_size, sequence_length, num_tags]`.
+            lengths (`Tensor`): 
+                The input length. Its dtype is int64 and has a shape of `[batch_size]`.
 
         Returns:
-            Tensor: The normalizers tensor, with shape `[batch_size]`.
+            norm_score (`Tensor`): 
+                The normalizers tensor. Its dtype is float32 and has a shape of `[batch_size]`.
         """
         batch_size, seq_len, n_labels = inputs.shape
         inputs_t_exp = inputs.transpose([1, 0, 2]).unsqueeze(-1).expand(
@@ -150,16 +152,20 @@ class LinearChainCrf(nn.Layer):
         $$ score(x,y) = \\sum_i Emit(x_i,y_i) + Trans(y_{i-1}, y_i) $$
 
         Args:
-            inputs (Tensor): The input tensor with shape `[batch_size, sequence_length, num_tags]`.
-            labels (Tensor) : The label tensor with shape `[batch_size, sequence_length]`
-            lengths (Tensor): The input length with shape `[batch_size]`.
+            inputs (`Tensor`): 
+                The input predicted tensor. Its dtype is float32 and has a shape of `[batch_size, sequence_length, num_tags]`.
+            labels (`Tensor`) : 
+                The input label tensor. Its dtype is int64 and has a shape of `[batch_size, sequence_length]`
+            lengths (`Tensor`): 
+                The input length. Its dtype is int64 and has a shape of `[batch_size]`.
 
         Returns:
-            Tensor: The unnormalized sequence scores tensor, with shape `[batch_size]`.
+            unnorm_score (`Tensor`): 
+                The unnormalized sequence scores tensor. Its dtype is float32 and has a shape of `[batch_size]`.
         """
-
-        return self._point_score(inputs, labels, lengths) + self._trans_score(
-            labels, lengths)
+        unnorm_score = self._point_score(
+            inputs, labels, lengths) + self._trans_score(labels, lengths)
+        return unnorm_score
 
     def _point_score(self, inputs, labels, lengths):
         batch_size, seq_len, n_labels = inputs.shape
@@ -256,13 +262,12 @@ class LinearChainCrf(nn.Layer):
 
 
 class LinearChainCrfLoss(nn.Layer):
-    """The negative log-likelihood for linear chain Conditional Random Field (CRF).
-
-    let $$ Z(x) = \\sum_{y'}exp(score(x,y')) $$, means the sum of all path scores,
-    then we have $$ loss = -logp(y|x) = -log(exp(score(x,y))/Z(x)) = -score(x,y) + logZ(x) $$
+    """
+    The negative log-likelihood for linear chain Conditional Random Field (CRF).
 
     Args:
-        crf (LinearChainCrf): The LinearChainCrf network.
+        crf (`LinearChainCrf`): 
+            The `LinearChainCrf` network object. Its parameter will be used to calculate the loss.
     """
 
     def __init__(self, crf):
@@ -274,6 +279,22 @@ class LinearChainCrfLoss(nn.Layer):
             )
 
     def forward(self, inputs, lengths, labels, old_version_labels=None):
+        """
+        Calculate the crf loss. Let $$ Z(x) = \\sum_{y'}exp(score(x,y')) $$, means the sum of all path scores,
+        then we have $$ loss = -logp(y|x) = -log(exp(score(x,y))/Z(x)) = -score(x,y) + logZ(x) $$
+
+        Args:
+            inputs (`Tensor`): 
+                The input predicted tensor. Its dtype is float32 and has a shape of `[batch_size, sequence_length, num_tags]`.
+            lengths (`Tensor`): 
+                The input length. Its dtype is int64 and has a shape of `[batch_size]`.
+            labels (`Tensor`) : 
+                The input label tensor. Its dtype is int64 and has a shape of `[batch_size, sequence_length]`
+            old_version_labels (`Tensor`, optional): Unnecessary parameter for compatibility with older versions. Defaults to ``None``.
+
+        Returns:
+            loss (`Tensor`): The crf loss. Its dtype is float32 and has a shape of `[batch_size]`.
+        """
         # Note: When closing to convergence, the loss could be a small negative number. This may caused by underflow when calculating exp in logsumexp.
         #       We add relu here to avoid negative loss. In theory, the crf loss must be greater than or equal to 0, relu will not impact on it.
         if old_version_labels is not None:
@@ -284,19 +305,23 @@ class LinearChainCrfLoss(nn.Layer):
                     'Compatibility Warning: The params of LinearChainCrfLoss.forward has been modified. The third param is `labels`, and the fourth is not necessary. Please update the usage.'
                 )
                 self.has_warn = True
-        return nn.functional.relu(
+        loss = nn.functional.relu(
             self.crf.forward(inputs, lengths) - self.crf.gold_score(
                 inputs, labels, lengths))
+        return loss
 
 
 class ViterbiDecoder(nn.Layer):
-    """ ViterbiDecoder can decode the highest scoring sequence of tags, it should only be used at test time.
+    """ 
+    ViterbiDecoder can decode the highest scoring sequence of tags, it should only be used at test time.
 
     Args:
-        transitions (tensor): The transition matrix with shape `[num_tags, num_tags]`.
-        with_start_stop_tag (bool): If set to True, the last row and the last column of transitions will be considered as start tag,
-                                the the penultimate row and the penultimate column of transitions will be considered as stop tag.
-                                Else, all the rows and columns will be considered as the real tag.
+        transitions (`Tensor`): 
+            The transition matrix.  Its dtype is float32 and has a shape of `[num_tags, num_tags]`.
+        with_start_stop_tag (`bool`, optional): 
+            If set to True, the last row and the last column of transitions will be considered as start tag,
+            the the penultimate row and the penultimate column of transitions will be considered as stop tag.
+            Else, all the rows and columns will be considered as the real tag. Defaults to ``None``.
     """
 
     def __init__(self, transitions, with_start_stop_tag=True):
@@ -337,11 +362,15 @@ class ViterbiDecoder(nn.Layer):
         Decode the highest scoring sequence of tags.
 
         Args:
-            inputs:  The unary emission tensor with shape `[batch_size, sequence_length, num_tags]`.
-            length: The input length tensor with shape `[batch_size]`, storing real length of each sequence for correctness.
+            inputs (`Tensor`):  
+                The unary emission tensor. Its dtype is float32 and has a shape of `[batch_size, sequence_length, num_tags]`.
+            length (`Tensor`):  
+                The input length tensor storing real length of each sequence for correctness. Its dtype is int64 and has a shape of `[batch_size]`.
         Returns:
-            scores: The scores tensor containing the score for the Viterbi sequence, with shape `[batch_size]`.
-            paths: The paths tensor containing the highest scoring tag indices, with shape `[batch_size, sequence_length`].
+            scores(`Tensor`): 
+                The scores tensor containing the score for the Viterbi sequence. Its dtype is float32 and has a shape of `[batch_size]`.
+            paths(`Tensor`): 
+                The paths tensor containing the highest scoring tag indices. Its dtype is int64 and has a shape of `[batch_size, sequence_length`].
         """
         input_shape = paddle.shape(inputs)
         batch_size = input_shape[0]
