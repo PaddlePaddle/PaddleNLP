a843c5f19f01b53e8ba934b64833bb9ce0050089
Jack Zhou <zhoushunjie@baidu.com>
2021-06-15 10:43:56 +0800

    add encoding in duee, duie (#564)

--- examples/information_extraction/DuEE/classifier.py
+++ examples/information_extraction/DuEE/classifier.py
@@ -133,7 +133,7 @@ class DuEventExtraction(paddle.io.Dataset):
 
     def _read_tsv(self, input_file, quotechar=None):
         """Reads a tab separated value file."""
-        with open(input_file, "r") as f:
+        with open(input_file, "r", encoding="UTF-8") as f:
             reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
             headers = next(reader)
             text_indices = [
