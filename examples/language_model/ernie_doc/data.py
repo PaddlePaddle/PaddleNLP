fix the wrong acc calculation

examples/language_model/ernie_doc/data.py
@@ -168,7 +168,10 @@ class ClassifierIterator(object):
                 cnt += 1
         return cnt
 
-    def _convert_to_features(self, example, qid=None):
+    def _convert_to_features(self, example, qid):
+        """
+        Convert example to features fed into model
+        """
         if "text" in example:  # imdb
             text = example["text"]
         elif "sentence" in example:  # iflytek
@@ -192,14 +195,15 @@ class ClassifierIterator(object):
             start_offset += min(length, self.memory_len)
 
         features = []
-        Feature = namedtuple("Feature", ["src_ids", "label_id", "cal_loss"])
+        Feature = namedtuple("Feature",
+                             ["src_ids", "label_id", "qid", "cal_loss"])
         for (doc_span_index, doc_span) in enumerate(doc_spans):
             tokens = tokens_a[doc_span.start:doc_span.start +
                               doc_span.length] + ["[SEP]"] + ["[CLS]"]
             token_ids = self.tokenizer.convert_tokens_to_ids(tokens)
             features.append(
                 Feature(
-                    src_ids=token_ids, label_id=label, cal_loss=1))
+                    src_ids=token_ids, label_id=label, qid=qid, cal_loss=1))
 
         if self.repeat_input:
             features_repeat = features
@@ -236,6 +240,12 @@ class ClassifierIterator(object):
                 [-1, 1])
         else:
             batch_labels = np.array([]).astype("int64").reshape([-1, 1])
+        # qid
+        if batch_records[-1].qid is not None:
+            batch_qids = [record.qid for record in batch_records]
+            batch_qids = np.array(batch_qids).astype("int64").reshape([-1, 1])
+        else:
+            batch_qids = np.array([]).astype("int64").reshape([-1, 1])
 
         if gather_idx:
             batch_gather_idx = np.array(gather_idx).astype("int64").reshape(
@@ -256,7 +266,7 @@ class ClassifierIterator(object):
 
         return_list = [
             padded_token_ids, padded_position_ids, padded_task_ids, input_mask,
-            batch_labels, batch_gather_idx, need_cal_loss
+            batch_labels, batch_qids, batch_gather_idx, need_cal_loss
         ]
         return return_list
 
@@ -285,8 +295,8 @@ class ClassifierIterator(object):
         examples = self.dataset
         pre_batch_list = []
         insert_idx = []
-        for index, example in enumerate(examples):
-            features = self._convert_to_features(example, index)
+        for qid, example in enumerate(examples):
+            features = self._convert_to_features(example, qid)
             if self._cnt_list(
                     pre_batch_list) < self.batch_size * self.trainer_num:
                 if insert_idx:
@@ -327,6 +337,7 @@ class ClassifierIterator(object):
     def get_num_examples(self):
         if self.num_examples is None:
             self.num_examples = 0
-            for example in self.dataset:
-                self.num_examples += len(self._convert_to_features(example))
+            for qid, example in enumerate(self.dataset):
+                self.num_examples += len(
+                    self._convert_to_features(example, qid))
         return self.num_examples

examples/language_model/ernie_doc/run_classifier.py
@@ -14,6 +14,7 @@
 
 import argparse
 import collections
+from collections import namedtuple, defaultdict
 
 import os
 import random
@@ -90,6 +91,9 @@ def init_memory(batch_size, memory_length, d_model, n_layers):
 def evaluate(model, criterion, metric, data_loader, memories0):
     """
     Given a dataset, it evals model and computes the metric.
+    Because the same sample may be splited into several samples and evaluated in 
+    different steps, the method to compute ERNIE-DOC classification acc has slightly
+    different. It merges the same sample results into one result. 
 
     Args:
         model(obj:`paddle.nn.Layer`): A model to classify texts.
@@ -103,22 +107,53 @@ def evaluate(model, criterion, metric, data_loader, memories0):
     memories = list(memories0)
     tic_train = time.time()
     eval_logging_step = 500
+
+    probs_dict = defaultdict(list)
+    label_dict = dict()
+    global_steps = 0
     for step, batch in enumerate(data_loader, start=1):
-        input_ids, position_ids, token_type_ids, attn_mask, labels, gather_idxs, need_cal_loss = batch
+        input_ids, position_ids, token_type_ids, attn_mask, labels, qids, \
+            gather_idxs, need_cal_loss = batch
         logits, memories = model(input_ids, memories, token_type_ids,
                                  position_ids, attn_mask)
-        logits, labels = list(
-            map(lambda x: paddle.gather(x, gather_idxs), [logits, labels]))
-        loss = criterion(logits, labels) * need_cal_loss
+        logits, labels, qids = list(
+            map(lambda x: paddle.gather(x, gather_idxs),
+                [logits, labels, qids]))
+        # need to collect probs for each qid, so use softmax_with_cross_entropy
+        loss, probs = nn.functional.softmax_with_cross_entropy(
+            logits, labels, return_softmax=True)
         losses.append(loss.mean().numpy())
-        correct = metric.compute(logits, labels) * need_cal_loss
-        metric.update(correct)
+        # shape: [B, NUM_LABELS]
+        np_probs = probs.numpy()
+        # shape: [B, 1]
+        np_qids = qids.numpy()
+        np_labels = labels.numpy().flatten()
+        for i, qid in enumerate(np_qids.flatten()):
+            probs_dict[qid].append(np_probs[i])
+            label_dict[qid] = np_labels[i]  # same qid share same label.
+
+        global_steps += 1
+        # loss = criterion(logits, labels) * need_cal_loss
+        # correct = metric.compute(logits, labels) * need_cal_loss
+        # metric.update(correct)
         if step % eval_logging_step == 0:
-            logger.info("Step %d: loss:  %.5f, accu: %.5f, speed: %.5f steps/s"
-                        % (step, np.mean(losses), metric.accumulate(),
-                           eval_logging_step / (time.time() - tic_train)))
+            logger.info("Step %d: loss:  %.5f, speed: %.5f steps/s" %
+                        (step, np.mean(losses),
+                         eval_logging_step / (time.time() - tic_train)))
             tic_train = time.time()
-    acc = metric.accumulate()
+
+    # calculate the acc
+    predict_labels = []
+    labels = []
+    for qid, probs in probs_dict.items():
+        mean_prob = np.mean(np.array(probs), axis=0)
+        predict_labels.append(np.argmax(mean_prob))
+        labels.append(label_dict[qid])
+
+    predict_labels = np.array(predict_labels, dtype='int64')
+    labels = np.array(labels, dtype='int64')
+    acc = (predict_labels == labels).sum() * 1.0 / labels.size
+
     logger.info("Eval loss: %.5f, accu: %.5f" % (np.mean(losses), acc))
     model.train()
     metric.reset()
@@ -245,7 +280,8 @@ def do_train(args):
         train_dataloader.set_batch_generator(train_ds_iter, paddle.get_device())
         for step, batch in enumerate(train_dataloader, start=1):
             global_steps += 1
-            input_ids, position_ids, token_type_ids, attn_mask, labels, gather_idx, need_cal_loss = batch
+            input_ids, position_ids, token_type_ids, attn_mask, labels, qids, \
+                gather_idx, need_cal_loss = batch
             logits, memories = model(input_ids, memories, token_type_ids,
                                      position_ids, attn_mask)
 
@@ -257,7 +293,7 @@ def do_train(args):
             optimizer.step()
             lr_scheduler.step()
             optimizer.clear_grad()
-
+            # rough acc result, not a precise acc
             acc = metric.compute(logits, labels) * need_cal_loss
             metric.update(acc)
 