6552a580218f0839897dbc64277d1be718449103
wawltor <fangzeyang0904@hotmail.com>
2021-07-14 17:25:05 +0800

    fix the example doc for the taskflow (#748)

--- paddlenlp/taskflow/sentiment_analysis.py
+++ paddlenlp/taskflow/sentiment_analysis.py
@@ -48,7 +48,7 @@ usage = r"""
            task = TaskFlow("sentiment_analysis")
            task("怀着十分激动的心情放映，可是看着看着发现，在放映完毕后，出现一集米老鼠的动画片")
            '''
-           [{'text': '怀着十分激动的心情放映，可是看着看着发现，在放映完毕后，出现一集米老鼠的动画片', 'label': 'positive'}]
+           [{'text': '怀着十分激动的心情放映，可是看着看着发现，在放映完毕后，出现一集米老鼠的动画片', 'label': 'negative'}]
            '''
 
            task = TaskFlow("sentiment_analysis", network="lstm")
