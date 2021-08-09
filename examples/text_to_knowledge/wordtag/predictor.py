bc178ab7dcfc7d6bd76e8fe52bcd469bae941d29
wawltor <fangzeyang0904@hotmail.com>
2021-08-09 17:00:14 +0800

    optimize the time cost for the wordtag (#863)
    * optimize the time cost for the wordtag
    
    * update the optimize code for the predictor

--- examples/text_to_knowledge/wordtag/predictor.py
+++ examples/text_to_knowledge/wordtag/predictor.py
@@ -217,7 +217,6 @@ class WordtagPredictor(object):
 
     def _split_long_text2short_text_list(self, input_texts, max_text_len):
         short_input_texts = []
-        short_input_texts_lens = []
         for text in input_texts:
             if len(text) <= max_text_len:
                 short_input_texts.append(text)
@@ -235,13 +234,35 @@ class WordtagPredictor(object):
                     ]
                     short_input_texts.extend(temp_text_list)
                 else:
-                    count = 0
-                    for temp_text in temp_text_list:
-                        if len(temp_text) + count < lens:
-                            temp_text = text[:len(temp_text) + count + 1]
-                        count += len(temp_text)
+                    list_len = len(temp_text_list)
+                    start = 0
+                    end = 0
+                    for i in range(0, list_len):
+                        if len(temp_text_list[i]) + 1 >= max_text_len:
+                            if start != end:
+                                short_input_texts.extend(
+                                    self._split_long_text_input(
+                                        [text[start:end]], max_text_len))
+                            short_input_texts.extend(
+                                self._split_long_text_input([
+                                    text[end:end + len(temp_text_list[i]) + 1]
+                                ], max_text_len))
+                            start = end + len(temp_text_list[i]) + 1
+                            end = start
+                        else:
+                            if start + len(temp_text_list[
+                                    i]) + 1 > max_text_len:
+                                short_input_texts.extend(
+                                    self._split_long_text_input(
+                                        [text[start:end]], max_text_len))
+                                start = end
+                                end = end + len(temp_text_list[i]) + 1
+                            else:
+                                end = len(temp_text_list[i]) + 1
+                    if start != end:
                         short_input_texts.extend(
-                            self._split_long_text2short_text_list([temp_text]))
+                            self._split_long_text_input([text[start:end]],
+                                                        max_text_len))
         return short_input_texts
 
     def _convert_short_text2long_text_result(self, input_texts, results):
@@ -268,7 +289,7 @@ class WordtagPredictor(object):
                     raise Exception("The len of text must same as raw text.")
         return concat_results
 
-    def _pre_process_text(self, input_texts, max_seq_len=128, batch_size=1):
+    def _pre_process_text(self, input_texts, max_seq_len=512, batch_size=1):
         infer_data = []
         max_predict_len = max_seq_len - self.summary_num - 1
         short_input_texts = self._split_long_text2short_text_list(
@@ -341,7 +362,7 @@ class WordtagPredictor(object):
     @paddle.no_grad()
     def run(self,
             input_texts,
-            max_seq_len=128,
+            max_seq_len=512,
             batch_size=1,
             return_hidden_states=None):
         """Predict a input text by wordtag.
