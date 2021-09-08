87758566829d6cfa8213c616206c6619eddc51d6
Jack Zhou <zhoushunjie@baidu.com>
2021-09-08 22:59:02 +0800

    Add text correction taskflow (#988)
    * add text_correction in task flow
    
    * add readme
    
    * modify model and pinyin vocab url
    
    * add is_chinese_char
    
    * display errors in results
    
    * add taskflow in ernie-csc README
    
    * move argmax to model implementation
    
    * remove unnecessary is_chinese_char
    
    * add more comments to introduce ErnieForCSC

--- examples/text_correction/ernie-csc/utils.py
+++ examples/text_correction/ernie-csc/utils.py
@@ -15,6 +15,8 @@
 from pypinyin import lazy_pinyin, Style
 import paddle
 
+from paddlenlp.transformers import is_chinese_char
+
 
 def read_train_ds(data_path):
     with open(data_path, 'r', encoding='utf-8') as f:
@@ -106,29 +108,6 @@ def create_dataloader(dataset,
         batch_sampler=batch_sampler,
         collate_fn=batchify_fn,
         return_list=True)
-
-
-def is_chinese_char(cp):
-    """Checks whether CP is the codepoint of a CJK character."""
-    # This defines a "chinese character" as anything in the CJK Unicode block:
-    #     https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
-    #
-    # Note that the CJK Unicode block is NOT all Japanese and Korean characters,
-    # despite its name. The modern Korean Hangul alphabet is a different block,
-    # as is Japanese Hiragana and Katakana. Those alphabets are used to write
-    # space-separated words, so they are not treated specially and handled
-    # like the all of the other languages.
-    if ((cp >= 0x4E00 and cp <= 0x9FFF) or  #
-        (cp >= 0x3400 and cp <= 0x4DBF) or  #
-        (cp >= 0x20000 and cp <= 0x2A6DF) or  #
-        (cp >= 0x2A700 and cp <= 0x2B73F) or  #
-        (cp >= 0x2B740 and cp <= 0x2B81F) or  #
-        (cp >= 0x2B820 and cp <= 0x2CEAF) or
-        (cp >= 0xF900 and cp <= 0xFAFF) or  #
-        (cp >= 0x2F800 and cp <= 0x2FA1F)):  #
-        return True
-
-    return False
 
 
 def parse_decode(words, corr_preds, det_preds, lengths, tokenizer,
