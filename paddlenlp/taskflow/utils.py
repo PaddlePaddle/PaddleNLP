3892227f02199ecb531186c94b5f2b985fa22ae1
zhoushunjie <zhoushunjie@baidu.com>
2021-09-08 14:50:31 +0800

    add text_correction in task flow

--- paddlenlp/taskflow/utils.py
+++ paddlenlp/taskflow/utils.py
@@ -39,7 +39,7 @@ def download_file(save_dir, filename, url, md5=None, task=None):
 
     Args:
         save_dir(string): The specified directory saving the file.
-        fiename(string): The specified filename saveing the file.
+        filename(string): The specified filename saving the file.
         url(string): The url downling the file.
         md5(string, optional): The md5 value that checking the version downloaded. 
     """
@@ -487,3 +487,26 @@ class TermTree(object):
                 node = self[nid]
                 if node.node_type == "term":
                     print(node, file=fp)
+
+
+def is_chinese_char(cp):
+    """Checks whether CP is the codepoint of a CJK character."""
+    # This defines a "chinese character" as anything in the CJK Unicode block:
+    #     https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
+    #
+    # Note that the CJK Unicode block is NOT all Japanese and Korean characters,
+    # despite its name. The modern Korean Hangul alphabet is a different block,
+    # as is Japanese Hiragana and Katakana. Those alphabets are used to write
+    # space-separated words, so they are not treated specially and handled
+    # like the all of the other languages.
+    if ((cp >= 0x4E00 and cp <= 0x9FFF) or  #
+        (cp >= 0x3400 and cp <= 0x4DBF) or  #
+        (cp >= 0x20000 and cp <= 0x2A6DF) or  #
+        (cp >= 0x2A700 and cp <= 0x2B73F) or  #
+        (cp >= 0x2B740 and cp <= 0x2B81F) or  #
+        (cp >= 0x2B820 and cp <= 0x2CEAF) or
+        (cp >= 0xF900 and cp <= 0xFAFF) or  #
+        (cp >= 0x2F800 and cp <= 0x2FA1F)):  #
+        return True
+
+    return False
