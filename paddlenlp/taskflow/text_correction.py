6fbcd6a0c316821a9b72a11e0672895d3409fd76
zhoushunjie <zhoushunjie@baidu.com>
2021-09-08 15:12:13 +0800

    add readme

--- paddlenlp/taskflow/text_correction.py
+++ paddlenlp/taskflow/text_correction.py
@@ -34,6 +34,18 @@ from .task import Task
 usage = r"""
            from paddlenlp import Taskflow
            text_correction = Taskflow("text_correction")
+           text_correction('遇到逆竟时，我们必须勇于面对，而且要愈挫愈勇，这样我们才能朝著成功之路前进。')
+           '''
+           [{'source': '遇到逆竟时，我们必须勇于面对，而且要愈挫愈勇，这样我们才能朝著成功之路前进。',
+             'target': '遇到逆境时，我们必须勇于面对，而且要愈挫愈勇，这样我们才能朝著成功之路前进。'}]
+           '''
+
+           text_correction('人生就是如此，经过磨练才能让自己更加拙壮，才能使自己更加乐观。')
+           '''
+           [{'source': '人生就是如此，经过磨练才能让自己更加拙壮，才能使自己更加乐观。',
+             'target': '人生就是如此，经过磨练才能让自己更加茁壮，才能使自己更加乐观。'}]
+           '''
+
          """
 
 URLS = {
