8c764855b9aaa58ca674036736a47362c4c5bd1b
shenliang03 <shenliang03@baidu.com>
2021-09-09 04:17:59 +0000

    fix code for sh

--- examples/language_model/gpt-3/dygraph/run.sh
+++ examples/language_model/gpt-3/dygraph/run.sh
@@ -1,8 +1,3 @@
-export PYTHONPATH=$PYTHONPATH:../../../../
-
-rm -rf dp2_pp2_mp2
-export NCCL_DEBUG=INFO
-#export NCCL_DEBUG_SUBSYS=ALL
 log_dir=dp2_pp2_mp2
 rm -rf $log_dir
 
@@ -26,26 +21,3 @@ python -m paddle.distributed.launch --log_dir $log_dir --gpus "0,1,2,3,4,5,6,7" 
     --pp_degree 1\
     --use_amp True\
     --scale_loss 32768
-
-# just for performance
-
-#nsys profile --stats=true -t cuda python -m paddle.distributed.launch --log_dir dp2_pp1_mp4 --gpus "0,1,2,3,4,5,6,7" run_pretrain.py \
-    #--model_type gpt \
-    #--model_name_or_path gpt2-small-en \
-    #--input_dir "./data"\
-    #--output_dir "output"\
-    #--weight_decay 0.01\
-    #--grad_clip 1.0\
-    #--max_steps 500000\
-    #--save_steps 100000\
-    #--decay_steps 320000\
-    #--device gpu\
-    #--eval_freq 1000\
-    #--warmup_rate 0.01\
-    #--micro_batch_size 8\
-    #--local_batch_size 8\
-    #--dp_degree 2\
-    #--mp_degree 2\
-    #--pp_degree 2\
-    #--use_amp True\
-    #--scale_loss 32768
