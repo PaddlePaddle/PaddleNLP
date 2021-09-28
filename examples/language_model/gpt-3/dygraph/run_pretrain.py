a200d3af357064f8fab07bdaf7033929bcb07ac9
zhangbo9674 <zhangbo54@baidu.com>
2021-09-28 11:31:50 +0000

    test

--- examples/language_model/gpt-3/dygraph/run_pretrain.py
+++ examples/language_model/gpt-3/dygraph/run_pretrain.py
@@ -214,7 +214,7 @@ def do_train(args):
             logger.warning("No optimizer checkpoint file found in %s." %
                            opt_path)
     
-    model, optimizer = paddle.amp.decorate(models=model, optimizers=optimizer, level='O1', master_weight=True)
+    #model, optimizer = paddle.amp.decorate(models=model, optimizers=optimizer, level='O1', master_weight=True)
 
     global_step = 0
     tic_train = time.time()
@@ -242,7 +242,7 @@ def do_train(args):
 
             for step, batch in enumerate(train_data_loader()):
                 global_step += 1
-                """
+                
                 # add nsight test
                 if step == 20:
                     core.nvprof_start()
@@ -256,7 +256,7 @@ def do_train(args):
                     core.nvprof_nvtx_pop()
                     core.nvprof_nvtx_push(str(step))
                 #-----------------
-                """
+                
 
                 tokens, loss_mask, position_ids, labels = batch
 
