a7d7788fde78125410f0083df0fb62f7125a1c20
Zhong Hui <zhonghui.net@gmail.com>
2021-10-25 21:30:14 +0800

    fix small problems as QA

--- tests/benchmark/run_benchmark.sh
+++ tests/benchmark/run_benchmark.sh
@@ -6,12 +6,15 @@ set -xe
 
 function _set_params(){
     run_mode=${1:-"sp"}         # sp or mp
-    batch_size=${2:-"2"}
+    base_batch_size=${2:-"2"}
     fp_item=${3:-"fp32"}        # fp32 or fp16
     max_iter=${4:-"100"}
     model_name=${5:-"model_name"}
     dygraph_name=${6:-"static"}
-    gpt_repo=${7:-"gpt2"}
+    need_profile=${7:-"off"}
+    gpt_repo=${8:-"gpt2"}
+
+    model_name=${model_name}_${gpt_repo}
     mission_name="语义表示"
     direction_id=1
     run_log_path=${TRAIN_LOG_DIR:-$(pwd)}
@@ -19,45 +22,64 @@ function _set_params(){
     device=${CUDA_VISIBLE_DEVICES//,/ }
     arr=(${device})
     num_gpu_devices=${#arr[*]}
-    log_file=${run_log_path}/${model_name}_${gpt_repo}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}_${dygraph_name}
-    log_folder=${run_log_path}/${model_name}_${gpt_repo}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}_${dygraph_name}_logdir
-    log_profile=${run_log_path}/${model_name}_${gpt_repo}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}_${dygraph_name}_model.profile
+
+
+    log_file=${run_log_path}/${model_name}_${run_mode}_bs${base_batch_size}_${fp_item}_${num_gpu_devices}_${dygraph_name}
+    log_folder=${run_log_path}/${model_name}_${run_mode}_bs${base_batch_size}_${fp_item}_${num_gpu_devices}_${dygraph_name}_logdir
+    log_profile=${run_log_path}/${model_name}_${run_mode}_bs${base_batch_size}_${fp_item}_${num_gpu_devices}_${dygraph_name}_model.profile
     OUTPUT_PATH=${run_log_path}/output
+
+
+    log_with_profiler=$log_file
+    profiler_path=$log_profile
+    keyword="ips:" 
+    keyword_loss=""
+    separator=""
+    position=""
+    range=""
+    skip_steps=20
+    model_mode=-1
+    ips_unit='seqs/s'
+    index=""
+    gpu_num=$num_gpu_devices 
 }
 
 
 function _train(){
     echo "Train on ${num_gpu_devices} GPUs"
-    echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
+    echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$base_batch_size"
 
     if [ -d $OUTPUT_PATH ]; then
         rm -rf $OUTPUT_PATH
     fi
 
     if [ $fp_item = "fp16" ]; then
-        use_fp16_cmd="--use_amp true"
+        use_fp16_cmd="--use_amp true" 
+        if [ $dygraph_name = "dygraph" && $gpt_repo = "gpt3" ]; then
+            use_fp16_cmd="--use_pure_fp16 true"
+        fi
     fi
 
+    profiler_cmd=""
     profiler_options="batch_range=[100,110];profile_path=${log_profile}"
-    if [ $num_gpu_devices = "1" ]; then
+    if [ $need_profile = "on" ]; then
         profiler_cmd="--profiler_options=${profiler_options}"
     fi
 
     script_cmd="run_pretrain_static.py"
-    if [ $dygraph_name == "dygraph" ]; then
+    if [ $dygraph_name = "dygraph" ]; then
         script_cmd="run_pretrain.py"
-        profiler_cmd=""
     fi
 
     base_path="examples/language_model/gpt/"
-    if [ $gpt_repo == 'gpt3' ]; then
+    if [ $gpt_repo = 'gpt3' ]; then
         base_path=examples/language_model/gpt-3/${dygraph_name}
     fi
     data_path=$(pwd)"/data"
 
     train_cmd="${profiler_cmd}\
-               --micro_batch_size=${batch_size} \
-               --global_batch_size=$((${batch_size}*${num_gpu_devices})) \
+               --micro_batch_size=${base_batch_size} \
+               --global_batch_size=$((${base_batch_size}*${num_gpu_devices})) \
                --model_type="gpt"\
                --model_name_or_path="gpt2-en"\
                --input_dir=${data_path}\
@@ -106,21 +128,20 @@ function _train(){
     rm -r ${log_folder}
 }
 
-function _analysis_log(){
-    # gpu_num is 1, because we multiplied gpu num in the log
-    python analysis.py --filename ${log_file} \
-        --keyword "ips:" \
-        --base_batch_size $(($batch_size*1024)) \
-        --skip_steps 20 \
-        --model_mode -1 \
-        --model_name ${model_name}\
-        --mission_name ${mission_name}\
-        --direction_id ${direction_id} \
-        --run_mode ${run_mode} \
-        --gpu_num 1 \
-        --ips_unit 'tokens/s'
-}
+# function _analysis_log(){
+#     # gpu_num is 1, because we multiplied gpu num in the log
+#     python analysis.py --filename ${log_file} \
+#         --keyword "ips:" \
+#         --base_batch_size $(($base_batch_size*1024)) \
+#         --skip_steps 20 \
+#         --model_mode -1 \
+#         --model_name ${model_name}\
+#         --mission_name ${mission_name}\
+#         --direction_id ${direction_id} \
+#         --run_mode ${run_mode} \
+#         --gpu_num 1 \
+#         --ips_unit 'tokens/s'
+# }
 
 _set_params $@
 _train
-_analysis_log
