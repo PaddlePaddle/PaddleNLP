0f212ad400c3464bcbf57814e6924413c0311b2f
yingyibiao <yyb0576@163.com>
2021-10-29 13:48:54 +0800

    add xlnet benchmark

--- tests/benchmark/xlnet/run_benchmark.sh
+++ tests/benchmark/xlnet/run_benchmark.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xe
 
-# 运行示例：CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh ${run_mode} ${bs_item} ${fp_item} 500 ${model_mode}
+# 运行示例：CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh ${run_mode} ${batch_size} ${fp_item} 500 ${model_mode}
 # 参数说明
 function _set_params(){
     run_mode=${1:-"sp"}          # 单卡sp|多卡mp
@@ -18,37 +18,28 @@ function _set_params(){
     log_file=${run_log_path}/${model_name}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}
 }
 
-python -m paddle.distributed.launch --gpus "0" ../../../examples/language_model/xlnet/run_glue.py \
-    --model_name_or_path xlnet-base-cased \
-    --task_name SST-2 \
-    --max_seq_length 128 \
-    --batch_size 32 \
-    --learning_rate 2e-5 \
-    --num_train_epochs 3 \
-    --logging_steps 100 \
-    --save_steps 500 \
-    --output_dir ./tmp/
-
 
 function _train(){
     echo "Train on ${num_gpu_devices} GPUs"
     echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
 
     train_cmd="--model_name_or_path=${model_name}
-               --task_name=${task_name}
-               --max_seq_length=${max_seq_length}
+               --task_name=SST-2
+               --max_seq_length=128
                --batch_size=${batch_size}
-               --learning_rate=${learning_rate}
+               --learning_rate=2e-5
                --max_steps=${max_iter}
                --output_dir=${run_log_path}
                --fp=${fp_item}
-               "
+    "
 
     case ${run_mode} in
     sp)
-        train_cmd="python -m paddle.distributed.launch --gpus=$CUDA_VISIBLE_DEVICES ./run_glue.py ${train_cmd}" ;;
+        train_cmd="python -m paddle.distributed.launch --gpus=$CUDA_VISIBLE_DEVICES \
+        ../../../examples/language_model/xlnet/run_glue.py ${train_cmd}" ;;
     mp)
-        train_cmd="python -m paddle.distributed.launch --gpus=$CUDA_VISIBLE_DEVICES ./run_glue.py ${train_cmd}" ;;
+        train_cmd="python -m paddle.distributed.launch --gpus=$CUDA_VISIBLE_DEVICES \
+        ../../../examples/language_model/xlnet/run_glue.py ${train_cmd}" ;;
         log_parse_file="mylog/workerlog.0" ;;
     *) echo "choose run_mode(sp or mp)"; exit 1;
     esac
