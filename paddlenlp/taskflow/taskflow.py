b7426da656fd97c90ef696a6444e79a04787b649
linjieccc <623543001@qq.com>
2021-11-02 14:03:46 +0800

    Update task name

--- paddlenlp/taskflow/taskflow.py
+++ paddlenlp/taskflow/taskflow.py
@@ -28,7 +28,6 @@ from .poetry_generation import PoetryGenerationTask
 from .question_answering import QuestionAnsweringTask
 from .dependency_parsing import DDParserTask
 from .text_correction import CSCTask
-from .noun_phrase_tagging import NPTagTask
 
 warnings.simplefilter(action='ignore', category=Warning, lineno=0, append=False)
 
