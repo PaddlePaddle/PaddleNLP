fc628ab5f186cf4e39bd67eb4fa7f45f5ff26188
zhoushunjie <zhoushunjie@baidu.com>
2021-11-03 15:30:53 +0800

    add tf_tokenizer performance test

--- examples/experimental/faster_tokenizer/demo.py
+++ examples/experimental/faster_tokenizer/demo.py
@@ -2,6 +2,9 @@ import io
 import os
 import time
 
+import tensorflow as tf
+import tensorflow_text as tf_text
+
 import paddle
 import paddlenlp
 from paddlenlp.transformers import BertTokenizer
@@ -13,9 +16,12 @@ from transformers import AutoTokenizer
 
 max_seq_length = 128
 batch_size = 32
+epochs = 10
+steps = 100
+total_tokens = epochs * steps * max_seq_length
 
 text = '小说是文学的一种样式，一般描写人物故事，塑造多种多样的人物形象，但亦有例外。它是拥有不完整布局、发展及主题的文学作品。而对话是不是具有鲜明的个性，每个人物说的没有独特的语言风格，是衡量小说水准的一个重要标准。与其他文学样式相比，小说的容量较大，它可以细致的展现人物性格和命运，可以表现错综复杂的矛盾冲突，同时还可以描述人物所处的社会生活环境。小说一词，最早见于《庄子·外物》：“饰小说以干县令，其于大达亦远矣。”这里所说的小说，是指琐碎的言谈、小的道理，与现时所说的小说相差甚远。文学中，小说通常指长篇小说、中篇、短篇小说和诗的形式。小说是文学的一种样式，一般描写人物故事，塑造多种多样的人物形象，但亦有例外。它是拥有不完整布局、发展及主题的文学作品。而对话是不是具有鲜明的个性，每个人物说的没有独特的语言风格，是衡量小说水准的一个重要标准。与其他文学样式相比，小说的容量较大，它可以细致的展现人物性格和命运，可以表现错综复杂的矛盾冲突，同时还可以描述人物所处的社会生活环境。小说一词，最早见于《庄子·外物》：“饰小说以干县令，其于大达亦远矣。”这里所说的小说，是指琐碎的言谈、小的道理，与现时所说的小说相差甚远。文学中'
-data = [text[:max_seq_length]] * 100
+data = [text[:max_seq_length]] * steps
 
 pp_tokenizer = FasterTokenizer.from_pretrained("bert-base-chinese")
 
@@ -29,12 +35,12 @@ for batch_data in batches:
         text=batch_data, max_seq_len=max_seq_length)
 
 start = time.time()
-for _ in range(10):
+for _ in range(epochs):
     for batch_data in batches:
         input_ids, token_type_ids = pp_tokenizer(
             batch_data, max_seq_len=max_seq_length)
 end = time.time()
-print("pp_tokenizer: %.5f" % (end - start))
+print("pp_faster_tokenizer: %.5f tokens/s" % (total_tokens / (end - start)))
 
 hf_tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese", use_fast=True)
 
@@ -45,23 +51,23 @@ for batch_data in batches:
     encoded_inputs = hf_tokenizer(batch_data)
 
 start = time.time()
-for _ in range(10):
+for _ in range(epochs):
     for batch_data in batches:
         encoded_inputs = hf_tokenizer(
             batch_data)  #, padding=True, truncation=True)
 end = time.time()
-print("hf_tokenizer: %.5f" % (end - start))
+print("hf_tokenizer: %.5f tokens/s" % (total_tokens / (end - start)))
 
 py_tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
 for batch_data in batches:
     encoded_inputs = py_tokenizer(batch_data)
 
 start = time.time()
-for _ in range(10):
+for _ in range(epochs):
     for batch_data in batches:
         encoded_inputs = py_tokenizer(batch_data)
 end = time.time()
-print("py_tokenizer: %.5f" % (end - start))
+print("pp_py_tokenizer: %.5f tokens/s" % (total_tokens / (end - start)))
 
 hf_tokenizer = AutoTokenizer.from_pretrained(
     "bert-base-chinese", use_fast=False)
@@ -73,9 +79,34 @@ for batch_data in batches:
     encoded_inputs = hf_tokenizer(batch_data)
 
 start = time.time()
-for _ in range(10):
+for _ in range(epochs):
     for batch_data in batches:
         encoded_inputs = hf_tokenizer(
             batch_data)  #, padding=True, truncation=True)
 end = time.time()
-print("hf_py_tokenizer: %.5f" % (end - start))
+print("hf_py_tokenizer: %.5f tokens/s" % (total_tokens / (end - start)))
+
+vocab_list = list(py_tokenizer.vocab.token_to_idx.keys())
+lookup_table = tf.lookup.StaticVocabularyTable(
+    tf.lookup.KeyValueTensorInitializer(
+        keys=vocab_list,
+        key_dtype=tf.string,
+        values=tf.range(
+            tf.size(
+                vocab_list, out_type=tf.int64), dtype=tf.int64),
+        value_dtype=tf.int64),
+    num_oov_buckets=1)
+batches = [
+    data[idx:idx + batch_size] for idx in range(0, len(data), batch_size)
+]
+tf_tokenizer = tf_text.BertTokenizer(lookup_table)
+
+for batch_data in batches:
+    input_ids = tf_tokenizer.tokenize(batch_data)
+
+start = time.time()
+for _ in range(epochs):
+    for batch_data in batches:
+        input_ids = tf_tokenizer.tokenize(batch_data)
+end = time.time()
+print("tf_tokenizer: %.5f tokens/s" % (total_tokens / (end - start)))
