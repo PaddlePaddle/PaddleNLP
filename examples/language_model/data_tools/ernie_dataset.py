df7d1b8a6aa2a5525bbc2064e8f81ea7697bc829
wawltor <fangzeyang0904@hotmail.com>
2021-11-03 14:22:31 +0000

    format the some copyright

--- examples/language_model/data_tools/ernie_dataset.py
+++ examples/language_model/data_tools/ernie_dataset.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021, PadddlePaddle authors.  All rights reserved.
+# Copyright (c) 2021, PadddlePaddle authors. All Rights Reserved.
 # Copyright (c) 2020, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- examples/language_model/gpt/predict.py
+++ examples/language_model/gpt/predict.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
 # Copyright (c) 2020 TsinghuaAI Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
