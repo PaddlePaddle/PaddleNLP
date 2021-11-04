741dfe4a3f8e29deb25009a7b4c7dd618956be20
huhuiwen <1050615501@qq.com>
2021-11-04 16:16:46 +0800

    tokenizer docs update

--- paddlenlp/transformers/distilbert/tokenizer.py
+++ paddlenlp/transformers/distilbert/tokenizer.py
@@ -19,9 +19,9 @@ __all__ = ['DistilBertTokenizer']
 
 class DistilBertTokenizer(BertTokenizer):
     """
-    Constructs a DistilBertTokenizer.
-    The usage of DistilBertTokenizer is the same as
+    Constructs a DistilBert tokenizer. The usage of DistilBertTokenizer is the same as
     `BertTokenizer <https://paddlenlp.readthedocs.io/zh/latest/source/paddlenlp.transformers.bert.tokenizer.html>`__.
+    For more information regarding those methods, please refer to this superclass.
     """
     resource_files_names = {"vocab_file": "vocab.txt"}  # for save_pretrained
     pretrained_resource_files_map = {
