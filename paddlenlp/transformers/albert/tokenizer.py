f207fde89f77959dcb63721c82468c06129d7e92
yingyibiao <yyb0576@163.com>
2021-11-05 19:35:47 +0800

    Update code license and authors (#1279)

--- paddlenlp/transformers/albert/tokenizer.py
+++ paddlenlp/transformers/albert/tokenizer.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
-# Copyright 2018 Google AI, Google Brain and Carnegie Mellon University Authors and the HuggingFace Inc. team.
+# Copyright 2018 Google AI, Google Brain and the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
