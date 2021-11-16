0ea5fd43a19f8cd84caa0e5f0bbb77c3cd7ce569
Zeyu Chen <chenzeyu01@baidu.com>
2021-11-16 18:43:54 +0800

    Remove py2 compatible import

--- paddlenlp/utils/downloader.py
+++ paddlenlp/utils/downloader.py
@@ -11,11 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
 import sys
 import os.path as osp
