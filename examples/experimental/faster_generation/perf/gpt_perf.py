14094a4727f79d2a9ec450f0ac5c1b2834a5e96a
smallv0221 <397551318@qq.com>
2021-11-16 12:58:55 +0000

    add license

--- examples/experimental/faster_generation/perf/gpt_perf.py
+++ examples/experimental/faster_generation/perf/gpt_perf.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import argparse
 import time
 from pprint import pprint
