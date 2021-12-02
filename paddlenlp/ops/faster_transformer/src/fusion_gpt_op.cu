ce4ff5f17c7507ab9a73e562b80a28171d680edc
FrostML <380185688@qq.com>
2021-12-02 07:54:46 +0000

    fix gpt

--- paddlenlp/ops/faster_transformer/src/fusion_gpt_op.cu
+++ paddlenlp/ops/faster_transformer/src/fusion_gpt_op.cu
@@ -97,7 +97,7 @@ std::vector<paddle::Tensor> gpt2_kernel(
   decoding_params.request_batch_size = batch_size_;
   decoding_params.max_input_len = start_len;
   decoding_params.request_input_len = start_len;
-  decoding_params.request_output_len = max_len - start_len;
+  decoding_params.request_output_len = max_len;
 
   decoding_params.d_start_ids = input.data<int>();
   decoding_params.d_attn_mask =
@@ -107,7 +107,7 @@ std::vector<paddle::Tensor> gpt2_kernel(
   gpt_decoding =
       new DecodingGpt<DecodingTraits_::OpType>(allocator_,
                                                batch_size_,
-                                               max_len,
+                                               max_len + start_len,
                                                n_head,
                                                size_per_head,
                                                vocab_size,
@@ -128,10 +128,9 @@ std::vector<paddle::Tensor> gpt2_kernel(
       new DecoderInitParam<DataType_>[num_layer];
 
   for (int i = 0; i < num_layer; ++i) {
-    // TODO: multi-cards supports.
-    // if(layer_parallel_param.is_valid(i) == false) {
-    //     continue;
-    // }
+    if (layer_parallel_param.is_valid(i) == false) {
+      continue;
+    }
 
     params[i].stream = stream;
     params[i].cublas_handle = cublas_handle_;
