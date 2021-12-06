Fix GPT fp16 attention mask dtype (#1396)
* fix gpt fp16

* user input

paddlenlp/ops/faster_transformer/transformer/decoding.py
@@ -864,7 +864,11 @@ class InferGptDecoding(nn.Layer):
         if attention_mask is None:
             batch_size = paddle.shape(input_ids)[0]
             attention_mask = paddle.tril(
-                paddle.ones([batch_size, mem_seq_len, mem_seq_len]))
+                paddle.ones(
+                    [batch_size, mem_seq_len, mem_seq_len],
+                    dtype="float16" if self.use_fp16_decoding else "float32"))
+        elif self.use_fp16_decoding and attention_mask.dtype == paddle.float32:
+            attention_mask = paddle.cast(attention_mask, dtype="float16")
 
         output_ids = infer_gpt_decoding(
             input=[input_ids],