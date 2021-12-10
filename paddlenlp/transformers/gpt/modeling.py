e5b01c5d024837a40b162f50401c6e6f8d839723
guosheng <guosheng@baidu.com>
2021-12-10 20:52:47 +0800

    Fix GPT chinese detokenizing for FasterGPT samples.

--- paddlenlp/transformers/gpt/modeling.py
+++ paddlenlp/transformers/gpt/modeling.py
@@ -1132,7 +1132,7 @@ class GPTLMHeadModel(GPTPretrainedModel):
             "num_attention_heads"]
         if size_per_head not in [32, 64, 128]:
             raise AttributeError(
-                "'size_per_head = %d ' is not supported yet in the faster version of GPT"
+                "'size_per_head = %d' is not supported yet in the faster version of GPT"
                 % size_per_head)
         self._faster_entry = FasterGPT(
             self, use_fp16_decoding=use_fp16_decoding).forward
