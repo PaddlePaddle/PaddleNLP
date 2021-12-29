4b50fdb885a4711cbad46c320ad09ec9d43b41f4
吴高升 <w5688414@gmail.com>
2021-12-29 23:40:49 +0800

    update neural search readme (#1530)
    * add recall inference similarity
    
    * update examples
    
    * updatea readme
    
    * update dir name
    
    * update neural search readme
    
    * update milvus readme
    
    * update domain adaptive pretraining readme
    
    * fix the mistakes
    
    Co-authored-by: Zeyu Chen <chenzeyu01@baidu.com>

--- applications/neural_search/recall/simcse/scripts/predict.sh
+++ applications/neural_search/recall/simcse/scripts/predict.sh
@@ -10,11 +10,11 @@ python -u -m paddle.distributed.launch --gpus "3" \
     --text_pair_file "recall/test.csv"
 
 # cpu
-root_dir="checkpoints" 
-python  predict.py \
-    --device cpu \
-    --params_path "${root_dir}/model_20000/model_state.pdparams" \
-    --output_emb_size 256 \
-    --batch_size 128 \
-    --max_seq_length 64 \
-    --text_pair_file "recall/test.csv"
+# root_dir="checkpoints" 
+# python  predict.py \
+#     --device cpu \
+#     --params_path "${root_dir}/model_20000/model_state.pdparams" \
+#     --output_emb_size 256 \
+#     --batch_size 128 \
+#     --max_seq_length 64 \
+#     --text_pair_file "recall/test.csv"
