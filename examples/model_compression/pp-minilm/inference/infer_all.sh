0351d957f445bf095bd6921955dabb0b7149e1c3
Jiaqi Liu <liujiaqi06@baidu.com>
2022-01-17 19:19:50 +0800

    Update shape range setting method (#1599)
    * update shape range setting method
    
    * fix readme
    
    * fix readme
    
    * update readme

--- examples/model_compression/pp-minilm/inference/infer_all.sh
+++ examples/model_compression/pp-minilm/inference/infer_all.sh
@@ -19,7 +19,6 @@ do
     do
         for algo in abs_max avg hist mse
         do
-            python infer.py --task_name ${task}  --model_path  ../quantization/${task}_quant_models/${algo}${bs}/int8  --int8 --use_trt --collect_shape
             python infer.py --task_name ${task}  --model_path  ../quantization/${task}_quant_models/${algo}${bs}/int8  --int8 --use_trt
             echo this is ${task}, ${algo}, ${bs}
         done
