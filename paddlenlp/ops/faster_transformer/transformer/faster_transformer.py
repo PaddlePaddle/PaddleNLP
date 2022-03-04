8cc775534bb3f4dcb364665534ddf1abbda981d9
FrostML <380185688@qq.com>
2022-03-04 03:45:55 +0000

    update

--- paddlenlp/ops/faster_transformer/transformer/faster_transformer.py
+++ paddlenlp/ops/faster_transformer/transformer/faster_transformer.py
@@ -818,9 +818,14 @@ class FasterUnifiedTransformer(UnifiedTransformerPretrainedModel):
             normalize_before=self._normalize_before,
             hidden_act=self._hidden_act)
 
-    def prepare_inputs_for_generation(self, input_ids, token_type_ids,
-                                      attention_mask, seq_len, position_ids,
-                                      role_ids, **kwargs):
+    def prepare_inputs_for_generation(self,
+                                      input_ids,
+                                      token_type_ids,
+                                      attention_mask,
+                                      seq_len,
+                                      position_ids=None,
+                                      role_ids=None,
+                                      **kwargs):
         input_ids = input_ids[:, :-1]
         if input_ids.dtype == paddle.int64:
             input_ids = paddle.cast(input_ids, dtype="int32")
@@ -977,9 +982,8 @@ class FasterUnifiedTransformer(UnifiedTransformerPretrainedModel):
             raise ValueError(
                 "Only greedy search, beam search and sampling are supported. ")
 
-        model_inputs = self.prepare_inputs_for_generation(
-            input_ids, token_type_ids, attention_mask, seq_len, position_ids,
-            role_ids)
+        model_inputs = self.prepare_inputs_for_generation(input_ids,
+                                                          **model_kwargs)
 
         seq_len = model_inputs.pop('seq_len')
         decoder_type_ids = model_inputs.pop('decoder_type_ids')
