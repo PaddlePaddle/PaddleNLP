573e2bdec66223ff6abfb4bf6572aab1632204c5
Steffy-zxf <zhangxuefei@baidu.com>
2022-03-09 10:47:37 +0800

    remove redudant code

--- examples/text_classification/rnn/train.py
+++ examples/text_classification/rnn/train.py
@@ -33,7 +33,6 @@ parser.add_argument("--lr", type=float, default=5e-5, help="Learning rate used t
 parser.add_argument("--save_dir", type=str, default='checkpoints/', help="Directory to save model checkpoint")
 parser.add_argument("--batch_size", type=int, default=64, help="Total examples' number of a batch for training.")
 parser.add_argument("--vocab_path", type=str, default="./vocab.json", help="The directory to vocab_path.")
-parser.add_argument("--stop_words_path", type=str, default="./stop_words.txt", help="The path to stop words.")
 parser.add_argument('--network', choices=['bow', 'lstm', 'bilstm', 'gru', 'bigru', 'rnn', 'birnn', 'bilstm_attn', 'cnn'],
     default="bilstm", help="Select which network to train, defaults to bilstm.")
 parser.add_argument("--init_from_ckpt", type=str, default=None, help="The path of checkpoint to be loaded.")
