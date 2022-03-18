06288677639fc8712e9bd9dfbaa66ebe8d615b7b
yingyibiao <yyb0576@163.com>
2022-03-18 21:15:30 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.append(os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
