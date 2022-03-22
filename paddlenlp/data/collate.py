0e997ea20d5140357c26bf7327e09ee448016d84
smallv0221 <397551318@qq.com>
2022-03-22 06:39:25 +0000

    fix squad sample

--- paddlenlp/data/collate.py
+++ paddlenlp/data/collate.py
@@ -335,9 +335,6 @@ def default_data_collator(data):
     first = data[0]
     batch = {}
 
-    # Special handling for labels.
-    # Ensure that tensor is created with the correct type
-    # (it should be automatically the case, but let's make sure of it.)
     if "label" in first and first["label"] is not None:
         label = first["label"]
         dtype = 'int64' if isinstance(label, int) else 'float32'
@@ -346,8 +343,6 @@ def default_data_collator(data):
         dtype = 'int64' if type(first["label_ids"][0]) is int else 'float32'
         batch["labels"] = Stack(dtype=dtype)([d["label_ids"] for d in data])
 
-    # Handling of all other possible keys.
-    # Again, we will use the first element to figure out which key/values are not None for this model.
     for k, v in first.items():
         if k not in ("label", "label_ids") and v is not None and not isinstance(
                 v, str):
@@ -361,25 +356,8 @@ class DataCollatorWithPadding:
     Data collator that will dynamically pad the inputs received.
 
     Args:
-        tokenizer (:class:`~transformers.PreTrainedTokenizer` or :class:`~transformers.PreTrainedTokenizerFast`):
+        tokenizer (`paddlenlp.transformers.PreTrainedTokenizer`):
             The tokenizer used for encoding the data.
-        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.file_utils.PaddingStrategy`, `optional`, defaults to :obj:`True`):
-            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
-            among:
-
-            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
-              sequence if provided).
-            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
-              maximum acceptable input length for the model if that argument is not provided.
-            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
-              different lengths).
-        max_length (:obj:`int`, `optional`):
-            Maximum length of the returned list and optionally padding length (see above).
-        pad_to_multiple_of (:obj:`int`, `optional`):
-            If set will pad the sequence to a multiple of the provided value.
-
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
     """
 
     def __init__(self, tokenizer):
@@ -411,3 +389,58 @@ class DataCollatorWithPadding:
                                    dtype='int64')([d[k] for d in data])
 
         return batch
+
+
+'''
+class DataCollatorForTokenClassification:
+    """
+    Data collator that will dynamically pad the inputs received, as well as the labels.
+
+    Args:
+        tokenizer ([`PreTrainedTokenizer`] or [`PreTrainedTokenizerFast`]):
+            The tokenizer used for encoding the data.
+    """
+
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    label_pad_token_id: int = -100
+    return_tensors: str = "pt"
+
+    def torch_call(self, features):
+        import torch
+
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature[label_name] for feature in
+                  features] if label_name in features[0].keys() else None
+        batch = self.tokenizer.pad(
+            features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            # Conversion to tensors will fail if we have labels as they are not of the same length yet.
+            return_tensors="pt" if labels is None else None, )
+
+        if labels is None:
+            return batch
+
+        sequence_length = torch.tensor(batch["input_ids"]).shape[1]
+        padding_side = self.tokenizer.padding_side
+        if padding_side == "right":
+            batch[label_name] = [
+                list(label) + [self.label_pad_token_id] *
+                (sequence_length - len(label)) for label in labels
+            ]
+        else:
+            batch[label_name] = [[self.label_pad_token_id] *
+                                 (sequence_length - len(label)) + list(label)
+                                 for label in labels]
+
+        batch = {
+            k: torch.tensor(
+                v, dtype=torch.int64)
+            for k, v in batch.items()
+        }
+        return batch
+'''
