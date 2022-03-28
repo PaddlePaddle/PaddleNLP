026a035aa840603d582a43adfaf5b935b122104e
smallv0221 <397551318@qq.com>
2022-03-28 08:30:48 +0000

    add more check

--- paddlenlp/data/collate.py
+++ paddlenlp/data/collate.py
@@ -141,7 +141,8 @@ class Pad(object):
         """
 
         # return data itself for rare unexpected cases when 1-D array is passed to Pad
-        if not isinstance(data[0], list):
+        if not isinstance(data[0], list) and not isinstance(data[0],
+                                                            np.ndarray):
             return np.asarray(
                 data,
                 dtype=self._dtype if self._dtype is not None else np.int64)
