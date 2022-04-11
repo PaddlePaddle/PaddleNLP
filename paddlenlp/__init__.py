a3f9a763c8c57c04f588c3b9aba6e971ba73bd12
smallv0221 <397551318@qq.com>
2022-04-11 08:16:40 +0000

    enhence import warning

--- paddlenlp/__init__.py
+++ paddlenlp/__init__.py
@@ -17,9 +17,10 @@ import sys
 if 'datasets' in sys.modules.keys():
     from paddlenlp.utils.log import logger
     logger.warning(
-        "datasets module loaded before paddlenlp. "
+        "Detected that datasets module was imported before paddlenlp. "
         "This may cause PaddleNLP datasets to be unavalible in intranet"
-        "or download speed to be low in some area")
+        "Please import paddlenlp before datasets module to avoid download issues"
+    )
 from . import data
 from . import datasets
 from . import embeddings
