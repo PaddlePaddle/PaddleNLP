aecacd043b9979192afa1538d20547f2952a4e6b
Zhong Hui <zhonghui.net@gmail.com>
2022-04-13 12:02:29 +0800

    add more detail for code source.

--- paddlenlp/trainer/trainer_utils.py
+++ paddlenlp/trainer/trainer_utils.py
@@ -12,6 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Notice, most of this file is modified from 
+#  https://github.com/huggingface/transformers/blob/main/src/transformers/trainer_utils.py
+# Thanks a lot.
 """
 Utilities for the Trainer class. 
 """
