4370832a63dc79fb6aa25087d634b83d299cdd4f
小皮 <1963697267@qq.com>
2022-04-13 22:08:19 +0800

    fix bug

--- paddlenlp/transformers/rembert/modeling.py
+++ paddlenlp/transformers/rembert/modeling.py
@@ -125,8 +125,7 @@ class RemBertEmbeddings(nn.Layer):
                  hidden_dropout_prob=0,
                  layer_norm_eps=1e-12):
         super(RemBertEmbeddings, self).__init__()
-        self.word_embeddings = nn.Embedding(
-            vocab_size, input_embedding_size)
+        self.word_embeddings = nn.Embedding(vocab_size, input_embedding_size)
         self.position_embeddings = nn.Embedding(max_position_embeddings,
                                                 input_embedding_size)
         self.token_type_embeddings = nn.Embedding(type_vocab_size,
