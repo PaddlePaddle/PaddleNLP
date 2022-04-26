c8347b0f70e559d05c9460df583e5975774215e6
Yaojie Lu <yaojie@iscas.ac.cn>
2022-04-26 10:40:10 +0800

    Update inference.py and change to Pad

--- examples/information_extraction/DuUIE/inference.py
+++ examples/information_extraction/DuUIE/inference.py
@@ -2,20 +2,15 @@
 # -*- coding:utf-8 -*-
 import json
 import os
-import re
 import math
 from tqdm import tqdm
 
 import paddle
+from paddlenlp.data import Pad
 from paddlenlp.transformers import T5ForConditionalGeneration
 
-from uie.evaluation.sel2record import (
-    RecordSchema,
-    MapConfig,
-    SEL2Record
-)
+from uie.evaluation.sel2record import (RecordSchema, MapConfig, SEL2Record)
 from uie.seq2struct.t5_bert_tokenizer import T5BertTokenizer
-
 
 special_to_remove = {'<pad>', '</s>'}
 
@@ -24,11 +19,13 @@ def read_json_file(file_name):
     return [json.loads(line) for line in open(file_name)]
 
 
-def schema_to_prompt(schema: RecordSchema):
-    prompt = "<spot> " + "<spot> ".join(sorted(schema.type_list))
-    prompt += "<asoc> " + "<asoc> ".join(sorted(schema.role_list))
-    prompt += "<extra_id_2> "
-    return prompt
+def schema_to_ssi(schema: RecordSchema):
+    # Convert Schema to SSI
+    # <spot> spot type ... <asoc> asoc type <text>
+    ssi = "<spot>" + "<spot>".join(sorted(schema.type_list))
+    ssi += "<asoc>" + "<asoc>".join(sorted(schema.role_list))
+    ssi += "<extra_id_2>"
+    return ssi
 
 
 def post_processing(x):
@@ -37,98 +34,95 @@ def post_processing(x):
     return x.strip()
 
 
-class PaddlePredictor:
-    def __init__(self, model_path, max_source_length=256, max_target_length=192) -> None:
-        self._tokenizer = T5BertTokenizer.from_pretrained(model_path)
-        self._model = T5ForConditionalGeneration.from_pretrained(model_path)
-        self._model.eval()
-        self._max_source_length = max_source_length
-        self._max_target_length = max_target_length
-
-    def get_tokenizer(self):
-        return self._tokenizer
+class Predictor:
+    def __init__(self, model_path, max_source_length=256,
+                 max_target_length=192) -> None:
+        self.tokenizer = T5BertTokenizer.from_pretrained(model_path)
+        self.model = T5ForConditionalGeneration.from_pretrained(model_path)
+        self.model.eval()
+        self.max_source_length = max_source_length
+        self.max_target_length = max_target_length
 
     @paddle.no_grad()
     def predict(self, text, schema):
-        prompt_ids = self._tokenizer(
-            schema_to_prompt(schema),
+        def to_tensor(x):
+            return paddle.to_tensor(x, dtype='int64')
+
+        ssi = schema_to_ssi(schema=schema)
+
+        text = [ssi + x for x in text]
+
+        inputs = self.tokenizer(
+            text,
             return_token_type_ids=False,
-            return_attention_mask=False
-        )['input_ids'][:-1]
+            return_attention_mask=True,
+            max_seq_len=self.max_source_length)
 
-        def process(x):
-            return self._tokenizer(
-                x,
-                return_token_type_ids=False,
-                return_attention_mask=False,
-                max_seq_len=self._max_source_length
-            )
-
-        def process_tensor(x):
-            return paddle.to_tensor(x, dtype='int64')[:, :self._max_source_length]
-
-        raw_inputs = [{
-            'input_ids': prompt_ids + process(x)['input_ids']} for x in text
-        ]
-
-        max_length = max([len(x['input_ids']) for x in raw_inputs])
         inputs = {
-            'input_ids': [x['input_ids'] + [self._tokenizer.pad_token_id] * (max_length - len(x['input_ids'])) for x in raw_inputs],
-            'attention_mask': [[1] * len(x['input_ids']) + [0] * (max_length - len(x['input_ids'])) for x in raw_inputs],
+            'input_ids': to_tensor(
+                Pad(pad_val=self.tokenizer.pad_token_id)(inputs['input_ids'])),
+            'attention_mask':
+            to_tensor(Pad(pad_val=0)(inputs['attention_mask'])),
         }
 
-        inputs['input_ids'] = process_tensor(inputs['input_ids'])
-        inputs['attention_mask'] = process_tensor(inputs['attention_mask'])
-
-        result = self._model.generate(
+        pred, _ = self.model.generate(
             input_ids=inputs['input_ids'],
             attention_mask=inputs['attention_mask'],
-            max_length=self._max_target_length,
-        )
-        pred, _ = result
-        return [self._tokenizer.decode(x, clean_up_tokenization_spaces=False) for x in pred.numpy()]
+            max_length=self.max_target_length, )
+
+        pred = self.tokenizer.batch_decode(pred.numpy())
+
+        return [post_processing(x) for x in pred]
 
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', '-d', required=True, nargs='+')
-    parser.add_argument('--model', '-m', required=True)
-    parser.add_argument('--max_source_length', default=384, type=int)
+    parser.add_argument(
+        '--model', '-m', required=True, help='Trained model for inference')
+    parser.add_argument(
+        '--max_source_length',
+        default=384,
+        type=int,
+        help='Max source length for inference, ssi + text')
     parser.add_argument('--max_target_length', default=192, type=int)
     parser.add_argument('--batch_size', default=512, type=int)
-    parser.add_argument('-c', '--config', dest='map_config',
-                        help='Offset Re-mapping Config',
-                        default='longer_first_zh')
+    parser.add_argument(
+        '-c',
+        '--config',
+        dest='map_config',
+        help='Offset mapping config, maping generated sel to offset record',
+        default='longer_first_zh')
     parser.add_argument('--verbose', action='store_true')
     options = parser.parse_args()
 
     data_folder = options.data
     model_path = options.model
 
-    predictor = PaddlePredictor(
+    predictor = Predictor(
         model_path=model_path,
         max_source_length=options.max_source_length,
-        max_target_length=options.max_target_length
-    )
+        max_target_length=options.max_target_length)
 
     for task_folder in data_folder:
-    
+
         print(f'Extracting on {task_folder}')
-        schema = RecordSchema.read_from_file(os.path.join(task_folder, 'record.schema'))
+        schema = RecordSchema.read_from_file(
+            os.path.join(task_folder, 'record.schema'))
         sel2record = SEL2Record(
             schema_dict=SEL2Record.load_schema_dict(task_folder),
             map_config=MapConfig.load_by_name(options.map_config),
-            tokenizer=predictor.get_tokenizer(),
-        )
+            tokenizer=predictor.tokenizer, )
 
         test_filename = os.path.join(f"{task_folder}", "test.json")
         if not os.path.exists(test_filename):
             print(f'{test_filename} not found, skip ...')
             continue
 
-        text_list = [x['text'] for x in read_json_file(test_filename)]
-        char_list = [list(text) for text in text_list]
+        instances = read_json_file(test_filename)
+        text_list = [x['text'] for x in instances]
+        token_list = [x['tokens'] for x in instances]
 
         batch_num = math.ceil(len(text_list) / options.batch_size)
 
@@ -136,15 +130,12 @@ def main():
         for index in tqdm(range(batch_num)):
             start = index * options.batch_size
             end = index * options.batch_size + options.batch_size
-
-            pred_seq2seq = predictor.predict(text_list[start: end], schema=schema)
-            predict += [post_processing(x) for x in pred_seq2seq]
+            predict += predictor.predict(text_list[start:end], schema=schema)
 
         records = list()
-        for p, text, tokens in zip(predict, text_list, char_list):
-            r = sel2record.sel2record(pred=p, text=text, tokens=tokens)
-            records += [r]
-        
+        for p, text, tokens in zip(predict, text_list, token_list):
+            records += [sel2record.sel2record(pred=p, text=text, tokens=tokens)]
+
         pred_filename = os.path.join(f"{task_folder}", "pred.json")
         with open(pred_filename, 'w') as output:
             for record in records:
