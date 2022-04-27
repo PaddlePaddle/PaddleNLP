7d47e0961e203191e8b3fd5832a10608ed4d6fad
Yaojie Lu <yaojie.lu@outlook.com>
2022-04-27 18:20:05 +0800

    [DuUIE] encoding fixing for windows

--- examples/information_extraction/DuUIE/uie/evaluation/sel2record.py
+++ examples/information_extraction/DuUIE/uie/evaluation/sel2record.py
@@ -2,7 +2,6 @@
 # -*- coding:utf-8 -*-
 from typing import Tuple, List, Dict
 from collections import defaultdict, OrderedDict, Counter
-from dataclasses import dataclass
 import os
 import numpy
 import logging
@@ -155,14 +154,14 @@ class RecordSchema:
 
     @staticmethod
     def read_from_file(filename):
-        lines = open(filename).readlines()
+        lines = open(filename, encoding='utf8').readlines()
         type_list = json.loads(lines[0])
         role_list = json.loads(lines[1])
         type_role_dict = json.loads(lines[2])
         return RecordSchema(type_list, role_list, type_role_dict)
 
     def write_to_file(self, filename):
-        with open(filename, 'w') as output:
+        with open(filename, 'w', encoding='utf8') as output:
             output.write(json.dumps(self.type_list, ensure_ascii=False) + '\n')
             output.write(json.dumps(self.role_list, ensure_ascii=False) + '\n')
             output.write(
