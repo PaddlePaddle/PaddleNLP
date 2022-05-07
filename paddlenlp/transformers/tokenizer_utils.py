8cb27ad829f5263dee3fd88109e74caa3c868714
smallv0221 <33639025+smallv0221@users.noreply.github.com>
2022-05-07 15:48:29 +0800

    fix compatibility for ppnimilm, skep, unifiedtransformer, gpt2 (#2063)
    * upgrade tokenizer
    
    * upgrade tokenizer
    
    * upgrade datacollator
    
    * minor fix
    
    * minor fix
    
    * fix mbart tokenizer
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * fix compatity for autotokenizer
    
    * fix offset mapping
    
    * fix tokenizer doc
    
    * fix doc
    
    * fix tokenizer
    
    * fix ppminilm tokenizer
    
    * fix offset mapping
    
    * fix tokenizer warning
    
    * fix get_input_ids for compatibility
    
    * add doc
    
    * minor fix
    
    Co-authored-by: Jiaqi Liu <709153940@qq.com>

--- paddlenlp/transformers/tokenizer_utils.py
+++ paddlenlp/transformers/tokenizer_utils.py
@@ -983,14 +983,8 @@ class PretrainedTokenizer(PretrainedTokenizerBase):
             elif isinstance(text,
                             (list, tuple)) and len(text) > 0 and isinstance(
                                 text[0], str):
-                if is_split_into_words:
-                    tokens = list(
-                        itertools.chain(*(self.tokenize(
-                            t, is_split_into_words=True, **kwargs)
-                                          for t in text)))
-                    return self.convert_tokens_to_ids(tokens)
-                else:
-                    return self.convert_tokens_to_ids(text)
+                #TODO aligns with HuggingFace here in breaking change
+                return self.convert_tokens_to_ids(text)
             elif isinstance(text,
                             (list, tuple)) and len(text) > 0 and isinstance(
                                 text[0], int):
@@ -1065,14 +1059,8 @@ class PretrainedTokenizer(PretrainedTokenizerBase):
             elif isinstance(text,
                             (list, tuple)) and len(text) > 0 and isinstance(
                                 text[0], str):
-                if is_split_into_words:
-                    tokens = list(
-                        itertools.chain(*(self.tokenize(
-                            t, is_split_into_words=True, **kwargs)
-                                          for t in text)))
-                    return self.convert_tokens_to_ids(tokens)
-                else:
-                    return self.convert_tokens_to_ids(text)
+                #TODO aligns with HuggingFace here in breaking change
+                return self.convert_tokens_to_ids(text)
             elif isinstance(text,
                             (list, tuple)) and len(text) > 0 and isinstance(
                                 text[0], int):
