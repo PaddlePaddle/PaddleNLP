16edc7621b55adaf96220d52982c5e7d139558c3
yeliang2258 <1047690002@qq.com>
2022-05-08 12:09:24 +0000

    update daynamic quantize func

--- examples/language_model/ernie-3.0/python/infer_backend.py
+++ examples/language_model/ernie-3.0/python/infer_backend.py
@@ -31,16 +31,10 @@ def optimize(onnx_model_path: Path) -> Path:
 
 def dynamic_quantize(input_float_model: str, dynamic_quantized_model: str):
     import onnx
-    from onnxruntime.quantization import QuantizationMode, quantize
+    from onnxruntime.quantization import QuantizationMode, quantize_dynamic
     onnx_model = Path(input_float_model).absolute()
     optimized_output = optimize(onnx_model)
-    onnx_model = onnx.load(optimized_output.as_posix())
-    quantized_model = quantize(
-        model=onnx_model,
-        quantization_mode=QuantizationMode.IntegerOps,
-        force_fusions=True,
-        symmetric_weight=True, )
-    onnx.save_model(quantized_model, dynamic_quantized_model)
+    quantize_dynamic(optimized_output, dynamic_quantized_model)
 
 
 class InferBackend(object):
