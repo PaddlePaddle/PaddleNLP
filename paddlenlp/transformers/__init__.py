3c7bc1fd00dfce7e3ad9e8f877eb028f248b093c
smallv0221 <33639025+smallv0221@users.noreply.github.com>
2022-05-10 18:45:40 +0800

    Fix Prophetnet model (#1885)
    * temp add word ids
    
    * fix prophetnet model
    
    * minor fix
    
    * fix prophetnet
    
    * minor fix
    
    * minor fix
    
    Co-authored-by: yingyibiao <yyb0576@163.com>

--- paddlenlp/transformers/__init__.py
+++ paddlenlp/transformers/__init__.py
@@ -70,6 +70,8 @@ from .mbart.modeling import *
 from .mbart.tokenizer import *
 from .megatronbert.modeling import *
 from .megatronbert.tokenizer import *
+from .prophetnet.modeling import *
+from .prophetnet.tokenizer import *
 from .mobilebert.modeling import *
 from .mobilebert.tokenizer import *
 from .mpnet.modeling import *
