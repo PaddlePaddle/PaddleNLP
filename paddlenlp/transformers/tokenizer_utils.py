5b04af7e6ce67152043257497bbc9c3e25ee1635
wawltor <fangzeyang0904@hotmail.com>
2022-05-13 18:28:19 +0800

    fix the encode of tokenizer

--- paddlenlp/transformers/tokenizer_utils.py
+++ paddlenlp/transformers/tokenizer_utils.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# coding=utf-8
 # Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
 # Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
 # Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
