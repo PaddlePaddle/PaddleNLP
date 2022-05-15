5b5bc667f4a9f663f80542d6aa9d19097ebbafff
yeliang2258 <1047690002@qq.com>
2022-05-15 07:47:41 +0000

    probs and logits

--- model_zoo/ernie-3.0/deploy/python/ernie_predictor.py
+++ model_zoo/ernie-3.0/deploy/python/ernie_predictor.py
@@ -242,13 +242,13 @@ class ErniePredictor(object):
         }
 
     def seq_cls_postprocess(self, infer_data, input_data):
-        infer_data = np.array(infer_data[0])
-        max_value = np.max(infer_data, axis=1, keepdims=True)
-        exp_data = np.exp(infer_data - max_value)
-        softmax_data = exp_data / np.sum(exp_data, axis=1, keepdims=True)
+        logits = np.array(infer_data[0])
+        max_value = np.max(logits, axis=1, keepdims=True)
+        exp_data = np.exp(logits - max_value)
+        probs = exp_data / np.sum(exp_data, axis=1, keepdims=True)
         out_dict = {
-            "label": softmax_data.argmax(axis=-1),
-            "confidence": softmax_data.max(axis=-1)
+            "label": probs.argmax(axis=-1),
+            "confidence": probs.max(axis=-1)
         }
         return out_dict
 
