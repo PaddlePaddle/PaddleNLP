f6d85fcfb9f6dddd768fb5475f2a89bae972d36a
milen <1649759610@qq.com>
2022-05-31 14:35:39 +0000

    rename max_seq_length to max_seq_len

--- applications/doc_vqa/Extraction/run_train.sh
+++ applications/doc_vqa/Extraction/run_train.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 python3 ./run_docvqa.py \
     --model_name_or_path "layoutxlm-base-uncased" \
-    --max_seq_length 512 \
+    --max_seq_len 512 \
     --train_file "data/train.json" \
     --init_checkpoint "checkpoints/base_model" \
 	--do_train true \
