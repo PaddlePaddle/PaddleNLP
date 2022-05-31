ba69fd7e9c14c969e274c5f9f6cd467e7a8e2c7b
Milen <35913314+1649759610@users.noreply.github.com>
2022-05-31 22:46:17 +0800

    optimize code in ocr and mrc module (#2377)
    * initial commit
    
    * modify _calc_img_embeddings to support running without img embedding.
    
    * remove commented code
    
    * delete README
    
    * refine readme.md
    
    * change question
    
    * modify layoutxlm to support traing without image embedding
    
    * modify _calc_img_embeddings in layoutxlm to support training without img_embeddings
    
    * modify _calc_img_embeddings in layoutxlm to support training without img_embeddings
    
    * refine .gitignore
    
    * refine Rerank with pre-commit
    
    * refine Extraction with pre-commit
    
    * refine code and readme details
    
    * refine coding
    
    * refine code style about imports
    
    * refine README
    
    * set CUDA_VISIBLE_DEVICES 0
    
    * refine code style
    
    * refine readme
    
    * refine readme
    
    * delete ocr parsing file
    
    * refine readme
    
    * refine readme
    
    * refine README.md
    
    * refine readme
    
    * refine readme
    
    * refine readme
    
    * optimize ocr and mrc module
    
    * refine code style
    
    * set params with argparse
    
    * rename max_seq_length to max_seq_len
    
    Co-authored-by: yingyibiao <yyb0576@163.com>
    Co-authored-by: tianxin <tianxin04@baidu.com>

--- applications/doc_vqa/Extraction/run_train.sh
+++ applications/doc_vqa/Extraction/run_train.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 python3 ./run_docvqa.py \
     --model_name_or_path "layoutxlm-base-uncased" \
-    --max_seq_length 512 \
+    --max_seq_len 512 \
     --train_file "data/train.json" \
     --init_checkpoint "checkpoints/base_model" \
 	--do_train true \
@@ -10,6 +10,7 @@ python3 ./run_docvqa.py \
     --eval_steps 24000 \
     --save_steps 40 \
     --output_dir "output" \
+    --save_path "data/decode_res.json" \
     --learning_rate 3e-5 \
     --warmup_steps 40 \
     --per_gpu_train_batch_size 4 \
