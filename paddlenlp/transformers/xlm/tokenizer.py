f08a6786491603a642f3e664e437a746a5691ae6
YuuuW <573009727@qq.com>
2022-06-07 20:33:25 +0800

    update xlm tokenizer

--- paddlenlp/transformers/xlm/tokenizer.py
+++ paddlenlp/transformers/xlm/tokenizer.py
@@ -890,8 +890,8 @@ class XLMTokenizer(PretrainedTokenizer):
         """
         Build model inputs from a sequence or a pair of sequence for sequence classification tasks by concatenating and
         adding special tokens. An XLM sequence has the following format:
-        - single sequence: `</s> X </s>`
-        - pair of sequences: `</s> A </s> B </s>`
+        - single sequence: `<s> X </s>`
+        - pair of sequences: `<s> A </s> B </s>`
 
         Args:
             token_ids_0 (`List[int]`):
@@ -902,12 +902,37 @@ class XLMTokenizer(PretrainedTokenizer):
         Returns:
             `List[int]`: The model input with special tokens.
         """
-        cls = [self.cls_token_id]
+        bos = [self.bos_token_id]
         sep = [self.sep_token_id]
 
         if token_ids_1 is None:
-            return cls + token_ids_0 + sep
-        return cls + token_ids_0 + sep + token_ids_1 + sep
+            return bos + token_ids_0 + sep
+        return bos + token_ids_0 + sep + token_ids_1 + sep
+
+    def create_token_type_ids_from_sequences(
+            self, token_ids_0: List[int],
+            token_ids_1: Optional[List[int]]=None) -> List[int]:
+        """
+        Create a mask from the two sequences passed to be used in a sequence-pair classification task. An XLM sequence
+        pair mask has the following format:
+        ```
+        0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1
+        | first sequence    | second sequence |
+        ```
+        If `token_ids_1` is `None`, this method only returns the first portion of the mask (0s).
+        Args:
+            token_ids_0 (`List[int]`):
+                List of IDs.
+            token_ids_1 (`List[int]`, *optional*):
+                Optional second list of IDs for sequence pairs.
+        Returns:
+            `List[int]`: List of [token type IDs] according to the given sequence(s).
+        """
+        sep = [self.sep_token_id]
+        cls = [self.cls_token_id]
+        if token_ids_1 is None:
+            return len(cls + token_ids_0 + sep) * [0]
+        return len(cls + token_ids_0 + sep) * [0] + len(token_ids_1 + sep) * [1]
 
     def get_special_tokens_mask(
             self,
