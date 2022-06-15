0eee7010ffce3d2a842acd632252ca94533b51a5
wj-Mcat <1435130236@qq.com>
2022-06-15 08:38:42 +0000

    save tokenizer config & additional resource files

--- paddlenlp/transformers/tokenizer_utils_base.py
+++ paddlenlp/transformers/tokenizer_utils_base.py
@@ -1497,8 +1497,6 @@ class PretrainedTokenizerBase(SpecialTokensMixin):
         vocab_files = {}
         init_configuration = {}
 
-        pretrained_models = list(cls.pretrained_init_configuration.keys())
-
         additional_files_names = {
             "added_tokens_file": ADDED_TOKENS_FILE,
             "special_tokens_map_file": SPECIAL_TOKENS_MAP_FILE,
@@ -1511,7 +1509,7 @@ class PretrainedTokenizerBase(SpecialTokensMixin):
         }
 
         # From built-in pretrained models
-        if pretrained_model_name_or_path in pretrained_models:
+        if pretrained_model_name_or_path in cls.pretrained_init_configuration:
             for file_id, map_list in cls.pretrained_resource_files_map.items():
                 vocab_files[file_id] = map_list[pretrained_model_name_or_path]
             init_configuration = copy.deepcopy(
@@ -1577,6 +1575,7 @@ class PretrainedTokenizerBase(SpecialTokensMixin):
                 init_kwargs = json.load(f)
         else:
             init_kwargs = init_configuration
+
         # position args are stored in kwargs, maybe better not include
         init_args = init_kwargs.pop("init_args", ())
         init_kwargs.pop("init_class", None)
@@ -1689,6 +1688,10 @@ class PretrainedTokenizerBase(SpecialTokensMixin):
             logger.info(
                 "Special tokens have been added in the vocabulary, make sure the associated word embeddings are fine-tuned or trained."
             )
+
+        # save all of related things into default root dir
+        if pretrained_model_name_or_path in cls.pretrained_init_configuration:
+            tokenizer.save_pretrained(default_root)
 
         return tokenizer
 
