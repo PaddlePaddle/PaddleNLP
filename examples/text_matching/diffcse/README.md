a83a902393e7ad0172c7a94523b0f6fae23b78e9
milen <1649759610@qq.com>
2022-07-11 11:37:21 +0000

    initial commit

--- examples/text_matching/diffcse/README.md
+++ examples/text_matching/diffcse/README.md
@@ -29,28 +29,28 @@ export CUDA_VISIBLE_DEVICES=${gpu_ids}
 log_dir="log_diffcse_ErG3D3"
 
 python -u -m paddle.distributed.launch --gpus ${gpu_ids} --log_dir ${log_dir} \
-	run_diffcse.py \
-	--mode "train" \
-	--encoder_name "rocketqa-zh-dureader-query-encoder" \
-	--generator_name "ernie-3.0-base-zh" \
-	--discriminator_name "ernie-3.0-base-zh" \
-	--max_seq_length "128" \
-	--output_emb_size "32" \
-	--train_set_file "../dataset/100w/train_webdata.txt" \
-	--eval_set_file "../dataset/100w/test_v1.txt" \
-	--save_dir "./checkpoints_diffcse_ErG3D3" \
-	--log_dir ${log_dir} \
-	--save_steps "50000" \
-	--eval_steps "1000" \
-	--batch_size "32" \
-	--epochs "3" \
-	--mlm_probability "0.15" \
-	--lambda_weight "0.15" \
-	--learning_rate "3e-5" \
-	--weight_decay "0.01" \
-	--warmup_proportion "0.01" \
-	--seed "0" \
-	--device "gpu"
+    run_diffcse.py \
+    --mode "train" \
+    --encoder_name "rocketqa-zh-dureader-query-encoder" \
+    --generator_name "ernie-3.0-base-zh" \
+    --discriminator_name "ernie-3.0-base-zh" \
+    --max_seq_length "128" \
+    --output_emb_size "32" \
+    --train_set_file "../dataset/100w/train_webdata.txt" \
+    --eval_set_file "../dataset/100w/test_v1.txt" \
+    --save_dir "./checkpoints_diffcse_ErG3D3" \
+    --log_dir ${log_dir} \
+    --save_steps "50000" \
+    --eval_steps "1000" \
+    --batch_size "32" \
+    --epochs "3" \
+    --mlm_probability "0.15" \
+    --lambda_weight "0.15" \
+    --learning_rate "3e-5" \
+    --weight_decay "0.01" \
+    --warmup_proportion "0.01" \
+    --seed "0" \
+    --device "gpu"
 ```
 
 可支持配置的参数：
@@ -97,15 +97,15 @@ export CUDA_VISIBLE_DEVICES=${gpu_ids}
 log_dir="log_eval"
 python -u -m paddle.distributed.launch --gpus ${gpu_ids} --log_dir ${log_dir} \
     run_diffcse.py \
-	--mode "eval" \
-	--encoder_name "rocketqa-zh-dureader-query-encoder" \
-	--max_seq_length "128" \
-	--output_emb_size "32" \
-	--eval_set_file "../diffcse/data/test_v1.txt" \
-	--ckpt_dir "./checkpoints_diffcse_ErG3D3/best_spearman" \
-	--batch_size "8" \
-	--seed "0" \
-	--device "gpu"
+    --mode "eval" \
+    --encoder_name "rocketqa-zh-dureader-query-encoder" \
+    --max_seq_length "128" \
+    --output_emb_size "32" \
+    --eval_set_file "../diffcse/data/test_v1.txt" \
+    --ckpt_dir "./checkpoints_diffcse_ErG3D3/best_spearman" \
+    --batch_size "8" \
+    --seed "0" \
+    --device "gpu"
 
 ```
 可支持配置的参数：
@@ -123,16 +123,16 @@ export CUDA_VISIBLE_DEVICES=${gpu_ids}
 log_dir="log_infer"
 python -u -m paddle.distributed.launch --gpus ${gpu_ids} --log_dir ${log_dir} \
     run_diffcse.py \
-	--mode "infer" \
-	--encoder_name "rocketqa-zh-dureader-query-encoder" \
-	--max_seq_length "128" \
-	--output_emb_size "32" \
-	--infer_set_file "../diffcse/data/test.txt" \
-	--ckpt_dir "./checkpoints_diffcse_ErG3D3/best_spearman" \
+    --mode "infer" \
+    --encoder_name "rocketqa-zh-dureader-query-encoder" \
+    --max_seq_length "128" \
+    --output_emb_size "32" \
+    --infer_set_file "../diffcse/data/test.txt" \
+    --ckpt_dir "./checkpoints_diffcse_ErG3D3/best_spearman" \
     --save_infer_path "./infer_result.txt" \
-	--batch_size "8" \
-	--seed "0" \
-	--device "gpu"
+    --batch_size "8" \
+    --seed "0" \
+    --device "gpu"
 
 
 ```
--- examples/text_matching/diffcse/data.py
+++ examples/text_matching/diffcse/data.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import paddle
 
 import os
@@ -23,25 +22,39 @@ import numpy as np
 def get_special_tokens():
     return ["[PAD]", "[CLS]", "[SEP]", "[MASK]", "[UNK]"]
 
+
 def get_special_token_ids(tokenizer):
     special_tokens = ["[PAD]", "[CLS]", "[SEP]", "[MASK]", "[UNK]"]
     return tokenizer.convert_tokens_to_ids(special_tokens)
 
+
 def get_special_token_dict(tokenizer):
     special_tokens = ["[PAD]", "[CLS]", "[SEP]", "[MASK]", "[UNK]"]
-    special_token_dict = dict(zip(special_tokens, tokenizer.convert_tokens_to_ids(special_tokens)))
+    special_token_dict = dict(
+        zip(special_tokens, tokenizer.convert_tokens_to_ids(special_tokens)))
     return special_token_dict
 
 
-def create_dataloader(dataset, mode="train", batch_size=1, batchify_fn=None, trans_fn=None):
+def create_dataloader(dataset,
+                      mode="train",
+                      batch_size=1,
+                      batchify_fn=None,
+                      trans_fn=None):
     if trans_fn:
         dataset = dataset.map(trans_fn)
     shuffle = True if mode == "train" else False
     if mode == "train":
-        batch_sampler = paddle.io.DistributedBatchSampler(dataset, batch_size=batch_size, shuffle=shuffle)
+        batch_sampler = paddle.io.DistributedBatchSampler(dataset,
+                                                          batch_size=batch_size,
+                                                          shuffle=shuffle)
     else:
-        batch_sampler = paddle.io.BatchSampler(dataset, batch_size=batch_size, shuffle=shuffle)
-    return paddle.io.DataLoader(dataset=dataset, batch_sampler=batch_sampler, collate_fn=batchify_fn, return_list=True)
+        batch_sampler = paddle.io.BatchSampler(dataset,
+                                               batch_size=batch_size,
+                                               shuffle=shuffle)
+    return paddle.io.DataLoader(dataset=dataset,
+                                batch_sampler=batch_sampler,
+                                collate_fn=batchify_fn,
+                                return_list=True)
 
 
 def convert_example(example, tokenizer, max_seq_length=512, do_evalute=False):
@@ -52,7 +65,9 @@ def convert_example(example, tokenizer, max_seq_length=512, do_evalute=False):
             result += [example["label"]]
         else:
             # do_train
-            encoded_inputs = tokenizer(text=text, max_seq_len=max_seq_length, return_attention_mask=True)
+            encoded_inputs = tokenizer(text=text,
+                                       max_seq_len=max_seq_length,
+                                       return_attention_mask=True)
             input_ids = encoded_inputs["input_ids"]
             token_type_ids = encoded_inputs["token_type_ids"]
             attention_mask = encoded_inputs["attention_mask"]
@@ -66,9 +81,11 @@ def read_text_single(data_path):
             data = line.rstrip()
             yield {"text_a": data, "text_b": data}
 
+
 def masked_fill(x, mask, value):
     y = paddle.full(x.shape, value, x.dtype)
     return paddle.where(mask, y, x)
+
 
 def mask_tokens(batch_inputs, tokenizer, mlm_probability=0.15):
     """
@@ -78,24 +95,34 @@ def mask_tokens(batch_inputs, tokenizer, mlm_probability=0.15):
     mlm_labels = batch_inputs.clone()
 
     probability_matrix = paddle.full(mlm_inputs.shape, mlm_probability)
-    
-    special_tokens_mask = paddle.cast(paddle.zeros(mlm_inputs.shape), dtype=bool)
+
+    special_tokens_mask = paddle.cast(paddle.zeros(mlm_inputs.shape),
+                                      dtype=bool)
     for special_token_id in get_special_token_ids(tokenizer):
-        special_tokens_mask |= (mlm_inputs==special_token_id)
+        special_tokens_mask |= (mlm_inputs == special_token_id)
 
-    
-    probability_matrix = masked_fill(probability_matrix, special_tokens_mask, 0.0)
+    probability_matrix = masked_fill(probability_matrix, special_tokens_mask,
+                                     0.0)
 
-    masked_indices = paddle.cast(paddle.bernoulli(probability_matrix), dtype=bool)
+    masked_indices = paddle.cast(paddle.bernoulli(probability_matrix),
+                                 dtype=bool)
     mlm_labels = masked_fill(mlm_labels, ~masked_indices, -100)
 
     # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
-    indices_replaced = paddle.cast(paddle.bernoulli(paddle.full(mlm_inputs.shape, 0.8)), dtype=bool) & masked_indices
-    mlm_inputs = masked_fill(mlm_inputs, indices_replaced, tokenizer.mask_token_id)
+    indices_replaced = paddle.cast(paddle.bernoulli(
+        paddle.full(mlm_inputs.shape, 0.8)),
+                                   dtype=bool) & masked_indices
+    mlm_inputs = masked_fill(mlm_inputs, indices_replaced,
+                             tokenizer.mask_token_id)
 
     # 10% of the time, we replace masked input tokens with random word
-    indices_random = paddle.cast(paddle.bernoulli(paddle.full(mlm_inputs.shape, 0.5)) ,dtype=bool) & masked_indices & ~indices_replaced
-    random_words = paddle.randint(0, len(tokenizer), mlm_inputs.shape, dtype=mlm_inputs.dtype)
+    indices_random = paddle.cast(
+        paddle.bernoulli(paddle.full(mlm_inputs.shape, 0.5)),
+        dtype=bool) & masked_indices & ~indices_replaced
+    random_words = paddle.randint(0,
+                                  len(tokenizer),
+                                  mlm_inputs.shape,
+                                  dtype=mlm_inputs.dtype)
     mlm_inputs = paddle.where(indices_random, random_words, mlm_inputs)
 
     # The rest of the time (10% of the time) we keep the masked input tokens unchanged
@@ -133,7 +160,8 @@ def word_repetition(input_ids, token_type_ids, dup_rate=0.32):
         if (actual_len > 5):
             dup_len = random.randint(a=0, b=max(2, int(dup_rate * actual_len)))
             # Skip cls and sep position
-            dup_word_index = random.sample(list(range(1, actual_len - 1)), k=dup_len)
+            dup_word_index = random.sample(list(range(1, actual_len - 1)),
+                                           k=dup_len)
 
         r_input_id = []
         r_token_type_id = []
@@ -156,5 +184,5 @@ def word_repetition(input_ids, token_type_ids, dup_rate=0.32):
         repetitied_input_ids[batch_id] += [0] * pad_len
         repetitied_token_type_ids[batch_id] += [0] * pad_len
 
-    return paddle.to_tensor(repetitied_input_ids), paddle.to_tensor(repetitied_token_type_ids)
-
+    return paddle.to_tensor(repetitied_input_ids), paddle.to_tensor(
+        repetitied_token_type_ids)
