fe9d03c49f832f648d00e2ed33f226e63bbc722a
Yuang Liu <liuyuang@baidu.com>
2022-07-18 14:00:35 +0800

    [GPT-3] Optimize gpt-3 with fused op (#2650)
    * use fuse in benchmark
    * optim dygraph benchmark

--- tests/test_tipc/dygraph/hybrid_parallelism/gpt/benchmark_common/run_benchmark.sh
+++ tests/test_tipc/dygraph/hybrid_parallelism/gpt/benchmark_common/run_benchmark.sh
@@ -94,7 +94,8 @@ function _train(){
                 --use_pure_fp16 ${use_pure_fp16}\
                 --use_recompute False\
                 --sharding_stage 1\
-                --sharding_offload False"
+                --sharding_offload False \
+                --fuse_transformer True"
 
     # 以下为通用执行命令，无特殊可不用修改
     case ${run_mode} in
