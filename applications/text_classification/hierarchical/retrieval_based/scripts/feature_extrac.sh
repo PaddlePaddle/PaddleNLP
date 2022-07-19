e2edbee8c61b94080a20564afe694c17cee964da
w5688414 <w5688414@gmail.com>
2022-07-19 17:33:34 +0800

    Add requirements and update readme

--- applications/text_classification/hierarchical/retrieval_based/scripts/feature_extrac.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python utils/feature_extract.py \
-        --data_name corpus \
-        --model_dir ./output \
-        --output_dir data \
-        --corpus_file "./data/corpus.txt" 
