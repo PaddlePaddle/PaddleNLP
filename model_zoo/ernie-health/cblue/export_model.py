7c4b52f8ae6f671baac569308dba64b01cd804d1
Noel <wanghuijuan03@baidu.com>
2022-07-25 19:27:47 +0800

    [ernie-health] add onnxruntime deployment (#2329)

--- model_zoo/ernie-health/cblue/export_model.py
+++ model_zoo/ernie-health/cblue/export_model.py
@@ -16,17 +16,9 @@ import argparse
 import os
 
 import paddle
+from paddlenlp.utils.log import logger
 from paddlenlp.transformers import ElectraForSequenceClassification
 from model import ElectraForBinaryTokenClassification, ElectraForSPO
-
-# yapf: disable
-parser = argparse.ArgumentParser()
-parser.add_argument('--train_dataset', choices=['KUAKE-QIC', 'KUAKE-QQR', 'KUAKE-QTR', 'CHIP-STS', 'CHIP-CTC', 'CHIP-CDN-2C', 'CMeEE', 'CMeIE'],
-                                       required=True, type=str, help='The name of dataset used for training.')
-parser.add_argument('--params_path', type=str, required=True, default='./checkpoint/model_state.pdparams', help='The path to model parameters to be loaded.')
-parser.add_argument('--output_path', type=str, default='./export', help='The path of model parameter in static graph to be saved.')
-args = parser.parse_args()
-# yapf: enable
 
 NUM_CLASSES = {
     'CHIP-CDN-2C': 2,
@@ -39,39 +31,42 @@ NUM_CLASSES = {
     'CMeIE': 44
 }
 
+# yapf: disable
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_dataset', required=True, type=str, help='The name of dataset used for training.')
+parser.add_argument('--params_path', type=str, required=True, default='./checkpoint/model_state.pdparams', help='The path to model parameters to be loaded.')
+parser.add_argument('--output_path', type=str, default='./export', help='The path of model parameter in static graph to be saved.')
+args = parser.parse_args()
+# yapf: enable
+
 if __name__ == "__main__":
+    # Load the model parameters.
+    if args.train_dataset not in NUM_CLASSES:
+        raise ValueError(f"Please modify the code to fit {args.dataset}")
+
     if args.train_dataset == 'CMeEE':
         model = ElectraForBinaryTokenClassification.from_pretrained(
-            'ernie-health-chinese', num_classes=NUM_CLASSES[args.train_dataset])
+            args.params_path, num_classes=NUM_CLASSES[args.train_dataset])
     elif args.train_dataset == 'CMeIE':
         model = ElectraForSPO.from_pretrained(
-            'ernie-health-chinese', num_classes=NUM_CLASSES[args.train_dataset])
+            args.params_path, num_classes=NUM_CLASSES[args.train_dataset])
     else:
         model = ElectraForSequenceClassification.from_pretrained(
-            'ernie-health-chinese',
+            args.params_path,
             num_classes=NUM_CLASSES[args.train_dataset],
             activation='tanh')
 
-    if args.params_path and os.path.isfile(args.params_path):
-        state_dict = paddle.load(args.params_path)
-        model.set_dict(state_dict)
-        print('Loaded parameters from %s' % args.params_path)
     model.eval()
 
-    # Convert to static graph with specific input description
+    # Convert to static graph with specific input description:
+    # input_ids, token_type_ids and position_ids.
     input_spec = [
-        paddle.static.InputSpec(shape=[None, None], dtype='int64'),  # input_ids
-        paddle.static.InputSpec(shape=[None, None],
-                                dtype='int64'),  # token_type_ids
-        paddle.static.InputSpec(shape=[None, None],
-                                dtype='int64')  # position_ids
+        paddle.static.InputSpec(shape=[None, None], dtype='int64'),
+        paddle.static.InputSpec(shape=[None, None], dtype='int64'),
+        paddle.static.InputSpec(shape=[None, None], dtype='int64')
     ]
-    if args.train_dataset in ['CMeEE', 'CMeIE']:
-        input_spec.append(
-            paddle.static.InputSpec(shape=[None, None],
-                                    dtype='float32'))  # masks
-
     model = paddle.jit.to_static(model, input_spec=input_spec)
+
     # Save in static graph model.
     save_path = os.path.join(args.output_path, 'inference')
     paddle.jit.save(model, save_path)
