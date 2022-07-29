fa0228dc63d697cdcab2f3f15c1cc7f1fe9ca110
FrostML <380185688@qq.com>
2022-07-29 11:13:48 +0000

    update license

--- paddlenlp/datasets/hf_datasets/duconv.py
+++ paddlenlp/datasets/hf_datasets/duconv.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# Copyright 2020 The TensorFlow Datasets Authors and the HuggingFace Datasets Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Lint as: python3
 
 import json
 import os
