834e923c023b88bb291e497942c3040bba370b1a
westfish <westfish@126.com>
2022-08-02 09:26:01 +0000

    add qg_example

---
+++ examples/question_generation/t5_qg/generate_run.sh
@@ -0,0 +1,16 @@
+python generate.py \
+    --model_name_or_path=t5-base-finetuned-question-generation-ap \
+    --dataset_name=squad \
+    --output_path=generate.txt \
+    --max_source_length=1024 \
+    --max_target_length=142 \
+    --decode_strategy=greedy_search \
+    --top_k=2 \
+    --top_p=1.0 \
+    --num_beams=1 \
+    --length_penalty=0.0 \
+    --batch_size=64 \
+    --seed=42 \
+    --ignore_pad_token_for_loss=True \
+    --logging_steps=20 \
+    --device=gpu
