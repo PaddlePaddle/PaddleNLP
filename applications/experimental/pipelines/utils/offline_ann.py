0d7894531d174aa43c273b06fed2cf91e460965c
吴高升 <w5688414@gmail.com>
2022-08-09 21:54:43 +0800

    Fix faiss index batch_size bug on python3.7 and update es config for … (#2965)
    * Fix faiss index batch_size bug on python3.7 and update es config for pipelines
    
    * Fix the nltk download bug and Add FAQ for mac support
    
    * Remove update_batch_size for fais

--- applications/experimental/pipelines/utils/offline_ann.py
+++ applications/experimental/pipelines/utils/offline_ann.py
@@ -1,10 +1,16 @@
 import argparse
 
 import paddle
-from pipelines.utils import convert_files_to_dicts
+from pipelines.utils import convert_files_to_dicts, fetch_archive_from_http
 from pipelines.document_stores import ElasticsearchDocumentStore
 from pipelines.nodes import DensePassageRetriever
 from pipelines.utils import launch_es
+
+data_dict = {
+    'data/dureader_dev':
+    "https://paddlenlp.bj.bcebos.com/applications/dureader_dev.zip",
+    "data/baike": "https://paddlenlp.bj.bcebos.com/applications/baike.zip"
+}
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--index_name",
@@ -15,6 +21,17 @@ parser.add_argument("--doc_dir",
                     default='data/baike/',
                     type=str,
                     help="The doc path of the corpus")
+
+parser.add_argument('--host',
+                    type=str,
+                    default="127.0.0.1",
+                    help='host ip of elastic search')
+
+parser.add_argument('--port',
+                    type=str,
+                    default="9200",
+                    help='port of elastic search')
+
 parser.add_argument(
     '--delete_index',
     action='store_true',
@@ -27,8 +44,8 @@ def offline_ann(index_name, doc_dir):
 
     launch_es()
 
-    document_store = ElasticsearchDocumentStore(host="127.0.0.1",
-                                                port="9200",
+    document_store = ElasticsearchDocumentStore(host=args.host,
+                                                port=args.port,
                                                 username="",
                                                 password="",
                                                 index=index_name)
@@ -59,8 +76,8 @@ def offline_ann(index_name, doc_dir):
 
 
 def delete_data(index_name):
-    document_store = ElasticsearchDocumentStore(host="127.0.0.1",
-                                                port="9200",
+    document_store = ElasticsearchDocumentStore(host=args.host,
+                                                port=args.port,
                                                 username="",
                                                 password="",
                                                 index=index_name)
@@ -70,6 +87,9 @@ def delete_data(index_name):
 
 
 if __name__ == "__main__":
+    if (args.doc_dir in data_dict):
+        fetch_archive_from_http(url=data_dict[args.doc_dir],
+                                output_dir=args.doc_dir)
     if (args.delete_index):
         delete_data(args.index_name)
     offline_ann(args.index_name, args.doc_dir)
