5cf80265d65bf2ef832d9ef8c42b68067256d886
Huijuan Wang <wanghuijuan03@baidu.com>
2022-08-11 10:28:32 +0800

    [few-shot] update multi-label task

--- applications/text_classification/multi_label/few-shot/train.py
+++ applications/text_classification/multi_label/few-shot/train.py
@@ -17,18 +17,15 @@ import os
 import sys
 import paddle
 import paddle.nn.functional as F
-from paddle.static import InputSpec
 from paddlenlp.utils.log import logger
-from paddlenlp.transformers import ErnieTokenizer, ErnieForMaskedLM, export_model
-from paddlenlp.trainer import PdArgumentParser, get_scheduler
+from paddlenlp.transformers import AutoTokenizer, AutoModelForMaskedLM
+from paddlenlp.trainer import PdArgumentParser
 from paddlenlp.prompt import (
     AutoTemplate,
     SoftVerbalizer,
-    MLMTokenizerWrapper,
     PromptTuningArguments,
     PromptTrainer,
     PromptModelForClassification,
-    FewShotSampler,
 )
 from utils import load_local_dataset
 
@@ -36,36 +33,21 @@ sys.path.append("../")
 from metric import MetricReport
 
 
+# yapf: disable
 @dataclass
 class DataArguments:
-    data_dir: str = field(
-        metadata={
-            "help":
-            "The dataset dictionary includes train.txt, dev.txt and label.txt files."
-        })
-    prompt: str = field(metadata={"help": "The input prompt for tuning."})
-    soft_encoder: str = field(
-        default=None,
-        metadata={
-            "help": "The encoder type of soft template, `lstm`, `mlp` or None."
-        })
-    verbalizer: str = field(
-        default=None, metadata={"help": "The mapping from labels to words."})
-    train_sample_per_label: int = field(
-        default=None,
-        metadata={"help": "Number of examples sampled per label for training."})
+    data_dir: str = field(default=None, metadata={"help": "The dataset dictionary includes train.txt, dev.txt and label.txt files."})
+    prompt: str = field(default=None, metadata={"help": "The input prompt for tuning."})
+    soft_encoder: str = field(default=None, metadata={"help": "The encoder type of soft template, `lstm`, `mlp` or None."})
+    encoder_hidden_size: int = field(default=None, metadata={"help": "The dimension of soft embeddings."})
+    verbalizer: str = field(default=None, metadata={"help": "The mapping from labels to words."})
 
 
 @dataclass
 class ModelArguments:
-    model_name_or_path: str = field(
-        default="ernie-3.0-base-zh",
-        metadata={
-            "help": "The build-in pretrained model or the path to local model."
-        })
-    export_type: str = field(
-        default='paddle',
-        metadata={"help": "The type to export. Support `paddle` and `onnx`."})
+    model_name_or_path: str = field(default="ernie-3.0-base-zh", metadata={"help": "The build-in pretrained model or the path to local model."})
+    export_type: str = field(default='paddle', metadata={"help": "The type to export. Support `paddle` and `onnx`."})
+# yapf: enable
 
 
 def main():
@@ -79,14 +61,16 @@ def main():
     paddle.set_device(training_args.device)
 
     # Load the pretrained language model.
-    model = ErnieForMaskedLM.from_pretrained(model_args.model_name_or_path)
-    tokenizer = ErnieTokenizer.from_pretrained(model_args.model_name_or_path)
+    model = AutoModelForMaskedLM.from_pretrained(model_args.model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
 
     # Define the template for preprocess and the verbalizer for postprocess.
-    template = AutoTemplate.create_from(data_args.prompt,
-                                        tokenizer,
-                                        model=model,
-                                        prompt_encoder=data_args.soft_encoder)
+    template = AutoTemplate.create_from(
+        data_args.prompt,
+        tokenizer,
+        model=model,
+        prompt_encoder=data_args.soft_encoder,
+        encoder_hidden_size=data_args.encoder_hidden_size)
     logger.info("Using template: {}".format(template.template))
 
     label_file = os.path.join(data_args.data_dir, "label.txt")
@@ -94,13 +78,10 @@ def main():
     logger.info("Using verbalizer: {}".format(data_args.verbalizer))
 
     # Load the few-shot datasets.
-    train_ds, dev_ds = load_local_dataset(data_path=data_args.data_dir,
-                                          splits=["train", "dev"],
-                                          label_list=verbalizer.labels_to_ids)
-    if data_args.train_sample_per_label is not None:
-        sampler = FewShotSampler(
-            num_sample_per_label=data_args.train_sample_per_label)
-        train_ds = sampler.sample_datasets(train_ds, seed=training_args.seed)
+    train_ds, dev_ds, test_ds = load_local_dataset(
+        data_path=data_args.data_dir,
+        splits=["train", "dev", "test"],
+        label_list=verbalizer.labels_to_ids)
 
     # Define the criterion.
     criterion = paddle.nn.BCEWithLogitsLoss()
@@ -113,25 +94,6 @@ def main():
         freeze_plm=training_args.freeze_plm,
         freeze_dropout=training_args.freeze_dropout)
 
-    # Only update the prompt-related parameters to reduce memory cost.
-    training_args = PromptTrainer.init_num_steps(training_args, len(train_ds))
-    lr_scheduler = get_scheduler(training_args.lr_scheduler_type,
-                                 training_args.learning_rate,
-                                 training_args.warmup_steps,
-                                 training_args.num_training_steps)
-    optimizer = paddle.optimizer.AdamW(
-        learning_rate=lr_scheduler,
-        parameters=[{
-            'params':
-            [p for p in prompt_model.verbalizer.non_head_parameters()] +
-            [p for p in prompt_model.plm.parameters() if not p.stop_gradient]
-        }, {
-            'params': [p for p in prompt_model.verbalizer.head_parameters()] +
-            [p for p in prompt_model.template.parameters()],
-            'learning_rate':
-            training_args.ppt_learning_rate / training_args.learning_rate
-        }])
-
     # Define the metric function.
     def compute_metrics(eval_preds):
         metric = MetricReport()
@@ -143,15 +105,16 @@ def main():
             "macro_f1_score": macro_f1_score
         }
 
+    # Initialize the trainer.
     trainer = PromptTrainer(model=prompt_model,
                             tokenizer=tokenizer,
                             args=training_args,
                             criterion=criterion,
                             train_dataset=train_ds,
                             eval_dataset=dev_ds,
-                            optimizers=[optimizer, lr_scheduler],
                             compute_metrics=compute_metrics)
 
+    # Training.
     if training_args.do_train:
         train_result = trainer.train(resume_from_checkpoint=None)
         metrics = train_result.metrics
@@ -160,16 +123,15 @@ def main():
         trainer.save_metrics("train", metrics)
         trainer.save_state()
 
+    # Prediction.
+    if training_args.do_predict:
+        test_ret = trainer.predict(test_ds)
+        trainer.log_metrics("test", test_ret.metrics)
+
+    # Export static model.
     if training_args.do_export:
-        input_spec = [
-            InputSpec(shape=[None, None], dtype="int64"),  # input_ids
-            InputSpec(shape=[None, None], dtype="int64"),  # mask_ids
-            InputSpec(shape=[None, None], dtype="int64")  # soft_token_ids
-        ]
         export_path = os.path.join(training_args.output_dir, 'export')
-        os.makedirs(export_path, exist_ok=True)
-        export_model(prompt_model, input_spec, export_path,
-                     model_args.export_type)
+        trainer.export_model(export_path, export_type=model_args.export_type)
 
 
 if __name__ == '__main__':
