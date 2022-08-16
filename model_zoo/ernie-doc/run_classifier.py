677e33a71009d702f5c7b0962c8ed9c0bebd3fcf
zhoushunjie <zhoushunjie@baidu.com>
2022-08-16 11:36:30 +0800

    Adjust thucnews load

--- model_zoo/ernie-doc/run_classifier.py
+++ model_zoo/ernie-doc/run_classifier.py
@@ -152,7 +152,7 @@ def do_train(args):
     tokenizer_class, eval_name, test_name, preprocess_text_fn, eval_metric = DATASET_INFO[
         args.dataset]
     tokenizer = tokenizer_class.from_pretrained(args.model_name_or_path)
-    if args.dataset == "hyp":
+    if args.dataset in ["hyp", "thucnews"]:
         from paddlenlp.datasets import load_dataset
         train_ds, eval_ds, test_ds = load_dataset(
             args.dataset, splits=["train", eval_name, test_name])
