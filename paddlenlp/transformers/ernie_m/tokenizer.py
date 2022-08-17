60842000da0b616cb7ae1eb6414234c230e489fe
wj-Mcat <1435130236@qq.com>
2022-08-17 06:34:25 +0000

    update ernie-m tokenizer

--- paddlenlp/transformers/ernie_m/tokenizer.py
+++ paddlenlp/transformers/ernie_m/tokenizer.py
@@ -209,12 +209,7 @@ class ErnieMTokenizer(PretrainedTokenizer):
         return self.sp_model.vocab_size()
 
     def get_vocab(self):
-        vocab = {
-            self.convert_ids_to_tokens(i): i
-            for i in range(self.vocab_size)
-        }
-        vocab.update(self.added_tokens_encoder)
-        return vocab
+        return dict(self.vocab.token_to_idx, **self.added_tokens_encoder)
 
     def clean_text(self, text):
         """Performs invalid character removal and whitespace cleanup on text."""
