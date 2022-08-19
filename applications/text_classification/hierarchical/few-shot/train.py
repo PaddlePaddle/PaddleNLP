703d2481abeda933c9f3dad7202ae87ca59cd5e4
Huijuan Wang <wanghuijuan03@baidu.com>
2022-08-19 10:44:12 +0800

    [few-shot] add input_spec in train.py

--- applications/text_classification/hierarchical/few-shot/train.py
+++ applications/text_classification/hierarchical/few-shot/train.py
@@ -18,6 +18,7 @@ import sys
 
 import paddle
 import paddle.nn.functional as F
+from paddle.static import InputSpec
 from paddlenlp.utils.log import logger
 from paddlenlp.transformers import AutoTokenizer, AutoModelForMaskedLM
 from paddlenlp.trainer import PdArgumentParser
@@ -131,8 +132,15 @@ def main():
 
     # Export static model.
     if training_args.do_export:
+        input_spec = [
+            InputSpec(shape=[None, None], dtype="int64"),  # input_ids
+            InputSpec(shape=[None, None], dtype="int64"),  # mask_ids
+            InputSpec(shape=[None, None], dtype="int64"),  # soft_token_ids
+        ]
         export_path = os.path.join(training_args.output_dir, 'export')
-        trainer.export_model(export_path, export_type=model_args.export_type)
+        trainer.export_model(export_path,
+                             input_spec=input_sepc,
+                             export_type=model_args.export_type)
 
 
 if __name__ == '__main__':
--- applications/text_classification/multi_class/few-shot/train.py
+++ applications/text_classification/multi_class/few-shot/train.py
@@ -16,6 +16,7 @@ from dataclasses import dataclass, field
 import os
 
 import paddle
+from paddle.static import InputSpec
 from paddle.metric import Accuracy
 from paddlenlp.utils.log import logger
 from paddlenlp.transformers import AutoTokenizer, AutoModelForMaskedLM
@@ -125,8 +126,15 @@ def main():
 
     # Export static model.
     if training_args.do_export:
+        input_spec = [
+            InputSpec(shape=[None, None], dtype="int64"),  # input_ids
+            InputSpec(shape=[None, None], dtype="int64"),  # mask_ids
+            InputSpec(shape=[None, None], dtype="int64"),  # soft_token_ids
+        ]
         export_path = os.path.join(training_args.output_dir, 'export')
-        trainer.export_model(export_path, export_type=model_args.export_type)
+        trainer.export_model(export_path,
+                             input_spec=input_sepc,
+                             export_type=model_args.export_type)
 
 
 if __name__ == '__main__':
