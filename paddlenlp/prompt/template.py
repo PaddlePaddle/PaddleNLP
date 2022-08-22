c1196dfeac9a39e9704709a7aa2d5c17b5a3c5b3
Huijuan Wang <wanghuijuan03@baidu.com>
2022-08-22 17:07:14 +0800

    [few-shot] update readme

--- paddlenlp/prompt/template.py
+++ paddlenlp/prompt/template.py
@@ -512,7 +512,6 @@ class AutoTemplate(object):
 
         if 'mask' not in template_keys:
             template.append({'add_prefix_space': ' ', 'mask': None})
-            template.append({'add_prefix_space': '', 'hard': '。'})
 
         if 'soft' in template_keys:
             return SoftTemplate(tokenizer=tokenizer,
