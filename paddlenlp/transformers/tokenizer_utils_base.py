52a18de38f31065e2e60b5f172db666c26997aef
wj-Mcat <1435130236@qq.com>
2022-08-23 03:01:51 +0000

    fix multi-layer-inherit

--- paddlenlp/transformers/tokenizer_utils_base.py
+++ paddlenlp/transformers/tokenizer_utils_base.py
@@ -1353,8 +1353,15 @@ class PretrainedTokenizerBase(SpecialTokensMixin):
     truncation_side: str = "right"
     slow_tokenizer_class = None
 
+    # tag for init_kwargs
+    _have_done_init: bool = False
+
     def __init__(self, **kwargs):
         # inputs and kwargs for saving and re-loading (see ``from_pretrained`` and ``save_pretrained``)
+        if self._have_done_init:
+            return
+        self._have_done_init = True
+
         self.init_inputs = ()
         self.init_kwargs = copy.deepcopy(kwargs)
         self.name_or_path = kwargs.pop("name_or_path", "")
