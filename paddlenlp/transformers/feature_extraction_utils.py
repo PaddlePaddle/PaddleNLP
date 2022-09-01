06932280312d9cd5112f043caef46c72369da616
yujun <50394665+JunnYu@users.noreply.github.com>
2022-09-01 13:11:47 +0800

    update copyright order

--- paddlenlp/transformers/feature_extraction_utils.py
+++ paddlenlp/transformers/feature_extraction_utils.py
@@ -1,6 +1,6 @@
 # coding=utf-8
-# Copyright 2021 The HuggingFace Inc. team.
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# Copyright 2021 The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
