45425b4a2ccc2950a24e34ba79dbf4a8cdb89213
gongenlei <gongel@qq.com>
2022-09-02 13:00:02 +0800

    Add FasterCodeGen/FasterGPTJ (#3017)
    * add FasterCodeGen/FasterGPTJ
    
    * remove useless
    
    * fix output and hypeparams
    
    * fix review comment
    
    * fix
    
    * rm split
    
    * add perf and sample
    
    * fix: memory allocator
    
    * add memory perf
    
    * update convert_params
    
    * add perf doc
    
    * fix
    
    * update perf
    
    * add finetune and doc
    
    * update hyper params
    
    * update docs
    
    * add license
    
    * use apply_logits_mask_kernelLauncher
    
    * ker_curand_setup_bsz_one for bsz=1
    
    * rm const for constant
    
    * rm ret
    
    * update license
    
    * fix kernel
    
    * use apply_logits_mask_kernelLauncher
    
    * add blank_lines
    
    * match new paddle
    
    * update seed and add TODO

--- paddlenlp/transformers/__init__.py
+++ paddlenlp/transformers/__init__.py
@@ -121,6 +121,8 @@ from .artist.modeling import *
 from .artist.tokenizer import *
 from .dallebart.modeling import *
 from .dallebart.tokenizer import *
+from .gptj.modeling import *
+from .gptj.tokenizer import *
 
 # For faster tokenizer
 from ..utils.import_utils import is_faster_tokenizer_available
