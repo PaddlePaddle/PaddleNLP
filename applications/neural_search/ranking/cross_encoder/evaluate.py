4b9e9599d4144e0fbb0eb335b03ed873f2b51ddb
w5688414 <w5688414@gmail.com>
2022-09-05 07:30:04 +0000

    Add CrossEncoder and Integrate it into pipelines

--- applications/neural_search/ranking/cross_encoder/evaluate.py
+++ applications/neural_search/ranking/cross_encoder/evaluate.py
@@ -19,28 +19,24 @@ import random
 import time
 
 import numpy as np
-import paddle
-import paddle.nn.functional as F
-
-from paddlenlp.data import Stack, Tuple, Pad
-from paddlenlp.datasets import load_dataset
-from paddlenlp.transformers import LinearDecayWithWarmup, AutoModel, AutoTokenizer
-
-from data import create_dataloader, gen_pair
-from data import convert_pairwise_example as convert_example
-from model import PairwiseMatching
 import pandas as pd
 from tqdm import tqdm
+import paddle
+import paddle.nn.functional as F
+from paddlenlp.data import Stack, Tuple, Pad
+from paddlenlp.datasets import load_dataset
+from paddlenlp.transformers import LinearDecayWithWarmup, AutoModelForSequenceClassification, AutoTokenizer
+
+from data import create_dataloader
+from data import convert_example, read_data
 
 # yapf: disable
 parser = argparse.ArgumentParser()
-parser.add_argument("--margin", default=0.1, type=float, help="Margin for pos_score and neg_score.")
 parser.add_argument("--test_file", type=str, required=True, help="The full path of test file")
-
 parser.add_argument("--max_seq_length", default=128, type=int, help="The maximum total input sequence length after tokenization. "
     "Sequences longer than this will be truncated, sequences shorter will be padded.")
 parser.add_argument("--batch_size", default=32, type=int, help="Batch size per GPU/CPU for training.")
-parser.add_argument('--model_name_or_path', default="ernie-3.0-medium-zh", help="The pretrained model used for training")
+parser.add_argument('--model_name_or_path', default="rocketqa-base-cross-encoder", help="The pretrained model used for training")
 parser.add_argument("--init_from_ckpt", type=str, default=None, help="The path of checkpoint to be loaded.")
 parser.add_argument("--seed", type=int, default=1000, help="Random seed for initialization.")
 parser.add_argument('--device', choices=['cpu', 'gpu'], default="gpu", help="Select which device to train model, defaults to gpu.")
@@ -71,36 +67,14 @@ def evaluate(model, metric, data_loader, phase="dev"):
     for idx, batch in enumerate(data_loader):
         input_ids, token_type_ids, labels = batch
 
-        pos_probs = model.predict(input_ids=input_ids,
-                                  token_type_ids=token_type_ids)
+        pos_probs = model(input_ids=input_ids, token_type_ids=token_type_ids)
 
-        neg_probs = 1.0 - pos_probs
-
-        preds = np.concatenate((neg_probs, pos_probs), axis=1)
-        metric.update(preds=preds, labels=labels)
+        sim_score = F.softmax(pos_probs)
+        metric.update(preds=sim_score.numpy(), labels=labels)
 
     print("eval_{} auc:{:.3}".format(phase, metric.accumulate()))
     metric.reset()
     model.train()
-
-
-# 构建读取函数，读取原始数据
-def read(src_path, is_predict=False):
-    data = pd.read_csv(src_path, sep='\t')
-    for index, row in tqdm(data.iterrows()):
-        query = row['query']
-        title = row['title']
-        neg_title = row['neg_title']
-        yield {'query': query, 'title': title, 'neg_title': neg_title}
-
-
-def read_test(src_path, is_predict=False):
-    data = pd.read_csv(src_path, sep='\t')
-    for index, row in tqdm(data.iterrows()):
-        query = row['query']
-        title = row['title']
-        label = row['label']
-        yield {'query': query, 'title': title, 'label': label}
 
 
 def main():
@@ -111,16 +85,16 @@ def main():
 
     set_seed(args.seed)
 
-    dev_ds = load_dataset(read_test, src_path=args.test_file, lazy=False)
-    print(dev_ds[0])
+    dev_ds = load_dataset(read_data, data_path=args.test_file, lazy=False)
 
-    pretrained_model = AutoModel.from_pretrained(args.model_name_or_path)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        args.model_name_or_path, num_classes=2)
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
 
     trans_func_eval = partial(convert_example,
                               tokenizer=tokenizer,
                               max_seq_length=args.max_seq_length,
-                              phase="eval")
+                              is_pair=True)
 
     batchify_fn_eval = lambda samples, fn=Tuple(
         Pad(axis=0, pad_val=tokenizer.pad_token_id, dtype="int64"
@@ -136,11 +110,12 @@ def main():
                                         batchify_fn=batchify_fn_eval,
                                         trans_fn=trans_func_eval)
 
-    model = PairwiseMatching(pretrained_model, margin=args.margin)
-
     if args.init_from_ckpt and os.path.isfile(args.init_from_ckpt):
         state_dict = paddle.load(args.init_from_ckpt)
         model.set_dict(state_dict)
+    else:
+        raise ValueError(
+            "Please set --params_path with correct pretrained model file")
 
     metric = paddle.metric.Auc()
     evaluate(model, metric, dev_data_loader, "dev")
