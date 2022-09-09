1aff118237b49c5c3b791292d1dd9f6220e3070f
gongel <gongel@qq.com>
2022-09-09 12:26:55 +0000

    update attention mask

--- examples/code_generation/polycoder/tio_dataset.py
+++ examples/code_generation/polycoder/tio_dataset.py
@@ -37,19 +37,24 @@ def get_train_valid_test_split_(splits_string, size):
 
 
 class TioDataset(paddle.io.Dataset):
-    def __init__(self, ids, idx, los, eos,
-                 shf, seq_len, base, bound,
-                 cnt, name: str = 'tio'):
+
+    def __init__(self,
+                 ids,
+                 idx,
+                 los,
+                 eos,
+                 shf,
+                 seq_len,
+                 base,
+                 bound,
+                 cnt,
+                 name: str = 'tio'):
         super().__init__()
         self.name, self.len, self.siz = name, cnt + 1, bound - base
         self.ids, self.idx, self.los, self.shf = ids, idx, los, shf
         self.seq_len, self.base, self.bound = seq_len + 1, base, bound
         self.eos = eos
-        sl = seq_len
-        atten = np.tri(sl, sl).reshape((1, sl, sl))
-        atten = (atten - 1.0) * 1e9
-        self.atten = atten.astype("float32")
-        self.pos_ids = np.arange(0, sl, dtype="int64")
+        self.pos_ids = np.arange(0, seq_len, dtype="int64")
 
     def __len__(self):
         return self.len
@@ -59,19 +64,35 @@ class TioDataset(paddle.io.Dataset):
 
         tokens = self.ids[self.idx[ix]:self.idx[ix + 1]]
         loss_msk = np.concatenate((np.zeros(self.los[ix] - 1, dtype='float32'),
-                                   np.ones(len(tokens) - self.los[ix], dtype='float32')))
+                                   np.ones(len(tokens) - self.los[ix],
+                                           dtype='float32')))
+        attention_mask = np.ones(len(tokens), dtype='float32')
         if len(tokens) < self.seq_len:
-            loss_msk = np.concatenate((loss_msk, np.zeros(self.seq_len - len(tokens), dtype='float32')))
+            loss_msk = np.concatenate(
+                (loss_msk, np.zeros(self.seq_len - len(tokens),
+                                    dtype='float32')))
+            attention_mask = np.concatenate(
+                (attention_mask,
+                 np.zeros(self.seq_len - len(tokens), dtype='float32')))
             tokens = tokens.tolist() + [self.eos] * (self.seq_len - len(tokens))
-
         labels, tokens = tokens[1:], tokens[:-1]
+        attention_mask = attention_mask[1:]
         labels = np.array(labels, dtype="int64")
-        return [tokens, loss_msk, self.atten, self.pos_ids, labels]
+        return [tokens, loss_msk, attention_mask, self.pos_ids, labels]
 
 
 class TioDatasetLoader(object):
-    def __init__(self, path, split, seed, seq_len, eos,
-                 train_cnt, valid_cnt, test_cnt, build_shf: bool = False):
+
+    def __init__(self,
+                 path,
+                 split,
+                 seed,
+                 seq_len,
+                 eos,
+                 train_cnt,
+                 valid_cnt,
+                 test_cnt,
+                 build_shf: bool = False):
         for sfx in ['_ids_tio.npy', '_idx_tio.npz']:
             if not os.path.isfile(path + sfx):
                 raise ValueError("File Not found, %s" % (path + sfx))
@@ -84,14 +105,17 @@ class TioDatasetLoader(object):
         siz = len(self.los)
         seg = get_train_valid_test_split_(split, siz)
 
-        self.train = TioDataset(self.ids, self.idx, self.los, eos,
-                                self.shf, seq_len, seg[0], seg[1], train_cnt, 'train')
-        self.valid = TioDataset(self.ids, self.idx, self.los, eos,
-                                self.shf, seq_len, seg[1], seg[2], valid_cnt, 'valid')
-        self.test = TioDataset(self.ids, self.idx, self.los, eos,
-                               self.shf, seq_len, seg[2], seg[3], test_cnt, 'test')
+        self.train = TioDataset(self.ids, self.idx, self.los, eos, self.shf,
+                                seq_len, seg[0], seg[1], train_cnt, 'train')
+        self.valid = TioDataset(self.ids, self.idx, self.los, eos, self.shf,
+                                seq_len, seg[1], seg[2], valid_cnt, 'valid')
+        self.test = TioDataset(self.ids, self.idx, self.los, eos, self.shf,
+                               seq_len, seg[2], seg[3], test_cnt, 'test')
 
-    def shuffle(self, path, on_build: bool = False, seed: int = 19260817) -> object:
+    def shuffle(self,
+                path,
+                on_build: bool = False,
+                seed: int = 19260817) -> object:
         fl = path + '_shf_tio.npy'
         if on_build:
             siz = len(self.los)
@@ -110,23 +134,25 @@ class TioDatasetLoader(object):
                     np.load(fl, allow_pickle=True, mmap_mode='r')
                     break
                 except Exception as e:
-                    print("%s file is still being written or damaged, please wait a moment." % fl)
+                    print(
+                        "%s file is still being written or damaged, please wait a moment."
+                        % fl)
                     time.sleep(3)
         return np.load(fl)
 
 
 def create_pretrained_dataset(
-        args,
-        input_path,
-        local_rank,
-        data_world_rank,
-        data_world_size,
-        eos_id,
-        worker_init=None,
-        max_seq_len=1024,
-        places=None,
-        data_holders=None,
-        pipeline_mode=False,
+    args,
+    input_path,
+    local_rank,
+    data_world_rank,
+    data_world_size,
+    eos_id,
+    worker_init=None,
+    max_seq_len=1024,
+    places=None,
+    data_holders=None,
+    pipeline_mode=False,
 ):
     device_world_size = paddle.distributed.get_world_size()
     device_world_rank = paddle.distributed.get_rank()
@@ -169,19 +195,18 @@ def create_pretrained_dataset(
                                      return_list=False)
         return data_loader
 
-    loaders = TioDatasetLoader(input_prefix, args.split, args.seed, max_seq_len, eos_id,
-                               args.micro_batch_size * args.max_steps * data_world_size,
-                               args.micro_batch_size * (args.max_steps // args.eval_freq + 1) *
-                               args.eval_iters * data_world_size,
-                               args.micro_batch_size * args.test_iters * data_world_size,
-                               build_shf=local_rank == 0)
+    loaders = TioDatasetLoader(
+        input_prefix,
+        args.split,
+        args.seed,
+        max_seq_len,
+        eos_id,
+        args.micro_batch_size * args.max_steps * data_world_size,
+        args.micro_batch_size * (args.max_steps // args.eval_freq + 1) *
+        args.eval_iters * data_world_size,
+        args.micro_batch_size * args.test_iters * data_world_size,
+        build_shf=local_rank == 0)
 
     if pipeline_mode:
         return build(loaders.train), None, None
     return build(loaders.train), build(loaders.valid), build(loaders.test)
-
-
-if __name__ == '__main__':
-    ld = TioDatasetLoader('finetune9t/input', '90,5,5', 19260817, 1024,
-                          0, 0, 0, 0)
-    np.save('head_len.npy', sorted(ld.los))
