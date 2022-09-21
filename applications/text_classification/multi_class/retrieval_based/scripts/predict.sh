67f9c3b2b1b72a23a8b98b214e7f707f2a7865fc
w5688414 <w5688414@gmail.com>
2022-09-21 16:08:57 +0800

    restore gpu config

--- applications/text_classification/multi_class/retrieval_based/scripts/predict.sh
+++ applications/text_classification/multi_class/retrieval_based/scripts/predict.sh
@@ -13,16 +13,16 @@
 # limitations under the License.
 
 # gpu version
-# root_dir="checkpoints/inbatch/model_best" 
-# python -u -m paddle.distributed.launch --gpus "0" \
-#     predict.py \
-#     --device gpu \
-#     --params_path "${root_dir}/model_state.pdparams" \
-#     --model_name_or_path rocketqa-zh-dureader-query-encoder \
-#     --output_emb_size 0 \
-#     --batch_size 128 \
-#     --max_seq_length 384 \
-#     --text_pair_file "data/dev.txt"
+root_dir="checkpoints/inbatch/model_best" 
+python -u -m paddle.distributed.launch --gpus "0" \
+    predict.py \
+    --device gpu \
+    --params_path "${root_dir}/model_state.pdparams" \
+    --model_name_or_path rocketqa-zh-dureader-query-encoder \
+    --output_emb_size 0 \
+    --batch_size 128 \
+    --max_seq_length 384 \
+    --text_pair_file "data/dev.txt"
 
 
 # cpu
