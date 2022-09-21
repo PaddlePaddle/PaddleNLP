b59fca0ce21aa62bf09f40b25ea0f2f4d3ccf36f
zhengya01 <zhengya01@baidu.com>
2022-09-21 12:54:55 +0800

    code style

--- tests/test_tipc/common_func.sh
+++ tests/test_tipc/common_func.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 function func_parser_key(){
     strs=$1
     IFS=":"
@@ -53,7 +67,7 @@ function func_parser_params(){
 }
 
 function status_check(){
-    last_status=$1   # the exit code
+    last_status=$1   # the exit code. 
     run_command=$2
     run_log=$3
     model_name=$4
