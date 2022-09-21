5178466ec82035e9a9cf2db43d4003466994c36d
w5688414 <w5688414@gmail.com>
2022-09-21 16:06:53 +0800

    Update cpu predict script

--- applications/text_classification/multi_class/retrieval_based/scripts/predict.sh
+++ applications/text_classification/multi_class/retrieval_based/scripts/predict.sh
@@ -13,25 +13,26 @@
 # limitations under the License.
 
 # gpu version
-root_dir="checkpoints/inbatch/model_best" 
-python -u -m paddle.distributed.launch --gpus "0" \
-    predict.py \
-    --device gpu \
-    --params_path "${root_dir}/model_state.pdparams" \
-    --model_name_or_path rocketqa-zh-dureader-query-encoder \
-    --output_emb_size 0 \
-    --batch_size 128 \
-    --max_seq_length 384 \
-    --text_pair_file "data/dev.txt"
+# root_dir="checkpoints/inbatch/model_best" 
+# python -u -m paddle.distributed.launch --gpus "0" \
+#     predict.py \
+#     --device gpu \
+#     --params_path "${root_dir}/model_state.pdparams" \
+#     --model_name_or_path rocketqa-zh-dureader-query-encoder \
+#     --output_emb_size 0 \
+#     --batch_size 128 \
+#     --max_seq_length 384 \
+#     --text_pair_file "data/dev.txt"
 
 
 # cpu
 # root_dir="checkpoints/inbatch/model_best" 
-# python -u -m paddle.distributed.launch \
+# python -m paddle.distributed.launch --nproc_per_node 8 --backend "gloo" \
 #     predict.py \
 #     --device cpu \
 #     --params_path "${root_dir}/model_state.pdparams" \
 #     --output_emb_size 0 \
+#     --model_name_or_path rocketqa-zh-dureader-query-encoder \
 #     --batch_size 128 \
 #     --max_seq_length 384 \
-#     --text_pair_file "data/train.txt"
+#     --text_pair_file "data/dev.txt"
