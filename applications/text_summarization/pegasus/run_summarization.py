713ac630e030d156bec7ed2609a894406a25ec50
gongel <gongel@qq.com>
2022-10-18 12:38:20 +0000

    add requirements

--- applications/text_summarization/pegasus/run_summarization.py
+++ applications/text_summarization/pegasus/run_summarization.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
 
-sys.path.insert(0, '/home/gongenlei/pegasus/')
 import os
 import json
 import argparse
