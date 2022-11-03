4244257f6d4185442a27bdfafadd8352a335b7ce
Yam0214 <oyhy0214@163.com>
2022-11-03 08:06:20 +0000

    set seed in test_sample/greedy_generate method

--- tests/transformers/test_generation_utils.py
+++ tests/transformers/test_generation_utils.py
@@ -433,6 +433,7 @@ class GenerationTesterMixin:
             )
             pretrained_model = self.all_generative_model_classes[model_class][
                 0](**config)
+            paddle.seed(128)
             model = model_class(pretrained_model)
             model.eval()
 
@@ -452,6 +453,7 @@ class GenerationTesterMixin:
             )
             pretrained_model = self.all_generative_model_classes[model_class][
                 0](**config)
+            paddle.seed(128)
             model = model_class(pretrained_model)
             model.eval()
 
