cce0262b889b14fb39fc56a18d74723b2256388c
Sijun He <sijun.he@hotmail.com>
2022-11-17 12:10:54 +0800

    precommit

--- applications/text_classification/multi_class/train.py
+++ applications/text_classification/multi_class/train.py
@@ -16,7 +16,6 @@ import functools
 import os
 from dataclasses import dataclass, field
 
-
 import paddle
 from paddle.metric import Accuracy
 from paddlenlp.data import DataCollatorWithPadding
@@ -26,7 +25,13 @@ from paddlenlp.transformers import AutoModelForSequenceClassification, AutoToken
 
 from utils import preprocess_function, read_local_dataset
 
-SUPPORTED_MODELS = ["ernie-1.0-large-zh-cw","ernie-3.0-xbase-zh", "ernie-3.0-base-zh", "ernie-3.0-medium-zh", "ernie-3.0-micro-zh", "ernie-3.0-mini-zh", "ernie-3.0-nano-zh", "ernie-2.0-base-en", "ernie-2.0-large-en","ernie-m-base","ernie-m-large"]
+SUPPORTED_MODELS = [
+    "ernie-1.0-large-zh-cw", "ernie-3.0-xbase-zh", "ernie-3.0-base-zh",
+    "ernie-3.0-medium-zh", "ernie-3.0-micro-zh", "ernie-3.0-mini-zh",
+    "ernie-3.0-nano-zh", "ernie-2.0-base-en", "ernie-2.0-large-en",
+    "ernie-m-base", "ernie-m-large"
+]
+
 
 # yapf: disable
 @dataclass
@@ -40,6 +45,7 @@ class ModelArguments:
     model_name_or_path: str = field(default="ernie-3.0-base-zh", metadata={"help": "Build-in pretrained model name or the path to local model."})
 # yapf: enable
 
+
 def main():
     """
     Training a binary or multi classification model
@@ -52,9 +58,13 @@ def main():
     training_args.print_config(data_args, "Data")
     print(not os.path.isdir(model_args.model_name_or_path))
     print(model_args.model_name_or_path not in SUPPORTED_MODELS)
-    if not os.path.isdir(model_args.model_name_or_path) and model_args.model_name_or_path not in SUPPORTED_MODELS:
-        raise ValueError(f"{model_args.model_name_or_path} is not a supported model type. Either use a local model path or select a model from {SUPPORTED_MODELS}")
-    
+    if not os.path.isdir(
+            model_args.model_name_or_path
+    ) and model_args.model_name_or_path not in SUPPORTED_MODELS:
+        raise ValueError(
+            f"{model_args.model_name_or_path} is not a supported model type. Either use a local model path or select a model from {SUPPORTED_MODELS}"
+        )
+
     paddle.set_device(training_args.device)
 
     # load and preprocess dataset
@@ -67,22 +77,20 @@ def main():
             label_list[l] = i
 
     train_ds = load_dataset(read_local_dataset,
-                            path=os.path.join(data_args.data_dir,
-                                              "train.txt"),
+                            path=os.path.join(data_args.data_dir, "train.txt"),
                             label_list=label_list,
                             lazy=False)
     dev_ds = load_dataset(read_local_dataset,
                           path=os.path.join(data_args.data_dir, "dev.txt"),
                           label_list=label_list,
                           lazy=False)
-    
+
     tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
     trans_func = functools.partial(preprocess_function,
                                    tokenizer=tokenizer,
                                    max_seq_length=data_args.max_seq_length)
     train_ds = train_ds.map(trans_func)
     dev_ds = dev_ds.map(trans_func)
-    
 
     # Define model
     model = AutoModelForSequenceClassification.from_pretrained(
@@ -99,23 +107,24 @@ def main():
 
     # Define the early-stopping callback.
     callbacks = [
-        EarlyStoppingCallback(early_stopping_patience=data_args.early_stopping_patience,
-                              early_stopping_threshold=0.)
+        EarlyStoppingCallback(
+            early_stopping_patience=data_args.early_stopping_patience,
+            early_stopping_threshold=0.)
     ]
 
-    # Define loss function 
+    # Define loss function
     criterion = paddle.nn.loss.CrossEntropyLoss()
 
     # Define Trainer
     trainer = Trainer(model=model,
-                            tokenizer=tokenizer,
-                            args=training_args,
-                            criterion=criterion,
-                            train_dataset=train_ds,
-                            eval_dataset=dev_ds,
-                            callbacks=callbacks,
-                            data_collator = DataCollatorWithPadding(tokenizer),
-                            compute_metrics=compute_metrics)
+                      tokenizer=tokenizer,
+                      args=training_args,
+                      criterion=criterion,
+                      train_dataset=train_ds,
+                      eval_dataset=dev_ds,
+                      callbacks=callbacks,
+                      data_collator=DataCollatorWithPadding(tokenizer),
+                      compute_metrics=compute_metrics)
 
     if training_args.do_train:
         train_result = trainer.train(resume_from_checkpoint=None)
@@ -124,6 +133,7 @@ def main():
         trainer.log_metrics("train", metrics)
         trainer.save_metrics("train", metrics)
         trainer.save_state()
+
 
 if __name__ == "__main__":
     main()
