fa9d6e1dd3164e5c0546eececfb453a37a23f413
linjieccc <623543001@qq.com>
2022-11-29 08:59:59 +0000

    update result plot

--- paddlenlp/utils/doc_parser.py
+++ paddlenlp/utils/doc_parser.py
@@ -13,30 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
-import os
-import math
-import random
 import base64
 import mimetypes
-from PIL import Image
+import os
+import random
+import re
 from io import BytesIO
-import requests
-import numpy as np
 
-from .log import logger
+import numpy as np
+import requests
+from PIL import Image, ImageDraw
+
 from .image_utils import np2base64
+from .log import logger
 
 
 class DocParser(object):
     """DocParser"""
 
-    def __init__(self,
-                 ocr_lang="ch",
-                 layout_analysis=False,
-                 pdf_parser_config=None,
-                 use_gpu=None,
-                 device_id=None):
+    def __init__(self, ocr_lang="ch", layout_analysis=False, pdf_parser_config=None, use_gpu=None, device_id=None):
         self.ocr_lang = ocr_lang
         self.use_angle_cls = False
         self.layout_analysis = layout_analysis
@@ -49,36 +44,32 @@ class DocParser(object):
         """
         parse
         """
-        doc_type = mimetypes.guess_type(doc['doc'])[0]
+        doc_type = mimetypes.guess_type(doc["doc"])[0]
 
         if not doc_type or doc_type.startswith("image"):
-            image = self.read_image(doc['doc'])
+            image = self.read_image(doc["doc"])
         elif doc_type == "application/pdf":
-            image = self.read_pdf(doc['doc'])
+            image = self.read_pdf(doc["doc"])
         offset_x, offset_y = 0, 0
         if expand_to_a4_size:
-            image, offset_x, offset_y = self.expand_image_to_a4_size(
-                image, center=True)
+            image, offset_x, offset_y = self.expand_image_to_a4_size(image, center=True)
         img_h, img_w = image.shape[:2]
-        doc['image'] = np2base64(image)
-        doc['offset_x'] = offset_x
-        doc['offset_y'] = offset_y
-        doc['img_w'] = img_w
-        doc['img_h'] = img_h
+        doc["image"] = np2base64(image)
+        doc["offset_x"] = offset_x
+        doc["offset_y"] = offset_y
+        doc["img_w"] = img_w
+        doc["img_h"] = img_h
         if do_ocr:
             ocr_result = self.ocr(image)
             if expand_to_a4_size:
                 layout = []
                 for segment in ocr_result:
                     box = segment[0]
-                    org_box = [
-                        box[0] - offset_x, box[1] - offset_y, box[2] - offset_x,
-                        box[3] - offset_y
-                    ]
+                    org_box = [box[0] - offset_x, box[1] - offset_y, box[2] - offset_x, box[3] - offset_y]
                     layout.append((org_box, segment[1]))
-                doc['layout'] = layout
+                doc["layout"] = layout
             else:
-                doc['layout'] = ocr_result
+                doc["layout"] = ocr_result
         return doc
 
     def __call__(self, *args, **kwargs):
@@ -103,7 +94,7 @@ class DocParser(object):
 
         def _is_ch(s):
             for ch in s:
-                if u'\u4e00' <= ch <= u'\u9fff':
+                if "\u4e00" <= ch <= "\u9fff":
                     return True
             return False
 
@@ -124,22 +115,22 @@ class DocParser(object):
         else:
             layout_result = self.layout_analysis_engine(image)
             for region in layout_result:
-                if region['type'] != "table":
-                    ocr_result = region['res']
+                if region["type"] != "table":
+                    ocr_result = region["res"]
                     for segment in ocr_result:
-                        box = segment['text_region']
+                        box = segment["text_region"]
                         box = _get_box(box)
-                        text = segment['text']
+                        text = segment["text"]
                         layout.append((box, text))
                 else:
-                    bbox = region['bbox']
-                    table_result = region['res']
-                    html = table_result['html']
-                    cell_bbox = table_result['cell_bbox']
+                    bbox = region["bbox"]
+                    table_result = region["res"]
+                    html = table_result["html"]
+                    cell_bbox = table_result["cell_bbox"]
                     table_list = []
-                    lines = re.findall('<tr>(.*?)</tr>', html)
+                    lines = re.findall("<tr>(.*?)</tr>", html)
                     for line in lines:
-                        table_list.extend(re.findall('<td.*?>(.*?)</td>', line))
+                        table_list.extend(re.findall("<td.*?>(.*?)</td>", line))
                     for cell_box, text in zip(cell_bbox, table_list):
                         box = [
                             bbox[0] + cell_box[0],
@@ -157,12 +148,11 @@ class DocParser(object):
         buff = None
         if len(data) < 1024:
             if os.path.exists(data):
-                buff = open(data, 'rb').read()
+                buff = open(data, "rb").read()
             elif data.startswith("http://") or data.startswith("https://"):
                 resp = requests.get(data, stream=True)
                 if not resp.ok:
-                    raise RuntimeError(
-                        "Failed to download the file from {}".format(data))
+                    raise RuntimeError("Failed to download the file from {}".format(data))
                 buff = resp.raw.read()
             else:
                 raise FileNotFoundError("Image file {} not found!".format(data))
@@ -191,8 +181,8 @@ class DocParser(object):
             import fitz
         except ImportError:
             raise RuntimeError(
-                "Need PyMuPDF to process pdf input. "
-                "Please install module by: python3 -m pip install pymupdf")
+                "Need PyMuPDF to process pdf input. " "Please install module by: python3 -m pip install pymupdf"
+            )
         if isinstance(pdf, fitz.Document):
             return pdf
         pdf_buff = self._get_buffer(pdf)
@@ -205,9 +195,7 @@ class DocParser(object):
                 raise ValueError("The password of pdf is incorrect.")
 
         if pdf_doc.page_count > 1:
-            logger.warning(
-                "Currently only parse the first page for PDF input with more than one page."
-            )
+            logger.warning("Currently only parse the first page for PDF input with more than one page.")
 
         page = pdf_doc.load_page(0)
         image = np.array(self.get_page_image(page).convert("RGB"))
@@ -219,7 +207,7 @@ class DocParser(object):
         get page image
         """
         pix = page.get_pixmap()
-        image_buff = pix.pil_tobytes('jpeg', optimize=True)
+        image_buff = pix.pil_tobytes("jpeg", optimize=True)
         return Image.open(BytesIO(image_buff))
 
     def init_ocr_inference(self):
@@ -247,10 +235,7 @@ class DocParser(object):
                     "Need paddleocr to process image input. "
                     "Please install module by: python3 -m pip install paddleocr"
                 )
-            self.layout_analysis_engine = PPStructure(table=True,
-                                                      ocr=True,
-                                                      show_log=False,
-                                                      lang=self.ocr_lang)
+            self.layout_analysis_engine = PPStructure(table=True, ocr=True, show_log=False, lang=self.ocr_lang)
 
     @classmethod
     def _normalize_box(self, box, old_size, new_size, offset_x=0, offset_y=0):
@@ -271,42 +256,85 @@ class DocParser(object):
             exp_w = int(h / 1.414 - w)
             if center:
                 offset_x = int(exp_w / 2)
-                exp_img = np.zeros((h, offset_x, 3), dtype='uint8')
+                exp_img = np.zeros((h, offset_x, 3), dtype="uint8")
                 exp_img.fill(255)
                 image = np.hstack([exp_img, image, exp_img])
             else:
-                exp_img = np.zeros((h, exp_w, 3), dtype='uint8')
+                exp_img = np.zeros((h, exp_w, 3), dtype="uint8")
                 exp_img.fill(255)
                 image = np.hstack([image, exp_img])
         elif h * 1.0 / w <= 1.40:
             exp_h = int(w * 1.414 - h)
             if center:
                 offset_y = int(exp_h / 2)
-                exp_img = np.zeros((offset_y, w, 3), dtype='uint8')
+                exp_img = np.zeros((offset_y, w, 3), dtype="uint8")
                 exp_img.fill(255)
                 image = np.vstack([exp_img, image, exp_img])
             else:
-                exp_img = np.zeros((exp_h, w, 3), dtype='uint8')
+                exp_img = np.zeros((exp_h, w, 3), dtype="uint8")
                 exp_img.fill(255)
                 image = np.vstack([image, exp_img])
         return image, offset_x, offset_y
 
     @classmethod
-    def write_image_with_results(self,
-                                 image,
-                                 layouts=None,
-                                 results=None,
-                                 save_path=None,
-                                 font_path=None,
-                                 return_pil_image=False,
-                                 format=None,
-                                 max_size=None):
+    def write_image_with_results(
+        self, image, layouts=None, results=None, save_path=None, return_pil_image=False, format=None, max_size=None
+    ):
         """
         write image with boxes and results
         """
-        from PIL import Image, ImageDraw, ImageFont
-        if font_path is None:
-            font_path = '/workspace/uie-layout/PaddleNLP/fonts/SourceHanSansCN-Regular.ttf'
+
+        def _flatten_results(results):
+            """flatten results"""
+            is_single = False
+            if not isinstance(results, list):
+                results = [results]
+                is_single = True
+            flat_results = []
+
+            def _flatten(result):
+                flat_result = []
+                for key, vals in result.items():
+                    for val in vals:
+                        new_val = val.copy()
+                        if val.get("relations"):
+                            new_val["relations"] = _flatten(val["relations"])
+                        new_val["label"] = key
+                        flat_result.append(new_val)
+                return flat_result
+
+            for result in results:
+                flat_results.append(_flatten(result))
+            if is_single:
+                return flat_results[0]
+            return flat_results
+
+        def _write_results(results):
+            for segment in results:
+                if isinstance(segment, dict):
+                    boxes = segment["bbox"]
+                else:
+                    boxes = segment[0]
+                if not isinstance(boxes[0], list):
+                    boxes = [boxes]
+                boxes = [
+                    [
+                        (box[0], box[1]),
+                        (box[2], box[1]),
+                        (box[2], box[3]),
+                        (box[0], box[3]),
+                    ]
+                    for box in boxes
+                ]
+                while True:
+                    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                    if sum(color) < 480:
+                        break
+                for box in boxes:
+                    draw_render.polygon(box, fill=color)
+                if isinstance(segment, dict) and segment.get("relations"):
+                    _write_results(segment["relations"])
+
         random.seed(0)
         _image = self.read_image(image)
         _image = Image.fromarray(np.uint8(_image))
@@ -314,45 +342,12 @@ class DocParser(object):
         img_render = _image.copy()
         draw_render = ImageDraw.Draw(img_render)
 
-        def _get_text_height(layouts):
-            hght_list = []
-            for seg in layouts:
-                if isinstance(seg, dict):
-                    box = seg['bbox']
-                else:
-                    box = seg[0]
-                if isinstance(box[0], list):
-                    box = box[0]
-                box = [
-                    (box[0], box[1]),
-                    (box[2], box[1]),
-                    (box[2], box[3]),
-                    (box[0], box[3]),
-                ]
-                box_height = int(
-                    math.sqrt((box[0][0] - box[3][0])**2 +
-                              (box[0][1] - box[3][1])**2))
-                box_width = int(
-                    math.sqrt((box[0][0] - box[1][0])**2 +
-                              (box[0][1] - box[1][1])**2))
-                if box_height > 2 * box_width:
-                    hght_list.append(box_width)
-                else:
-                    hght_list.append(box_height)
-            if hght_list:
-                return int(0.9 * sum(hght_list) / len(hght_list))
-            return 0
-
         if layouts:
-            def_font_size = _get_text_height(layouts)
-            def_font = ImageFont.truetype(font_path,
-                                          def_font_size,
-                                          encoding="utf-8")
-            for i, seg in enumerate(layouts):
-                if isinstance(seg, dict):
-                    box, seg_text = seg['bbox'], seg['text']
+            for segment in layouts:
+                if isinstance(segment, dict):
+                    box = segment["bbox"]
                 else:
-                    box, seg_text = seg[:2]
+                    box = segment[0]
                 box = [
                     (box[0], box[1]),
                     (box[2], box[1]),
@@ -360,92 +355,17 @@ class DocParser(object):
                     (box[0], box[3]),
                 ]
                 while True:
-                    color = (random.randint(0, 255), random.randint(0, 255),
-                             random.randint(0, 255))
+                    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
                     if sum(color) < 480:
                         break
                 draw_render.polygon(box, fill=color)
 
         elif results:
-
-            def _flatten_results(results):
-                """flatten results"""
-                is_single = False
-                if not isinstance(results, list):
-                    results = [results]
-                    is_single = True
-                flat_results = []
-
-                def _flatten(result):
-                    flat_result = []
-                    for key, vals in result.items():
-                        for val in vals:
-                            new_val = val.copy()
-                            if val.get('relations'):
-                                new_val['relations'] = _flatten(
-                                    val['relations'])
-                            new_val['label'] = key
-                            flat_result.append(new_val)
-                    return flat_result
-
-                for result in results:
-                    flat_results.append(_flatten(result))
-                if is_single:
-                    return flat_results[0]
-                return flat_results
-
             results = _flatten_results(results)
-
-            # draw boxes and label
-            font_size = _get_text_height(results)
-            line_height = int(font_size * 3)
-            box_height = 1
-            font = ImageFont.truetype(font_path, font_size, encoding="utf-8")
-            cur_y = 30
-            cur_x = 20
-            max_x = 20
-
-            def _write_results(results, cur_x, cur_y, max_x, offset_x=0):
-                cur_x += offset_x
-                for i, seg in enumerate(results):
-                    if isinstance(seg, dict):
-                        boxes = seg['bbox']
-                    else:
-                        boxes, seg_text = seg[:2]
-                        score = seg[2] if len(seg) > 2 else None
-                        label = seg[3] if len(seg) > 3 else None
-                    if not isinstance(boxes[0], list):
-                        boxes = [boxes]
-                    boxes = [[
-                        (box[0], box[1]),
-                        (box[2], box[1]),
-                        (box[2], box[3]),
-                        (box[0], box[3]),
-                    ] for box in boxes]
-                    while True:
-                        color = (random.randint(0, 255), random.randint(0, 255),
-                                 random.randint(0, 255))
-                        if sum(color) < 480:
-                            break
-                    for box in boxes:
-                        draw_render.polygon(box, fill=color)
-                    cur_y += line_height + 8
-                    if cur_y + 2 * line_height + 30 > h:
-                        cur_y = 30
-                        cur_x = max_x + 20
-                    if isinstance(seg, dict) and seg.get('relations'):
-                        cur_x, cur_y, max_x = _write_results(seg['relations'],
-                                                             cur_x,
-                                                             cur_y,
-                                                             max_x,
-                                                             offset_x=30)
-                cur_x -= offset_x
-                return cur_x, cur_y, max_x
-
-            _write_results(results, cur_x, cur_y, max_x)
+            _write_results(results)
 
         img_render = Image.blend(_image, img_render, 0.3)
-        img_show = Image.new('RGB', (w, h), (255, 255, 255))
+        img_show = Image.new("RGB", (w, h), (255, 255, 255))
         img_show.paste(img_render, (0, 0, w, h))
         w, h = img_show.width, img_show.height
         if max_size and max(w, h) > max_size:
@@ -466,8 +386,8 @@ class DocParser(object):
         else:
             buff = BytesIO()
             if format is None:
-                format = 'jpeg'
-            if format.lower() == 'jpg':
-                format = 'jpeg'
+                format = "jpeg"
+            if format.lower() == "jpg":
+                format = "jpeg"
             img_show.save(buff, format=format, quality=90)
             return buff
