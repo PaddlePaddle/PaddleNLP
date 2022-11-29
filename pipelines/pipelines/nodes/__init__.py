cc03b7a1e611b52a391c7b3f06e5ffa655e3f3d3
w5688414 <w5688414@gmail.com>
2022-11-29 08:13:49 +0000

    Update __init__.py

--- pipelines/pipelines/nodes/__init__.py
+++ pipelines/pipelines/nodes/__init__.py
@@ -11,26 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from pipelines.utils.import_utils import safe_import
-
-from pipelines.nodes.base import BaseComponent
-from pipelines.nodes.file_classifier import FileTypeClassifier
-from pipelines.nodes.file_converter import (
-    BaseConverter,
-    DocxToTextConverter,
-    ImageToTextConverter,
-    MarkdownConverter,
-    PDFToTextConverter,
-    PDFToTextOCRConverter,
-    TextConverter,
-)
-from pipelines.nodes.preprocessor import BasePreProcessor, PreProcessor
-from pipelines.nodes.ranker import BaseRanker, ErnieRanker
-from pipelines.nodes.reader import BaseReader, ErnieReader
-from pipelines.nodes.retriever import BaseRetriever, DensePassageRetriever, BM25Retriever
-from pipelines.nodes.document import DocOCRProcessor, DocPrompter
-from pipelines.nodes.text_to_image_generator import ErnieTextToImageGenerator
-from pipelines.nodes.answer_extractor import AnswerExtractor, QAFilter, AnswerExtractorPreprocessor, QAFilterPostprocessor
-from pipelines.nodes.question_generator import QuestionGenerator
-from pipelines.nodes.other import JoinDocuments
