c388c23da6105cfa16e450a64f466ab19de5a5bd
Jack Zhou <zhoushunjie@baidu.com>
2022-11-29 23:36:39 +0800

    [PPDiffuers] Fix fd pipeline export model doc (#3952)
    * Fix export model
    
    * Fix sd export model

--- ppdiffusers/deploy/export_model.py
+++ ppdiffusers/deploy/export_model.py
@@ -12,68 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import paddle
-import paddlenlp
-
 import distutils.util
+import os
 
-from ppdiffusers import UNet2DConditionModel, AutoencoderKL
-from ppdiffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
+import paddle
+
 from paddlenlp.transformers import CLIPTextModel
+from ppdiffusers import AutoencoderKL, UNet2DConditionModel
 
 
 def parse_arguments():
     import argparse
-    import ast
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--inpaint",
-                        type=distutils.util.strtobool,
-                        default=False,
-                        help="Wheter to export inpaint model")
-    parser.add_argument("--pretrained_model_name_or_path",
-                        default='CompVis/stable-diffusion-v1-4',
-                        help="The pretrained diffusion model.")
-    parser.add_argument("--output_path",
-                        type=str,
-                        required=True,
-                        help="The pretrained diffusion model.")
+    parser.add_argument(
+        "--inpaint", type=distutils.util.strtobool, default=False, help="Wheter to export inpaint model"
+    )
+    parser.add_argument(
+        "--pretrained_model_name_or_path",
+        default="CompVis/stable-diffusion-v1-4",
+        help="The pretrained diffusion model.",
+    )
+    parser.add_argument("--output_path", type=str, required=True, help="The pretrained diffusion model.")
     return parser.parse_args()
 
 
 class VAEDecoder(AutoencoderKL):
-
     def forward(self, z):
         return self.decode(z, True).sample
 
 
 class VAEEncoder(AutoencoderKL):
-
     def forward(self, z):
-        return self.encode(z, True).latent_dist.sample()
+        return self.encode(z, True).latent_dist.mode()
 
 
 if __name__ == "__main__":
-    paddle.set_device('cpu')
+    paddle.set_device("cpu")
     args = parse_arguments()
     # Load models and create wrapper for stable diffusion
-    text_encoder = CLIPTextModel.from_pretrained(
-        os.path.join(args.pretrained_model_name_or_path, "text_encoder"))
-    vae_decoder = VAEDecoder.from_pretrained(args.pretrained_model_name_or_path,
-                                             subfolder="vae")
-    vae_encoder = VAEEncoder.from_pretrained(args.pretrained_model_name_or_path,
-                                             subfolder="vae")
-    unet = UNet2DConditionModel.from_pretrained(
-        args.pretrained_model_name_or_path, subfolder="unet")
+    text_encoder = CLIPTextModel.from_pretrained(os.path.join(args.pretrained_model_name_or_path, "text_encoder"))
+    vae_decoder = VAEDecoder.from_pretrained(args.pretrained_model_name_or_path, subfolder="vae")
+    vae_encoder = VAEEncoder.from_pretrained(args.pretrained_model_name_or_path, subfolder="vae")
+    unet = UNet2DConditionModel.from_pretrained(args.pretrained_model_name_or_path, subfolder="unet")
 
     # Convert to static graph with specific input description
     text_encoder = paddle.jit.to_static(
         text_encoder,
-        input_spec=[
-            paddle.static.InputSpec(shape=[None, None],
-                                    dtype="int64",
-                                    name="input_ids")  # input_ids
-        ])
+        input_spec=[paddle.static.InputSpec(shape=[None, None], dtype="int64", name="input_ids")],  # input_ids
+    )
 
     # Save text_encoder in static graph model.
     save_path = os.path.join(args.output_path, "text_encoder", "inference")
@@ -84,10 +71,11 @@ if __name__ == "__main__":
     vae_decoder = paddle.jit.to_static(
         vae_decoder,
         input_spec=[
-            paddle.static.InputSpec(shape=[None, 4, None, None],
-                                    dtype="float32",
-                                    name="latent_sample"),  # latent_sample
-        ])
+            paddle.static.InputSpec(
+                shape=[None, 4, None, None], dtype="float32", name="latent_sample"
+            ),  # latent_sample
+        ],
+    )
     # Save vae_decoder in static graph model.
     save_path = os.path.join(args.output_path, "vae_decoder", "inference")
     paddle.jit.save(vae_decoder, save_path)
@@ -98,10 +86,10 @@ if __name__ == "__main__":
         vae_encoder,
         input_spec=[
             paddle.static.InputSpec(
-                shape=[None, 3, None, None],  # N, C, H, W
-                dtype="float32",
-                name="sample"),  # latent
-        ])
+                shape=[None, 3, None, None], dtype="float32", name="sample"  # N, C, H, W
+            ),  # latent
+        ],
+    )
     # Save vae_encoder in static graph model.
     save_path = os.path.join(args.output_path, "vae_encoder", "inference")
     paddle.jit.save(vae_encoder, save_path)
@@ -112,16 +100,13 @@ if __name__ == "__main__":
     unet = paddle.jit.to_static(
         unet,
         input_spec=[
-            paddle.static.InputSpec(shape=[None, unet_channels, None, None],
-                                    dtype="float32",
-                                    name="sample"),  # sample
-            paddle.static.InputSpec(shape=[1], dtype="int64",
-                                    name="timestep"),  # timesteps
+            paddle.static.InputSpec(shape=[None, unet_channels, None, None], dtype="float32", name="sample"),  # sample
+            paddle.static.InputSpec(shape=[1], dtype="int64", name="timestep"),  # timesteps
             paddle.static.InputSpec(
-                shape=[None, None, 768],
-                dtype="float32",
-                name="encoder_hidden_states")  # encoder_hidden_states
-        ])
+                shape=[None, None, 768], dtype="float32", name="encoder_hidden_states"
+            ),  # encoder_hidden_states
+        ],
+    )
     save_path = os.path.join(args.output_path, "unet", "inference")
     paddle.jit.save(unet, save_path)
     print(f"Save unet model in {save_path} successfully.")
