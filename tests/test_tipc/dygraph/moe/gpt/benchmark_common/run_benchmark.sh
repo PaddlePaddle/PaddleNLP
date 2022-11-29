b6724f7f48696521e9f660e0c7e5f7f4170b7f53
fightfat <18510030324@126.com>
2022-11-29 12:18:54 +0000

    fix random rank in pdc

--- tests/test_tipc/dygraph/moe/gpt/benchmark_common/run_benchmark.sh
+++ tests/test_tipc/dygraph/moe/gpt/benchmark_common/run_benchmark.sh
@@ -97,31 +97,36 @@ function _train(){
                 --scale_loss 32768 \
                 --gate gshard \
                 --balance_loss_weight 1.0"
-
+    if [ ${PADDLE_TRAINER_ID} ]
+    then
+        PADDLE_RANK_OPTION=" --rank ${PADDLE_TRAINER_ID}"
+    else
+        PADDLE_RANK_OPTION=""
+    fi
     # 以下为通用执行命令，无特殊可不用修改
     case ${run_mode} in
     DP_MoE_C1) echo "run run_mode: DP_MoE_C1"
-        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0 \
+        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0 ${PADDLE_RANK_OPTION}\
               run_moe_pretrain.py ${train_cmd}"
         workerlog_id=0
         ;;
     DP_MoE_C8|DP_MoE_C16) echo "run run_mode: ${run_mode}"
-        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 \
+        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 ${PADDLE_RANK_OPTION}\
               run_moe_pretrain.py ${train_cmd}"
         workerlog_id=0
         ;;
     DP_MoE_C32) echo "run run_mode: DP_MoE_C32"
-        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 \
+        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 ${PADDLE_RANK_OPTION}\
               run_moe_pretrain.py ${train_cmd}"
         workerlog_id=0
         ;;
     Sharding_MoE_C8|Sharding_MoE_C16) echo "run run_mode: ${run_mode}"
-        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 \
+        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 ${PADDLE_RANK_OPTION}\
               run_moe_pretrain.py ${train_cmd}"
         workerlog_id=0
         ;;
     Sharding_MoE_C32) echo "run run_mode: Sharding_MoE_C32"
-        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 \
+        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 ${PADDLE_RANK_OPTION}\
               run_moe_pretrain.py ${train_cmd}"
         workerlog_id=0
         ;;
