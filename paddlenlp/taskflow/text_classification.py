9f7b70a902ca68a14c7c5f9aaa5909d701f57b9a
Sijun He <sijun.he@hotmail.com>
2022-11-30 20:49:46 +0800

    roll back accidental taskflw change

--- paddlenlp/taskflow/text_classification.py
+++ paddlenlp/taskflow/text_classification.py
@@ -15,17 +15,14 @@
 
 from typing import Any, Dict, List, Union
 
-import numpy as np
-import paddle
-import paddle.nn.functional as F
-from scipy.special import expit as np_sigmoid
-from scipy.special import softmax as np_softmax
-
 from paddlenlp.data import DataCollatorWithPadding
 from paddlenlp.transformers import AutoModelForSequenceClassification, AutoTokenizer
 
+import numpy as np
+import paddle
+import paddle.nn.functional as F
+from .utils import static_mode_guard, dygraph_mode_guard
 from .task import Task
-from .utils import dygraph_mode_guard, static_mode_guard
 
 usage = r"""
         from paddlenlp import Taskflow
@@ -80,11 +77,19 @@ class TextClassificationTask(Task):
         task_path (string): The local file path to the model path or a pre-trained model
         id2label (string): The dictionary to map the predictions from class ids to class names
         is_static_model (string): Whether the model is a static model
-        kwargs (dict, optional): Additional keyword arguments passed along to the specific task.
+        kwargs (dict, optional): Additional keyword arguments passed along to the specific task. 
     """
 
-    def __init__(self, task: str, model: str, id2label: Dict[int, str], is_static_model: bool = False, **kwargs):
-        super().__init__(task=task, model=model, is_static_model=is_static_model, **kwargs)
+    def __init__(self,
+                 task: str,
+                 model: str,
+                 id2label: Dict[int, str],
+                 is_static_model: bool = False,
+                 **kwargs):
+        super().__init__(task=task,
+                         model=model,
+                         is_static_model=is_static_model,
+                         **kwargs)
         self.id2label = id2label
         self.is_static_model = is_static_model
         self._construct_tokenizer(self._task_path)
@@ -97,13 +102,16 @@ class TextClassificationTask(Task):
         """
         Construct the input spec for the convert dygraph model to static model.
         """
-        raise NotImplementedError("Conversion from dygraph to static graph is not supported in TextClassificationTask")
+        raise NotImplementedError(
+            "Conversion from dygraph to static graph is not supported in TextClassificationTask"
+        )
 
     def _construct_model(self, model: str):
         """
         Construct the inference model for the predictor.
         """
-        model_instance = AutoModelForSequenceClassification.from_pretrained(model, num_labels=len(self.id2label))
+        model_instance = AutoModelForSequenceClassification.from_pretrained(
+            model, num_labels=len(self.id2label))
         # Load the model parameter for the predict
         model_instance.eval()
         self._model = model_instance
@@ -122,12 +130,21 @@ class TextClassificationTask(Task):
         """
         inputs = self._check_input_text(inputs)
         # Get the config from the kwargs
-        batch_size = self.kwargs["batch_size"] if "batch_size" in self.kwargs else 1
+        batch_size = self.kwargs[
+            'batch_size'] if 'batch_size' in self.kwargs else 1
 
-        max_length = self.kwargs["max_length"] if "max_length" in self.kwargs else 512
-        collator = DataCollatorWithPadding(self._tokenizer, return_tensors="np" if self.is_static_model else "pd")
-        tokenized_inputs = [self._tokenizer(i, max_length=max_length) for i in inputs]
-        batches = [tokenized_inputs[idx : idx + batch_size] for idx in range(0, len(tokenized_inputs), batch_size)]
+        max_length = self.kwargs[
+            'max_length'] if 'max_length' in self.kwargs else 512
+        collator = DataCollatorWithPadding(
+            self._tokenizer,
+            return_tensors="np" if self.is_static_model else "pd")
+        tokenized_inputs = [
+            self._tokenizer(i, max_length=max_length) for i in inputs
+        ]
+        batches = [
+            tokenized_inputs[idx:idx + batch_size]
+            for idx in range(0, len(tokenized_inputs), batch_size)
+        ]
         outputs = {}
         outputs["text"] = inputs
         outputs["batches"] = [collator(batch) for batch in batches]
@@ -136,108 +153,49 @@ class TextClassificationTask(Task):
 
     def _run_model(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         """
-        Run the task model from the outputs of the `_tokenize` function.
+        Run the task model from the outputs of the `_tokenize` function. 
         """
-        # TODO: support hierachical classification
-        outputs = {}
-        outputs["text"] = inputs["text"]
-        outputs["batch_logits"] = []
+        # TODO: support multi_label, hierachical classification
+        model_outputs = []
         if self.is_static_model:
             with static_mode_guard():
                 for batch in inputs["batches"]:
-                    for i, input_name in enumerate(self.predictor.get_input_names()):
+                    for i, input_name in enumerate(
+                            self.predictor.get_input_names()):
                         self.input_handles[i].copy_from_cpu(batch[input_name])
                     self.predictor.run()
                     logits = self.output_handle[0].copy_to_cpu().tolist()
-                    outputs["batch_logits"].append(logits)
-                    # model_outputs = self._process_logits(logits)
-                    # pred_indices = np.argmax(logits, axis=-1)
-                    # probs = softmax(logits, axis=-1)
-                    # for prob, pred_index in zip(probs, pred_indices):
-                    #     model_outputs.append({
-                    #         "label": pred_index,
-                    #         "score": prob[pred_index]
-                    #     })
+                    pred_indices = np.argmax(logits, axis=-1)
+                    probs = softmax(logits, axis=-1)
+                    for prob, pred_index in zip(probs, pred_indices):
+                        model_outputs.append({
+                            "label": pred_index,
+                            "score": prob[pred_index]
+                        })
         else:
             with dygraph_mode_guard():
                 for batch in inputs["batches"]:
                     logits = self._model(**batch)
-                    outputs["batch_logits"].append(logits)
-                    # model_outputs = self._process_logits(logits)
-                    # probs = F.softmax(logits, axis=-1).tolist()
-                    # pred_indices = paddle.argmax(logits, axis=-1).tolist()
-                    # for prob, pred_index in zip(probs, pred_indices):
-                    #     model_outputs.append({
-                    #         "label": pred_index,
-                    #         "score": prob[pred_index]
-                    #     })
-        # model_outputs
+                    probs = F.softmax(logits, axis=-1).tolist()
+                    pred_indices = paddle.argmax(logits, axis=-1).tolist()
+                    for prob, pred_index in zip(probs, pred_indices):
+                        model_outputs.append({
+                            "label": pred_index,
+                            "score": prob[pred_index]
+                        })
+        outputs = {}
+        outputs["text"] = inputs["text"]
+        outputs["model_outputs"] = model_outputs
         return outputs
-
-    def _process_logits(self, logits: Union[paddle.Tensor, np.ndarray]) -> List[Any]:
-        model_outputs = []
-        if self.model == "multi_class":
-            # dygraph
-            if isinstance(logits, paddle.Tensor):
-                scores = F.softmax(logits, axis=-1).numpy()
-                labels = paddle.argmax(logits, axis=-1).numpy()
-            # static graph
-            else:
-                scores = np_softmax(logits, axis=-1)
-                labels = np.argmax(logits, axis=-1)
-            for score, label in zip(scores, labels):
-                model_outputs.append([{"label": label, "score": score[label]}])
-        # multi_label
-        else:
-            # dygraph
-            if isinstance(logits, paddle.Tensor):
-                scores = F.sigmoid(logits).numpy()
-            # static graph
-            else:
-                scores = np_sigmoid(logits)
-            for score in scores:
-                labels = []
-                # TODO: make the threshold configurable
-                for i, class_score in enumerate(score):
-                    if class_score > 0.5:
-                        labels.append({"label": i, "score": class_score})
-                model_outputs.append(labels)
-        return model_outputs
 
     def _postprocess(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         """
         The model output is tag ids, this function will convert the model output to raw text.
         """
-        # TODO: support hierachical classification
+        # TODO: support multi_label, hierachical classification
         postprocessed_outputs = []
-        for logits in inputs["batch_logits"]:
-            if self.model == "multi_class":
-                if isinstance(logits, paddle.Tensor):  # dygraph
-                    scores = F.softmax(logits, axis=-1).numpy()
-                    labels = paddle.argmax(logits, axis=-1).numpy()
-                else:  # static graph
-                    scores = np_softmax(logits, axis=-1)
-                    labels = np.argmax(logits, axis=-1)
-                for score, label in zip(scores, labels):
-                    postprocessed_output = {}
-                    # postprocessed_output["text"] = inputs["text"][i]
-                    postprocessed_output["predictions"] = [{"label": self.id2label[label], "score": score[label]}]
-            else:  # multi_label
-                if isinstance(logits, paddle.Tensor):  # dygraph
-                    scores = F.sigmoid(logits).numpy()
-                else:  # static graph
-                    scores = np_sigmoid(logits)
-                for score in scores:
-                    postprocessed_output = {}
-                    postprocessed_output["predictions"] = []
-                    for i, class_score in enumerate(score):
-                        # TODO: make the threshold configurable
-                        if class_score > 0.5:
-                            postprocessed_output["predictions"].append(
-                                {"label": self.id2label[i], "score": class_score}
-                            )
-            postprocessed_outputs.append(postprocessed_output)
-
-        for i, postprocessed_output in enumerate(postprocessed_outputs):
-            postprocessed_output["text"] = inputs["text"][i]
+        for i, model_output in enumerate(inputs["model_outputs"]):
+            model_output["label"] = self.id2label[model_output["label"]]
+            model_output["text"] = inputs["text"][i]
+            postprocessed_outputs.append(model_output)
         return postprocessed_outputs
