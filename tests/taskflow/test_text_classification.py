6e5ba93877fc1ad33d425a7c533bcf199a5d17ae
Sijun He <sijun.he@hotmail.com>
2022-11-30 17:46:22 +0800

    style

--- tests/taskflow/test_text_classification.py
+++ tests/taskflow/test_text_classification.py
@@ -14,22 +14,21 @@
 
 import os
 import unittest
+from tempfile import TemporaryDirectory
 
 import paddle
 from parameterized import parameterized
-from tempfile import TemporaryDirectory
+
 from paddlenlp.taskflow import Taskflow
 from paddlenlp.taskflow.text_classification import TextClassificationTask
 from paddlenlp.transformers import AutoTokenizer, ErnieForSequenceClassification
 
 
 class TestTextClassificationTask(unittest.TestCase):
-
     def setUp(self):
         self.temp_dir = TemporaryDirectory()
         self.dygraph_model_path = os.path.join(self.temp_dir.name, "dygraph")
-        model = ErnieForSequenceClassification.from_pretrained(
-            "__internal_testing__/ernie", num_classes=2)
+        model = ErnieForSequenceClassification.from_pretrained("__internal_testing__/ernie", num_classes=2)
         tokenizer = AutoTokenizer.from_pretrained("__internal_testing__/ernie")
         model.save_pretrained(self.dygraph_model_path)
         tokenizer.save_pretrained(self.dygraph_model_path)
@@ -37,12 +36,8 @@ class TestTextClassificationTask(unittest.TestCase):
         # export to static
         self.static_model_path = os.path.join(self.temp_dir.name, "static")
         input_spec = [
-            paddle.static.InputSpec(shape=[None, None],
-                                    dtype="int64",
-                                    name='input_ids'),
-            paddle.static.InputSpec(shape=[None, None],
-                                    dtype="int64",
-                                    name='token_type_ids')
+            paddle.static.InputSpec(shape=[None, None], dtype="int64", name="input_ids"),
+            paddle.static.InputSpec(shape=[None, None], dtype="int64", name="token_type_ids"),
         ]
         static_model = paddle.jit.to_static(model, input_spec=input_spec)
         paddle.jit.save(static_model, self.static_model_path)
@@ -51,73 +46,83 @@ class TestTextClassificationTask(unittest.TestCase):
     def tearDown(self):
         self.temp_dir.cleanup()
 
-    @parameterized.expand([(1, ), (2, )])
-    def test_text_classification_task(self, batch_size):
+    @parameterized.expand(
+        [
+            (1, "multi_class"),
+            # (2, "multi_task"),
+            # (1, "multi_label"),
+            (2, "multi_label"),
+        ]
+    )
+    def test_classification_task(self, batch_size, model):
         # input_text is a tuple to simulate the args passed from Taskflow to TextClassificationTask
-        input_text = (["百度", "深度学习框架", "飞桨", "PaddleNLP"], )
+        input_text = (["百度", "深度学习框架", "飞桨", "PaddleNLP"],)
         id2label = {
             0: "negative",
             1: "positive",
         }
         dygraph_taskflow = TextClassificationTask(
-            model="multi_class",
+            model=model,
             task="text_classification",
             task_path=self.dygraph_model_path,
             id2label=id2label,
             batch_size=batch_size,
-            device_id=0)
+            device_id=0,
+        )
 
         dygraph_results = dygraph_taskflow(input_text)
+        print(dygraph_results, len(dygraph_results))
+
         self.assertEqual(len(dygraph_results), len(input_text[0]))
 
         static_taskflow = TextClassificationTask(
-            model="multi_class",
+            model=model,
             task="text_classification",
             is_static_model=True,
             task_path=self.static_model_path,
             id2label=id2label,
             batch_size=batch_size,
-            device_id=0)
+            device_id=0,
+        )
 
         static_results = static_taskflow(input_text)
         self.assertEqual(len(static_results), len(input_text[0]))
 
-        for dygraph_result, static_result in zip(dygraph_results,
-                                                 static_results):
-            self.assertEqual(dygraph_result["label"], static_result["label"])
-            self.assertAlmostEqual(dygraph_result["score"],
-                                   static_result["score"],
-                                   delta=1e-6)
+        for dygraph_result, static_result in zip(dygraph_results, static_results):
+            for dygraph_pred, static_pred in zip(dygraph_result["predictions"], static_result["predictions"]):
+                self.assertEqual(dygraph_pred["label"], static_pred["label"])
+                self.assertAlmostEqual(dygraph_pred["score"], static_pred["score"], delta=1e-6)
 
-    @parameterized.expand([(1, ), (2, )])
+    @parameterized.expand([(1,), (2,)])
     def test_taskflow(self, batch_size):
         input_text = ["百度", "深度学习框架", "飞桨", "PaddleNLP"]
         id2label = {
             0: "negative",
             1: "positive",
         }
-        dygraph_taskflow = Taskflow(model="multi_class",
-                                    task="text_classification",
-                                    task_path=self.dygraph_model_path,
-                                    id2label=id2label,
-                                    batch_size=batch_size)
+        dygraph_taskflow = Taskflow(
+            model="multi_class",
+            task="text_classification",
+            task_path=self.dygraph_model_path,
+            id2label=id2label,
+            batch_size=batch_size,
+        )
 
         dygraph_results = dygraph_taskflow(input_text)
         self.assertEqual(len(dygraph_results), len(input_text))
 
-        static_taskflow = Taskflow(model="multi_class",
-                                   task="text_classification",
-                                   is_static_model=True,
-                                   task_path=self.static_model_path,
-                                   id2label=id2label,
-                                   batch_size=batch_size)
+        static_taskflow = Taskflow(
+            model="multi_class",
+            task="text_classification",
+            is_static_model=True,
+            task_path=self.static_model_path,
+            id2label=id2label,
+            batch_size=batch_size,
+        )
 
         static_results = static_taskflow(input_text)
         self.assertEqual(len(static_results), len(input_text))
 
-        for dygraph_result, static_result in zip(dygraph_results,
-                                                 static_results):
+        for dygraph_result, static_result in zip(dygraph_results, static_results):
             self.assertEqual(dygraph_result["label"], static_result["label"])
-            self.assertAlmostEqual(dygraph_result["score"],
-                                   static_result["score"],
-                                   delta=1e-6)
+            self.assertAlmostEqual(dygraph_result["score"], static_result["score"], delta=1e-6)
