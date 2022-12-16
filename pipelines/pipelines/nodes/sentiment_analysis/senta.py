661e94439d722fd8f93365134c1d2f585331ebcd
milen <1649759610@qq.com>
2022-12-16 12:54:24 +0000

    refine codestyle

--- pipelines/pipelines/nodes/sentiment_analysis/senta.py
+++ pipelines/pipelines/nodes/sentiment_analysis/senta.py
@@ -27,21 +27,24 @@ class UIESenta(BaseComponent):
     """
     Senta: sentiment analysis for user's comments based on Taskflow
     """
+
     outgoing_edges = 1
-    
-    def __init__(self,
-                 model,
-                 schema,
-                 task="sentiment_analysis",
-                 aspects=None,
-                 max_seq_len=512,
-                 batch_size=1,
-                 split_sentence=False,
-                 position_prob=0.5,
-                 lazy_load=False,
-                 num_workers=0,
-                 use_fast=False,
-                 **kwargs):
+
+    def __init__(
+        self,
+        model,
+        schema,
+        task="sentiment_analysis",
+        aspects=None,
+        max_seq_len=512,
+        batch_size=1,
+        split_sentence=False,
+        position_prob=0.5,
+        lazy_load=False,
+        num_workers=0,
+        use_fast=False,
+        **kwargs
+    ):
         """
         Init UIESenta for Sentiment Analysis.
         :param model: the model name that you wanna use, you can choose it in [use-base, uie-medium, uie-micro, uie-mini, uie-nano].
@@ -52,30 +55,31 @@ class UIESenta(BaseComponent):
         :param batch_size: Number of samples the model receives in one batch for inference.
         :param split_sentence: If True, sentence-level split will be performed on the inputing examples.
         :param position_prob: Probability threshold for start/end index probabiliry.
-        :param lazy_load: whether to using `MapDataset` or an `IterDataset` when performing inference with UIE. True for `IterDataset`. False for `MapDataset`. 
+        :param lazy_load: whether to using `MapDataset` or an `IterDataset` when performing inference with UIE. True for `IterDataset`. False for `MapDataset`.
         :num_workers: the number of subprocess to load data for dataloader, 0 for no subprocess used and loading data in main process. Default 0.
         :use_fast: whether to fast tokenizer for UIE.
         """
-        
+
         self.set_config(
-            model = model,
-            schema = schema,
-            task = task,
-            aspects = aspects,
-            max_seq_len = max_seq_len,
-            batch_size = batch_size,
-            split_sentence = split_sentence,
-            position_prob = position_prob,
-            lazy_load = lazy_load,
-            num_workers = num_workers,
-            use_fast = use_fast,
-            **kwargs)
+            model=model,
+            schema=schema,
+            task=task,
+            aspects=aspects,
+            max_seq_len=max_seq_len,
+            batch_size=batch_size,
+            split_sentence=split_sentence,
+            position_prob=position_prob,
+            lazy_load=lazy_load,
+            num_workers=num_workers,
+            use_fast=use_fast,
+            **kwargs,
+        )
 
         self.senta = Taskflow(**self.pipeline_config["params"])
 
     def _predict(self, examples: str):
         return self.senta(examples)
-    
+
     def _save_json_file(self, save_path: str, results: List[dict]):
         dir_name = os.path.dirname(save_path)
         if not os.path.exists(dir_name):
@@ -85,7 +89,7 @@ class UIESenta(BaseComponent):
                 line = json.dumps(result, ensure_ascii=False) + "\n"
                 f.write(line)
 
-    def run(self,  examples: List[str], sr_save_path: str):        
+    def run(self, examples: List[str], sr_save_path: str):
         # predict with taskflow
         results = self._predict(examples)
         # save the result of sentiment analysis
