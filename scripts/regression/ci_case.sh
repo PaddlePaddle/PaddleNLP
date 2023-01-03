37be45db809858041acfda44637235e2d7137a39
zhang junjun <1834792141@qq.com>
2023-01-03 14:35:14 +0800

    add fast_generation samples test (#4311)

--- scripts/regression/ci_case.sh
+++ scripts/regression/ci_case.sh
@@ -1062,8 +1062,8 @@ for apicase in `ls`;do
     fi
 done
 }
-faster_generation(){
-cd ${nlp_dir}/faster_generation/samples
+fast_generation(){
+cd ${nlp_dir}/fast_generation/samples
 python codegen_sample.py >${log_path}/fast_generation_codegen >>${log_path}/fast_generation_codegen 2>&1
 print_info $? fast_generation_codegen
 
@@ -1079,7 +1079,7 @@ print_info $? fast_generation_plato
 python t5_sample.py --use_faster >${log_path}/fast_generation_t5 >>${log_path}/fast_generation_t5 2>&1
 print_info $? fast_generation_t5
 
-cd ${nlp_dir}/paddlenlp/ops/faster_transformer/sample/
+cd ${nlp_dir}/paddlenlp/ops/fast_transformer/sample/
 python bart_decoding_sample.py >${log_path}/fast_generation_bart >>${log_path}/fast_generation_bart 2>&1
 print_info $? fast_generation_bart
 
