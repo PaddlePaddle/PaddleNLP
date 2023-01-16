5b58ec129ecc8e8a3f978b6f300254680d6a51c4
linjieccc <623543001@qq.com>
2023-01-16 07:22:24 +0000

    support recompute for ernie-layout

--- applications/information_extraction/document/finetune.py
+++ applications/information_extraction/document/finetune.py
@@ -103,7 +103,7 @@ def main():
             )
 
     # Define model and tokenizer
-    model = UIEX.from_pretrained(model_args.model_name_or_path)
+    model = UIEX.from_pretrained(model_args.model_name_or_path, enable_recompute=training_args.enable_recompute)
     tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
 
     # Load and preprocess dataset
