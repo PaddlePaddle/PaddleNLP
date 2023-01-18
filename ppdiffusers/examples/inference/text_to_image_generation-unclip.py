4c171d0c1459b63f2a2420c5ac6cb4860c02df59
westfish <westfish@126.com>
2023-01-18 18:48:03 +0800

    [PPDiffusers]update docs and add new infer scripts (#4390)
    * pipeliens doc update
    
    * update
    
    * update docs
    
    * update
    
    * add unclip scripts
    
    * rm score_sde_vp
    
    * add release note
    
    * remove novel

--- ppdiffusers/examples/inference/text_to_image_generation-unclip.py
+++ ppdiffusers/examples/inference/text_to_image_generation-unclip.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,5 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from ppdiffusers import UnCLIPPipeline
 
-# TODO
+# 加载模型和scheduler
+pipe = UnCLIPPipeline.from_pretrained("kakaobrain/karlo-v1-alpha")
+
+# 执行pipeline进行推理
+prompt = "a high-resolution photograph of a big red frog on a green leaf."
+image = pipe([prompt]).images[0]
+
+# 保存图片
+image.save("./frog.png")
