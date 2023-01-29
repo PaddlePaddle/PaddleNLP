4867dfb5e9d74816b318706c48cc6ee343f63ec0
Sijun He <sijun.he@hotmail.com>
2023-01-29 16:10:54 +0800

    remove six

--- applications/document_intelligence/doc_vqa/Rerank/src/tokenization.py
+++ applications/document_intelligence/doc_vqa/Rerank/src/tokenization.py
@@ -20,7 +20,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
-import six
 import collections
 import unicodedata
 from io import open
