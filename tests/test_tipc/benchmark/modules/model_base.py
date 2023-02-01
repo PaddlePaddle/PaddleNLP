19acd0bb67a8f94bb271e82f48ee0d928afdebd7
liu zhengxi <380185688@qq.com>
2023-02-01 12:22:29 +0800

    [Benchmark] Add BERT GPT T5 finetune process (#4492)

--- tests/test_tipc/benchmark/modules/model_base.py
+++ tests/test_tipc/benchmark/modules/model_base.py
@@ -1,4 +1,17 @@
-import paddle
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from paddlenlp.utils.log import logger
 
 
@@ -15,6 +28,12 @@ class BenchmarkBase(object):
 
     def build_model(self, args, **kwargs):
         raise NotImplementedError
+
+    def generate_inputs_for_model(self, args, **kwargs):
+        raise NotImplementedError
+
+    def create_input_specs(self):
+        return None
 
     def forward(self, model, args, input_data=None, **kwargs):
         raise NotImplementedError
