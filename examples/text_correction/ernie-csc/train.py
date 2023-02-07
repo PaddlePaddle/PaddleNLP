7addeb40c03b71fffdf2b6df358bd28e5ea14850
Zhong Hui <zhonghui.net@gmail.com>
2023-02-07 20:12:27 +0800

    Update train.py

--- examples/text_correction/ernie-csc/train.py
+++ examples/text_correction/ernie-csc/train.py
@@ -35,7 +35,7 @@ from utils import convert_example, create_dataloader, read_train_ds
 # yapf: disable
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=8, type=int, help="Batch size per GPU/CPU for training.")
-parser.add_argument("--model_name_or_path", type=str, default="ernie-1.0", choices=["ernie-1.0"], help="Pretraining model name or path")
+parser.add_argument("--model_name_or_path", type=str, default="ernie-1.0-base-zh", choices=["ernie-1.0-base-zh"], help="Pretraining model name or path")
 parser.add_argument("--max_seq_length", type=int, default=128, help="The maximum total input sequence length after SentencePiece tokenization.")
 parser.add_argument("--learning_rate", type=float, default=5e-5, help="Learning rate used to train.")
 parser.add_argument("--save_steps", type=int, default=1000, help="Save checkpoint every X updates steps.")
