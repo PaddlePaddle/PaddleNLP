772d1b0704e29495b8de19737bdb57c5e1b2183b
Zhong Hui <zhonghui.net@gmail.com>
2023-02-09 11:14:31 +0800

    add test file

--- examples/language_model/markuplm/markuplmft/test.py
+++ examples/language_model/markuplm/markuplmft/test.py
@@ -18,14 +18,12 @@ import numpy as np
 
 
 class TestMarkupLM(unittest.TestCase):
-    def setUp(self):
-        # sys.path.remove(".")
-        pass
-
     def testLoad(self):
         """load torch weight"""
-        # import torch;a = torch.load("/ssd2/zhonghui03/.paddlenlp/models/models--microsoft--markuplm-base/snapshots/7b1ef356189e6803bd5b58b305c783fd1d395ae6/pytorch_model.bin", map_location="cpu");[print(x) for x in sorted(a.keys())];
-        # python -c 'import torch;a = torch.load("/ssd2/zhonghui03/.paddlenlp/models/models--microsoft--markuplm-base/snapshots/7b1ef356189e6803bd5b58b305c783fd1d395ae6/pytorch_model.bin", map_location="cpu");[print(x) for x in sorted(a.keys())];'
+        # import torch;
+        # a = torch.load("~/.paddlenlp/models/models--microsoft--markuplm-base/snapshots/7b1ef356189e6803bd5b58b305c783fd1d395ae6/pytorch_model.bin", map_location="cpu");
+        # [print(x) for x in sorted(a.keys())];
+        # python -c 'import torch;a = torch.load("~/.paddlenlp/models/models--microsoft--markuplm-base/snapshots/7b1ef356189e6803bd5b58b305c783fd1d395ae6/pytorch_model.bin", map_location="cpu");[print(x) for x in sorted(a.keys())];'
         pass
 
     def testForwardLM(self):
@@ -36,11 +34,7 @@ class TestMarkupLM(unittest.TestCase):
 
         model = MarkupLMModel.from_pretrained("microsoft/markuplm-base", from_hf_hub=True)
         model.eval()
-        # pad_token_id have diff for nn.Embedding
+        # pad_token_id=1 have diff for nn.Embedding
         out = model(input_ids=paddle.to_tensor([[2, 2, 4, 5, 6]], dtype="int64"), return_dict=True)
-        # E       ValueError: Tensor(shape=[1], dtype=float32, place=Place(gpu:0), stop_gradient=False,
-        # E              [0.32763067])
         result = out.pooler_output.abs().mean().item()
-        print(result)
-        # print("sasxxx\n"*20)
         np.testing.assert_allclose(result, 0.31490570306777954, atol=1e-6)
