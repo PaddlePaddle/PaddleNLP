ca4006ba33f0c2c0c02e8b1531376ab5928a9cb1
wj-Mcat <1435130236@qq.com>
2023-02-13 03:03:58 +0000

    commit mvp auto-encoder

--- paddlenlp/transformers/encoder.py
+++ paddlenlp/transformers/encoder.py
@@ -14,26 +14,53 @@
 from __future__ import annotations
 
 import inspect
+from typing import Dict, List
 
 from paddle import nn
 
-from paddlenlp.transformers import AutoModel, PretrainedConfig
+from paddlenlp.transformers import AutoModel, PretrainedConfig, PretrainedModel
 from paddlenlp.transformers.auto.modeling import get_name_mapping
 from paddlenlp.utils.import_utils import import_module
 
 
-def _load_text_model():
+def _load_text_model_names() -> List[str]:
+    """load text model names
+
+    Returns:
+        List[str]: the names of text model
+    """
     model = get_name_mapping("Model")
     return {value for key, value in model.items() if key.endswith("Model_Import_Class")}
 
 
-def _load_model_class_mapping(config: EncoderConfig):
-    pass
+def _load_image_model_names() -> List[str]:
+    """load image model names
+
+    Returns:
+        List[str]: the names of image model
+    """
+    return [
+        "ErnieViLVisionModel",
+        "CLIPViLVisionModel",
+        "ChineseCLIPViLVisionModel",
+    ]
+
+
+def _load_text_image_model_names() -> List[str]:
+    """load image model names
+
+    Returns:
+        List[str]: the names of text & image model
+    """
+    return ["CLIPModel", "CLIPChineseModel", "ErnieViLModel"]
 
 
 class AutoConfig:
+    """TODO: to construct AutoConfig to resolve `model-clas`<architectures>"""
+
     @staticmethod
     def from_pretrained(name_or_path: str, mode: str | list[str] | None = "text_image", **kwargs):
+
         # auto model
         transformer_module = import_module("paddlenlp.transformers")
         for name in dir(transformer_module):
@@ -66,145 +93,143 @@ class AutoConfig:
 
 
 class EncoderConfig(PretrainedConfig):
+
+    mode_mapping: Dict[str, List[str]] = {
+        "text": _load_text_model_names(),
+        "text_image": _load_text_image_model_names(),
+        "image": _load_image_model_names(),
+    }
+
     def __init__(
         self,
-        model_name,
-        mode: str | list[str] = None,
+        pretrained_model_name_or_path,
+        architecture: str,
+        mode: str | list[str] | None = None,
         embedding_type: str = "pool",
-        dropout: float | None = None,
-        type="text",
-        architecture: str = None,
-        **kwargs
     ):
-        super().__init__(**kwargs)
-        self.model_name = model_name
-        self.dropout = dropout
+        super().__init__()
+        self.pretrained_model_name_or_path = pretrained_model_name_or_path
+        self.architecture = architecture
         self.mode = mode
         self.embedding_type = embedding_type
-        self.architecture = architecture
 
-    @staticmethod
-    def from_pretrained(name_or_path: str, mode: str | list[str] | None = "text_image", **kwargs):
+    @classmethod
+    def from_pretrained(cls, pretrained_model_name_or_path: str, mode: str | list[str] | None = None, **kwargs):
         # auto model
-        transformer_module = import_module("paddlenlp.transformers")
-        for name in dir(transformer_module):
-            module = getattr(transformer_module, name)
-            if inspect.isclass(module) and issubclass(module, PretrainedConfig):
-                init_configuration = getattr(module, "pretrained_init_configuration")
-                if name_or_path in init_configuration:
-                    config = EncoderConfig(model_name=name_or_path, mode=mode, **kwargs)
-                    return config
+        config = AutoConfig.from_pretrained(pretrained_model_name_or_path)
 
-        # clip model
-        from paddlenlp.transformers.clip.modeling import (
-            CLIP_PRETRAINED_MODEL_ARCHIVE_LIST,
-        )
+        # resolve the architectures from PretrainedModel
+        if not config.architectures:
+            for model_name in _load_text_model_names():
+                model_module = import_module(f"paddlenlp.transformers.{model_name}")
+                # TODO(wj-Mcat): assume that `pretrained_model_name_or_path` is name not dir
+                if (
+                    issubclass(model_module, PretrainedModel)
+                    and pretrained_model_name_or_path in model_module.pretrained_init_configuration
+                ):
+                    config.architectures = [model_name]
+                    break
 
-        if name_or_path in CLIP_PRETRAINED_MODEL_ARCHIVE_LIST:
-            if "text_image" in mode:
-                return EncoderConfig(name_or_path, mode="text_image", architecture="CLIPModel")
-            if "text" in mode:
-                return EncoderConfig(model_name=name_or_path, mode="text", architecture="CLIPTextModel", **kwargs)
-            if "image" in mode:
-                return EncoderConfig.from_pretrained(
-                    name_or_path, mode="image", architecture="CLIPVisionModel", **kwargs
-                )
+        architecture = None
+        if not mode and config.architectures:
+            architecture = config.architectures
+            if architecture in mode.mapping["text_image"]:
+                mode = "text_image"
+            elif architecture in mode.mapping["text"]:
+                mode = "text"
+            elif architecture in mode.mapping["image"]:
+                mode = "image"
+
+        # TODO(wj-Mcat): check the configuration of EncoderConfig
+        return cls(pretrained_model_name_or_path, architecture, mode=mode, **kwargs)
 
 
 class Encoder(nn.Layer):
-    mode_mapping = {
-        "image": {"CLIPVisionModel", "ChineseCLIPVisionModel", "ErnieViLVisionModel"},
-        "text_image": {"CLIPModel", "ChineseCLIPModel", "ErnieViLModel"},
-        "text": {"CLIPTextModel", "ChineseCLIPTextModel", "ErnieViLTextModel"}.union(_load_text_model()),
-    }
-
     def __init__(self, config: EncoderConfig):
         super().__init__()
         self.config: EncoderConfig = config
 
     @staticmethod
-    def from_pretrained(name_or_path: str, mode: str | list[str] = "text", **kwargs):
-        config = EncoderConfig.from_pretrained(name_or_path, mode=mode, **kwargs)
+    def from_pretrained(pretrained_model_name_or_path: str, mode: str | list[str] | None = None, **kwargs):
+        config = EncoderConfig.from_pretrained(pretrained_model_name_or_path, mode=mode, **kwargs)
         if "text_image" in config.mode:
             return TextImageEncoder(config)
         if "text" in config.mode:
             return TextEncoder(config)
         if "image" in config.mode:
             return ImageEncoder(config)
+        raise ValueError(f"can not detect the mode of model from <{pretrained_model_name_or_path}>")
 
 
-class TextEncoder(Encoder):
+class _TextFeatureMixin:
+    def get_text_features(self, *args, **kwargs):
+        # resolve text features
+        attribute = getattr(self.model, "get_text_features", None)
+        if attribute is not None:
+            if not inspect.ismethod(attribute):
+                raise ValueError("`get_text_features` is not a method")
+
+            return self.model.get_text_features(*args, **kwargs)
+
+        model_class_name = self.model.__class__.__name__
+
+        # TODO(wj-Mcat): should support detecting features
+        if model_class_name not in self.get_features_mapping:
+            raise ValueError(f"<{model_class_name}> not supported `get_text_features`")
+
+        attribute = getattr(self.model, self.get_features_mapping[model_class_name]["get_text_features"])
+        return attribute(*args, **kwargs)
+
+
+class TextEncoder(Encoder, _TextFeatureMixin):
     def __init__(self, config: EncoderConfig):
         super().__init__(config)
         model_class = import_module(f"paddlenlp.transformers.{config.architecture}")
-        self.model = model_class.from_pretrained(config.model_name)
+        self.model = model_class.from_pretrained(config.pretrained_model_name_or_path)
 
     def forward(self, *args, **kwargs):
         return self.get_text_features(*args, **kwargs)
 
-    def get_text_features(self, *args, **kwargs):
-        # resolve text features
-        kwargs["return_dict"] = True
-        output = self.model(*args, **kwargs)
-        if self.config.embedding_type == "cls":
-            return output.last_hidden_state[:, 0, :]
-        if self.config.embedding_type == "pool":
-            return output.pooler_output
 
-        raise NotImplementedError(f"embedding_type<{self.config.embedding_type}> not implemented")
+class _ImageFeatureMixin:
+    def get_image_features(self, *args, **kwargs):
+        # resolve image features
+        attribute = getattr(self.model, "get_image_features", None)
+        if attribute is not None:
+            if not inspect.ismethod(attribute):
+                raise ValueError("`get_image_features` is not a method")
+
+            return self.model.get_image_features(*args, **kwargs)
+
+        model_class_name = self.model.__class__.__name__
+
+        # TODO(wj-Mcat): should support detecting features
+        if model_class_name not in self.get_features_mapping:
+            raise ValueError(f"<{model_class_name}> not supported `get_image_features`")
+
+        attribute = getattr(self.model, self.get_features_mapping[model_class_name]["get_image_features"])
+        return attribute(*args, **kwargs)
 
 
-class ImageEncoder(Encoder):
+class ImageEncoder(Encoder, _ImageFeatureMixin):
     def __init__(self, config: EncoderConfig):
         super().__init__(config)
         model_class = import_module(f"paddlenlp.transformers.{config.architecture}")
-        self.model = model_class.from_pretrained(config.model_name)
+        self.model = model_class.from_pretrained(config.pretrained_model_name_or_path)
 
     def forward(self, *args, **kwargs):
         # resolve text features
         return self.image_features(*args, **kwargs)
 
-    def get_image_features(self, *args, **kwargs):
-        # resolve text features
-        kwargs["return_dict"] = True
-        output = self.model(*args, **kwargs)
-        if self.config.embedding_type == "cls":
-            return output.last_hidden_state[:, 0, :]
-        if self.config.embedding_type == "pool":
-            return output.pooler_output
 
-        raise NotImplementedError(f"embedding_type<{self.config.embedding_type}> not implemented")
-
-
-class TextImageEncoder(TextEncoder, ImageEncoder):
+class TextImageEncoder(Encoder, _TextFeatureMixin, _ImageFeatureMixin):
     def __init__(self, config: EncoderConfig):
-        super().super().__init__()
-        self.model = AutoModel.from_pretrained(config.name_or_path)
+        super().__init__(config)
+        model_class = import_module(f"paddlenlp.transformers.{config.architecture}")
+        self.model = model_class.from_pretrained(config.pretrained_model_name_or_path)
 
     def forward(self, input_ids):
         return self.model(input_ids)
-
-    def get_text_features(self, *args, **kwargs):
-        # resolve text features
-        kwargs["return_dict"] = True
-        output = self.model(*args, **kwargs)
-        if self.config.embedding_type == "cls":
-            return output.last_hidden_state[:, 0, :]
-        if self.config.embedding_type == "pool":
-            return output.pooler_output
-
-        raise NotImplementedError(f"embedding_type<{self.config.embedding_type}> not implemented")
-
-    def get_image_features(self, *args, **kwargs):
-        # resolve text features
-        kwargs["return_dict"] = True
-        output = self.model(*args, **kwargs)
-        if self.config.embedding_type == "cls":
-            return output.last_hidden_state[:, 0, :]
-        if self.config.embedding_type == "pool":
-            return output.pooler_output
-
-        raise NotImplementedError(f"embedding_type<{self.config.embedding_type}> not implemented")
 
 
 class EncoderForTextImageRetrieval(TextImageEncoder):
@@ -219,7 +244,7 @@ class EncoderForTextImageRetrieval(TextImageEncoder):
 
     def __init__(self, config: EncoderConfig):
         super().__init__()
-        self.model = AutoModel.from_pretrained(config.model_name)
+        self.model = AutoModel.from_pretrained(config.pretrained_model_name_or_path)
         self.dropout = nn.Dropout(config.dropout)
         self.projection_layer = nn.Linear(config.projection_dim)
 
@@ -227,39 +252,3 @@ class EncoderForTextImageRetrieval(TextImageEncoder):
     #     kwargs["return_dict"] = True
     #     output = self.model(**kwargs)
     # loss = output["loss"]
-
-    def get_text_features(self, **text_inputs):
-        # resolve text features
-        attribute = getattr(self.model, "get_text_features", None)
-        if attribute is not None:
-            if not inspect.ismethod(attribute):
-                raise ValueError("`get_text_features` is not a method")
-
-            return self.model.get_text_features(**text_inputs)
-
-        model_class_name = self.model.__class__.__name__
-
-        # TODO(wj-Mcat): should support detecting features
-        if model_class_name not in self.get_features_mapping:
-            raise ValueError(f"<{model_class_name}> not supported `get_text_features`")
-
-        attribute = getattr(self.model, self.get_features_mapping[model_class_name]["get_text_features"])
-        return attribute(**text_inputs)
-
-    def get_image_features(self, **image_inputs):
-        # resolve image features
-        attribute = getattr(self.model, "get_image_features", None)
-        if attribute is not None:
-            if not inspect.ismethod(attribute):
-                raise ValueError("`get_image_features` is not a method")
-
-            return self.model.get_image_features(**image_inputs)
-
-        model_class_name = self.model.__class__.__name__
-
-        # TODO(wj-Mcat): should support detecting features
-        if model_class_name not in self.get_features_mapping:
-            raise ValueError(f"<{model_class_name}> not supported `get_image_features`")
-
-        attribute = getattr(self.model, self.get_features_mapping[model_class_name]["get_image_features"])
-        return attribute(**image_inputs)
