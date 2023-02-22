47d4be30de64f3b958bf3824b0d2a1fea7eeeccc
Biaolin Wen <robinbg@foxmail.com>
2023-02-22 21:31:38 +0800

    Update configuration.py

--- paddlenlp/transformers/fnet/configuration.py
+++ paddlenlp/transformers/fnet/configuration.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" BERT model configuration"""
+""" FNet model configuration"""
 from __future__ import annotations
 
 from typing import Dict
