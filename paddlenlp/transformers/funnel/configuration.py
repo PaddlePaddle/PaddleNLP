cc043f8e014a4fec0258662ecc5f0a4071d142aa
Biaolin Wen <robinbg@foxmail.com>
2023-02-23 03:16:45 +0800

    Update configuration.py

--- paddlenlp/transformers/funnel/configuration.py
+++ paddlenlp/transformers/funnel/configuration.py
@@ -14,51 +14,50 @@
 """ BERT model configuration"""
 from __future__ import annotations
 
-from typing import Dict
-
 from paddlenlp.transformers.configuration_utils import PretrainedConfig
 
 __all__ = ["FUNNEL_PRETRAINED_INIT_CONFIGURATION", "FunnelConfig", "FUNNEL_PRETRAINED_RESOURCE_FILES_MAP"]
 
-FUNNEL_PRETRAINED_INIT_CONFIGURATION =  {
-        "funnel-transformer/small": {},  # B4-4-4H768
-        "funnel-transformer/small-base": {},  # B4-4-4H768, no decoder
-        "funnel-transformer/medium": {},  # B6-3x2-3x2H768
-        "funnel-transformer/medium-base": {},  # B6-3x2-3x2H768, no decoder
-        "funnel-transformer/intermediate": {},  # B6-6-6H768
-        "funnel-transformer/intermediate-base": {},  # B6-6-6H768, no decoder
-        "funnel-transformer/large": {},  # B8-8-8H1024
-        "funnel-transformer/large-base": {},  # B8-8-8H1024, no decoder
-        "funnel-transformer/xlarge-base": {},  # B10-10-10H1024
-        "funnel-transformer/xlarge": {},  # B10-10-10H1024, no decoder
-    }
+FUNNEL_PRETRAINED_INIT_CONFIGURATION = {
+    "funnel-transformer/small": {},  # B4-4-4H768
+    "funnel-transformer/small-base": {},  # B4-4-4H768, no decoder
+    "funnel-transformer/medium": {},  # B6-3x2-3x2H768
+    "funnel-transformer/medium-base": {},  # B6-3x2-3x2H768, no decoder
+    "funnel-transformer/intermediate": {},  # B6-6-6H768
+    "funnel-transformer/intermediate-base": {},  # B6-6-6H768, no decoder
+    "funnel-transformer/large": {},  # B8-8-8H1024
+    "funnel-transformer/large-base": {},  # B8-8-8H1024, no decoder
+    "funnel-transformer/xlarge-base": {},  # B10-10-10H1024
+    "funnel-transformer/xlarge": {},  # B10-10-10H1024, no decoder
+}
 
-FUNNEL_PRETRAINED_RESOURCE_FILES_MAP =  {
-        "model_state": {
-            "funnel-transformer/small": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/small/model_state.pdparams",
-            "funnel-transformer/small-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/small-base/model_state.pdparams",
-            "funnel-transformer/medium": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/medium/model_state.pdparams",
-            "funnel-transformer/medium-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/medium-base/model_state.pdparams",
-            "funnel-transformer/intermediate": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/intermediate/model_state.pdparams",
-            "funnel-transformer/intermediate-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/intermediate-base/model_state.pdparams",
-            "funnel-transformer/large": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/large/model_state.pdparams",
-            "funnel-transformer/large-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/large-base/model_state.pdparams",
-            "funnel-transformer/xlarge-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/xlarge-base/model_state.pdparams",
-            "funnel-transformer/xlarge": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/xlarge/model_state.pdparams",
-        },
-        "model_config": {
-            "funnel-transformer/small": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/small/model_config.json",
-            "funnel-transformer/small-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/small-base/model_config.json",
-            "funnel-transformer/medium": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/medium/model_config.json",
-            "funnel-transformer/medium-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/medium-base/model_config.json",
-            "funnel-transformer/intermediate": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/intermediate/model_config.json",
-            "funnel-transformer/intermediate-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/intermediate-base/model_config.json",
-            "funnel-transformer/large": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/large/model_config.json",
-            "funnel-transformer/large-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/large-base/model_config.json",
-            "funnel-transformer/xlarge-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/xlarge-base/model_config.json",
-            "funnel-transformer/xlarge": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/xlarge/model_config.json",
-        },
-    }
+FUNNEL_PRETRAINED_RESOURCE_FILES_MAP = {
+    "model_state": {
+        "funnel-transformer/small": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/small/model_state.pdparams",
+        "funnel-transformer/small-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/small-base/model_state.pdparams",
+        "funnel-transformer/medium": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/medium/model_state.pdparams",
+        "funnel-transformer/medium-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/medium-base/model_state.pdparams",
+        "funnel-transformer/intermediate": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/intermediate/model_state.pdparams",
+        "funnel-transformer/intermediate-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/intermediate-base/model_state.pdparams",
+        "funnel-transformer/large": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/large/model_state.pdparams",
+        "funnel-transformer/large-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/large-base/model_state.pdparams",
+        "funnel-transformer/xlarge-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/xlarge-base/model_state.pdparams",
+        "funnel-transformer/xlarge": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/xlarge/model_state.pdparams",
+    },
+    "model_config": {
+        "funnel-transformer/small": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/small/model_config.json",
+        "funnel-transformer/small-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/small-base/model_config.json",
+        "funnel-transformer/medium": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/medium/model_config.json",
+        "funnel-transformer/medium-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/medium-base/model_config.json",
+        "funnel-transformer/intermediate": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/intermediate/model_config.json",
+        "funnel-transformer/intermediate-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/intermediate-base/model_config.json",
+        "funnel-transformer/large": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/large/model_config.json",
+        "funnel-transformer/large-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/large-base/model_config.json",
+        "funnel-transformer/xlarge-base": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/xlarge-base/model_config.json",
+        "funnel-transformer/xlarge": "https://bj.bcebos.com/paddlenlp/models/transformers/funnel-transformer/xlarge/model_config.json",
+    },
+}
+
 
 class FunnelConfig(PretrainedConfig):
     r"""
