c90e2b1228c1c3da2e47b46209101ac680d1e625
Biaolin Wen <robinbg@foxmail.com>
2023-02-23 22:46:38 +0800

    Update configuration.py

--- paddlenlp/transformers/mpnet/configuration.py
+++ paddlenlp/transformers/mpnet/configuration.py
@@ -1,6 +1,4 @@
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-# Copyright 2018 Microsoft Research and The HuggingFace Inc. team. All rights reserved.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
