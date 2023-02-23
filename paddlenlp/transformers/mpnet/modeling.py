cc0ea3adb48f9d251d63d9e8a7e53b7335995566
Biaolin Wen <robinbg@foxmail.com>
2023-02-23 22:45:55 +0800

    Update modeling.py

--- paddlenlp/transformers/mpnet/modeling.py
+++ paddlenlp/transformers/mpnet/modeling.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
-# Copyright 2018 Microsoft Research and The HuggingFace Inc. team. All rights reserved.
+# Copyright 2018 The HuggingFace Inc. team, Microsoft Corporation.
 # Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
