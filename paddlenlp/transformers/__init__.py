2eedd9c228016b68442d669e1984ecfbf8dc8501
ELLIOTT <45143396+ZwhElliott@users.noreply.github.com>
2023-03-02 14:19:44 +0800

    [Artist P0] add PretrainedConfig and unit test (#4992)
    * "add pretraing config and unit test"
    
    * "add tokenizer test"
    
    * "fix bug"
    
    * "fix"
    
    * Apply suggestions from code review
    
    Co-authored-by: 骑马小猫 <1435130236@qq.com>
    
    * retrigger checks
    
    ---------
    
    Co-authored-by: 骑马小猫 <1435130236@qq.com>

--- paddlenlp/transformers/__init__.py
+++ paddlenlp/transformers/__init__.py
@@ -176,6 +176,7 @@ from .codegen.tokenizer import *
 from .codegen.configuration import *
 from .artist.modeling import *
 from .artist.tokenizer import *
+from .artist.configuration import *
 from .dallebart.modeling import *
 from .dallebart.tokenizer import *
 from .clip.modeling import *
