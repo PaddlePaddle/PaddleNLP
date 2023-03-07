b65556cde439299eec6740de119a39c20e4ea21d
westfish <westfish@126.com>
2023-03-07 12:18:43 +0000

    fix update

--- pipelines/examples/unsupervised-question-answering/unsupervised_question_answering_example.py
+++ pipelines/examples/unsupervised-question-answering/unsupervised_question_answering_example.py
@@ -99,6 +99,7 @@ def dense_faq_pipeline():
 
 
 def qa_generation_pipeline():
+    # breakpoint()
     answer_extractor = AnswerExtractor(
         model="uie-base-answer-extractor",
         device=args.device,
