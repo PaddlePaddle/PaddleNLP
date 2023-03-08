d8f3d2ed5c06e7500a55a20c17d1203650559e6a
whzhou <1197515930@qq.com>
2023-03-08 11:37:31 +0800

    ”fix“

--- paddlenlp/transformers/dallebart/modeling.py
+++ paddlenlp/transformers/dallebart/modeling.py
@@ -107,7 +107,7 @@ class DalleBartPretrainedModel(PretrainedModel):
                 layer.weight.set_value(
                     paddle.normal(
                         mean=0.0,
-                        std=self.init_std if hasattr(self, "init_std") else self.config.init_std,
+                        std=self.config.init_std,
                         shape=layer.weight.shape,
                     )
                 )
