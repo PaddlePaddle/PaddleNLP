f0dc21f5088e6137764abf5e46c32456cc51848a
zhoushunjie <zhoushunjie@baidu.com>
2023-03-08 05:19:51 +0000

    delete_pass->disable_trt_ops

--- ppdiffusers/deploy/text_guided_img_to_img_infer.py
+++ ppdiffusers/deploy/text_guided_img_to_img_infer.py
@@ -122,10 +122,9 @@ def create_paddle_inference_runtime(
         if use_fp16:
             option.trt_option.enable_fp16 = True
         else:
-            # Note(zhoushunjie): These two passes don't support fp32 now.
-            # Remove these 2 lines of code in future.
-            option.paddle_infer_option.delete_pass("trt_flash_multihead_matmul_fuse_pass")
-            option.paddle_infer_option.delete_pass("trt_cross_multihead_matmul_fuse_pass")
+            # Note(zhoushunjie): These two tensorrt ops don't support fp32 now.
+            # Remove this line of code in future.
+            option.paddle_infer_option.disable_trt_ops(["flash_multihead_matmul", "cross_multihead_matmul"])
         cache_file = os.path.join(model_dir, model_prefix, "_opt_cache/")
         option.trt_option.serialize_file = cache_file
         # Need to enable collect shape for ernie
