51a416673289e19f7cf36f61d64531a2f31d4de6
wjx-sudo <895515977@qq.com>
2023-03-09 08:08:31 +0000

    fix test requirment.txt

--- tests/transformers/xlm/test_tokenizer.py
+++ tests/transformers/xlm/test_tokenizer.py
@@ -26,6 +26,7 @@ from ..test_tokenizer_common import TokenizerTesterMixin
 class XLMTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
     tokenizer_class = XLMTokenizer
     test_fast_tokenizer = False
+    test_offsets = False
 
     def setUp(self):
         super().setUp()
@@ -77,25 +78,62 @@ class XLMTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         self.assertEqual(len(vocab_keys), 34)
 
     def test_add_tokens_tokenizer(self):
-        pass
+        tokenizers = self.get_tokenizers(do_lower_case=False)
+        for tokenizer in tokenizers:
+            with self.subTest(f"{tokenizer.__class__.__name__}"):
+                vocab_size = tokenizer.vocab_size
+                all_size = len(tokenizer.get_vocab())
 
-    def test_added_token_serializable(self):
-        pass
+                self.assertNotEqual(vocab_size, 0)
+
+                # We usually have added tokens from the start in tests because our vocab fixtures are
+                # smaller than the original vocabs - let's not assert this
+                # self.assertEqual(vocab_size, all_size)
+
+                new_toks = ["aaaaa bbbbbb", "cccccccccdddddddd"]
+                added_toks = tokenizer.add_tokens(new_toks)
+                vocab_size_2 = tokenizer.vocab_size
+                all_size_2 = len(tokenizer.get_vocab())
+
+                self.assertNotEqual(vocab_size_2, 0)
+                self.assertEqual(vocab_size, vocab_size_2)
+                self.assertEqual(added_toks, len(new_toks))
+                self.assertEqual(all_size_2, all_size + len(new_toks))
+
+                tokens = tokenizer.encode(
+                    "aaaaa bbbbbb low cccccccccdddddddd l", return_token_type_ids=None, add_special_tokens=False
+                )["input_ids"]
+                self.assertGreaterEqual(len(tokens), 4)
+                self.assertGreater(tokens[0], tokenizer.vocab_size - 1)
+                self.assertGreater(tokens[-2], tokenizer.vocab_size - 1)
+
+                new_toks_2 = {"eos_token": ">>>>|||<||<<|<<", "pad_token": "<<<<<|||>|>>>>|>"}
+                added_toks_2 = tokenizer.add_special_tokens(new_toks_2)
+                vocab_size_3 = tokenizer.vocab_size
+                all_size_3 = len(tokenizer.get_vocab())
+
+                self.assertNotEqual(vocab_size_3, 0)
+                self.assertEqual(vocab_size, vocab_size_3)
+                self.assertEqual(added_toks_2, len(new_toks_2))
+                self.assertEqual(all_size_3, all_size_2 + len(new_toks_2))
+
+                tokens = tokenizer.encode(
+                    ">>>>|||<||<<|<< aaaaabbbbbb low cccccccccdddddddd <<<<<|||>|>>>>|> l",
+                    return_token_type_ids=None,
+                    add_special_tokens=False,
+                )["input_ids"]
+                self.assertGreaterEqual(len(tokens), 6)
+                self.assertGreater(tokens[0], tokenizer.vocab_size - 1)
+                self.assertGreater(tokens[0], tokens[1])
+                self.assertGreater(tokens[-2], tokenizer.vocab_size - 1)
+                self.assertEqual(tokens[0], tokenizer.eos_token_id)
+                self.assertEqual(tokens[-2], tokenizer.pad_token_id)
 
     def test_consecutive_unk_string(self):
         pass
 
-    def test_offsets_mapping(self):
-        pass
-
-    def test_save_and_load_tokenizer(self):
-        pass
-
-    def test_special_tokens_initialization_with_non_empty_additional_special_tokens(self):
-        pass
-
     def test_add_tokens(self):
-        tokenizer = XLMTokenizer.from_pretrained("xlm-mlm-en-2048")
+        tokenizer = XLMTokenizer(self.vocab_file, self.merges_file)
         vocab_size = len(tokenizer)
         self.assertEqual(tokenizer.add_tokens(""), 0)
         self.assertEqual(tokenizer.add_tokens("testoken"), 1)
