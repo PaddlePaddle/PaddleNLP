efabd14f47d9c1ef6e64289b759eaa57a5e8e9c1
milen <1649759610@qq.com>
2023-03-13 12:22:03 +0000

    [OPT P0] modify normalize_before in gpt

--- paddlenlp/transformers/gpt/modeling.py
+++ paddlenlp/transformers/gpt/modeling.py
@@ -330,7 +330,7 @@ class TransformerDecoderLayer(nn.Layer):
         activation = config.hidden_act
         attn_dropout = config.attention_probs_dropout_prob
         act_dropout = config.hidden_dropout_prob
-        normalize_before = config.normalize_before if config.normalize_before else True
+        normalize_before = getattr(config, "normalize_before", True)
 
         weight_attr = paddle.ParamAttr(initializer=nn.initializer.Normal(mean=0.0, std=config.initializer_range))
         bias_attr = None
