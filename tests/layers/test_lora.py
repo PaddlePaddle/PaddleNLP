ba4e1fc8c3deeb34e8208c22b0e5293c77f1d145
Sijun He <sijun.he@hotmail.com>
2023-03-15 14:44:06 +0800

    config

--- tests/layers/test_lora.py
+++ tests/layers/test_lora.py
@@ -21,7 +21,7 @@ from tempfile import TemporaryDirectory
 import numpy as np
 import paddle
 
-from paddlenlp.layers import LoRALinear, get_lora_model
+from paddlenlp.layers import LoRAConfig, LoRALinear, get_lora_model
 from paddlenlp.transformers import AutoModel
 
 
@@ -77,7 +77,12 @@ class TestLoraLayer(unittest.TestCase):
 
 class TestLoraModel(unittest.TestCase):
     def test_get_lora_model(self):
-        lora_config = {"target_modules": [".*q_proj.*", ".*v_proj.*"], "r": 4, "lora_alpha": 8}
+        lora_config = LoRAConfig(
+            target_modules=[".*q_proj.*", ".*v_proj.*"],
+            r=4,
+            lora_alpha=8,
+            merge_weights=True,
+        )
         # turn off plm dropout for to test train vs test
         model = AutoModel.from_pretrained(
             "__internal_testing__/tiny-random-bert", hidden_dropout_prob=0, attention_probs_dropout_prob=0
@@ -85,7 +90,7 @@ class TestLoraModel(unittest.TestCase):
         lora_model = get_lora_model(model, lora_config)
         state_dict = lora_model.state_dict()
         for weight_name in state_dict:
-            for target_module in lora_config["target_modules"]:
+            for target_module in lora_config.target_modules:
                 if re.fullmatch(target_module, weight_name):
                     if "lora" in weight_name:
                         self.assertFalse(state_dict[weight_name].stop_gradient)
@@ -99,6 +104,13 @@ class TestLoraModel(unittest.TestCase):
         eval_forward_results = model(input_ids)
         self.assertIsNotNone(eval_forward_results)
         for i, j in zip(train_forward_results, eval_forward_results):
-            print(i[:2, :2])
-            print(j[:2, :2])
             self.assertTrue(paddle.allclose(i, j))
+
+
+class TestLoRAConfig(unittest.TestCase):
+    def test_save_load(self):
+        with TemporaryDirectory() as tempdir:
+            lora_config = LoRAConfig()
+            lora_config.save_pretrained(tempdir)
+            loaded_lora_config = LoRAConfig.from_pretrained(tempdir)
+            self.assertEqual(lora_config, loaded_lora_config)
