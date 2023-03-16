79f47f25fcc6dcb9ddaa0a81d74305c617808289
lugimzzz <63761690+lugimzzz@users.noreply.github.com>
2023-03-16 23:59:11 +0800

    [P0 albert]add model config (#5275)
    * update albert
    
    * fix_format

--- paddlenlp/transformers/__init__.py
+++ paddlenlp/transformers/__init__.py
@@ -46,6 +46,7 @@ from .roberta.configuration import *
 from .electra.modeling import *
 from .electra.tokenizer import *
 from .electra.configuration import *
+from .albert.configuration import *
 from .albert.modeling import *
 from .albert.tokenizer import *
 from .bit.modeling import *
