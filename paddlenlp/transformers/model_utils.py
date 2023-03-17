453fe23298ec632352f06f32218c90f032c82658
Zhong Hui <zhonghui.net@gmail.com>
2023-03-17 11:23:21 +0800

    [LLM] Support Lazy init from pre-trained model (#5270)

--- paddlenlp/transformers/model_utils.py
+++ paddlenlp/transformers/model_utils.py
@@ -21,6 +21,7 @@ import os
 import re
 import shutil
 import tempfile
+from contextlib import contextmanager
 from typing import Any, Dict, List, Optional, Tuple, Type
 
 import numpy as np
@@ -64,9 +65,11 @@ from .configuration_utils import PretrainedConfig
 from .conversion_utils import ConversionMixin
 from .generation_utils import GenerationMixin
 from .utils import (
+    ContextManagers,
     InitTrackerMeta,
     adapt_stale_fwd_patch,
     fn_args_to_dict,
+    is_paddle_support_lazy_init,
     resolve_cache_dir,
 )
 
@@ -79,6 +82,26 @@ __all__ = [
 def unwrap_model(model, *args, **kwargs):
     raw_model = model._layers if isinstance(model, paddle.DataParallel) else model
     return raw_model
+
+
+_init_weights = True
+
+
+@contextmanager
+def no_init_weights(_enable=True):
+    """
+    Context manager to globally disable weight initialization to speed up loading large models.
+
+    TODO(Patrick): Delete safety argument `_enable=True` at next major version. .
+    """
+    global _init_weights
+    old_init_weights = _init_weights
+    if _enable:
+        _init_weights = False
+    try:
+        yield
+    finally:
+        _init_weights = old_init_weights
 
 
 def get_parameter_dtype(parameter: nn.Layer) -> paddle.dtype:
@@ -1106,8 +1129,8 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
         Returns:
             Tuple[List[str]]: _description_
         """
-
         model_state_dict = model.state_dict()
+
         expected_keys = list(model_state_dict.keys())
         prefix = model.base_model_prefix
 
@@ -1217,7 +1240,10 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
             if dtype not in ["float32", "float16"]:
                 raise ValueError(f"the value of `dtype` should be one of [`float32`, `float16`], but received {dtype}")
             for key in state_dict.keys():
-                state_dict[key] = paddle.cast(state_dict[key], dtype=dtype)
+                if isinstance(state_dict[key], np.ndarray):
+                    state_dict[key] = state_dict[key].astype(dtype=dtype)
+                else:
+                    state_dict[key] = paddle.cast(state_dict[key], dtype=dtype)
         else:
             dtype_prefix_len = len("paddle.")
             for k, v in model_to_load.state_dict().items():
@@ -1310,6 +1336,15 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
         ignore_mismatched_sizes = kwargs.pop("ignore_mismatched_sizes", None)
         dtype = kwargs.pop("dtype", None)
         cache_dir = kwargs.pop("cache_dir", None)
+        low_cpu_mem_usage = kwargs.pop("low_cpu_mem_usage", None)
+
+        init_contexts = []
+        if low_cpu_mem_usage:
+            load_state_as_np = True
+            # Instantiate model.
+            init_contexts.append(no_init_weights(_enable=True))
+            if is_paddle_support_lazy_init():
+                init_contexts.append(paddle.LazyGuard())
 
         cache_dir = resolve_cache_dir(pretrained_model_name_or_path, from_hf_hub, cache_dir)
 
@@ -1359,7 +1394,9 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
 
         # 3. init the model
         init_args = config["init_args"] or ()
-        model = cls(config, *init_args, **model_kwargs)
+
+        with ContextManagers(init_contexts):
+            model = cls(config, *init_args, **model_kwargs)
 
         loaded_state_dict_keys = list(model_state_dict.keys())
         # TODO(wj-Mcat): load shard checkpoint weight file, refer to: https://github.com/huggingface/transformers/pull/16343
