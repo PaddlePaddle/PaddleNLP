77cfcbe1e041b4e49a1f536bde9145602bd2f24d
w5688414 <w5688414@gmail.com>
2023-03-17 16:38:30 +0800

    flake8 neural search (#5290)

--- applications/neural_search/recall/simcse/evaluate.py
+++ applications/neural_search/recall/simcse/evaluate.py
@@ -12,20 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import argparse
 
 import numpy as np
-
-from paddlenlp.utils.log import logger
 
 # yapf: disable
 parser = argparse.ArgumentParser()
 parser.add_argument("--similar_text_pair", type=str, default='', help="The full path of similat pair file")
 parser.add_argument("--recall_result_file", type=str, default='', help="The full path of recall result file")
 parser.add_argument("--recall_num", type=int, default=10, help="Most similair number of doc recalled from corpus per query")
-
-
 args = parser.parse_args()
 # yapf: enable
 
