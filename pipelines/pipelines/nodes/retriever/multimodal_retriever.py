88f1087e4ed8ca5669061fc32f0db8299815cb10
w5688414 <w5688414@gmail.com>
2023-03-23 08:27:55 +0000

    Upgrade query_type config

--- pipelines/pipelines/nodes/retriever/multimodal_retriever.py
+++ pipelines/pipelines/nodes/retriever/multimodal_retriever.py
@@ -75,6 +75,7 @@ class MultiModalRetriever(BaseRetriever):
         self.progress_bar = progress_bar
         self.top_k = top_k
         self.scale_score = scale_score
+        self.query_type = query_type
 
         self.document_embedder = MultiModalEmbedder(
             embedding_models=document_embedding_models,
@@ -125,6 +126,8 @@ class MultiModalRetriever(BaseRetriever):
                             value range is scaled to a range of [0,1], where 1 means extremely relevant.
                             Otherwise raw similarity scores (for example, cosine or dot_product) are used.
         """
+        if query_type is None:
+            query_type = self.query_type
         return self.retrieve_batch(
             queries=[query],
             queries_type=query_type,
