0b5040a0e4e2b5fcf13a4c77efd60a98999364b7
Sijun He <sijun.he@hotmail.com>
2023-03-23 10:58:12 +0800

    [CodeStyles] fix `examples` (#5369)
    * fix few shot
    
    * fix benchmarks
    
    * dialogue
    
    * examples
    
    * examples
    
    * lex
    
    * working

--- examples/dependency_parsing/ddparser/criterion.py
+++ examples/dependency_parsing/ddparser/criterion.py
@@ -12,11 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import numpy as np
 import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
-
 from utils import index_sample
 
 
