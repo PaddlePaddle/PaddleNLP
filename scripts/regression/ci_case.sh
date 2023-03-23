5b25039847a8935324ce2ea34e17f4358b285296
zhang junjun <1834792141@qq.com>
2023-03-23 19:41:14 +0800

    [CI] Fix distilbert pretrained teacher model (#5390)
    * [CI] fix examples api names bug
    
    * [CI] fix distilbert
    
    * Update ci_case.sh

--- scripts/regression/ci_case.sh
+++ scripts/regression/ci_case.sh
@@ -691,7 +691,8 @@ print_info $? ernie-ctm_eval
 # 20 distilbert
 distilbert (){
 cd ${nlp_dir}/examples/model_compression/distill_lstm/
-mv ${nlp_dir}/examples/benchmark/glue/SST-2/* ./
+wget -q https://paddle-qa.bj.bcebos.com/SST-2_GLUE.tar
+tar -xzvf SST-2_GLUE.tar 
 time (
     python small.py \
     --task_name sst-2 \
@@ -715,7 +716,7 @@ time (
     --batch_size 128 \
     --model_name bert-base-uncased \
     --output_dir distilled_models/SST-2 \
-    --teacher_dir ./sst-2_ft_model_1.pdparams/ \
+    --teacher_dir ./SST-2/sst-2_ft_model_1.pdparams/ \
     --save_steps 1000 \
     --n_iter 1 \
     --embedding_name w2v.google_news.target.word-word.dim300.en >${log_path}/distilbert_teacher_train) >>${log_path}/distilbert_teacher_train 2>&1
