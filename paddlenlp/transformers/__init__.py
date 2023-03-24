01c164c8e2b64e5e55fd3a35d815612ef3404ef8
骑马小猫 <1435130236@qq.com>
2023-03-24 10:56:59 +0800

    [ErnieCtm P0] add testing & inputs_embeds supporting (#5221)
    * add testing for ernie-ctm
    
    * update ernie-ctm modeling
    
    * update ernie-ctm unittest

--- paddlenlp/transformers/__init__.py
+++ paddlenlp/transformers/__init__.py
@@ -86,6 +86,7 @@ from .ernie.modeling import *
 from .ernie.tokenizer import *
 from .ernie_ctm.modeling import *
 from .ernie_ctm.tokenizer import *
+from .ernie_ctm.configuration import *
 from .ernie_doc.modeling import *
 from .ernie_doc.tokenizer import *
 from .ernie_doc.configuration import *
