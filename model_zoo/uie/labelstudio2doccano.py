29f25ab21428b401dd6923c1a7bb33253852e279
46319943 <495384481@qq.com>
2023-03-26 17:28:01 +0800

    Update labelstudio2doccano.py
    Fix default output filename suffix.
    Fix json file reading.
    Add support for setting default value for missing relation type.

--- model_zoo/uie/labelstudio2doccano.py
+++ model_zoo/uie/labelstudio2doccano.py
@@ -17,8 +17,7 @@ import os
 import json
 
 
-def append_attrs(data, item, label_id, relation_id):
-
+def append_attrs(data, item, label_id, relation_id, default_relation_type):
     mapp = {}
 
     for anno in data["annotations"][0]["result"]:
@@ -42,7 +41,7 @@ def append_attrs(data, item, label_id, relation_id):
                     "id": relation_id,
                     "from_id": mapp[anno["from_id"]],
                     "to_id": mapp[anno["to_id"]],
-                    "type": anno["labels"][0],
+                    "type": anno["labels"][0] if len(anno["labels"]) else default_relation_type,
                 }
             )
 
@@ -58,7 +57,8 @@ def convert(dataset, task_type):
         for data in dataset:
             outer_id += 1
             item = {"id": outer_id, "text": data["data"]["text"], "entities": [], "relations": []}
-            item, label_id, relation_id = append_attrs(data, item, label_id, relation_id)
+            item, label_id, relation_id = append_attrs(data, item, label_id, relation_id,
+                                                       default_relation_type=args.default_relation_type)
             results.append(item)
     # for the classification task
     else:
@@ -75,13 +75,11 @@ def convert(dataset, task_type):
 
 
 def do_convert(args):
-
     if not os.path.exists(args.labelstudio_file):
         raise ValueError("Please input the correct path of label studio file.")
 
     with open(args.labelstudio_file, "r", encoding="utf-8") as infile:
-        for content in infile:
-            dataset = json.loads(content)
+        dataset = json.load(infile)
         results = convert(dataset, args.task_type)
 
     with open(args.doccano_file, "w", encoding="utf-8") as outfile:
@@ -91,19 +89,22 @@ def do_convert(args):
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         "--labelstudio_file", type=str, help="The export file path of label studio, only support the JSON format."
     )
-    parser.add_argument("--doccano_file", type=str, default="doccano_ext.jsonl", help="Saving path in doccano format.")
+    parser.add_argument("--doccano_file", type=str, default="doccano_ext.json", help="Saving path in doccano format.")
     parser.add_argument(
         "--task_type",
         type=str,
         choices=["ext", "cls"],
         default="ext",
         help="Select task type, ext for the extraction task and cls for the classification task, defaults to ext.",
+    )
+    parser.add_argument(
+        '--default_relation_type', type=str, default="relation",
+        help="The default relation type for missing relation type, defaults to relation."
     )
 
     args = parser.parse_args()
