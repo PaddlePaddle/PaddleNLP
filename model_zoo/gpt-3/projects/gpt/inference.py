006efcf4ea5c54a5d6f918d9bc678a0ccc688ef9
Sijun He <sijun.he@hotmail.com>
2023-04-03 22:41:41 +0800

    Fix FleetX GPT-3 styles (#5516)
    * fix styles
    
    * fix
    
    * changes

--- model_zoo/gpt-3/projects/gpt/inference.py
+++ model_zoo/gpt-3/projects/gpt/inference.py
@@ -15,15 +15,10 @@
 from __future__ import absolute_import, division, print_function
 
 import argparse
-import os
-import time
 
-import numpy as np
-import paddle
 import paddle.distributed.fleet as fleet
-import ppfleetx_ops
 from ppfleetx.core.engine.inference_engine import InferenceEngine
-from ppfleetx.data import build_dataloader, tokenizers
+from ppfleetx.data import tokenizers
 
 
 def parse_args():
