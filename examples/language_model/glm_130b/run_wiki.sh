7e47163045187920f31fa404b40957d95f6e9148
LemonNoel <wanghuijuan03@baidu.com>
2023-04-04 11:25:13 +0000

    [eval] add lambada and wikitext scripts

--- examples/language_model/glm_130b/run_wiki.sh
+++ examples/language_model/glm_130b/run_wiki.sh
@@ -12,13 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-python -m paddle.distributed.launch --gpus 6,7 run_eval.py \
---model_path local_random_path_mp \
+python -m paddle.distributed.launch --gpus 0,1,2,3,4,5,6,7 run_eval.py \
+--model_path /local_path/glm-130b  \
 --eval_path ./wiki.test.tokens \
---overlapping_eval 256  \
---seq_length 1024 \
 --batch_size 8 \
 --device gpu \
 --logging_steps 10 \
---tensor_parallel_degree 2 \
---output_dir mp_ckpt 
+--output_dir mp_ckpt \
+--overlapping_eval 256  \
+--seq_length 1024 
+
