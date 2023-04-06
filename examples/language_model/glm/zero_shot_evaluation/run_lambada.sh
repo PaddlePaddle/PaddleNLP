5e79aabd3c1cbec7f6cf3b85801576da26124e47
Huijuan Wang <wanghuijuan03@baidu.com>
2023-04-06 11:53:25 +0800

    [clean] remove irrelavant files

--- examples/language_model/glm/zero_shot_evaluation/run_lambada.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=$1 python run_eval.py \
---model_name THUDM/glm-10b \
---eval_path ./lambada_test.jsonl \
---cloze_eval \
---batch_size 8 \
---device gpu
-
--- examples/language_model/glm/zero_shot_evaluation/run_wiki.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=$1 python run_eval.py \
---model_name THUDM/glm-10b \
---eval_path ./wikitext-103/wiki.test.tokens \
---overlapping_eval 256  \
---batch_size 8 \
---device gpu --seq_length 1024
