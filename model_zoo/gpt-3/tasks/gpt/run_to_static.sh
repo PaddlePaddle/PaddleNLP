edc937109f3cf4ca73fd6b778c9db1083d3b00b4
zhaoyingli <zhaoyingli@baidu.com>
2023-04-06 18:39:58 +0800

    add gpt to_static train.py

---
+++ model_zoo/gpt-3/tasks/gpt/run_to_static.sh
@@ -0,0 +1,21 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+export CUDA_VISIBLE_DEVICES=1
+python tasks/gpt/train_to_static.py \
+    -c ./ppfleetx/configs/nlp/gpt/pretrain_gpt_345M_single_card.yaml \
+    -o Global.micro_batch_size=4 \
+    -o Global.local_batch_size=4 \
+    -o Engine.mix_precision.enable=False \
