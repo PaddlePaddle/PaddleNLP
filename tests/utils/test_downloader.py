7a03b840f133de39856ca5f91649f3c5bb61282b
骑马小猫 <1435130236@qq.com>
2023-04-07 16:05:26 +0800

    [New features] enable dynamic community download url (#5544)
    * enable dyanmic community uri
    
    * add test for dynamic community
    
    * update downloader testing
    
    * update downloader url prefix
    
    * update test_downloader url
    
    * remove simple downloader test code

--- tests/utils/test_downloader.py
+++ tests/utils/test_downloader.py
@@ -19,8 +19,6 @@ import os
 import unittest
 from tempfile import TemporaryDirectory
 
-from paddlenlp.utils.downloader import get_path_from_url_with_filelock
-
 
 class LockFileTest(unittest.TestCase):
     test_url = (
@@ -28,6 +26,8 @@ class LockFileTest(unittest.TestCase):
     )
 
     def test_downloading_with_exist_file(self):
+
+        from paddlenlp.utils.downloader import get_path_from_url_with_filelock
 
         with TemporaryDirectory() as tempdir:
             lock_file_name = hashlib.md5((self.test_url + tempdir).encode("utf-8")).hexdigest()
@@ -43,6 +43,8 @@ class LockFileTest(unittest.TestCase):
             self.assertIsNotNone(config_file)
 
     def test_downloading_with_opened_exist_file(self):
+
+        from paddlenlp.utils.downloader import get_path_from_url_with_filelock
 
         with TemporaryDirectory() as tempdir:
             lock_file_name = hashlib.md5((self.test_url + tempdir).encode("utf-8")).hexdigest()
