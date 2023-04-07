77d65ecb74cb22fc61dd2b174fdf7895bada652f
yujun <573009727@qq.com>
2023-04-07 16:00:44 +0800

    同步https://github.com/dbolya/tomesd/commit/faff0eca8efe6d8d86bec8ab6787eed681cf3617

--- ppdiffusers/ppdiffusers/patches/tomesd_patch_utils.py
+++ ppdiffusers/ppdiffusers/patches/tomesd_patch_utils.py
@@ -217,7 +217,10 @@ def compute_merge(x: paddle.Tensor, tome_info: Dict[str, Any]) -> Tuple[Callable
         w = int(math.ceil(original_w / downsample))
         h = int(math.ceil(original_h / downsample))
         r = int(x.shape[1] * args["ratio"])
-        m, u = bipartite_soft_matching_random2d(x, w, h, args["sx"], args["sy"], r, not args["use_rand"])
+        # If the batch size is odd, then it's not possible for promted and unprompted images to be in the same
+        # batch, which causes artifacts with use_rand, so force it to be off.
+        use_rand = False if x.shape[0] % 2 == 1 else args["use_rand"]
+        m, u = bipartite_soft_matching_random2d(x, w, h, args["sx"], args["sy"], r, not use_rand)
     else:
         m, u = (do_nothing, do_nothing)
 
