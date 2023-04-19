e63cafd681857f847928d383be15df8431065a16
wjx-sudo <895515977@qq.com>
2023-04-19 08:31:33 +0000

    Add instruction data from belle

--- examples/language_model/glm/data.py
+++ examples/language_model/glm/data.py
@@ -15,6 +15,51 @@
 import numpy as np
 
 
+def custom_instruction_convert_example(example, tokenizer, data_args, is_test=True):
+    instruction = ""
+    input = ""
+    output = ""
+    if "instruction" in example and "output" in example:
+        instruction = example["instruction"]
+        output = example["output"]
+    else:
+        assert False, "Source and title are not in the input dictionary, nor are context and answer."
+    if "input" in example["input"]:
+        input = example["input"]
+    example["text_a"] = "Human: " + instruction + input + "\n Assistant: "
+    example["text_b"] = output
+    inputs = tokenizer.encode(example["text_a"], max_length=data_args.src_length - 1, truncation=True)
+    inputs["input_ids"] = inputs["input_ids"][:-1] + [tokenizer.gmask_token_id] + inputs["input_ids"][-1:]
+    pad_length = data_args.src_length - len(inputs["input_ids"])
+    inputs["input_ids"] = np.array([inputs["input_ids"] + [tokenizer.pad_token_id] * pad_length])
+    inputs["attention_mask"] = np.array([inputs["attention_mask"] + [1] + [0] * pad_length])
+    sep = inputs["input_ids"].shape[1]
+    inputs = tokenizer.build_inputs_for_generation(
+        inputs,
+        max_gen_length=data_args.tgt_length,
+        targets=" " + example["text_b"] if not is_test else None,
+        padding="max_length",
+    )
+
+    for input_name in inputs.keys():
+        inputs[input_name] = inputs[input_name].squeeze(0)
+    if is_test:
+        inputs["position_ids"] = inputs["position_ids"][:, : inputs["input_ids"].shape[-1]]
+        labels = tokenizer.encode(
+            " " + example["text_b"], add_special_tokens=False, max_length=data_args.tgt_length - 1
+        )["input_ids"]
+        loss_mask = [0] * sep + [1] * len(labels) + [0] * (data_args.tgt_length - len(labels))
+        labels = (
+            [0] * sep
+            + labels
+            + [tokenizer.eop_token_id]
+            + [tokenizer.pad_token_id] * (data_args.tgt_length - len(labels) - 1)
+        )
+        inputs["label_ids"] = labels
+        inputs["loss_mask"] = loss_mask
+    return inputs
+
+
 def custom_convert_example(example, tokenizer, data_args, is_test=True):
     source = None
     title = None
