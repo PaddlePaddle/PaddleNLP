ff1456a2971accc237490c82cea95c4f8d4b0d01
w5688414 <w5688414@gmail.com>
2023-04-21 09:45:39 +0000

    Add copyright information

--- pipelines/pipelines/nodes/preprocessor/text_splitter.py
+++ pipelines/pipelines/nodes/preprocessor/text_splitter.py
@@ -1,3 +1,4 @@
+# Copyright 2023 The LangChain Authors. All rights reserved.
 # Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
