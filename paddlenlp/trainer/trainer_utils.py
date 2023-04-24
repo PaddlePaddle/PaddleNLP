aa7be9f5920cb5e471aa932aa52b0a8a87ca7081
Zhong Hui <zhonghui.net@gmail.com>
2023-04-24 12:16:20 +0800

    fix seed.

--- paddlenlp/trainer/trainer_utils.py
+++ paddlenlp/trainer/trainer_utils.py
@@ -890,8 +890,8 @@ def set_hyrbid_parallel_seed(basic_seed, dataset_rank, tp_rank, pp_rank=0):
     paddle.seed(basic_seed + dataset_rank)
 
     # local_seed/ global_seed is used to control dropout in ModelParallel
-    local_seed = basic_seed + 123 + tp_rank * 10 + pp_rank * 1000
-    global_seed = basic_seed + dataset_rank
+    local_seed = basic_seed + 59999 + tp_rank * 10 + pp_rank * 1000
+    global_seed = basic_seed + 100003 + dataset_rank
     tracker = get_rng_state_tracker()
     tracker.add("global_seed", global_seed)
     tracker.add("local_seed", local_seed)
