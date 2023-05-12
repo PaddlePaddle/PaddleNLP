41e98927ba44550d08fe6decf647f99c10e62928
Sijun He <sijun.he@hotmail.com>
2023-05-12 15:32:43 +0800

    changes

--- paddlenlp/ops/fast_transformer/transformer/fast_transformer.py
+++ paddlenlp/ops/fast_transformer/transformer/fast_transformer.py
@@ -905,17 +905,17 @@ class FasterUnifiedTransformer(UnifiedTransformerPretrainedModel):
         self._model = model
         self._use_fp16_decoding = use_fp16_decoding
         self.vocab_size = model.lm_head.decoder_bias.shape[0]
-        self.unk_token_id = self._model.unk_token_id
-        self.mask_token_id = self._model.mask_token_id
-        self.bos_token_id = self._model.bos_token_id
-        self.pad_token_id = self._model.pad_token_id
+        self.unk_token_id = self._model.config.unk_token_id
+        self.mask_token_id = self._model.config.mask_token_id
+        self.bos_token_id = self._model.config.bos_token_id
+        self.pad_token_id = self._model.config.pad_token_id
         self.logits_mask = self.generate_logits_mask(use_fp16_decoding)
-        self._n_head = self._model.num_attention_heads
-        self._hidden_dims = self._model.hidden_size
-        self._normalize_before = self._model.normalize_before
+        self._n_head = self._model.config.num_attention_heads
+        self._hidden_dims = self._model.config.hidden_size
+        self._normalize_before = self._model.config.normalize_before
         self._size_per_head = self._hidden_dims // self._n_head
-        self._n_layer = self._model.num_hidden_layers
-        self._hidden_act = self._model.hidden_act
+        self._n_layer = self._model.config.num_hidden_layers
+        self._hidden_act = self._model.config.hidden_act
 
         self.decoding = InferUnifiedDecoding(
             model=self._model,
@@ -1108,19 +1108,19 @@ class FasterUNIMOText(UNIMOPretrainedModel):
         super(FasterUNIMOText, self).__init__(model.config)
         self._model = model
         self._use_fp16_decoding = use_fp16_decoding
-        self.unk_token_id = self._model.unk_token_id
-        self.mask_token_id = self._model.mask_token_id
-        self.bos_token_id = self._model.bos_token_id
-        self.pad_token_id = self._model.pad_token_id
+        self.unk_token_id = self._model.config.unk_token_id
+        self.mask_token_id = self._model.config.mask_token_id
+        self.bos_token_id = self._model.config.bos_token_id
+        self.pad_token_id = self._model.config.pad_token_id
         self.vocab_size = model.lm_head.decoder_bias.shape[0]
 
         self.logits_mask = self.generate_logits_mask(use_fp16_decoding)
-        self._n_head = self._model.num_attention_heads
-        self._hidden_dims = self._model.hidden_size
-        self._normalize_before = self._model.normalize_before
+        self._n_head = self._model.config.num_attention_heads
+        self._hidden_dims = self._model.config.hidden_size
+        self._normalize_before = self._model.config.normalize_before
         self._size_per_head = self._hidden_dims // self._n_head
-        self._n_layer = self._model.num_hidden_layers
-        self._hidden_act = self._model.hidden_act
+        self._n_layer = self._model.config.num_hidden_layers
+        self._hidden_act = self._model.config.hidden_act
         self.trans_out = kwargs.get("trans_out", False)
 
         self.decoding = InferUnifiedDecoding(
@@ -1279,19 +1279,19 @@ class FasterMIRO(UNIMOPretrainedModel):
         super(FasterMIRO, self).__init__(model.config)
         self._model = model
         self._use_fp16_decoding = use_fp16_decoding
-        self.unk_token_id = self._model.unk_token_id
-        self.mask_token_id = self._model.mask_token_id
-        self.bos_token_id = self._model.bos_token_id
-        self.pad_token_id = self._model.pad_token_id
+        self.unk_token_id = self._model.config.unk_token_id
+        self.mask_token_id = self._model.config.mask_token_id
+        self.bos_token_id = self._model.config.bos_token_id
+        self.pad_token_id = self._model.config.pad_token_id
         self.vocab_size = model.lm_head.decoder_bias.shape[0]
 
         self.logits_mask = self.generate_logits_mask(use_fp16_decoding)
-        self._n_head = self._model.num_attention_heads
-        self._hidden_dims = self._model.hidden_size
-        self._normalize_before = self._model.normalize_before
+        self._n_head = self._model.config.num_attention_heads
+        self._hidden_dims = self._model.config.hidden_size
+        self._normalize_before = self._model.config.normalize_before
         self._size_per_head = self._hidden_dims // self._n_head
-        self._n_layer = self._model.num_hidden_layers
-        self._hidden_act = self._model.hidden_act
+        self._n_layer = self._model.config.num_hidden_layers
+        self._hidden_act = self._model.config.hidden_act
         self.trans_out = kwargs.get("trans_out", False)
 
         self.decoding = InferMIRODecoding(
