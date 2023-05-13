65a72f331aea68aa3dc812a073751b339a169c38
w5688414 <w5688414@gmail.com>
2023-05-13 11:35:07 +0800

    [paddle-pipelines] Add chatglm for pipelines (#5898)
    * Add chatglm for pipelines
    
    * Add markdown support for convert_files_to_dicts
    
    * Add chatglm support for multi recall
    
    * Add chatpdf multi recall example
    
    * Fix ci bug
    
    * Add unitest for chatglm
    
    * Add the missing test files

--- pipelines/examples/chatbot/chat_pdf_multi_recall_example.py
+++ pipelines/examples/chatbot/chat_pdf_multi_recall_example.py
@@ -16,46 +16,55 @@ import argparse
 import glob
 import time
 
-from pipelines.document_stores import FAISSDocumentStore
+from pipelines.document_stores import ElasticsearchDocumentStore
 from pipelines.nodes import (
     CharacterTextSplitter,
+    ChatGLMBot,
     DensePassageRetriever,
     ErnieBot,
     ErnieRanker,
-    MarkdownConverter,
+    PDFToTextConverter,
     PromptTemplate,
-    TruncatedConversationHistory,
 )
 from pipelines.pipelines import Pipeline
+
+BOT_CLASSES = {
+    "chatglm": ChatGLMBot,
+}
 
 # yapf: disable
 parser = argparse.ArgumentParser()
 parser.add_argument('--device', choices=['cpu', 'gpu'], default="gpu", help="Select which device to run dense_qa system, defaults to gpu.")
 parser.add_argument("--index_name", default='dureader_index', type=str, help="The ann index name of ANN.")
-parser.add_argument("--file_paths", default='./data/md_files', type=str, help="The PDF file path.")
+parser.add_argument("--file_paths", default='./data/pdf_files', type=str, help="The PDF file path.")
 parser.add_argument("--max_seq_len_query", default=64, type=int, help="The maximum total length of query after tokenization.")
 parser.add_argument("--max_seq_len_passage", default=256, type=int, help="The maximum total length of passage after tokenization.")
 parser.add_argument("--retriever_batch_size", default=16, type=int, help="The batch size of retriever to extract passage embedding for building ANN index.")
-parser.add_argument("--query_embedding_model", default="rocketqa-zh-nano-query-encoder", type=str, help="The query_embedding_model path")
-parser.add_argument("--passage_embedding_model", default="rocketqa-zh-nano-query-encoder", type=str, help="The passage_embedding_model path")
+parser.add_argument("--query_embedding_model", default="rocketqa-zh-base-query-encoder", type=str, help="The query_embedding_model path")
+parser.add_argument("--passage_embedding_model", default="rocketqa-zh-base-query-encoder", type=str, help="The passage_embedding_model path")
 parser.add_argument("--params_path", default="checkpoints/model_40/model_state.pdparams", type=str, help="The checkpoint path")
-parser.add_argument("--embedding_dim", default=312, type=int, help="The embedding_dim of index")
-parser.add_argument("--chunk_size", default=300, type=int, help="The length of data for indexing by retriever")
+parser.add_argument("--embedding_dim", default=768, type=int, help="The embedding_dim of index")
+parser.add_argument("--chunk_size", default=384, type=int, help="The length of data for indexing by retriever")
 parser.add_argument('--host', type=str, default="localhost", help='host ip of ANN search engine')
 parser.add_argument('--embed_title', default=False, type=bool, help="The title to be  embedded into embedding")
+parser.add_argument('--chatbot', choices=['ernie_bot', 'chatglm'], default="chatglm", help="The chatbot models ")
 parser.add_argument('--model_type', choices=['ernie_search', 'ernie', 'bert', 'neural_search'], default="ernie", help="the ernie model types")
 parser.add_argument("--api_key", default=None, type=str, help="The API Key.")
 parser.add_argument("--secret_key", default=None, type=str, help="The secret key.")
+parser.add_argument("--port", type=str, default="9200", help="port of ANN search engine")
 args = parser.parse_args()
 # yapf: enable
 
 
-def chat_markdown_tutorial():
-    document_store = FAISSDocumentStore(
+def chat_pdf_tutorial():
+
+    document_store = ElasticsearchDocumentStore(
+        host=args.host,
+        port=args.port,
+        username="",
+        password="",
         embedding_dim=args.embedding_dim,
-        duplicate_documents="skip",
-        return_embedding=True,
-        faiss_index_factory_str="Flat",
+        index=args.index_name,
     )
     use_gpu = True if args.device == "gpu" else False
     retriever = DensePassageRetriever(
@@ -71,32 +80,33 @@ def chat_markdown_tutorial():
         embed_title=args.embed_title,
     )
 
-    # Indexing Markdowns
-    markdown_converter = MarkdownConverter()
-
-    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=args.chunk_size, chunk_overlap=0, filters=["\n"])
+    # Indexing PDFs
+    pdf_converter = PDFToTextConverter()
+    text_splitter = CharacterTextSplitter(separator="\f", chunk_size=args.chunk_size, chunk_overlap=0, filters=["\n"])
     indexing_pipeline = Pipeline()
-    indexing_pipeline.add_node(component=markdown_converter, name="MarkdownConverter", inputs=["File"])
-    indexing_pipeline.add_node(component=text_splitter, name="Splitter", inputs=["MarkdownConverter"])
+    indexing_pipeline.add_node(component=pdf_converter, name="pdf_converter", inputs=["File"])
+    indexing_pipeline.add_node(component=text_splitter, name="Splitter", inputs=["pdf_converter"])
     indexing_pipeline.add_node(component=retriever, name="Retriever", inputs=["Splitter"])
     indexing_pipeline.add_node(component=document_store, name="DocumentStore", inputs=["Retriever"])
-    files = glob.glob(args.file_paths + "/**/*.md", recursive=True)
-    indexing_pipeline.run(file_paths=files)
+    files_paths = glob.glob(args.file_paths + "/*.pdf")
+    indexing_pipeline.run(file_paths=files_paths)
 
-    # Query Markdowns
-    ernie_bot = ErnieBot(api_key=args.api_key, secret_key=args.secret_key)
+    # Query PDFs
+    if args.chatbot in ["ernie_bot"]:
+        ernie_bot = ErnieBot(api_key=args.api_key, secret_key=args.secret_key)
+    else:
+        ernie_bot = BOT_CLASSES[args.chatbot]()
     ranker = ErnieRanker(model_name_or_path="rocketqa-zh-dureader-cross-encoder", use_gpu=use_gpu)
     query_pipeline = Pipeline()
     query_pipeline.add_node(component=retriever, name="Retriever", inputs=["Query"])
     query_pipeline.add_node(component=ranker, name="Ranker", inputs=["Retriever"])
-    query_pipeline.add_node(component=PromptTemplate("背景：{documents} 问题：{query}"), name="Template", inputs=["Ranker"])
     query_pipeline.add_node(
-        component=TruncatedConversationHistory(max_length=256), name="TruncateHistory", inputs=["Template"]
+        component=PromptTemplate("请根据以下背景资料回答问题：\n 背景资料：{documents} \n问题：{query}"), name="Template", inputs=["Ranker"]
     )
-    query_pipeline.add_node(component=ernie_bot, name="ErnieBot", inputs=["TruncateHistory"])
-    query = "Jupyter 和 AI Studio Notebook 有什么区别？如何使用Jupyter？"
+    query_pipeline.add_node(component=ernie_bot, name="ErnieBot", inputs=["Template"])
+    query = "Tensorflow和Pytorch有哪些区别？"
     start_time = time.time()
-    prediction = query_pipeline.run(query=query, params={"Retriever": {"top_k": 30}, "Ranker": {"top_k": 2}})
+    prediction = query_pipeline.run(query=query, params={"Retriever": {"top_k": 10}, "Ranker": {"top_k": 2}})
     end_time = time.time()
     print("Time cost for query markdown conversion:", end_time - start_time)
     print("user: {}".format(query))
@@ -104,4 +114,4 @@ def chat_markdown_tutorial():
 
 
 if __name__ == "__main__":
-    chat_markdown_tutorial()
+    chat_pdf_tutorial()
