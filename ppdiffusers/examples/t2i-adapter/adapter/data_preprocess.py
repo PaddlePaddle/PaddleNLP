f0b79eceed82752320f41cd29b3406bf9499b84f
westfish <westfish@126.com>
2023-05-18 06:45:11 +0000

    data_preprocess_reduce

--- ppdiffusers/examples/t2i-adapter/adapter/data_preprocess.py
+++ ppdiffusers/examples/t2i-adapter/adapter/data_preprocess.py
@@ -29,26 +29,6 @@ def base64_to_image(base64_str):
     return img
 
 
-def vic_watermark(score_json, height, width):
-    if "vic_watermark_info" in score_json:
-        vic_watermark_score = score_json["vic_watermark_info"]
-        if vic_watermark_score == 0:
-            vic_watermark_info = 0
-        elif len(vic_watermark_score) > 1:
-            vic_watermark_info = 1
-        else:
-            left = vic_watermark_score[0]["left"]
-            right = vic_watermark_score[0]["right"]
-            bottom = vic_watermark_score[0]["bottom"]
-            top = vic_watermark_score[0]["top"]
-            # score = vic_watermark_score[0]["score"]
-            area = ((bottom - top) * height) * ((right - left) * width) / (height * width)
-            vic_watermark_info = area
-    else:
-        vic_watermark_info = 0
-    return vic_watermark_info
-
-
 def process_data(line, filename, data_format):
     try:
         data = line.strip().split("\t")
@@ -58,63 +38,18 @@ def process_data(line, filename, data_format):
             image_num = int(data[3])  # 2
             image_b64str = data[4 + image_num]  # data[6]
             control_image_b64str = data[4 + image_num + 1]  # data[7]
-            if len(data) == 10:
-                score_str = ""
-                score_json = {}
-                resolution = data[4 + image_num + image_num]  # data[8]
-            elif len(data) == 11:
-                score_json_idx = 4 + image_num + image_num  # 8
-                score_str = data[score_json_idx]
-                score_json = json.loads(score_str)
-                resolution = data[score_json_idx + 1]  # data[9]
-            else:
-                assert False, "invalid img2img data"
         else:
             text_id = data[0]
             text_json = json.loads(data[2])
             image_b64str = data[5]
-            score_str = data[6]
-            score_json = json.loads(score_str)
-            resolution = data[7]
             control_image_b64str = None
-        if resolution != "X":
-            try:
-                width, height = float(resolution.split("X")[0].replace("X", "")), float(
-                    resolution.split("X")[1].replace("X", "")
-                )
-            except:
-                width, height = float(resolution.split("x")[0].replace("x", "")), float(
-                    resolution.split("x")[1].replace("x", "")
-                )
+
+        caption = ""
+        caption += text_json.get("caption_en", text_json.get("blip_caption_en", ""))
+        if caption != "":
+            image_base64 = image_b64str
         else:
-            tmp = base64_to_image(image_b64str)
-            width, height = tmp.size[0], tmp.size[1]
-        min_resolution = min(height, width)
-        max_resolution = max(height, width)
-
-        # laion5B精选 laion-aes-v2
-        if "laion_aes" in text_id:
-            if (
-                "AESTHETIC_SCORE" in score_json
-                and score_json["AESTHETIC_SCORE"]
-                and float(score_json["AESTHETIC_SCORE"]) < 6
-            ):
-                return None
-            if min_resolution < 512 or max_resolution / min_resolution > 3:
-                return None
-            if "pwatermark" in score_json and score_json["pwatermark"] and float(score_json["pwatermark"]) > 0.3:
-                return None
-            if "ase_score" in score_json and float(score_json["ase_score"]) < 6:
-                return None
-            if "watermark_score" in score_json and score_json["watermark_score"] > 0.3:
-                return None
-
-            caption = ""
-            caption += text_json.get("caption_en", text_json.get("blip_caption_en", ""))
-            if caption != "":
-                image_base64 = image_b64str
-            else:
-                return None
+            return None
 
         return image_base64, caption, text_id, control_image_b64str
 
