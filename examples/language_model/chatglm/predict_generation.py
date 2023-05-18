1bb7e3d21c3b7f4639e623ae3cb79534ce4ae926
w5688414 <w5688414@gmail.com>
2023-05-18 13:05:25 +0000

    Convert eop_token_id to eos_token_id

--- examples/language_model/chatglm/predict_generation.py
+++ examples/language_model/chatglm/predict_generation.py
@@ -131,7 +131,7 @@ class Predictor(object):
             top_k=1,
             max_length=self.tgt_length,
             bos_token_id=self.tokenizer.bos_token_id,
-            eos_token_id=self.tokenizer.end_token_id,
+            eos_token_id=self.tokenizer.eos_token_id,
             pad_token_id=self.tokenizer.pad_token_id,
             use_cache=True,
         )
