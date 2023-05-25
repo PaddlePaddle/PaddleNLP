86d166e87d67f85e2839b8879bd3201b7b46af05
Milen <1649759610@qq.com>
2023-05-25 03:57:35 +0000

    merge from #5908

--- ppdiffusers/ppdiffusers/__init__.py
+++ ppdiffusers/ppdiffusers/__init__.py
@@ -1,4 +1,4 @@
-ppdiffusers / ppdiffusers / __init__.py  # Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 # Copyright 2023 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
