492c9adf0a9b39aa1b2e86fd5fad3fa92ddc47d2
yujun <573009727@qq.com>
2023-06-05 16:53:30 +0800

    update

--- model_zoo/bert/run_glue_trainer.py
+++ model_zoo/bert/run_glue_trainer.py
@@ -120,8 +120,8 @@ def do_train():
 
     def compute_metrics(p):
         preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
-        preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
-
+        if is_regression:
+            preds = np.squeeze(preds)
         preds = paddle.to_tensor(preds)
         label = paddle.to_tensor(p.label_ids)
 
