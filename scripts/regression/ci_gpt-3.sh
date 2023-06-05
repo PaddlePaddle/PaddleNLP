6ddb69d7eb04353b86fca799779ad0fe5c9e8e31
liujie44 <liujie44@baidu.com>
2023-06-05 17:09:49 +0800

    fix

--- scripts/regression/ci_gpt-3.sh
+++ scripts/regression/ci_gpt-3.sh
@@ -858,7 +858,7 @@ function gpt_auto_pass_o2_stage2() {
     loss1=`cat $log_dir/workerlog.4 | grep '4/4' | grep "lr:" | awk -F 'loss: ' '{print $2}' | awk -F ' ' '{print $1}'`
     loss2=`cat $log_dir/workerlog.6 | grep '4/4' | grep "lr:" | awk -F 'loss: ' '{print $2}' | awk -F ' ' '{print $1}'`
     loss=$(echo $loss1 $loss2 | awk '{printf("%.4f",($1+$2)/2)}')
-    check_result $FUNCNAME 11.1003 ${loss}
+    check_result $FUNCNAME 11.0779 ${loss}
     echo "=========== $FUNCNAME run  end ==========="
 }
 
