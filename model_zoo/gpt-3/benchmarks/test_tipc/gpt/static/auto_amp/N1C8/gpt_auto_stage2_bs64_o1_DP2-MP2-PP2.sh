3a01f5fa8ed62a5675aa8db74090d8acc33932be
Liujie0926 <44688141+Liujie0926@users.noreply.github.com>
2023-06-05 19:41:03 +0800

    [gpt-3] update CI/Benchmark (#6102)
    * [gpt-3]add auto_amp benchmark
    
    * fix
    
    * [benchmark]update static/gpt max_step
    
    * [gpt-3]update auto loss of ci
    
    * fix loss_base
    
    * fix

---
+++ model_zoo/gpt-3/benchmarks/test_tipc/gpt/static/auto_amp/N1C8/gpt_auto_stage2_bs64_o1_DP2-MP2-PP2.sh
@@ -0,0 +1,19 @@
+model_item=gpt_auto_stage2
+dp_degree=2
+mp_degree=2
+pp_degree=2
+bs_item=64
+fp_item=o1
+run_mode=DP2-MP2-PP2
+device_num=N1C8
+sharding_degree=2
+sharding_stage=2
+
+model=gpt
+micro_bs=8
+
+cd ./benchmarks
+bash ./test_tipc/gpt/static/auto_amp/benchmark_common/prepare.sh
+# run
+bash ./test_tipc/gpt/static/auto_amp/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${dp_degree} ${mp_degree} ${pp_degree} ${micro_bs} ${bs_item} ${run_mode} ${device_num} \
+${sharding_degree} ${sharding_stage} 2>&1;
