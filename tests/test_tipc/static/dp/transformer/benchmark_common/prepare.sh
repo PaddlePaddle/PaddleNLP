65cef645e2f9c7ecf8224842b12b5c0eba9a9952
yujun <573009727@qq.com>
2023-06-26 15:22:52 +0800

    add

--- tests/test_tipc/static/dp/transformer/benchmark_common/prepare.sh
+++ tests/test_tipc/static/dp/transformer/benchmark_common/prepare.sh
@@ -67,7 +67,7 @@ python -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
 python -m pip install setuptools_scm 
 python -m pip install Cython 
 python -m pip install -r ../requirements.txt  #-i https://pypi.tuna.tsinghua.edu.cn/simple
-python -m pip install easydict pyyaml #-i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install attrdict easydict pyyaml #-i https://pypi.tuna.tsinghua.edu.cn/simple
 
 python -m pip install -e ../
 # python -m pip install paddlenlp    # PDC 镜像中安装失败
