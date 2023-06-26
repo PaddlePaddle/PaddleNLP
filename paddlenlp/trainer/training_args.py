0f29ea49c6d9520a7ab9b58d07b1decd1e514374
chenxuyi <chenxuyi@baidu.com>
2023-06-26 19:58:01 +0800

    allow to use `main-grad` under TF32/FP32

--- paddlenlp/trainer/training_args.py
+++ paddlenlp/trainer/training_args.py
@@ -694,15 +694,13 @@ class TrainingArguments:
             self.use_hybrid_parallel = True
 
         if self.amp_master_grad:
-            if (
-                self.pipeline_parallel_degree <= 1 and self.tensor_parallel_degree <= 1
-            ) or self.fp16_opt_level != "O2":
+            if self.pipeline_parallel_degree <= 1 and self.tensor_parallel_degree <= 1:
                 raise ValueError(
-                    "Temporarily amp master grad only suport for tensor/pipeline parallel with fp16_opt_level O2. please set amp_master_grad to False."
+                    "Temporarily amp master grad only suport for tensor/pipeline parallel. please set amp_master_grad to False."
                 )
-            if not (self.bf16 or self.fp16):
-                logger.warning("set amp_master_grad to false since amp is disabled.")
-                self.amp_master_grad = False
+            # if not (self.bf16 or self.fp16):
+            #     logger.warning("set amp_master_grad to false since amp is disabled.")
+            #     self.amp_master_grad = False
 
         if self.use_hybrid_parallel:
             world_size = paddle.distributed.get_world_size()
