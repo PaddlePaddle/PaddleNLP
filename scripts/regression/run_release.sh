1b2a37ba0e144d01140e1282b7a7ae298f2184c9
zjjlivein <1834792141@qq.com>
2023-06-28 11:06:38 +0800

    disabled FT&FG

--- scripts/regression/run_release.sh
+++ scripts/regression/run_release.sh
@@ -56,8 +56,8 @@ declare -A all_P0case_dic
 get_diff_TO_P0case(){
 if [[ ${Testcase} =~ "all" ]];then
     P0case_list=(waybill_ie msra_ner glue bert skep bigbird electra gpt ernie-1.0 xlnet ofa  squad tinybert lexical_analysis seq2seq \
-     word_embedding ernie-ctm distilbert stacl transformer simbert ernie-doc transformer-xl pointer_summarizer question_matching ernie-csc \
-    nptag ernie-m clue taskflow transformers fast_generation ernie-3.0)
+    word_embedding ernie-ctm distilbert stacl transformer simbert ernie-doc transformer-xl pointer_summarizer question_matching ernie-csc \
+    nptag ernie-m clue taskflow transformers fast_generation ernie-3.0 fast_transformer fast_gpt llama)
 elif [[ ${Testcase} =~ "p0" ]];then
     P0case_list=(glue bert skep gpt ernie-1.0 transformer clue)
 else
