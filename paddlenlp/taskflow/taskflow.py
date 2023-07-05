dbdcf01aec9a04f7e2d8a1200049ed8cd974b9ad
qingzhong1 <137043369+qingzhong1@users.noreply.github.com>
2023-07-05 13:13:08 +0800

    [taskflow]Update SentenceeExtraction-m3e (#6294)
    * Add m3e-base model
    
    * Add m3e-base model
    
    * Add m3e-base model
    
    * Update taskflow.md
    
    * Update taskflow.md again
    
    * Update piplines-semantic-search
    
    * Update piplines-semantic-search

--- paddlenlp/taskflow/taskflow.py
+++ paddlenlp/taskflow/taskflow.py
@@ -36,7 +36,10 @@ from .sentiment_analysis import SentaTask, SkepTask, UIESentaTask
 from .text2text_generation import ChatGLMTask
 from .text_classification import TextClassificationTask
 from .text_correction import CSCTask
-from .text_feature_extraction import TextFeatureExtractionTask
+from .text_feature_extraction import (
+    SentenceFeatureExtractionTask,
+    TextFeatureExtractionTask,
+)
 from .text_similarity import TextSimilarityTask
 from .text_summarization import TextSummarizationTask
 from .word_segmentation import SegJiebaTask, SegLACTask, SegWordTagTask
@@ -664,6 +667,16 @@ TASKS = {
                 "task_class": MultimodalFeatureExtractionTask,
                 "task_flag": "feature_extraction-tiny-random-ernievil2",
                 "task_priority_path": "__internal_testing__/tiny-random-ernievil2",
+            },
+            "moka-ai/m3e-base": {
+                "task_class": SentenceFeatureExtractionTask,
+                "task_flag": "feature_extraction-moka-ai/m3e-base",
+                "task_priority_path": "moka-ai/m3e-base",
+            },
+            "__internal_testing__/tiny-random-m3e": {
+                "task_class": SentenceFeatureExtractionTask,
+                "task_flag": "__internal_testing__/tiny-random-m3e",
+                "task_priority_path": "__internal_testing__/tiny-random-m3e",
             },
         },
         "default": {"model": "PaddlePaddle/ernie_vil-2.0-base-zh"},
