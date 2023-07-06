8f0d2ad32fa2bb595075af6c0dc0ed0ce81d633d
w5688414 <w5688414@gmail.com>
2023-07-06 20:07:59 +0800

    [llm benchmark] Add inference benchmark (#6257)
    * Add inference benchmark
    
    * Update profiler
    
    * Update paddle benchmark
    
    * Update chatglm traningi config
    
    * Add comma
    
    * Update llama results
    
    * Update readme
    
    * Update chatglm and bloomz results
    
    * Fix recompute
    
    * Remove http proxy

--- examples/benchmark/peft/paddle/benchmark.py
+++ examples/benchmark/peft/paddle/benchmark.py
@@ -15,13 +15,15 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
+import paddle.profiler as profiler
 from datasets import load_dataset
-from utils import CustomTrainer
+from utils import CustomTrainer, ProfilerCallback
 
 from paddlenlp.data import DataCollatorForSeq2Seq
 from paddlenlp.peft import LoRAConfig, LoRAModel
 from paddlenlp.trainer import PdArgumentParser, TrainingArguments
-from paddlenlp.transformers import AutoModelForCausalLM, AutoTokenizer
+from paddlenlp.transformers import AutoModelForCausalLM, AutoTokenizer, ChatGLMForConditionalGeneration
+
 
 """
 单卡
@@ -57,6 +59,7 @@ class ModelArguments:
     model_name_or_path: str = field(default=None, metadata={"help": "model name or local path"})
     lora: Optional[bool] = field(default=False, metadata={"help": "whether to use LoRA"})
     english: Optional[bool] = field(default=False, metadata={"help": "whether to english benchmark dataset"})
+    profiler: Optional[bool] = field(default=False, metadata={"help": "whether to use profiler"})
 
 
 def main():
@@ -70,21 +73,35 @@ def main():
             dtype = "float16"
         if training_args.bf16:
             dtype = "bfloat16"
+    else:
+        dtype="float32"
 
     tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
     if "llama" in model_args.model_name_or_path:
         tokenizer.pad_token = tokenizer.unk_token
+    if 'chatglm' in model_args.model_name_or_path:
+        model = ChatGLMForConditionalGeneration.from_pretrained(
+            model_args.model_name_or_path,
+            load_state_as_np=True,
+            low_cpu_mem_usage=True,
+            # use_flash_attention=True,
+            dtype=dtype,
+            tensor_parallel_degree=training_args.tensor_parallel_degree,
+            tensor_parallel_rank=training_args.tensor_parallel_rank,
+            recompute=training_args.recompute,
+        )
 
-    model = AutoModelForCausalLM.from_pretrained(
-        model_args.model_name_or_path,
-        load_state_as_np=True,
-        low_cpu_mem_usage=True,
-        # use_flash_attention=True,
-        dtype=dtype,
-        tensor_parallel_degree=training_args.tensor_parallel_degree,
-        tensor_parallel_rank=training_args.tensor_parallel_rank,
-        use_recompute=training_args.recompute,
-    )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path,
+            load_state_as_np=True,
+            low_cpu_mem_usage=True,
+            # use_flash_attention=True,
+            dtype=dtype,
+            tensor_parallel_degree=training_args.tensor_parallel_degree,
+            tensor_parallel_rank=training_args.tensor_parallel_rank,
+            use_recompute=training_args.recompute,
+        )
 
     if model_args.lora:
         if "llama" in model_args.model_name_or_path:
@@ -119,6 +136,7 @@ def main():
         dataset = load_dataset("tatsu-lab/alpaca")
     else:
         dataset = load_dataset("Chinese-Vicuna/guanaco_belle_merge_v1.0")
+
     # select first 10k examples for benchmarking
     dataset = dataset["train"].select(range(10000))
     dataset = dataset.map(
@@ -126,17 +144,31 @@ def main():
     )
     total_effective_tokens = sum([len(i["input_ids"]) for i in dataset]) * training_args.num_train_epochs
 
+    if model_args.profiler:
+        prof = profiler.Profiler(
+            targets=[profiler.ProfilerTarget.CPU, profiler.ProfilerTarget.GPU],profile_memory=True,
+            scheduler = profiler.make_scheduler(closed=1, ready=2, record=1, repeat=1),
+            on_trace_ready = profiler.export_chrome_tracing('./log'),
+        )
+
+    
     trainer = CustomTrainer(
         model=model,
+        tokenizer=tokenizer,
         train_dataset=dataset,
+        callbacks=[ProfilerCallback(prof)] if model_args.profiler else [],
         args=training_args,
         data_collator=DataCollatorForSeq2Seq(return_tensors="pd", tokenizer=tokenizer),
     )
+
+
     train_metrics = trainer.train()
     tokens_per_second = trainer.total_observed_tokens / train_metrics.metrics["train_runtime"]
     effective_tokens_per_second = total_effective_tokens / train_metrics.metrics["train_runtime"]
     print(f"Tokens per second: {tokens_per_second:.2f}")
     print(f"Effective Tokens per second: {effective_tokens_per_second:.2f}")
+
+
 
 
 if __name__ == "__main__":
