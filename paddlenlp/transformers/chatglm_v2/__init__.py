25093bb4e9a6a7d00ed3cec698a9ae9310850c91
Sijun He <sijun.he@hotmail.com>
2023-07-06 12:45:03 +0800

    add predict examples

--- paddlenlp/transformers/chatglm_v2/__init__.py
+++ paddlenlp/transformers/chatglm_v2/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2023 ChatGLM2-6B Model Team and PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
