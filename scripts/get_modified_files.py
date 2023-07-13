ee6d43ab2c8c05a5a881cf4de19168e08a679522
Sijun He <sijun.he@hotmail.com>
2023-07-13 20:25:52 +0800

    stuff

--- scripts/get_modified_files.py
+++ scripts/get_modified_files.py
@@ -18,14 +18,16 @@ import sys
 
 try:
     fork_point_sha = subprocess.check_output("git merge-base develop HEAD".split()).decode("utf-8")
-except subprocess.CalledProcessError as e:
+    modified_files = (
+        subprocess.check_output(f"git diff --diff-filter=d --name-only {fork_point_sha}".split())
+        .decode("utf-8")
+        .split()
+    )
+
+    valid_dirs = "|".join(sys.argv[1:])
+    regex = re.compile(rf"^({valid_dirs}).*?\.py$")
+
+    relevant_modified_files = [x for x in modified_files if regex.match(x)]
+    print(" ".join(relevant_modified_files), end="")
+except Exception as e:
     print(e.output)
-modified_files = (
-    subprocess.check_output(f"git diff --diff-filter=d --name-only {fork_point_sha}".split()).decode("utf-8").split()
-)
-
-valid_dirs = "|".join(sys.argv[1:])
-regex = re.compile(rf"^({valid_dirs}).*?\.py$")
-
-relevant_modified_files = [x for x in modified_files if regex.match(x)]
-print(" ".join(relevant_modified_files), end="")
