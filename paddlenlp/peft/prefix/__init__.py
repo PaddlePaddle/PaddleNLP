8e8eb8f1d934c70ea97e5f9f481ae70fe4827bb1
Sijun He <sijun.he@hotmail.com>
2023-07-19 10:45:51 +0800

    Support ChatGLM2 Training (FT, LoRA and Prefix) (#6402)
    * remove non MQA
    
    * wip
    
    * changes
    
    * changes
    
    * fix unit test; add comments for assign_kv_heads
    
    * address comments
    
    * fix ids int32 -> int64
    
    * wip
    
    * wip
    
    * wip
    
    * ready for PR
    
    * remove lm shift logits
    
    * changes

--- paddlenlp/peft/prefix/__init__.py
+++ paddlenlp/peft/prefix/__init__.py
@@ -18,5 +18,6 @@ from .utils import (
     bloom_postprocess_past_key_value,
     chatglm_pad_attention_mask,
     chatglm_postprocess_past_key_value,
+    chatglm_v2_pad_attention_mask,
     llama_postprocess_past_key_value,
 )
