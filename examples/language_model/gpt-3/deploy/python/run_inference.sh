3fd1834cc624d581bba15aa03cfe02e066acb23d
wawltor <fangzeyang0904@hotmail.com>
2023-07-24 16:11:17 +0800

    Add llm gpt3 (#6455)
    * add support the gpt-3
    
    * romove the gpt-3 directory
    
    * addd the readme document for llm gpt
    
    * update the infer script
    
    * update the code for the trainer
    
    * update the predict script

--- examples/language_model/gpt-3/deploy/python/run_inference.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-export FLAGS_call_stack_level=2
-export CUDA_VISIBLE_DEVICES=0,1
-
-python -m paddle.distributed.launch \
-        inference.py --model_type gpt \
-        --model_path ../../static/inference_model_pp1mp2/
