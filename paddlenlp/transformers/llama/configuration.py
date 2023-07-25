db49faa95e69731a4b825054e2d8f9d004030f85
西瓜 <wtmlon@foxmail.com>
2023-07-25 20:54:26 +0800

    add Baichuan 7B/13B-base/13B-chat (#6500)
    * add Baichuan 7B/13B-base/13B-chat
    
    * README.md update
    
    * Remove redundant code

--- paddlenlp/transformers/llama/configuration.py
+++ paddlenlp/transformers/llama/configuration.py
@@ -216,6 +216,7 @@ class LlamaConfig(PretrainedConfig):
         bos_token_id=1,
         eos_token_id=2,
         tie_word_embeddings=False,
+        alibi=False,
         **kwargs,
     ):
         self.vocab_size = vocab_size
@@ -238,6 +239,7 @@ class LlamaConfig(PretrainedConfig):
         self.pad_token_id = pad_token_id
         self.bos_token_id = bos_token_id
         self.eos_token_id = eos_token_id
+        self.alibi = alibi
 
         super().__init__(
             pad_token_id=pad_token_id,
@@ -247,3 +249,7 @@ class LlamaConfig(PretrainedConfig):
             tensor_parallel_output=tensor_parallel_output,
             **kwargs,
         )
+
+    @property
+    def rope(self):
+        return not self.alibi
