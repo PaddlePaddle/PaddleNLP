7df6c7ee75214f057c42666c11d9bd1d37aafdde
KB-Ding <kunbo_ding@163.com>
2023-07-26 18:27:39 +0800

    fix BlendableDataset

--- model_zoo/gpt/dataset.py
+++ model_zoo/gpt/dataset.py
@@ -96,114 +96,60 @@ def build_train_valid_test_datasets(
     data_cache_path=None
 ):
     """Build train, valid, and test datasets."""
-    if data_prefix:
-        print_rank_0("Single data path provided for train, valid & test")
-        # Single dataset.
-        if len(data_prefix) == 1:
-            return _build_train_valid_test_datasets(
-                data_prefix[0],
-                data_impl,
-                splits_string,
-                train_valid_test_num_samples,
-                seq_length,
-                seed,
-                skip_warmup,
-                data_cache_path=data_cache_path,
-            )
 
-        # Blending dataset.
-        # Parse the values.
-        output = get_datasets_weights_and_num_samples(data_prefix, train_valid_test_num_samples)
-        prefixes, weights, datasets_train_valid_test_num_samples = output
-        train_num_samples, valid_num_samples, test_num_samples = map(sum, zip(*datasets_train_valid_test_num_samples))
+    # Single dataset.
+    if len(data_prefix) == 1:
+        return _build_train_valid_test_datasets(
+            data_prefix[0],
+            data_impl,
+            splits_string,
+            train_valid_test_num_samples,
+            seq_length,
+            seed,
+            skip_warmup,
+            data_cache_path=data_cache_path,
+        )
 
-        # Build individual datasets.
-        train_datasets = []
-        valid_datasets = []
-        test_datasets = []
-        for i in range(len(prefixes)):
-            train_ds, valid_ds, test_ds = _build_train_valid_test_datasets(
-                prefixes[i],
-                data_impl,
-                splits_string,
-                datasets_train_valid_test_num_samples[i],
-                seq_length,
-                seed,
-                skip_warmup,
-                return_doc_ids,
-                data_cache_path=data_cache_path,
-            )
-            if train_ds:
-                train_datasets.append(train_ds)
-            if valid_ds:
-                valid_datasets.append(valid_ds)
-            if test_ds:
-                test_datasets.append(test_ds)
+    # Blending dataset.
+    # Parse the values.
+    output = get_datasets_weights_and_num_samples(data_prefix, train_valid_test_num_samples)
+    prefixes, weights, datasets_train_valid_test_num_samples = output
+    train_num_samples, valid_num_samples, test_num_samples = map(sum, zip(*datasets_train_valid_test_num_samples))
 
-        # Blend.
-        blending_train_dataset = None
-        if train_datasets:
-            blending_train_dataset = BlendableDataset(
-                train_datasets, weights, train_num_samples, data_cache_path=data_cache_path
-            )
-        blending_valid_dataset = None
-        if valid_datasets:
-            blending_valid_dataset = BlendableDataset(
-                valid_datasets, weights, valid_num_samples, data_cache_path=data_cache_path
-            )
-        blending_test_dataset = None
-        if test_datasets:
-            blending_test_dataset = BlendableDataset(
-                test_datasets, weights, test_num_samples, data_cache_path=data_cache_path
-            )
+    # Build individual datasets.
+    train_datasets = []
+    valid_datasets = []
+    test_datasets = []
+    for i in range(len(prefixes)):
+        train_ds, valid_ds, test_ds = _build_train_valid_test_datasets(
+            prefixes[i],
+            data_impl,
+            splits_string,
+            datasets_train_valid_test_num_samples[i],
+            seq_length,
+            seed,
+            skip_warmup,
+            return_doc_ids,
+            data_cache_path=data_cache_path,
+        )
+        if train_ds:
+            train_datasets.append(train_ds)
+        if valid_ds:
+            valid_datasets.append(valid_ds)
+        if test_ds:
+            test_datasets.append(test_ds)
 
-        return (blending_train_dataset, blending_valid_dataset, blending_test_dataset)
+    blending_train_dataset = None
+    if train_datasets:
+        blending_train_dataset = BlendableDataset(train_datasets, weights)
+    blending_valid_dataset = None
+    if valid_datasets:
+        blending_valid_dataset = BlendableDataset(valid_datasets, weights)
+    blending_test_dataset = None
+    if test_datasets:
+        blending_test_dataset = BlendableDataset(test_datasets, weights)
 
-    else:
-        print_rank_0("Separate data paths provided for train, valid & test. Split string will be ignored.")
-
-        train_dataset, valid_dataset, test_dataset = None, None, None
-        # Single dataset.
-        if train_data_prefix is not None:
-            train_dataset = build_dataset(
-                "train",
-                train_data_prefix,
-                data_impl,
-                splits_string,
-                train_valid_test_num_samples[0],
-                seq_length,
-                seed,
-                skip_warmup,
-                data_cache_path=data_cache_path,
-            )
-
-        if valid_data_prefix is not None:
-            valid_dataset = build_dataset(
-                "valid",
-                valid_data_prefix,
-                data_impl,
-                splits_string,
-                train_valid_test_num_samples[1],
-                seq_length,
-                seed,
-                False,
-                data_cache_path=data_cache_path,
-            )
-
-        if test_data_prefix is not None:
-            test_dataset = build_dataset(
-                "test",
-                test_data_prefix,
-                data_impl,
-                splits_string,
-                train_valid_test_num_samples[2],
-                seq_length,
-                seed,
-                False,
-                data_cache_path=data_cache_path,
-            )
-
-        return (train_dataset, valid_dataset, test_dataset)
+    return (blending_train_dataset, blending_valid_dataset, blending_test_dataset)
 
 
 def _build_train_valid_test_datasets(
@@ -263,106 +209,6 @@ def _build_train_valid_test_datasets(
     test_dataset = build_dataset(2, "test")
 
     return (train_dataset, valid_dataset, test_dataset)
-
-
-def build_dataset(
-    dataset_name,
-    data_prefix,
-    data_impl,
-    splits_string,
-    num_samples,
-    seq_length,
-    seed,
-    skip_warmup,
-    *,
-    data_cache_path=None
-):
-    dataset = None
-    if len(data_prefix) == 1:
-        dataset = _build_dataset(
-            dataset_name,
-            data_prefix[0],
-            data_impl,
-            splits_string,
-            num_samples,
-            seq_length,
-            seed,
-            skip_warmup,
-            data_cache_path=data_cache_path,
-        )
-    else:
-        # Blending dataset.
-        # Parse the values.
-        output = get_datasets_weights_and_num_samples(data_prefix, num_samples)
-        prefixes, weights, dataset_num_samples = output
-        num_samples = sum(dataset_num_samples)
-
-        # Build individual datasets.
-        datasets = []
-        for i in range(len(prefixes)):
-            ds = _build_dataset(
-                dataset_name,
-                prefixes[i],
-                data_impl,
-                splits_string,
-                dataset_num_samples[i],
-                seq_length,
-                seed,
-                skip_warmup,
-                data_cache_path=data_cache_path,
-            )
-            if ds:
-                datasets.append(ds)
-
-        if datasets:
-            dataset = BlendableDataset(datasets, weights, num_samples, data_cache_path=data_cache_path)
-
-    return dataset
-
-
-def _build_dataset(
-    dataset_name,
-    data_prefix,
-    data_impl,
-    splits_string,
-    num_samples,
-    seq_length,
-    seed,
-    skip_warmup,
-    *,
-    data_cache_path=None
-):
-    """
-    Build dataset. This method is called when individual
-    train, valid, test datasets are provided
-    """
-
-    # Indexed dataset.
-    indexed_dataset = get_indexed_dataset_(data_prefix, data_impl, skip_warmup)
-
-    total_num_of_documents = indexed_dataset.sizes.shape[0]
-
-    print_rank_0("    {}:".format(dataset_name))
-    print_rank_0(
-        "     document indices in [0, {}) total of {} "
-        "documents".format(total_num_of_documents, total_num_of_documents)
-    )
-
-    documents = np.arange(start=0, stop=total_num_of_documents, step=1, dtype=np.int32)
-
-    dataset = GPTDataset(
-        dataset_name,
-        data_prefix,
-        documents,
-        indexed_dataset,
-        splits_string,
-        num_samples,
-        seq_length,
-        seed,
-        data_cache_path=data_cache_path,
-    )
-
-    return dataset
 
 
 def get_indexed_dataset_(data_prefix, data_impl, skip_warmup):
