470814e63026b42ec35330d1fc65f628b67afb7b
xiaoxiaohehe001 <hiteezsf@163.com>
2023-07-26 13:13:07 +0000

    fix_log

--- paddlenlp/transformers/generation_utils.py
+++ paddlenlp/transformers/generation_utils.py
@@ -1,4 +1,4 @@
-../../../paddlenlp/transformers/generation_utils.py# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
 # Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
