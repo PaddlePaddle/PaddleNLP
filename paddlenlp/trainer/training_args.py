cd5a67176b5278eabe1ae055bd4b3c14f5b0a4e2
liuyuang <liuyuang@baidu.com>
2023-08-08 08:10:57 +0800

    optim the order

--- paddlenlp/trainer/training_args.py
+++ paddlenlp/trainer/training_args.py
@@ -840,11 +840,17 @@ class TrainingArguments:
                 if tensor_parallel_degree > 1:
                     strategy.tensor_parallel_configs = {"tensor_init_seed": self.seed}
 
+                if tensor_parallel_degree == 1 and sharding_parallel_degree == 1:
+                    order = ["pp", "dp", "sharding", "mp"]
+                else:
+                    order = ["dp", "pp", "sharding", "mp"]
+
                 hybrid_configs = {
                     "dp_degree": self.data_parallel_degree,
                     "mp_degree": tensor_parallel_degree,
                     "pp_degree": pipeline_parallel_degree,
                     "sharding_degree": sharding_parallel_degree,
+                    "order": order,
                 }
 
                 if pipeline_parallel_degree > 1:
