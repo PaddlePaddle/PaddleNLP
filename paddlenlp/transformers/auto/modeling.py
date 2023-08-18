06fc15ccead8cf8c8ea8f5996845066e82f1061b
刘汀 <wtmlon@foxmail.com>
2023-08-18 16:44:34 +0800

    remove redundant print

--- paddlenlp/transformers/auto/modeling.py
+++ paddlenlp/transformers/auto/modeling.py
@@ -233,7 +233,6 @@ class _BaseAutoModelClass:
                     model_name = model_flag + "Model"
                     break
         if model_name is None:
-            print(cls, init_class, model_name)
             raise AttributeError(
                 f"Unable to parse 'architectures' or 'init_class' from {config_file_path}. Also unable to infer model class from 'pretrained_model_name_or_path'"
             )
