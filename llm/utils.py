9b2e8f0ef4a8e0a8f3559aa84d6e6be59e649725
西瓜 <wtmlon@foxmail.com>
2023-08-22 16:05:24 +0800

    add QWen (#6719)
    * add QWen
    
    * add QWen tokenizer, add generative unittest
    
    * generative unittest dtype unmatch on cpu platform
    
    * add qwen multi-gpu
    
    * remove type id, position id, attention mask standardize
    
    * remove redundant print
    
    * remove pad token hardcode
    
    * model prefix name change
    
    * code refinement: dtype, reshape

--- llm/utils.py
+++ llm/utils.py
@@ -120,6 +120,14 @@ def get_lora_target_modules(model):
             ".*linear1.*",
             ".*linear2.*",
         ]
+    elif model.base_model_prefix == "qwen":
+        target_modules = [
+            ".*attn.c_attn.*",
+            ".*attn.c_proj.*",
+            ".*mlp.w1.*",
+            ".*mlp.w2.*",
+            ".*mlp.c_proj.*",
+        ]
     else:
         raise ValueError(f"Unknown base_model_prefix: {model.base_model_prefix}.")
     return target_modules
