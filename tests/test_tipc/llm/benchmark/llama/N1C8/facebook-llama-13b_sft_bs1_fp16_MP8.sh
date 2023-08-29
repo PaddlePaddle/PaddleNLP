99349c8a542ad2fb46481e4811592640b865477c
lugimzzz <zhenglujing@baidu.com>
2023-08-29 20:42:23 +0800

    fix

--- tests/test_tipc/llm/benchmark/llama/N1C8/facebook-llama-13b_sft_bs1_fp16_MP8.sh
+++ tests/test_tipc/llm/benchmark/llama/N1C8/facebook-llama-13b_sft_bs1_fp16_MP8.sh
@@ -14,7 +14,7 @@
 
 model_name_or_path="facebook/llama-13b"
 dataset_name_or_path="llm_benchmark_en"
-max_length=4096
+max_length=1024
 learning_rate="3e-05"
 recompute="1"
 tensor_parallel_degree="8"
