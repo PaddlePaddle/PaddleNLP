37258e85771c4a464447009873cddfb43706a921
w5688414 <w5688414@gmail.com>
2023-08-30 18:46:00 +0800

    revert llama

--- examples/benchmark/peft/paddle/benchmark.py
+++ examples/benchmark/peft/paddle/benchmark.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from dataclasses import dataclass, field
 from typing import Optional
 
@@ -28,6 +29,8 @@ from paddlenlp.peft import LoRAConfig, LoRAModel
 from paddlenlp.trainer import PdArgumentParser, TrainingArguments
 from paddlenlp.transformers import AutoModelForCausalLM, AutoTokenizer
 
+os.environ["http_proxy"] = "http://172.19.56.199:3128"
+os.environ["https_proxy"] = "http://172.19.56.199:3128"
 """
 单卡
 python benchmark.py --model_name_or_path bigscience/bloomz-7b1-mt  \
