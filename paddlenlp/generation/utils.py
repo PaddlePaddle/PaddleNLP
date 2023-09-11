d1da0a1dbcaf795a93ed7ecfb84eab35027105b2
Sijun He <sijun.he@hotmail.com>
2023-09-11 15:05:32 +0800

    default max new tokens

--- paddlenlp/generation/utils.py
+++ paddlenlp/generation/utils.py
@@ -38,7 +38,7 @@ from paddlenlp.transformers.model_outputs import ModelOutput
 from paddlenlp.transformers.utils import get_scale_by_dtype
 from paddlenlp.utils.log import logger
 
-from .configuration_utils import DEFAULT_MAX_NEW_TOKEN, GenerationConfig
+from .configuration_utils import DEFAULT_MAX_NEW_TOKENS, GenerationConfig
 from .logits_process import (
     ForcedBOSTokenLogitsProcessor,
     ForcedEOSTokenLogitsProcessor,
@@ -902,7 +902,7 @@ class GenerationMixin(object):
             print("Setting `pad_token_id` to `eos_token_id`:{} for " "open-end generation.".format(eos_token_id))
             pad_token_id = eos_token_id
 
-        if generation_config.max_length != 0 and generation_config.max_new_tokens == DEFAULT_MAX_NEW_TOKEN:
+        if generation_config.max_length != 0 and generation_config.max_new_tokens == DEFAULT_MAX_NEW_TOKENS:
             logger.warning("`max_length` will be deprecated in future releases, use `max_new_tokens` instead.")
             generation_config.max_new_tokens = generation_config.max_length
 
