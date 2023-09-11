fecfafdd6d9ea3549e2c38079382723b5e89fce5
zjjlivein <1834792141@qq.com>
2023-09-11 19:57:30 +0800

    fix finetune output

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -20,6 +20,7 @@ import json
 import os
 import sys
 import unittest
+import subprocess
 from collections.abc import Mapping
 from contextlib import contextmanager
 
@@ -371,3 +372,20 @@ def update_params(json_file: str, params: dict):
         data.update(params)
     with open(json_file, "w") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+def run_command(command: List[str], return_stdout=False):
+    """
+    Runs `command` with `subprocess.check_output` and will potentially return the `stdout`. Will also properly capture
+    if an error occured while running `command`
+    """
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT,shell=True)
+        if return_stdout:
+            if hasattr(output, "decode"):
+                output = output.decode("utf-8")
+            return output
+    except subprocess.CalledProcessError as e:
+        raise SubprocessCallException(
+            f"Command `{' '.join(command)}` failed with the following error:\n\n{e.output.decode()}"
+        ) from e
