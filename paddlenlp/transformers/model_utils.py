0a68fe79806b51f00837683b444ee86d9929dd1c
DrownFish19 <DrownFish19@gmail.com>
2023-09-12 11:59:44 +0800

    fix fluid for gpt

--- paddlenlp/transformers/model_utils.py
+++ paddlenlp/transformers/model_utils.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 from __future__ import annotations
 
+import contextlib
 import copy
 import gc
 import inspect
@@ -83,6 +84,14 @@ __all__ = [
     "PretrainedModel",
     "register_base_model",
 ]
+
+
+def stridecheck_guard_context():
+    try:
+        context = paddle.base.framework._stride_in_no_check_dy2st_diff()
+    except:
+        context = contextlib.nullcontext()
+    return context
 
 
 def unwrap_optimizer(optimizer, optimizer_instances=()):
