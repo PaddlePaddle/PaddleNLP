6ff32d8d95409b8dbd51f0101683c2c80a107fd3
Sijun He <sijun.he@hotmail.com>
2023-09-14 11:53:21 +0800

    changes (#7029)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read_requirements_file(filepath):
     return requirements
 
 
-__version__ = "2.6.0.post"
+__version__ = "2.6.1.post"
 if os.getenv(PADDLENLP_STABLE_VERSION):
     __version__ = __version__.replace(".post", "")
 
