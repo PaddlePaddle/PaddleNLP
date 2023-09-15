78639c82883be91b5dafce5a73dc60a87eff0efc
zjjlivein <1834792141@qq.com>
2023-09-15 17:35:19 +0800

    [CI] add_llm_ci_ipipe

--- scripts/regression/ci_case.sh
+++ scripts/regression/ci_case.sh
@@ -1075,11 +1075,16 @@ print_info $? taskflow_unittest
 python -m pytest scripts/regression/test_taskflow.py >${log_path}/taskflow >>${log_path}/taskflow 2>&1
 print_info $? taskflow
 }
-transformers(){
-echo ' RUN all LLMs unittest'
-export RUN_SLOW_TEST=True
-python -m pytest tests/llm/test_*.py >${nlp_dir}/unittest_logs/llm_unittest.log 2>&1
-print_info $? llm_unittest
+llm(){
+cd ${nlp_dir}/csrc
+echo "build paddlenlp_op"
+
+pip install -r requirements.txt
+python setup_cuda.py install
+
+echo ' Testing all LLMs '
+python -m pytest tests/llm/test_*.py >${log_path}/llm >>${log_path}/llm 2>&1
+print_info $? llm
 }
 fast_generation(){
 cd ${nlp_dir}/fast_generation/samples
