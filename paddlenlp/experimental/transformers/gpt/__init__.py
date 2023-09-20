af28006018019d1b8fd17d337764a91388cd3c65
Yuanle Liu <yuanlehome@163.com>
2023-09-20 20:51:12 +0800

    [LLM] Support gpt3 fine grained dybatch v1 (#7080)
    * support batch_size=1
    
    * support batch_size > 1
    
    * update
    
    * support to_static
    
    * add benchmark for dybatch_preprocess
    
    * fix code style
    
    * fix code style
    
    * fix comment
    
    * update

--- paddlenlp/experimental/transformers/gpt/__init__.py
+++ paddlenlp/experimental/transformers/gpt/__init__.py
@@ -12,8 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .bloom import *
-from .chatglm import *
-from .fused_transformer_layers import *
-from .gpt import *
-from .llama import *
+from .modeling import *
