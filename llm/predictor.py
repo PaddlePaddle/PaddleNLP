15a4a0ded5aabdbc4b5d1c8fb5c2b0747140638c
xiaoxiaohehe001 <hiteezsf@163.com>
2023-09-21 16:21:54 +0800

    update_chatglm

--- llm/predictor.py
+++ llm/predictor.py
@@ -364,7 +364,6 @@ class InferencePredictorMixin:
                 length = inputs["seq_len_encoder"][i][0]
                 self.attention_mask[i, 0, :length, :length] = 1
                 self.attention_mask[i, 0, : length - 1, length - 1] = 0
-
                 self.tgt_pos[i, 0, 0] = paddle.to_tensor([length], dtype="int64")
 
                 if pre_caches_length > 0:
