0c63152614e970151cadfb4c8271dfd85a7bdf3a
lugimzzz <63761690+lugimzzz@users.noreply.github.com>
2023-10-09 11:11:31 +0800

    [llm]support lora for quantizationlinear (#7138)
    * support quantizationloralinear
    
    * Version compatible
    
    * add comment
    
    * add mp quantization lora
    
    * fix
    
    * fix
    
    * fix

--- paddlenlp/peft/prefix/prefix_model.py
+++ paddlenlp/peft/prefix/prefix_model.py
@@ -269,17 +269,15 @@ class PrefixModelForCausalLM(paddle.nn.Layer):
         trainable_numel = 0
         freeze_numel = 0
         for _, weight in self.model.state_dict().items():
-            weight_size = np.prod(weight.shape) if weight.dtype == paddle.int8 else weight.numel().item()
             if weight.stop_gradient:
-                freeze_numel += weight_size
+                freeze_numel += np.prod(weight.shape)
             else:
-                trainable_numel += weight_size
+                trainable_numel += np.prod(weight.shape)
         for _, weight in self.prefix_encoder.state_dict().items():
-            weight_size = np.prod(weight.shape) if weight.dtype == paddle.int8 else weight.numel().item()
             if weight.stop_gradient:
-                freeze_numel += weight_size
+                freeze_numel += np.prod(weight.shape)
             else:
-                trainable_numel += weight_size
+                trainable_numel += np.prod(weight.shape)
         logger.info(
             f"Frozen parameters: {freeze_numel:.2e} || Trainable parameters:{trainable_numel:.2e} || Total parameters:{freeze_numel+trainable_numel:.2e}|| Trainable:{trainable_numel / (freeze_numel+trainable_numel):.2%}"
         )
