ede6941c38667c92ef30586c5946f7485936d9a9
lugimzzz <zhenglujing@baidu.com>
2023-10-17 15:34:25 +0800

    fix quantization

--- paddlenlp/transformers/model_utils.py
+++ paddlenlp/transformers/model_utils.py
@@ -501,10 +501,7 @@ def shard_checkpoint(
     if shard_format == "naive":
         for key, weight in state_dict.items():
             # _C_ops.numel not yet support paddle.int8
-            if weight.dtype == paddle.int8:
-                weight_size = np.prod(weight.shape) * dtype_byte_size(weight.dtype)
-            else:
-                weight_size = weight.numel().item() * dtype_byte_size(weight.dtype)
+            weight_size = np.prod(weight.shape) * dtype_byte_size(weight.dtype)
             # If this weight is going to tip up over the maximal size, we split.
             if current_block_size + weight_size > max_shard_size:
                 # fix if the first param is large than max_shard_size
@@ -1320,7 +1317,7 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
         from_hf_hub: bool = False,
         from_aistudio: bool = False,
         cache_dir: str | None = None,
-        subfolder: Optional[str] = None,
+        subfolder: str = "",
         config: PretrainedConfig = None,
         convert_from_torch: bool = False,
         use_safetensors: bool | None = None,
@@ -1362,9 +1359,6 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
             return resolved_archive_file, sharded_metadata, is_sharded
 
         if pretrained_model_name_or_path is not None:
-            # the following code use a lot of os.path.join, hence setting subfolder to empty str if None
-            if subfolder is None:
-                subfolder = ""
             pretrained_model_name_or_path = str(pretrained_model_name_or_path)
             is_local = os.path.isdir(pretrained_model_name_or_path)
 
@@ -1771,7 +1765,9 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
                     tp_actions = cls.get_tensor_parallel_convert_actions(config, loaded_keys)
                 # Here we use expected_keys to optimize weights loading for pipeline model. Only works for safetensors
                 state_dict = load_state_dict(
-                    shard_file, tp_actions if pre_tensor_parallel_split else None, set(expected_keys)
+                    shard_file,
+                    tp_actions if pre_tensor_parallel_split else None,
+                    set(expected_keys) if quantization_config is None else None,
                 )
                 if quantization_config is not None:
                     state_dict = convert_to_quantize_state_dict(
@@ -1932,7 +1928,7 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
         dtype = kwargs.pop("dtype", None)
         from_hf_hub = kwargs.get("from_hf_hub", False)
         from_aistudio = kwargs.get("from_aistudio", False)
-        subfolder = kwargs.get("subfolder", None)
+        subfolder = kwargs.get("subfolder", "")
         variant = kwargs.pop("variant", None)
         use_safetensors = kwargs.pop("use_safetensors", None if is_safetensors_available() else False)
 
