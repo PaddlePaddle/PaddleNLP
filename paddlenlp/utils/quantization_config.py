ee2f124b0f5a5171db47de8c010bf6ec7394c17d
lugimzzz <zhenglujing@baidu.com>
2023-10-26 16:38:51 +0800

    update

--- paddlenlp/utils/quantization_config.py
+++ paddlenlp/utils/quantization_config.py
@@ -33,9 +33,10 @@ class QuantizationConfig:
             "weight_only_int8",
             "weight_only_int4",
             "llm.int8",
+            "a8w8",
         ]:
             raise ValueError(
-                f"weight_quantize_algo:{weight_quantize_algo} not in supported list ['weight_only_int8', 'weight_only_int4', 'llm.int8']"
+                f"weight_quantize_algo:{weight_quantize_algo} not in supported list ['weight_only_int8', 'weight_only_int4', 'llm.int8', 'a8w8']"
             )
         if quant_type is not None and quant_type not in ["weight_only_int8", "weight_only_int4", "a8w8"]:
             raise ValueError(
@@ -51,13 +52,13 @@ class QuantizationConfig:
         self.llm_int8_threshold = llm_int8_threshold
 
     def is_weight_quantize(self):
-        if self.weight_quantize_algo in ["weight_only_int8", "weight_only_int4", "llm.int8"]:
+        if self.weight_quantize_algo in ["weight_only_int8", "weight_only_int4", "llm.int8", "a8w8"]:
             return True
         else:
             return False
 
     def is_support_merge_tensor_parallel(self):
-        if self.weight_quantize_algo in ["weight_only_int8", "weight_only_int4", "llm.int8"]:
+        if self.weight_quantize_algo in ["weight_only_int8", "weight_only_int4", "llm.int8", "a8w8"]:
             return False
         else:
             return True
