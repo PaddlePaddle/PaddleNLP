fe8cdf0d795c559eecdac27c0678c331d1864931
lugimzzz <63761690+lugimzzz@users.noreply.github.com>
2023-10-27 18:06:24 +0800

    [quant] add QuantizationConfig (#7314)
    * add
    
    * add quantization config
    
    * update
    
    * fix
    
    * fix
    
    * add quantization_config

--- llm/argument.py
+++ llm/argument.py
@@ -66,7 +66,8 @@ class ModelArgument:
 @dataclass
 class QuantArgument:
     quant_type: str = field(
-        default="A8W8", metadata={"help": "Quantization type. Supported values: A8W8, WINT4,WINT8"}
+        default="a8w8",
+        metadata={"help": "Quantization type. Supported values: a8w8, weight_only_int4, weight_only_int8"},
     )
 
     # QAT related parameters
