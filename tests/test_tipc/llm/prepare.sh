aa1bc97e749284ffcf72438476d23dce44427d96
wj-Mcat <1435130236@qq.com>
2023-11-22 09:28:18 +0000

    update scripts

--- tests/test_tipc/llm/prepare.sh
+++ tests/test_tipc/llm/prepare.sh
@@ -13,8 +13,9 @@
 # limitations under the License.
 
 # install specific version of paddlepaddle
+pip uninstall -y paddlepaddle-gpu
 wget https://paddle-qa.bj.bcebos.com/paddle-pipeline/Develop-TagBuild-Training-Linux-Gpu-Cuda12.0-Cudnn8.9-Trt8.6-Mkl-Avx-Gcc12.2/9bf5a86f13aa85b38715c015693f973290c8f9da/paddlepaddle_gpu-0.0.0.post120-cp310-cp310-linux_x86_64.whl
-pip install paddlepaddle_gpu-0.0.0.post120-cp310-cp310-linux_x86_64.whl
+pip install --force-reinstall paddlepaddle_gpu-0.0.0.post120-cp310-cp310-linux_x86_64.whl
 
 # install requirements
 cd ..
