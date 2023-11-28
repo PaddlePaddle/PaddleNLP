239256f050d877843f9d4e1565e92aba810e5e16
sneaxiy <sneaxiy@126.com>
2023-11-28 10:56:26 +0800

    fix bugs

--- paddlenlp/trainer/trainer.py
+++ paddlenlp/trainer/trainer.py
@@ -164,6 +164,12 @@ def _save_func(obj, path, saved_signal_path, protocol):
         f.write("1")
 
 
+def check_exitcode(task):
+    exitcode = task.exitcode
+    if exitcode != 0:
+        print(f"Error: save ckpt process failed with exitcode {exitcode}!!!")
+
+
 def clear_async_save_task_queue():
     """
     wait until all async save task to be done.
@@ -176,9 +182,9 @@ def clear_async_save_task_queue():
                 logger.error("Error: save ckpt process timeout!!!")
                 async_save_queue.append(task)
             else:
-                exitcode = task.exitcode
-                if exitcode != 0:
-                    logger.error(f"Error: save ckpt process failed with exitcode {exitcode}!!!")
+                check_exitcode(task)
+        else:
+            check_exitcode(task)
 
 
 def async_save_optimizer(optimizer_state_dict, path, saved_signal_path, protocol=4):
