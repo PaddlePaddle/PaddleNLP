7aeaf0098e2a4c54365221bcd0082a4e519e2801
tianhaodongbd <tianhaodong@baidu.com>
2023-11-28 19:48:03 +0800

    rename model_item

--- tests/test_tipc/dygraph/hybrid_parallelism/ce_gpt/N1C1/CE_gpt-345m_seqlen1024_pretrain_bs32_bf16_DP1-mbs16-acc2.sh
+++ tests/test_tipc/dygraph/hybrid_parallelism/ce_gpt/N1C1/CE_gpt-345m_seqlen1024_pretrain_bs32_bf16_DP1-mbs16-acc2.sh
@@ -12,18 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-model_item=CE_gpt3
-dp_degree=8
+model_item=CE_gpt-345m_seqlen1024_pretrain
+dp_degree=1
 mp_degree=1
 pp_degree=1
 bs_item=32
 fp_item=bf16
-run_mode=DP8-mbs2-acc2
-device_num=N1C8
+run_mode=DP1-mbs16-acc2
+device_num=N1C1
 max_iter=50000
 
 model=gpt
-micro_bs=2
+micro_bs=16
 
 bash ./test_tipc/dygraph/hybrid_parallelism/ce_gpt/benchmark_common/prepare.sh
 # run
