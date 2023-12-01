9fbc173ff808ece242f6eeb83fe8abbf31a3efa3
wj-Mcat <1435130236@qq.com>
2023-12-01 12:32:14 +0000

    remove intergration test

--- tests/transformers/test_chat_template.py
+++ tests/transformers/test_chat_template.py
@@ -93,65 +93,6 @@ class ChatTemplateContextDataTest(unittest.TestCase):
         self.assertEqual(final_query, expected_query)
 
 
-class ChatTemplateIntegrationTest(unittest.TestCase):
-    def test_llama2_chat_template(self):
-        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat")
-        query = "who are you?"
-        final_query = tokenizer.apply_chat_template(query, tokenize=False)
-        expected_query = f"<<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n\n[INST] {query} [/INST] "
-        self.assertEqual(final_query, expected_query)
-
-    def test_linlyai_chinese_llama_2_chat_template(self):
-        tokenizer = AutoTokenizer.from_pretrained("linly-ai/chinese-llama-2-7b")
-        query = "你好"
-        final_query = tokenizer.apply_chat_template(query, tokenize=False)
-        expected_query = f"<s>### Instruction:{query}  ### Response:"
-        self.assertEqual(final_query, expected_query)
-
-        # test multi turns conversation
-        query = [["你好", "您好，我是个人人工智能助手"], ["今天吃啥"]]
-        final_query = tokenizer.apply_chat_template(query, tokenize=False)
-        expected_query = "<s>### Instruction: 你好  ### Response:您好，我是个人人工智能助手 </s>### Instruction:今天吃啥  ### Response:"
-        self.assertEqual(final_query, expected_query)
-
-    def test_chatglm_bellegroup(self):
-        # refer to: https://huggingface.co/THUDM/chatglm-6b/blob/main/modeling_chatglm.py#L1267
-        tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-v1.1")
-        query = [["你好", "您好，我是个人人工智能助手"], ["今天吃啥"]]
-        final_query = tokenizer.apply_chat_template(query, tokenize=False)
-        expected_query = "[Round 0]\n问：你好\n答：您好，我是个人人工智能助手\n[Round 1]\n问：今天吃啥\n答：[gMASK]<sop>"
-        self.assertEqual(final_query, expected_query)
-
-    def test_bloom_bellegroup(self):
-        # refer to: https://huggingface.co/BelleGroup/BELLE-7B-2M#use-model
-        tokenizer = AutoTokenizer.from_pretrained("bellegroup/belle-7b-2m")
-        query = "你好"
-        final_query = tokenizer.apply_chat_template(query, tokenize=False)
-        expected_query = f"Human: {query}\n\nAssistant:"
-        self.assertEqual(final_query, expected_query)
-
-    def test_qwen_14b_chat(self):
-        # refer to: https://huggingface.co/Qwen/Qwen-14B-Chat/blob/main/qwen_generation_utils.py#L119
-
-        # 1. test render base on query & conversation data
-        tokenizer = AutoTokenizer.from_pretrained("qwen/qwen-14b-chat")
-        query = "你好"
-        final_query = tokenizer.apply_chat_template(query, tokenize=False)
-
-        expected_query = "You are a helpful assistant.\n<|im_start|>user\n你好<|im_end|>\n<|im_start|>assistant\n"
-        self.assertEqual(final_query, expected_query)
-
-        query = [["你好", "您好，我是个人人工智能助手"], ["今天吃啥"]]
-        final_query = tokenizer.apply_chat_template(query, tokenize=False)
-
-        expected_query = (
-            "You are a helpful assistant.\n<|im_start|>user\n你好<|im_end|>"
-            "\n<|im_start|>assistant\n您好，我是个人人工智能助手<|im_end|>"
-            "\n<|im_start|>user\n今天吃啥<|im_end|>\n<|im_start|>assistant\n"
-        )
-        self.assertEqual(final_query, expected_query)
-
-
 @parameterized_class(
     ["model_name"],
     [
