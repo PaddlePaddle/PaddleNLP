1a7e7d45724169114967fb841512758a53571511
骑马小猫 <1435130236@qq.com>
2023-12-04 10:58:02 +0800

    [Bug fixes]fix intergration test (#7569)
    * remove intergration test
    
    * update llama2 chat-template

--- tests/transformers/test_chat_template.py
+++ tests/transformers/test_chat_template.py
@@ -98,7 +98,7 @@ class ChatTemplateIntegrationTest(unittest.TestCase):
         tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat")
         query = "who are you?"
         final_query = tokenizer.apply_chat_template(query, tokenize=False)
-        expected_query = f"<<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n\n[INST] {query} [/INST] "
+        expected_query = "<s>[INST] <<SYS>>\nYou are a helpful assistant.\n<</SYS>>[INST] who are you? [/INST]"
         self.assertEqual(final_query, expected_query)
 
     def test_linlyai_chinese_llama_2_chat_template(self):
