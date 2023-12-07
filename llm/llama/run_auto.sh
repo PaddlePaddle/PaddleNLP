af4c231533c4bf3334cc5c12efa7edcf689dccc9
lxd-cumt <1141051934@qq.com>
2023-12-07 07:21:55 +0000

    update scripts

--- llm/llama/run_auto.sh
+++ llm/llama/run_auto.sh
@@ -25,21 +25,21 @@ rm -rf debug_program*
 export FLAGS_call_stack_level=2
 export SOT_LOG_LEVEL=4
 PYTHONPATH=../../:$PYTHONPATH  \
-export CUDA_VISIBLE_DEVICES=2,6
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 export FLAGS_new_executor_micro_batching=True
 export FLAGS_enable_pir_in_executor=1
 export FLAGS_enable_prim_after_distribute=True
-# export GLOG_v=6
+# export GLOG_v=4
 
 # --pipeline_parallel_degree 1 \
 # --tensor_parallel_degree 2 \
 # --sharding_parallel_degree 2 \
 # --sharding "stage1" \
-
+# --sharding "" \
 
 python -u  -m paddle.distributed.launch \
-    --devices "2,6" \
+    --devices "0,1,2,3" \
     --log_dir "log_newir/$task_name""_log" \
     run_pretrain_auto.py \
     --model_type "llama" \
@@ -49,25 +49,26 @@ python -u  -m paddle.distributed.launch \
     --output_dir "log_newir/$task_name" \
     --split 949,50,1 \
     --max_seq_length 256 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
     --gradient_accumulation_steps 1 \
     --use_flash_attention 0 \
     --use_fused_rms_norm 0 \
+    --tensor_parallel_degree 2 \
+    --sharding_parallel_degree 2 \
+    --sharding "stage1" \
     --fp16 0 \
     --fp16_opt_level "O2"  \
     --scale_loss 1024 \
-    --tensor_parallel_degree 2 \
     --learning_rate 0.0001 \
     --min_learning_rate 0.00001 \
-    --max_steps 100 \
+    --max_steps 3 \
     --save_steps 5000 \
     --weight_decay 0.01 \
     --warmup_ratio 0.01 \
     --max_grad_norm 1.0 \
     --logging_steps 1\
     --dataloader_num_workers 1 \
-    --sharding "" \
     --eval_steps 1000 \
     --report_to "visualdl" \
     --disable_tqdm true \
--- model_zoo/gpt-3/projects/gpt/auto_gpt_345M_newir.sh
+++ model_zoo/gpt-3/projects/gpt/auto_gpt_345M_newir.sh
@@ -16,7 +16,7 @@
 
 export FLAGS_new_executor_micro_batching=True
 export FLAGS_enable_pir_in_executor=1
-export FLAGS_enable_prim_after_distribute=True
+# export FLAGS_enable_prim_after_distribute=True
 # export GLOG_v=4
 
 log_dir=log_newir
@@ -26,24 +26,24 @@ rm -rf debug_program_*
 
 # Only dp_degree, mp_degree and pp_degree can be changed
 # Only support pure dp, pure mp and pure pp
-export CUDA_VISIBLE_DEVICES=5,6
-python -m paddle.distributed.launch --log_dir $log_dir --devices "5,6" \
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+python -m paddle.distributed.launch --log_dir $log_dir --devices "0,1,2,3" \
     ./tools/auto.py \
     -c ./ppfleetx/configs/nlp/gpt/auto/pretrain_gpt_345M_single_card.yaml \
-    -o Model.num_layers=2 \
+    -o Model.num_layers=4 \
     -o Model.hidden_dropout_prob=0 \
     -o Model.attention_probs_dropout_prob=0 \
     -o Model.use_recompute=False \
-    -o Global.local_batch_size=2 \
-    -o Global.micro_batch_size=1 \
-    -o Distributed.dp_degree=1 \
-    -o Distributed.mp_degree=1 \
-    -o Distributed.pp_degree=2 \
+    -o Global.local_batch_size=4 \
+    -o Global.micro_batch_size=4 \
+    -o Distributed.dp_degree=2 \
+    -o Distributed.mp_degree=2 \
+    -o Distributed.pp_degree=1 \
     -o Distributed.sharding.sharding_degree=1 \
     -o Distributed.sharding.sharding_stage=1 \
-    -o Engine.max_steps=2 \
+    -o Engine.max_steps=100 \
     -o Engine.eval_freq=100000 \
     -o Engine.mix_precision.enable=False \
 
-cp log_newir/workerlog.0 log_newir/workerlog_0.log
-cp log_newir/workerlog.1 log_newir/workerlog_1.log
+# cp log_newir/workerlog.0 log_newir/workerlog_0.log
+# cp log_newir/workerlog.1 log_newir/workerlog_1.log
