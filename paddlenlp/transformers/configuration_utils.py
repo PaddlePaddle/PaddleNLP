865b6dd529e2144fddde025712aeeb02fa2a708e
yujun <573009727@qq.com>
2023-12-09 18:24:11 +0800

    update config download bug

--- paddlenlp/transformers/configuration_utils.py
+++ paddlenlp/transformers/configuration_utils.py
@@ -38,7 +38,6 @@ from ..utils.downloader import (
     COMMUNITY_MODEL_PREFIX,
     get_path_from_url_with_filelock,
     hf_file_exists,
-    is_url,
     url_file_exists,
 )
 from ..utils.log import logger
@@ -709,6 +708,7 @@ class PretrainedConfig:
 
         cache_dir = resolve_cache_dir(from_hf_hub, from_aistudio, cache_dir)
         kwargs["cache_dir"] = cache_dir
+        kwargs["subfolder"] = subfolder
 
         # Get config dict associated with the base config file
         config_dict, kwargs = cls._get_config_dict(pretrained_model_name_or_path, **kwargs)
@@ -749,15 +749,7 @@ class PretrainedConfig:
         # 1. get the configuration file from local file, eg: /cache/path/model_config.json
         if os.path.isfile(pretrained_model_name_or_path):
             resolved_config_file = pretrained_model_name_or_path
-
-        # 2. get the configuration file from url, eg: https://ip/path/to/model_config.json
-        elif is_url(pretrained_model_name_or_path):
-            resolved_config_file = get_path_from_url_with_filelock(
-                pretrained_model_name_or_path,
-                os.path.join(cache_dir, pretrained_model_name_or_path, subfolder),
-                check_exist=not force_download,
-            )
-        # 3. get the configuration file from local dir with default name, eg: /local/path
+        # 2. get the configuration file from local dir with default name, eg: /local/path
         elif os.path.isdir(pretrained_model_name_or_path):
             configuration_file = kwargs.pop("_configuration_file", CONFIG_NAME)
             configuration_file = os.path.join(pretrained_model_name_or_path, subfolder, configuration_file)
@@ -773,10 +765,10 @@ class PretrainedConfig:
                         "please make sure there is `model_config.json` under the dir, or you can pass the `_configuration_file` "
                         "param into `from_pretarined` method to specific the configuration file name"
                     )  # 4. load it as the community resource file
-        # 4. get the configuration file from aistudio
+        # 3. get the configuration file from aistudio
         elif from_aistudio:
             resolved_config_file = aistudio_download(repo_id=pretrained_model_name_or_path, filename=CONFIG_NAME)
-        # 5. get the configuration file from HF HUB
+        # 4. get the configuration file from HF HUB
         elif from_hf_hub:
             resolved_config_file = resolve_hf_config_path(
                 repo_id=pretrained_model_name_or_path, cache_dir=cache_dir, subfolder=subfolder
@@ -789,15 +781,22 @@ class PretrainedConfig:
                 url_list.insert(2, subfolder)
                 legacy_url_list.insert(2, subfolder)
             community_url = "/".join(url_list)
+            legacy_community_url = "/".join(legacy_url_list)
 
             if url_file_exists(community_url):
-                return cls._get_config_dict(community_url, cache_dir=cache_dir, **kwargs)
-
-            community_url = "/".join(legacy_url_list)
-            if url_file_exists(community_url):
-                return cls._get_config_dict(community_url, cache_dir=cache_dir, **kwargs)
-
-            raise FileNotFoundError(f"configuration file<{CONFIG_NAME}> or <{LEGACY_CONFIG_NAME}> not found")
+                resolved_config_file = get_path_from_url_with_filelock(
+                    community_url,
+                    cache_dir,
+                    check_exist=not force_download,
+                )
+            elif url_file_exists(legacy_community_url):
+                resolved_config_file = get_path_from_url_with_filelock(
+                    legacy_community_url,
+                    cache_dir,
+                    check_exist=not force_download,
+                )
+            else:
+                raise FileNotFoundError(f"configuration file<{CONFIG_NAME}> or <{LEGACY_CONFIG_NAME}> not found")
 
         try:
             logger.info(f"Loading configuration file {resolved_config_file}")
