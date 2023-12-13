4a27ea5088a693d6879fde6d3ff3ebfabc7d50db
MayYouBeProsperous <ljmhz@outlook.com>
2023-12-13 19:43:49 +0800

    codestyle

--- paddlenlp/trainer/trainer.py
+++ paddlenlp/trainer/trainer.py
@@ -263,7 +263,7 @@ class Trainer:
 
         if model is None:
             raise RuntimeError("`Trainer` requires either a `model` or `model_init` argument")
-            
+
         if self.args.to_static:
             model = paddle.jit.to_static(model)
             logger.info("Successfully to apply @to_static to the whole model.")
