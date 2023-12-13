d5a9dda0e8e11413a8d6fcd74800142de2cdafc7
骑马小猫 <1435130236@qq.com>
2023-12-13 14:03:39 +0800

    fix chatglm2 training for chat-template (#7637)

--- paddlenlp/transformers/chatglm_v2/tokenizer.py
+++ paddlenlp/transformers/chatglm_v2/tokenizer.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations
 
 import os
 from typing import Dict, List, Optional, Union
@@ -278,3 +279,13 @@ class ChatGLMv2Tokenizer(PretrainedTokenizer):
             encoded_inputs[self.model_input_names[0]] = [self.pad_token_id] * difference + required_input
 
         return encoded_inputs
+
+    def encode_chat_inputs(self, conversations: List[List[str, str]]):
+        # encode system
+        result = super().encode_chat_inputs(conversations)
+        if "system" in result:
+            result["system"] = self.get_prefix_tokens() + result["system"]
+        else:
+            result["conversations"][0][0] = self.get_prefix_tokens() + result["conversations"][0][0]
+
+        return result
