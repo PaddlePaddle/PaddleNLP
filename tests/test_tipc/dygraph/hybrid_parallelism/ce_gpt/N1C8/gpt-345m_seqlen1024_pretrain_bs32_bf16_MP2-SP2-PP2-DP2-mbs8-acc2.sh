1ea9b46e0f612bdf3988d51c35aea1db1766b73b
iosmers <yinwei_hust@163.com>
2023-12-14 19:34:30 +0800

    add

--- tests/test_tipc/dygraph/hybrid_parallelism/ce_gpt/N1C8/gpt-345m_seqlen1024_pretrain_bs32_bf16_MP2-SP2-PP2-DP2-mbs8-acc2.sh
+++ tests/test_tipc/dygraph/hybrid_parallelism/ce_gpt/N1C8/gpt-345m_seqlen1024_pretrain_bs32_bf16_MP2-SP2-PP2-DP2-mbs8-acc2.sh
@@ -27,7 +27,7 @@ max_iter=100
 model=gpt
 micro_bs=8
 
-# bash ./test_tipc/dygraph/hybrid_parallelism/ce_gpt/benchmark_common/prepare.sh
+bash ./test_tipc/dygraph/hybrid_parallelism/ce_gpt/benchmark_common/prepare.sh
 # run
 bash ./test_tipc/dygraph/hybrid_parallelism/ce_gpt/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${dp_degree} ${mp_degree} ${pp_degree} ${micro_bs} ${bs_item} ${run_mode} ${device_num} \
 ${max_iter} ${sharding} ${sharding_degree} 2>&1;
