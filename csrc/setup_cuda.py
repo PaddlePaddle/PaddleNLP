ab17ed88fc055d346637e8b8e607288ff59585e2
zhoutianzi666 <17801055074@163.com>
2023-12-21 05:19:01 +0000

    删掉一些东西

--- csrc/setup_cuda.py
+++ csrc/setup_cuda.py
@@ -29,7 +29,6 @@ setup(
     ext_modules=CUDAExtension(
         sources=[
             "./generation/save_with_output.cc",
-            "./generation/set_mask_value.cu",
             "./generation/set_value_by_flags.cu",
             "./generation/token_penalty_multi_scores.cu",
             "./generation/stop_generation_multi_ends.cu",
@@ -40,7 +39,6 @@ setup(
             "./generation/transpose_removing_padding.cu",
             "./generation/write_cache_kv.cu",
             "./generation/encode_rotary_qk.cu",
-            "./generation/set_alibi_mask_value.cu",
             "./generation/quant_int8.cu",
             "./generation/dequant_int8.cu",
         ],
