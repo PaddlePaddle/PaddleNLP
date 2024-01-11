956bb283ec1e135f53e2651f6d3b1ef55c4a1db0
ZeyuTeng96 <61420226@qq.com>
2024-01-11 07:28:03 +0000

    [fix][fix a bug of evaluate.py]

--- applications/neural_search/recall/in_batch_negative/evaluate.py
+++ applications/neural_search/recall/in_batch_negative/evaluate.py
@@ -72,6 +72,7 @@ if __name__ == "__main__":
                 relevance_labels.append(1)
             else:
                 relevance_labels.append(0)
+        rs.append(relevance_labels)
 
     recall_N = []
     recall_num = [1, 5, 10, 20, 50]
