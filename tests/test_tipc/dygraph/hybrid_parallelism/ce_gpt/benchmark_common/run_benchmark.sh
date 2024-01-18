6d667214a8068cd956faad1f5f6c094ff1482842
iosmers <yinwei_hust@163.com>
2024-01-18 22:45:37 +0800

    add

--- tests/test_tipc/dygraph/hybrid_parallelism/ce_gpt/benchmark_common/run_benchmark.sh
+++ tests/test_tipc/dygraph/hybrid_parallelism/ce_gpt/benchmark_common/run_benchmark.sh
@@ -153,9 +153,7 @@ function _train(){
             run_pretrain.py ${train_cmd}"
         workerlog_id=0
         ;;
-    DP8-mbs2-acc2|SD8-stage1-mbs2-acc2|SD8-stage2-mbs2-acc2|SD8-stage3-mbs2-acc2|PP8-mbs16-acc2|MP8-mbs16-acc2|
-    MP2-PP2-DP2-mbs8-acc2|MP2-PP2-SD2-Stage1-mbs8-acc2|MP2-SD4-stage1-mbs4-acc2|MP2-SP2-PP2-DP2-mbs8-acc2|MP2-SP2-PP2-SD2-Stage1-mbs8-acc2|
-    MP2-PP4-VPP2-DP2-mbs8-acc2|MP2-PP4-VPP2-SD2-stage1-mbs8-acc2|MP2-SP2-PP4-VPP2-DP2-mbs8-acc2|MP2-SP2-PP4-VPP2-SD2-stage1-mbs8-acc2) 
+    DP8-mbs2-acc2|SD8-stage1-mbs2-acc2|SD8-stage2-mbs2-acc2|SD8-stage3-mbs2-acc2|PP8-mbs16-acc2|MP8-mbs16-acc2|MP2-PP2-DP2-mbs8-acc2|MP2-PP2-SD2-Stage1-mbs8-acc2|MP2-SD4-stage1-mbs4-acc2|MP2-SP2-PP2-DP2-mbs8-acc2|MP2-SP2-PP2-SD2-Stage1-mbs8-acc2|MP2-PP4-VPP2-DP2-mbs8-acc2|MP2-PP4-VPP2-SD2-stage1-mbs8-acc2|MP2-SP2-PP4-VPP2-DP2-mbs8-acc2|MP2-SP2-PP4-VPP2-SD2-stage1-mbs8-acc2) 
     echo "run run_mode: ${run_mode}"
         train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --devices=0,1,2,3,4,5,6,7 ${PADDLE_RANK_OPTION}\
             run_pretrain.py ${train_cmd}"
