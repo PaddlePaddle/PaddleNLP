a4dd083b223859bbe495c666106806eb92e0c934
hitywt <yuwentao126@126.com>
2024-01-23 10:24:43 +0800

    update run_gpt_n3.sh

--- model_zoo/gpt-3/run_gpt_n3.sh
+++ model_zoo/gpt-3/run_gpt_n3.sh
@@ -1,6 +1,36 @@
 export NCCL_IB_GID_INDEX=3
 
 export log_dir=log_new
+node_1="yq02-inf-sci-k8s-a100-aa2ni5-0071.yq02.baidu.com"
+node_2="yq01-inf-hic-k8s-a100-aa24-0009.yq01.baidu.com"
+node_3="yq02-inf-sci-k8s-a100-aa2ni5-0065.yq02.baidu.com"
+
+# prepare for ckpt
+target_dir="/ywt01_heter/PaddleNLP/model_zoo/gpt-3/output/"
+
+prepare_ckpt() {
+    hostname=$(hostname)
+    ckpt_dir="output"
+    latest_ckpt_dir=""
+
+    if [ "$hostname" = "$node_1" ]; then
+        dir_list=$(ls "$ckpt_dir")
+        latest_ckpt_dir=$(ls -t "$ckpt_dir" | grep -v "^\.$" | head -1)
+
+        scp -P 8020 -r $node_2:$target_dir/$latest_ckpt_dir output/
+        scp -P 8020 -r $node_2:$target_dir/$latest_ckpt_dir output/
+
+        scp -P 8020 -r $target_dir/$latest_ckpt_dir $node_2:$target_dir
+        scp -P 8020 -r $target_dir/$latest_ckpt_dir $node_3:$target_dir
+
+
+    fi
+    echo $latest_ckpt_dir
+}
+
+latest_ckpt=$(prepare_ckpt)
+
+echo "latest_ckpt", $latest_ckpt
 
 #rm -rf $log_dir
 python -m paddle.distributed.launch --log_dir $log_dir --devices "0,1,2,3,4,5,6,7" \
