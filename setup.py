4e113e12c68909329e70415f51ea7dc040db7b53
Zhong Hui <zhonghui.net@gmail.com>
2024-01-24 14:18:23 +0800

    Release 2.7.2

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ def show():
         f.write(content)
 
 
-__version__ = "2.7.1.post"
+__version__ = "2.7.2.post"
 if os.getenv(PADDLENLP_STABLE_VERSION):
     __version__ = __version__.replace(".post", "")
 
