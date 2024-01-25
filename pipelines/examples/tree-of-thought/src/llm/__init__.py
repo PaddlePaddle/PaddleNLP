e793463c5864def1393dfe6ee4fa3634c9c0d9d3
Ernestina <48557439+ErnestinaQiu@users.noreply.github.com>
2024-01-26 00:14:19 +0800

    Update __init__.py
    add Ernie

--- pipelines/examples/tree-of-thought/src/llm/__init__.py
+++ pipelines/examples/tree-of-thought/src/llm/__init__.py
@@ -14,4 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from src.llm.llama import llamaChatCompletion
+from src.llm.llama import llamaChatCompletion, llm_config
+from src.llm.ernie_bot import Ernie, Ernie_llm_list
