48191b42faaf83afbd51fbd97245a85f439e840f
liujie44 <liujie44@baidu.com>
2024-01-25 16:40:36 +0800

    update years

--- tests/test_tipc/auto_tuner/llama_pretrain/benchmark_common/run_benchmark.sh
+++ tests/test_tipc/auto_tuner/llama_pretrain/benchmark_common/run_benchmark.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
