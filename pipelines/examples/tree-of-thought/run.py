8fc80a314d106aa0fb07b641b55a4981ad7f22d9
Ernestina <48557439+ErnestinaQiu@users.noreply.github.com>
2024-01-26 01:27:09 +0800

    format style

--- pipelines/examples/tree-of-thought/run.py
+++ pipelines/examples/tree-of-thought/run.py
@@ -14,41 +14,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 import argparse
 import json
 import os
+import time
+
+from src.llm.llama import Ernie, Ernie_llm_list, llamaChatCompletion, llm_config
 from src.tot.methods.bfs import naive_solve, solve
 from src.tot.models import gpt_usage
 from src.tot.tasks import get_task
-from src.llm.llama import llm_config, llamaChatCompletion, Ernie_llm_list, Ernie
 
 
 def run(args, chatter):
     task = get_task(args.task)
     logs, cnt_avg, cnt_any = [], 0, 0
     if args.naive_run:
-        file = f'./logs/{args.task}/{args.backend}_{args.temperature}_naive_{args.prompt_sample}_sample_{args.n_generate_sample}_start{args.task_start_index}_end{args.task_end_index}.json'
+        file = f"./logs/{args.task}/{args.backend}_{args.temperature}_naive_{args.prompt_sample}_sample_{args.n_generate_sample}_start{args.task_start_index}_end{args.task_end_index}.json"
         metric_fp = f"./logs/{args.task}/{args.backend}_{args.temperature}_{args.method_select}_{args.n_generate_sample}_start{args.task_start_index}_end{args.task_end_index}_metric.txt"
     else:
-        file = f'./logs/{args.task}/{args.backend}_{args.temperature}_{args.method_generate}{args.n_generate_sample}_{args.method_evaluate}{args.n_evaluate_sample}_{args.method_select}{args.n_select_sample}_start{args.task_start_index}_end{args.task_end_index}.json'
+        file = f"./logs/{args.task}/{args.backend}_{args.temperature}_{args.method_generate}{args.n_generate_sample}_{args.method_evaluate}{args.n_evaluate_sample}_{args.method_select}{args.n_select_sample}_start{args.task_start_index}_end{args.task_end_index}.json"
         metric_fp = f"./logs/{args.task}/{args.backend}_{args.temperature}_{args.method_generate}{args.n_generate_sample}_{args.method_evaluate}{args.n_evaluate_sample}_{args.method_select}{args.n_select_sample}_start{args.task_start_index}_end{args.task_end_index}_metric.txt"
     os.makedirs(os.path.dirname(file), exist_ok=True)
-
 
     for i in range(args.task_start_index, args.task_end_index):
         args.log_fp = f"./logs/{args.task}/{args.backend}_{args.temperature}_{args.prompt_sample}_sample_{args.n_generate_sample}_start{args.task_start_index}_end{args.task_end_index}.log"
         args.query_fp = f"./logs/{args.task}/{args.backend}_{args.temperature}_{args.prompt_sample}_sample_{args.n_generate_sample}_start{args.task_start_index}_end{args.task_end_index}_query.log"
-        f = open(args.log_fp, 'a', encoding='utf8')
+        f = open(args.log_fp, "a", encoding="utf8")
         f.write(f"------ index: {i}")
         f.close()
 
-        f = open(args.query_fp, 'a', encoding='utf8')
+        f = open(args.query_fp, "a", encoding="utf8")
         f.write(f"------ index: {i}")
         f.close()
 
         chatter.query = []
-        chatter.tokenizer.init_chat_template(os.path.join(os.getcwd(), "pipelines", "examples", "tree-of-thought", "src", "llm", "chat_template.json"))
+        chatter.tokenizer.init_chat_template(
+            os.path.join(os.getcwd(), "pipelines", "examples", "tree-of-thought", "src", "llm", "chat_template.json")
+        )
 
         # solve
         if args.naive_run:
@@ -58,34 +60,35 @@ def run(args, chatter):
 
         # log
         infos = [task.test_output(i, y) for y in ys]
-        info.update({'idx': i, 'ys': ys, 'infos': infos, 'usage_so_far': gpt_usage(args.backend)})
+        info.update({"idx": i, "ys": ys, "infos": infos, "usage_so_far": gpt_usage(args.backend)})
         logs.append(info)
-        with open(file, 'w') as f:
+        with open(file, "w") as f:
             json.dump(logs, f, indent=4)
 
         # log main metric
-        accs = [info['r'] for info in infos]
+        accs = [info["r"] for info in infos]
         cnt_avg += sum(accs) / len(accs)
         cnt_any += any(accs)
         mes = f"{i}, 'sum(accs)', {sum(accs)}, 'cnt_avg', {cnt_avg}, 'cnt_any', {cnt_any}, '\n'"
-        f = open(metric_fp, 'a', encoding="utf8")
+        f = open(metric_fp, "a", encoding="utf8")
         f.write(mes)
         f.close()
 
-        f = open(args.query_fp, 'a', encoding="utf8")
+        f = open(args.query_fp, "a", encoding="utf8")
         f.write(json.dumps(chatter.query))
         f.close()
 
     n = args.task_end_index - args.task_start_index
     mes2 = f"cnt_avg / n: {cnt_avg / n}, cnt_any / n: {cnt_any / n}"
     mes3 = f"'usage_so_far', {gpt_usage(args.backend)}"
-    f = open(metric_fp, 'a', encoding="utf8")
+    f = open(metric_fp, "a", encoding="utf8")
     f.write(mes2)
     f.write(mes3)
     f.close()
 
 
 llm_backend_choices = list(llm_config.keys())
+
 
 def parse_args():
     args = argparse.ArgumentParser()
