05929313cf3591c660952df0e1634d6524c048d0
Ernestina <48557439+ErnestinaQiu@users.noreply.github.com>
2024-01-26 01:34:58 +0800

    format style

--- pipelines/examples/tree-of-thought/src/tot/models.py
+++ pipelines/examples/tree-of-thought/src/tot/models.py
@@ -1,5 +1,4 @@
 # coding=utf8, ErnestinaQiu
-
 # Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,20 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-from src.llm import llamaChatCompletion, llm_config, Ernie_llm_list
 import logging
+import os
+
+from src.llm import Ernie_llm_list, llamaChatCompletion, llm_config
 
 completion_tokens = prompt_tokens = 0
 
 
 def completions_with_backoff(**kwargs):
     chatter = kwargs["chatter"]
-    return chatter.create(messages=kwargs["messages"], temperature=kwargs["temperature"], max_gen_len=kwargs["max_tokens"])
+    return chatter.create(
+        messages=kwargs["messages"], temperature=kwargs["temperature"], max_gen_len=kwargs["max_tokens"]
+    )
 
 
 def chatgpt(
-    messages, model="llama-2-7b-chat", temperature=0.6, max_tokens=1000, n=1, stop=None, chatter=None, args=None) -> list:
+    messages, model="llama-2-7b-chat", temperature=0.6, max_tokens=1000, n=1, stop=None, chatter=None, args=None
+) -> list:
     global completion_tokens, prompt_tokens
     if chatter is None:
         chatter = llamaChatCompletion(model="llama-2-7b-chat")
@@ -37,7 +40,7 @@ def chatgpt(
         cnt = min(n, 20)
         n -= cnt
         if model in Ernie_llm_list:
-            one_turn_mes = messages[0]   # one_turn_mes is like [{'role': 'user', 'content': "请问你能以《你好，世界》为题，写一首现代诗吗？"}]
+            one_turn_mes = messages[0]  # one_turn_mes is like [{'role': 'user', 'content': "请问你能以《你好，世界》为题，写一首现代诗吗？"}]
             out_content = chatter.create(model=model, messages=one_turn_mes)
             outputs.append(out_content)  # is like ['content']
             # log completion tokens
@@ -49,9 +52,9 @@ def chatgpt(
             # log completion tokens
             completion_tokens += res["usage"]["completion_tokens"]
             prompt_tokens += res["usage"]["prompt_tokens"]
-    
+
     if args is not None:
-        f = open(args.log_fp, 'a', encoding="utf8")
+        f = open(args.log_fp, "a", encoding="utf8")
         f.write(f"\n [messages]: \n {messages}")
         f.write("\n [outputs]:\n")
         f.write(str(outputs))
@@ -64,7 +67,7 @@ def chatgpt(
         f.write(str(outputs))
         f.close()
     assert len(outputs) == 1, f"len(outputs) == {len(outputs)}, \n outputs"
-    
+
     if model in llm_config.keys():
         outputs = outputs[0]
         return outputs
@@ -75,9 +78,7 @@ def chatgpt(
 def gpt(
     prompt, model="llama-2-7b-chat", temperature=0.6, max_tokens=512, n=1, stop=None, args=None, chatter=None
 ) -> list:
-    messages = [
-        [{"role": "user", "content": prompt}]
-    ]
+    messages = [[{"role": "user", "content": prompt}]]
     return chatgpt(
         messages=messages,
         model=model,
@@ -86,7 +87,7 @@ def gpt(
         n=1,
         stop=stop,
         args=args,
-        chatter=chatter
+        chatter=chatter,
     )
 
 
