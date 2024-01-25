030c97be568ce965887a3e85294e03c22f5d9380
liujie44 <liujie44@baidu.com>
2024-01-25 11:49:09 +0800

    update

--- tests/test_tipc/auto_tuner/llama_finetune/benchmark_common/run_benchmark.sh
+++ tests/test_tipc/auto_tuner/llama_finetune/benchmark_common/run_benchmark.sh
@@ -30,8 +30,8 @@ function _set_params(){
     model_repo="PaddleNLP"          # (必选) 模型套件的名字
     speed_unit="tokens/s"         # (必选)速度指标单位
     skip_steps=0                  # (必选)解析日志，跳过模型前几个性能不稳定的step
-    keyword="ips:"                 # (必选)解析日志，筛选出性能数据所在行的关键字
-    convergence_key="loss:"        # (可选)解析日志，筛选出收敛数据所在行的关键字 如：convergence_key="loss:"
+    keyword="Effective_Tokens_per_second:"                 # (必选)解析日志，筛选出性能数据所在行的关键字
+    convergence_key="train_loss:"        # (可选)解析日志，筛选出收敛数据所在行的关键字 如：convergence_key="loss:"
 
     fp_item="fp16"
     workerlog_id=0
