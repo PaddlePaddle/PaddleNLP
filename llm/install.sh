5139f1e05d31b50c9b189d599f03215b6704416c
Zhong Hui <zhonghui.net@gmail.com>
2024-01-26 14:35:45 +0800

    test for sci benchmark.

---
+++ llm/install.sh
@@ -0,0 +1,19 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+python=python3.10
+
+${python} -m pip install paddlenlp==2.7.1
+${python} -m pip install paddlepaddle_gpu==2.6.0
+${python} -m pip install tool_helpers visualdl==2.5.3
