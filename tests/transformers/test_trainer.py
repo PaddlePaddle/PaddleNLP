1f75bd50f006066da69be5b426dd4db859c25467
DrownFish19 <DrownFish19@gmail.com>
2024-01-28 06:08:32 +0000

    fix keep_optimizer_state_static_keys

--- tests/transformers/test_trainer.py
+++ tests/transformers/test_trainer.py
@@ -132,170 +132,176 @@ class RegressionModelConfig(PretrainedConfig):
         self.hidden_size = 1
 
 
-if is_paddle_available():
+class SampleIterableDataset(IterableDataset):
+    def __init__(self, a=2, b=3, length=64, seed=42, label_names=None):
+        self.dataset = RegressionDataset(a=a, b=b, length=length, seed=seed, label_names=label_names)
 
-    class SampleIterableDataset(IterableDataset):
-        def __init__(self, a=2, b=3, length=64, seed=42, label_names=None):
-            self.dataset = RegressionDataset(a=a, b=b, length=length, seed=seed, label_names=label_names)
+    def __iter__(self):
+        for i in range(len(self.dataset)):
+            yield self.dataset[i]
 
-        def __iter__(self):
-            for i in range(len(self.dataset)):
-                yield self.dataset[i]
 
-    class FiniteIterableDataset(SampleIterableDataset):
-        def __init__(self, a=2, b=3, length=64, seed=42, label_names=None):
-            super().__init__(a, b, length, seed, label_names)
-            self.current_sample = 0
+class FiniteIterableDataset(SampleIterableDataset):
+    def __init__(self, a=2, b=3, length=64, seed=42, label_names=None):
+        super().__init__(a, b, length, seed, label_names)
+        self.current_sample = 0
 
-        def __iter__(self):
-            while self.current_sample < len(self.dataset):
-                yield self.dataset[self.current_sample]
-                self.current_sample += 1
+    def __iter__(self):
+        while self.current_sample < len(self.dataset):
+            yield self.dataset[self.current_sample]
+            self.current_sample += 1
 
-    class MultiLoader:
-        def __init__(self, loaders):
-            self.loaders = loaders
 
-        def __len__(self):
-            return sum(len(loader) for loader in self.loaders)
+class MultiLoader:
+    def __init__(self, loaders):
+        self.loaders = loaders
 
-        def __iter__(self):
-            for loader in self.loaders:
-                yield from loader
+    def __len__(self):
+        return sum(len(loader) for loader in self.loaders)
 
-    class CustomDataloaderTrainer(Trainer):
-        def get_train_dataloader(self):
-            dataloaders = [super().get_train_dataloader(), super().get_train_dataloader()]
-            return MultiLoader(dataloaders)
+    def __iter__(self):
+        for loader in self.loaders:
+            yield from loader
 
-        def get_eval_dataloader(self, eval_dataset):
-            dataloaders = [super().get_eval_dataloader(eval_dataset), super().get_eval_dataloader(eval_dataset)]
-            return MultiLoader(dataloaders)
 
-    class RegressionModel(nn.Layer):
-        def __init__(self, a=0, b=0, double_output=False):
-            super().__init__()
-            self.a = paddle.create_parameter(shape=[], dtype=paddle.float32)
-            self.b = paddle.create_parameter(shape=[], dtype=paddle.float32)
-            self.a.set_value(paddle.to_tensor(a, paddle.float32))
-            self.b.set_value(paddle.to_tensor(b, paddle.float32))
-            self.double_output = double_output
-            self.config = None
+class CustomDataloaderTrainer(Trainer):
+    def get_train_dataloader(self):
+        dataloaders = [super().get_train_dataloader(), super().get_train_dataloader()]
+        return MultiLoader(dataloaders)
 
-        def forward(self, input_x, labels=None, **kwargs):
-            y = input_x * self.a + self.b
-            if labels is None:
-                return (y, y) if self.double_output else (y,)
-            loss = nn.functional.mse_loss(y, labels)
-            return (loss, y, y) if self.double_output else (loss, y)
+    def get_eval_dataloader(self, eval_dataset):
+        dataloaders = [super().get_eval_dataloader(eval_dataset), super().get_eval_dataloader(eval_dataset)]
+        return MultiLoader(dataloaders)
 
-    class RegressionDictModel(nn.Layer):
-        def __init__(self, a=0, b=0):
-            super().__init__()
-            self.a = paddle.create_parameter(shape=[], dtype=paddle.float32)
-            self.b = paddle.create_parameter(shape=[], dtype=paddle.float32)
-            self.a.set_value(paddle.to_tensor(a, paddle.float32))
-            self.b.set_value(paddle.to_tensor(b, paddle.float32))
-            self.config = None
 
-        def forward(self, input_x, labels=None, **kwargs):
-            y = input_x * self.a + self.b
-            result = {"output": y}
-            if labels is not None:
-                result["loss"] = nn.functional.mse_loss(y, labels)
-            return result
+class RegressionModel(nn.Layer):
+    def __init__(self, a=0, b=0, double_output=False):
+        super().__init__()
+        self.a = paddle.create_parameter(shape=[], dtype=paddle.float32)
+        self.b = paddle.create_parameter(shape=[], dtype=paddle.float32)
+        self.a.set_value(paddle.to_tensor(a, paddle.float32))
+        self.b.set_value(paddle.to_tensor(b, paddle.float32))
+        self.double_output = double_output
+        self.config = None
 
-    class RegressionPretrainedModel(PretrainedModel):
-        config_class = RegressionModelConfig
-        base_model_prefix = "regression"
+    def forward(self, input_x, labels=None, **kwargs):
+        y = input_x * self.a + self.b
+        if labels is None:
+            return (y, y) if self.double_output else (y,)
+        loss = nn.functional.mse_loss(y, labels)
+        return (loss, y, y) if self.double_output else (loss, y)
 
-        def __init__(self, config):
-            super().__init__(config)
-            self.a = paddle.create_parameter(shape=[], dtype=paddle.float32)
-            self.b = paddle.create_parameter(shape=[], dtype=paddle.float32)
-            self.a.set_value(paddle.to_tensor(config.a, paddle.float32))
-            self.b.set_value(paddle.to_tensor(config.b, paddle.float32))
-            self.double_output = config.double_output
 
-        def forward(self, input_x, labels=None, **kwargs):
-            y = input_x * self.a + self.b
-            if labels is None:
-                return (y, y) if self.double_output else (y,)
-            loss = nn.functional.mse_loss(y, labels)
-            return (loss, y, y) if self.double_output else (loss, y)
+class RegressionDictModel(nn.Layer):
+    def __init__(self, a=0, b=0):
+        super().__init__()
+        self.a = paddle.create_parameter(shape=[], dtype=paddle.float32)
+        self.b = paddle.create_parameter(shape=[], dtype=paddle.float32)
+        self.a.set_value(paddle.to_tensor(a, paddle.float32))
+        self.b.set_value(paddle.to_tensor(b, paddle.float32))
+        self.config = None
 
-    class RegressionRandomPretrainedModel(PretrainedModel):
-        config_class = RegressionModelConfig
-        base_model_prefix = "regression"
+    def forward(self, input_x, labels=None, **kwargs):
+        y = input_x * self.a + self.b
+        result = {"output": y}
+        if labels is not None:
+            result["loss"] = nn.functional.mse_loss(y, labels)
+        return result
 
-        def __init__(self, config):
-            super().__init__(config)
-            self.a = paddle.create_parameter(shape=[], dtype=paddle.float32)
-            self.b = paddle.create_parameter(shape=[], dtype=paddle.float32)
-            self.a.set_value(paddle.to_tensor(config.a, paddle.float32))
-            self.b.set_value(paddle.to_tensor(config.b, paddle.float32))
-            self.random_paddle = config.random_paddle
 
-        def forward(self, input_x, labels=None, **kwargs):
-            y = input_x * self.a + self.b
-            if self.random_paddle:
-                paddle_rand = paddle.randn(1).item()
-            np_rand = np.random.rand()
-            rand_rand = random.random()
+class RegressionPretrainedModel(PretrainedModel):
+    config_class = RegressionModelConfig
+    base_model_prefix = "regression"
 
-            if self.random_paddle:
-                y += 0.05 * paddle_rand
-            y += 0.05 * paddle.to_tensor(np_rand + rand_rand)
+    def __init__(self, config):
+        super().__init__(config)
+        self.a = paddle.create_parameter(shape=[], dtype=paddle.float32)
+        self.b = paddle.create_parameter(shape=[], dtype=paddle.float32)
+        self.a.set_value(paddle.to_tensor(config.a, paddle.float32))
+        self.b.set_value(paddle.to_tensor(config.b, paddle.float32))
+        self.double_output = config.double_output
 
-            if labels is None:
-                return (y,)
-            loss = nn.functional.mse_loss(y, labels)
-            return (loss, y)
+    def forward(self, input_x, labels=None, **kwargs):
+        y = input_x * self.a + self.b
+        if labels is None:
+            return (y, y) if self.double_output else (y,)
+        loss = nn.functional.mse_loss(y, labels)
+        return (loss, y, y) if self.double_output else (loss, y)
 
-    class TstLayer(nn.Layer):
-        def __init__(self, hidden_size):
-            super().__init__()
-            self.linear1 = nn.Linear(hidden_size, hidden_size)
-            self.ln1 = nn.LayerNorm(hidden_size)
-            self.linear2 = nn.Linear(hidden_size, hidden_size)
-            self.ln2 = nn.LayerNorm(hidden_size)
-            self.bias = paddle.create_parameter(shape=hidden_size.shape)
-            self.bias.set_value(paddle.zeros(hidden_size))
 
-        def forward(self, x):
-            h = self.ln1(nn.functional.relu(self.linear1(x)))
-            h = nn.functional.relu(self.linear2(x))
-            return self.ln2(x + h + self.bias)
+class RegressionRandomPretrainedModel(PretrainedModel):
+    config_class = RegressionModelConfig
+    base_model_prefix = "regression"
 
-    def get_regression_trainer(a=0, b=0, double_output=False, train_len=64, eval_len=64, pretrained=True, **kwargs):
-        label_names = kwargs.get("label_names", None)
-        train_dataset = RegressionDataset(length=train_len, label_names=label_names)
-        eval_dataset = RegressionDataset(length=eval_len, label_names=label_names)
+    def __init__(self, config):
+        super().__init__(config)
+        self.a = paddle.create_parameter(shape=[], dtype=paddle.float32)
+        self.b = paddle.create_parameter(shape=[], dtype=paddle.float32)
+        self.a.set_value(paddle.to_tensor(config.a, paddle.float32))
+        self.b.set_value(paddle.to_tensor(config.b, paddle.float32))
+        self.random_paddle = config.random_paddle
 
-        if pretrained:
-            config = RegressionModelConfig(a=a, b=b, double_output=double_output)
-            model = RegressionPretrainedModel(config)
-        else:
-            model = RegressionModel(a=a, b=b, double_output=double_output)
+    def forward(self, input_x, labels=None, **kwargs):
+        y = input_x * self.a + self.b
+        if self.random_paddle:
+            paddle_rand = paddle.randn(1).item()
+        np_rand = np.random.rand()
+        rand_rand = random.random()
 
-        compute_metrics = kwargs.pop("compute_metrics", None)
-        data_collator = kwargs.pop("data_collator", None)
-        optimizers = kwargs.pop("optimizers", (None, None))
-        output_dir = kwargs.pop("output_dir", "./regression")
-        preprocess_logits_for_metrics = kwargs.pop("preprocess_logits_for_metrics", None)
-        kwargs["keep_optimizer_state_static_keys"] = False
+        if self.random_paddle:
+            y += 0.05 * paddle_rand
+        y += 0.05 * paddle.to_tensor(np_rand + rand_rand)
 
-        args = RegressionTrainingArguments(output_dir, a=a, b=b, **kwargs)
-        return Trainer(
-            model,
-            args=args,
-            data_collator=data_collator,
-            train_dataset=train_dataset,
-            eval_dataset=eval_dataset,
-            compute_metrics=compute_metrics,
-            optimizers=optimizers,
-            preprocess_logits_for_metrics=preprocess_logits_for_metrics,
-        )
+        if labels is None:
+            return (y,)
+        loss = nn.functional.mse_loss(y, labels)
+        return (loss, y)
+
+
+class TstLayer(nn.Layer):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.linear1 = nn.Linear(hidden_size, hidden_size)
+        self.ln1 = nn.LayerNorm(hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.ln2 = nn.LayerNorm(hidden_size)
+        self.bias = paddle.create_parameter(shape=hidden_size.shape)
+        self.bias.set_value(paddle.zeros(hidden_size))
+
+    def forward(self, x):
+        h = self.ln1(nn.functional.relu(self.linear1(x)))
+        h = nn.functional.relu(self.linear2(x))
+        return self.ln2(x + h + self.bias)
+
+
+def get_regression_trainer(a=0, b=0, double_output=False, train_len=64, eval_len=64, pretrained=True, **kwargs):
+    label_names = kwargs.get("label_names", None)
+    train_dataset = RegressionDataset(length=train_len, label_names=label_names)
+    eval_dataset = RegressionDataset(length=eval_len, label_names=label_names)
+
+    if pretrained:
+        config = RegressionModelConfig(a=a, b=b, double_output=double_output)
+        model = RegressionPretrainedModel(config)
+    else:
+        model = RegressionModel(a=a, b=b, double_output=double_output)
+
+    compute_metrics = kwargs.pop("compute_metrics", None)
+    data_collator = kwargs.pop("data_collator", None)
+    optimizers = kwargs.pop("optimizers", (None, None))
+    output_dir = kwargs.pop("output_dir", "./regression")
+    preprocess_logits_for_metrics = kwargs.pop("preprocess_logits_for_metrics", None)
+
+    args = RegressionTrainingArguments(output_dir, a=a, b=b, **kwargs)
+    return Trainer(
+        model,
+        args=args,
+        data_collator=data_collator,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        compute_metrics=compute_metrics,
+        optimizers=optimizers,
+        preprocess_logits_for_metrics=preprocess_logits_for_metrics,
+    )
 
 
 class TrainerIntegrationCommon:
