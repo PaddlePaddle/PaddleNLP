2165d4b2f6e8a79871124b95610635dcf0c07b55
Southpika <513923576@qq.com>
2024-03-11 19:07:50 +0800

    rm useless notes

--- paddlenlp/transformers/gemma/configuration.py
+++ paddlenlp/transformers/gemma/configuration.py
@@ -47,8 +47,8 @@ GEMMA_PRETRAINED_INIT_CONFIGURATION = {
 # Hypothetical model weights (tiny-random-llama) for test only
 GEMMA_PRETRAINED_RESOURCE_FILES_MAP = {
     "model_state": {
-        "google/gemma-7b": "https://bj.bcebos.com/paddlenlp/models/community/__internal_testing__/micro-random-llama/model_state.pdparams",
-        "__internal_testing__/tiny-random-llama": "https://bj.bcebos.com/paddlenlp/models/community/__internal_testing__/tiny-random-llama/model_state.pdparams",
+        "google/gemma-7b": "https://bj.bcebos.com/paddlenlp/models/community/google/gemma-7b/model_state.pdparams",
+        "google/gemma-2b": "https://bj.bcebos.com/paddlenlp/models/community/google/gemma-2b/model_state.pdparams",
     },
 }
 
@@ -193,103 +193,3 @@ class GemmaConfig(PretrainedConfig):
     @property
     def rope(self):
         return not self.alibi
-
-
-###
-# class LLAMAConfig(PretrainedConfig):
-#     model_type = "llama"
-#     attribute_map = {
-#         "n_positions": "max_position_embeddings",
-#         "n_embd": "hidden_size",
-#         "n_layer": "num_hidden_layers",
-#         "n_head": "num_attention_heads",
-#         "n_inner": "intermediate_size",
-#         "activation_function": "hidden_act",
-#     }
-#     pretrained_init_configuration = LLAMA_PRETRAINED_INIT_CONFIGURATION
-
-#     def __init__(
-#         self,
-#         vocab_size=32000,
-#         hidden_size=4096,
-#         intermediate_size=11008,
-#         max_position_embeddings=2048,
-#         seq_length=2048,
-#         num_hidden_layers=32,
-#         num_attention_heads=32,
-#         num_key_value_heads=None,
-#         initializer_range=0.02,
-#         rms_norm_eps=1e-6,
-#         use_cache=True,
-#         use_recompute=False,
-#         recompute_granularity="full",
-#         pp_recompute_interval=1,
-#         no_recompute_layers=None,
-#         fuse_attention_qkv=False,
-#         fuse_attention_ffn=False,
-#         use_flash_attention=False,
-#         use_fused_rms_norm=False,
-#         use_fused_rope=False,
-#         tensor_parallel_output=True,
-#         sequence_parallel=False,
-#         fuse_sequence_parallel_allreduce=False,
-#         virtual_pp_degree=1,
-#         pad_token_id=0,
-#         bos_token_id=1,
-#         eos_token_id=2,
-#         tie_word_embeddings=False,
-#         alibi=False,
-#         rope_scaling_factor=1.0,
-#         rope_scaling_type=None,
-#         **kwargs,
-#     ):
-#         self.vocab_size = vocab_size
-#         self.hidden_size = hidden_size
-#         self.intermediate_size = intermediate_size
-#         self.max_position_embeddings = max_position_embeddings
-#         self.seq_length = seq_length
-#         self.num_hidden_layers = num_hidden_layers
-#         self.num_attention_heads = num_attention_heads
-
-#         if num_key_value_heads is None:
-#             num_key_value_heads = num_attention_heads
-#         self.num_key_value_heads = num_key_value_heads
-
-#         self.initializer_range = initializer_range
-#         self.rms_norm_eps = rms_norm_eps
-
-#         self.use_cache = use_cache
-#         self.use_recompute = use_recompute
-#         self.recompute_granularity = recompute_granularity
-#         self.no_recompute_layers = no_recompute_layers
-#         self.pp_recompute_interval = pp_recompute_interval
-#         self.fuse_attention_qkv = fuse_attention_qkv
-#         self.use_flash_attention = use_flash_attention
-#         self.fuse_attention_ffn = fuse_attention_ffn
-#         self.use_fused_rms_norm = use_fused_rms_norm
-#         self.tensor_parallel_output = tensor_parallel_output
-#         self.sequence_parallel = sequence_parallel
-#         self.fuse_sequence_parallel_allreduce = fuse_sequence_parallel_allreduce
-#         self.virtual_pp_degree = virtual_pp_degree
-
-#         self.pad_token_id = pad_token_id
-#         self.bos_token_id = bos_token_id
-#         self.eos_token_id = eos_token_id
-#         self.alibi = alibi
-
-#         self.use_fused_rope = use_fused_rope
-#         self.rope_scaling_factor = rope_scaling_factor
-#         self.rope_scaling_type = rope_scaling_type
-
-#         super().__init__(
-#             pad_token_id=pad_token_id,
-#             bos_token_id=bos_token_id,
-#             eos_token_id=eos_token_id,
-#             tie_word_embeddings=tie_word_embeddings,
-#             tensor_parallel_output=tensor_parallel_output,
-#             **kwargs,
-#         )
-
-#     @property
-#     def rope(self):
-#         return not self.alibi
