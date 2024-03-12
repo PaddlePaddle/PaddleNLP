c38c85cd1a07cc3a27f083f5c1148058b2b4df34
Southpika <513923576@qq.com>
2024-03-12 11:36:59 +0800

    fix des

--- paddlenlp/transformers/gemma/configuration.py
+++ paddlenlp/transformers/gemma/configuration.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Llama model configuration"""
+""" Gemma model configuration"""
 
 from paddlenlp.transformers.configuration_utils import PretrainedConfig
 
@@ -44,7 +44,7 @@ GEMMA_PRETRAINED_INIT_CONFIGURATION = {
     },
 }
 
-# Hypothetical model weights (tiny-random-llama) for test only
+
 GEMMA_PRETRAINED_RESOURCE_FILES_MAP = {
     "model_state": {
         "google/gemma-7b": "https://bj.bcebos.com/paddlenlp/models/community/google/gemma-7b/model_state.pdparams",
@@ -55,15 +55,15 @@ GEMMA_PRETRAINED_RESOURCE_FILES_MAP = {
 
 class GemmaConfig(PretrainedConfig):
     r"""
-    This is the configuration class to store the configuration of a [`~LlamaModel`]. It is used to instantiate an Llama
+    This is the configuration class to store the configuration of a [`~GemmaModel`]. It is used to instantiate a gemma
     model according to the specified arguments, defining the model architecture. Instantiating a configuration with the
-    defaults will yield a similar configuration to that of the Llama-7B.
+    defaults will yield a similar configuration to that of the Gemma-7B.
     Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
     documentation from [`PretrainedConfig`] for more information.
     Args:
         vocab_size (`int`, *optional*, defaults to 32000):
-            Vocabulary size of the Llama model. Defines the number of different tokens that can be represented by the
-            `inputs_ids` passed when calling [`~LlamaModel`] or [`~TFLlamaModel`].
+            Vocabulary size of the Gemma model. Defines the number of different tokens that can be represented by the
+            `inputs_ids` passed when calling [`~GemmaModel`]
         hidden_size (`int`, *optional*, defaults to 4096):
             Dimension of the hidden representations.
         intermediate_size (`int`, *optional*, defaults to 11008):
@@ -95,13 +95,13 @@ class GemmaConfig(PretrainedConfig):
             `num_attention_heads`.
         Example:
     ```python
-    >>> from paddlenlp.transformer import LlamaModel, LlamaConfig
+    >>> from paddlenlp.transformer import GemmaModel, GemmaModel
 
-    >>> # Initializing a Llama llama-7b style configuration
-    >>> configuration = LlamaConfig()
+    >>> # Initializing a Gemma gemma-7b style configuration
+    >>> configuration = GemmaModel()
 
-    >>> # Initializing a model from the llama-7b style configuration
-    >>> model = LlamaModel(configuration)
+    >>> # Initializing a model from the gemma-7b style configuration
+    >>> model = GemmaModel(configuration)
 
     >>> # Accessing the model configuration
     >>> configuration = model.config
