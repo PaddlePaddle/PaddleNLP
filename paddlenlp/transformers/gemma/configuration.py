325e5f5cfd99429928a7ce5da961f00d8aece344
Southpika <513923576@qq.com>
2024-03-12 11:09:38 +0800

    fix version

--- paddlenlp/transformers/gemma/configuration.py
+++ paddlenlp/transformers/gemma/configuration.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
-# Copyright 2022 EleutherAI and the HuggingFace Inc. team. All rights reserved.
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+# Copyright 2024 EleutherAI and the HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
