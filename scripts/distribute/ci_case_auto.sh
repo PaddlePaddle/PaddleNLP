d1d374fc280e48e84a71696caec96e57f1e23abf
liujie44 <liujie44@baidu.com>
2024-04-01 09:57:14 +0800

    update ips_base

--- scripts/distribute/ci_case_auto.sh
+++ scripts/distribute/ci_case_auto.sh
@@ -1508,7 +1508,7 @@ function llama_dygraph_auto_bs8_fp16_DP2-MP2-PP2() {
     mem=`cat $case_log_dir/workerlog.0 | grep 'global_step: 10' | awk -F 'max_memory_reserved: ' '{print $2}' | awk -F ',' '{print $1}'`
     echo "result: loss=$loss ips=$ips mem=$mem"
     loss_base=9.38256836
-    ips_base=13.6743
+    ips_base=11.9226
     mem_base=3347
     check_result $FUNCNAME ${loss_base} ${loss} ${ips_base} ${ips} ${mem_base} ${mem}
     echo "=========== $FUNCNAME run  end ==========="
