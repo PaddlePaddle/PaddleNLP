2a985e88ac8a0e7c9ef6fefaa8c33d722c9cf787
Wanglongzhi2001 <583087864@qq.com>
2024-04-19 16:57:53 +0800

    fix typo

--- paddlenlp/experimental/transformers/llama/modeling.py
+++ paddlenlp/experimental/transformers/llama/modeling.py
@@ -1173,12 +1173,10 @@ class LlamaForCausalLMInferenceModel(GenerationInferenceModel, LlamaPretrainedMo
         self.lm_head = LlamaLMHead(config)
 
     @classmethod
-    def from_pretrained(
-        cls, pretrained_model_name_or_path, from_hf_hub: bool = False, subfolder: str | None = None, *args, **kwargs
-    ):
+    def from_pretrained(cls, pretrained_model_name_or_path, *args, **kwargs):
         # TODO: Support safetensors loading.
         kwargs["use_safetensors"] = False
-        return super().from_pretrained(pretrained_model_name_or_path, from_hf_hub, subfolder, *args, **kwargs)
+        return super().from_pretrained(pretrained_model_name_or_path, *args, **kwargs)
 
     @classmethod
     def get_cache_kvs_shape(
