471e143fdb2c5c22cb78361642dcaa85f9d24434
DrownFish19 <DrownFish19@gmail.com>
2024-04-25 16:11:39 +0800

    [LLM] Support fuse attention q, k, v weights  (#8202)
    1. add use-interface & fuse action
    
    1.1. modify 1., code order
    
    2. switch to name_mapping
    
    3. solve tp branch
    
    3.2 follow hui, handel qkv separately
    
    3.3 handle pdparams
    
    3.4 from torch
    
    3.5 abandon low_cpu_mem_usage
    
    3.6 solve shard branch
    
    * 3.6.1 solve shard branch after rebase develop
    
    * code clean
    
    * remove debug comment
    
    * Redefine fuse and split functions
    
    * Redefine fuse and split functions
    
    * comment and fix
    
    * update method
    
    * update QKV fuse and split
    
    * support fuse weights in multi-files
    
    * add precision compare
    
    * simplify function call
    
    * support use_fast_ffn
    
    * clean modeling and configuration
    
    * add test for gpt and opt
    
    * fix tp_actions get
    
    * add fast_ffn test
    
    * add Qwen2Moe
    
    * Revert "add Qwen2Moe"
    
    This reverts commit 113b8838a7c53f1d131928c30bf1071dfa583445.
    
    * add test for split
    
    * update doc
    
    * update filter_dict_keys
    
    ---------
    
    Co-authored-by: Zii <ziangqin.baidu@gmail.com>

--- paddlenlp/transformers/opt/modeling.py
+++ paddlenlp/transformers/opt/modeling.py
@@ -650,6 +650,49 @@ class OPTPretrainedModel(PretrainedModel):
         return actions
 
     @classmethod
+    def _get_fuse_or_split_param_mappings(cls, config: OPTConfig, is_fuse=False):
+        # return parameter fuse utils
+        from paddlenlp.transformers.conversion_utils import split_or_fuse_func
+
+        fn = split_or_fuse_func(is_fuse=is_fuse)
+
+        # last key is fused key, other keys are to be fused.
+        fuse_qkv_keys = (
+            "decoder.layers.0.self_attn.q_proj.weight",
+            "decoder.layers.0.self_attn.k_proj.weight",
+            "decoder.layers.0.self_attn.v_proj.weight",
+            "decoder.layers.0.self_attn.qkv_proj.weight",
+        )
+        fuse_qkv_bias_keys = (
+            "decoder.layers.0.self_attn.q_proj.bias",
+            "decoder.layers.0.self_attn.k_proj.bias",
+            "decoder.layers.0.self_attn.v_proj.bias",
+            "decoder.layers.0.self_attn.qkv_proj.bias",
+        )
+        num_heads = config.num_attention_heads
+        num_key_value_heads = getattr(config, "num_key_value_heads", num_heads)
+        fuse_attention_qkv = getattr(config, "fuse_attention_qkv", False)
+
+        final_actions = {}
+        if is_fuse:
+            if fuse_attention_qkv:
+                for i in range(config.num_hidden_layers):
+                    for keys in [fuse_qkv_keys, fuse_qkv_bias_keys]:
+                        new_keys = tuple([key.replace("layers.0.", f"layers.{i}.") for key in keys])
+                        final_actions[new_keys] = partial(
+                            fn, is_qkv=True, num_heads=num_heads, num_key_value_heads=num_key_value_heads
+                        )
+        else:
+            if not fuse_attention_qkv:
+                for i in range(config.num_hidden_layers):
+                    for keys in [fuse_qkv_keys, fuse_qkv_bias_keys]:
+                        new_keys = tuple([key.replace("layers.0.", f"layers.{i}.") for key in keys])
+                        final_actions[new_keys] = partial(
+                            fn, split_nums=3, is_qkv=True, num_heads=num_heads, num_key_value_heads=num_key_value_heads
+                        )
+        return final_actions
+
+    @classmethod
     def _get_name_mappings(cls, config: OPTConfig) -> list[StateDictNameMapping]:
         mappings: list[StateDictNameMapping] = []
         model_mappings = [
