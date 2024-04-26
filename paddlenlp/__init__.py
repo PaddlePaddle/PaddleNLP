a37f53d6e04cfa89a7aa765c8a36cded57f8182b
w5688414 <w5688414@gmail.com>
2024-04-26 16:00:41 +0800

    cherry pick paddlenlp 2.8 (#8323)
    * cherry pick paddlenlp 2.8
    
    * Update README

--- paddlenlp/__init__.py
+++ paddlenlp/__init__.py
@@ -18,7 +18,7 @@ import sys
 PADDLENLP_STABLE_VERSION = "PADDLENLP_STABLE_VERSION"
 
 
-__version__ = "2.7.1.post"
+__version__ = "2.8.0.post"
 if os.getenv(PADDLENLP_STABLE_VERSION):
     __version__ = __version__.replace(".post", "")
 
