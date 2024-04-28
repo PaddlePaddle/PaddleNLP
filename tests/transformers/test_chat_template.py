5993027c5a2252ec6e015b2a4ad3705d2d8f6ad3
Southpika <513923576@qq.com>
2024-04-28 17:14:54 +0800

    add multi round chat ut

--- tests/transformers/test_chat_template.py
+++ tests/transformers/test_chat_template.py
@@ -354,3 +354,27 @@ class TemplateIntegrationTest(unittest.TestCase):
         )
         self.assertEqual(tgt_id[tgt_idx - 1], -100)
         self.assertNotEqual(tgt_id[tgt_idx], -100)
+
+    def test_train_format_multi(self):
+        from data import tokenize_rounds_example
+
+        fake_data_args = self.DataArg(50, src_length=50)
+        example = {"src": ["用户Round 1", "用户Round 2"], "tgt": ["回答Round 1", "回答Round 2"]}
+        result, tgt_id = tokenize_rounds_example(self.tokenizer, example, fake_data_args, add_generation_prompt=True)
+
+        tgt_idx_1 = len(
+            self.tokenizer.encode(
+                "<|im_start|>system\nYou are a helpful assistant<|im_end|>\n<|im_start|>user\n用户Round 1<|im_end|>\n<|im_start|>assistant\n"
+            )["input_ids"]
+        )
+        tgt_idx_2 = len(
+            self.tokenizer.encode(
+                "<|im_start|>system\nYou are a helpful assistant<|im_end|>\n<|im_start|>user\n用户Round 1<|im_end|>\n<|im_start|>assistant\n"
+                "回答Round 1<|im_end|>\n<|im_start|>user\n用户Round 2<|im_end|>\n<|im_start|>assistant\n"
+            )["input_ids"]
+        )
+
+        self.assertEqual(tgt_id[tgt_idx_1 - 1], -100)
+        self.assertNotEqual(tgt_id[tgt_idx_1], -100)
+        self.assertEqual(tgt_id[tgt_idx_2 - 1], -100)
+        self.assertNotEqual(tgt_id[tgt_idx_2], -100)
