37877eec89eac61fc769361e8bdde5634718e57b
liangjianzhong <liangjianzhong@baidu.com>
2024-05-10 19:53:27 +0800

    mp2 script

--- llm/llama/auto_parallel/run_pretrain_auto_pir.sh
+++ llm/llama/auto_parallel/run_pretrain_auto_pir.sh
@@ -17,7 +17,7 @@
 set -x
 unset CUDA_VISIBLE_DEVICES
 
-task_name="llama_auto_dp2mp2pp2"
+task_name="llama_auto_mp2"
 rm -rf output/$task_name/
 rm -rf "output/$task_name""_log"
 
@@ -33,11 +33,12 @@ export PYTHONPATH=../../../:$PYTHONPATH
 # export FLAGS_cudnn_deterministic=1
 # export NVIDIA_TF32_OVERRIDE=0
 
-to_static=0  # 是否开启动转静训练
+to_static=1  # 是否开启动转静训练
+export FLAGS_enable_pir_api=1
 
 python -u  -m paddle.distributed.launch \
-    --gpus "0,1,2,3,4,5,6,7" \
-    --log_dir "auto_3d" \
+    --gpus "0,1" \
+    --log_dir "log_pir_mp2" \
     run_pretrain_auto.py \
     --model_type "llama" \
     --model_name_or_path "facebook/llama-7b" \
@@ -47,19 +48,19 @@ python -u  -m paddle.distributed.launch \
     --split 949,50,1 \
     --max_seq_length 2048 \
     --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 2 \
-    --gradient_accumulation_steps 2 \
+    --per_device_eval_batch_size 0 \
+    --gradient_accumulation_steps 1 \
     --use_flash_attention 0 \
-    --use_fused_rms_norm 1 \
+    --use_fused_rms_norm 0 \
     --fp16 0 \
     --fp16_opt_level "O2"  \
     --scale_loss 1024 \
-    --pipeline_parallel_degree 2 \
+    --pipeline_parallel_degree 1 \
     --tensor_parallel_degree 2 \
     --sharding_parallel_degree 1 \
     --learning_rate 0.0001 \
     --min_learning_rate 0.00001 \
-    --max_steps 20000 \
+    --max_steps 10 \
     --save_steps 5000000 \
     --weight_decay 0.01 \
     --warmup_ratio 0.01 \
@@ -71,9 +72,12 @@ python -u  -m paddle.distributed.launch \
     --continue_training 0\
     --recompute 0 \
     --do_train \
-    --do_eval \
+    --do_eval 0\
     --device "gpu" \
     --data_impl "mmap" \
     --enable_auto_parallel 1 \
     --max_grad_norm 1.0 \
     --to_static $to_static \
+    --num_hidden_layers 2 \
+    --hidden_size 1024 \
+    --intermediate_size 4096 \
