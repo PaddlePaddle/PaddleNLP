b16c691234853d21b59888b3245718e4ceeb8a34
heavyrain_lzy <1528794076@qq.com>
2024-05-24 14:57:48 +0800

    change the config

--- scripts/distribute/ci_case_auto.sh
+++ scripts/distribute/ci_case_auto.sh
@@ -1835,11 +1835,10 @@ function llama_dy2st_auto_bs4_bf16_DP1-MP1-PP4-SD2-VPP3_split_bw() {
         --tensor_parallel_degree 1 \
         --virtual_pp_degree 3 \
         --pipeline_schedule_mode "VPP" \
-        --split_backward true \
         --sharding "stage2" \
         --pipeline_parallel_config "enable_send_recv_overlap" \
         --data_parallel_config "enable_allreduce_avg_in_gradinent_scale gradient_sync_after_accumulate" \
-        --sharding_parallel_config "enable_stage2_overlap" \
+        --sharding_parallel_config "enable_stage2_overlap enable_split_backward" \
         --tensor_parallel_config "enable_mp_async_allreduce" \
         --to_static 1 \
         --amp_custom_black_list "reduce_sum" "c_softmax_with_cross_entropy" \
