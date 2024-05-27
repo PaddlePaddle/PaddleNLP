00149e44a1e5a8267324c6530ae78aeac8a2be02
andsonder <changlu@keter.top>
2024-05-27 09:38:19 +0000

    update

--- paddlenlp/trainer/auto_trainer.py
+++ paddlenlp/trainer/auto_trainer.py
@@ -22,7 +22,7 @@ import paddle
 import paddle.distributed as dist
 import paddle.nn as nn
 from paddle.distributed import fleet
-from paddle.profiler.utils import job_schedule_profiler_range
+from paddle.profiler.utils import switch_job_schedule_profiler
 from tqdm.auto import tqdm
 
 from paddlenlp.trainer import Trainer
@@ -300,8 +300,7 @@ class AutoTrainer(Trainer):
                 if self.args.to_static:
                     schedule_start_step = self.args.get("job_schedule_profiler_start", -1)
                     schedule_end_step = self.args.get("job_schedule_profiler_end", -1)
-                    with job_schedule_profiler_range(step, schedule_start_step, schedule_end_step) as status:
-                        model.set_job_schedule_profiler_status(status)
+                    switch_job_schedule_profiler(model, step, schedule_start_step, schedule_end_step)
 
                 for inputs in inputs_list:
                     if step_control % args.gradient_accumulation_steps == 0:
