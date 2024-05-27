accf02e3946d168b8db1da39883066e84cf4a0f5
zhengzhonghui <zhengzhonghui@baidu.com>
2024-05-27 13:31:01 +0800

    update

--- llm/run_pretrain.py
+++ llm/run_pretrain.py
@@ -49,7 +49,7 @@ from paddlenlp.utils.log import logger
 from paddlenlp.utils.tools import get_env_device
 
 # Pretaining Environment Variables to support sharding stage1 overlap optimization.
-os.environ['USE_CASUAL_MASK'] = True
+os.environ['USE_CASUAL_MASK'] = 'True'
 
 def add_start_docstrings(*docstr):
     def docstring_decorator(fn):
