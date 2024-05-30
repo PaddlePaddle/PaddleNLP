8372c95103b52cecff22dc89151af8201499167d
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2024-05-30 18:22:37 +0800

    Update merge_lora_params.py

--- llm/merge_lora_params.py
+++ llm/merge_lora_params.py
@@ -125,7 +125,7 @@ def merge():
         model = AutoModelForCausalLM.from_pretrained(
             lora_config.base_model_name_or_path,
             config=config,
-            low_cpu_mem_usage=True,
+            low_cpu_mem_usage=False,
         )
         lora_config.merge_weights = True
         model = LoRAModel.from_pretrained(model=model, lora_path=args.lora_path, lora_config=lora_config)
