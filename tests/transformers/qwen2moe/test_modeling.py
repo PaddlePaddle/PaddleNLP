8d9970b3df8ba2a512ea3595f3828806d7760aa3
DrownFish19 <DrownFish19@gmail.com>
2024-05-31 07:33:42 +0000

    update Copyright

--- tests/transformers/qwen2moe/test_modeling.py
+++ tests/transformers/qwen2moe/test_modeling.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 # Copyright 2020 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
