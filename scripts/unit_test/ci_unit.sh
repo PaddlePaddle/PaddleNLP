ebccc1eb8e2dac9047a96d5c82fd3d9fd54ef6d8
Zhong Hui <zhonghui.net@gmail.com>
2024-06-11 11:13:28 +0800

    Update ci_unit.sh

--- scripts/unit_test/ci_unit.sh
+++ scripts/unit_test/ci_unit.sh
@@ -52,6 +52,7 @@ set_env() {
 install_requirements
 set_env
 pytest -v -n 8 \
+  --dist loadgroup \
   --retries 1 --retry-delay 1 \
   --timeout 200 --durations 20 \
   --cov paddlenlp --cov-report xml:coverage.xml
