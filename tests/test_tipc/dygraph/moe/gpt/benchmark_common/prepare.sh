726bfd2790a2aed5b1498144acc9f530b0a328f2
fightfat <18510030324@126.com>
2024-06-28 02:43:38 +0000

    fix benchmark dir and add CUDA_DEVICE_MAX_CONNECTIONS to qwen

--- tests/test_tipc/dygraph/moe/gpt/benchmark_common/prepare.sh
+++ tests/test_tipc/dygraph/moe/gpt/benchmark_common/prepare.sh
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 cd ../legacy/model_zoo/moe/data_tools/
 sed -i "s/python3/python3.10/g" Makefile
 cd -
