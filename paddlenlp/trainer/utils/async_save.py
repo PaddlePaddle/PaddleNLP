bf1629f650e9ae6da674141ee9207959117af3fb
ForFishes <2282912238@qq.com>
2024-08-05 16:22:27 +0800

    add async save

--- paddlenlp/trainer/utils/async_save.py
+++ paddlenlp/trainer/utils/async_save.py
@@ -20,56 +20,34 @@ import paddle
 from paddlenlp.utils.log import logger
 
 
+def _save_optimizer(obj, name_mapping, path, saved_signal_path, protocol):
+    for k, v in obj.items():
+        if k == "master_weights" and isinstance(v, dict):
+            for kk, vv in v.items():
+                if isinstance(vv, paddle.Tensor):
+                    vv.name = name_mapping["master_weights"][kk]
+        else:
+            if k in name_mapping and isinstance(v, paddle.Tensor):
+                v.name = name_mapping[k]
+    logger.info(f"====save {path}===")
+    paddle.save(obj, path, protocol)
+    # dump savd_siganl
+    with open(saved_signal_path, mode="w+") as f:
+        f.write("1")
+        f.flush()
+        os.fsync(f.fileno())
+
+
 class AsyncSaver:
     def __init__(self):
         self.context = multiprocessing.get_context("spawn")
         self.cpu_optimizer_state_dict = {}
-        self._queue = self.context.Queue()
-        self.trigger_event = self.context.Event()
-        self.completed_event = self.context.Event()
-        self.completed_event.set()
-        self._save_process = None
-
-    def _save_optimizer(
-        self,
-        queue,
-        # obj,
-        trigger_event,
-        completed_event,
-    ):
-        while True:
-            logger.info("===trigger_event wait")
-            trigger_event.wait()
-            trigger_event.clear()
-            # try:
-            obj, name_mapping, path, saved_signal_path, protocol = queue.get()
-            # obj = queue.get_nowait()
-            # if isinstance(obj, dict):
-            for k, v in obj.items():
-                if k == "master_weights" and isinstance(v, dict):
-                    for kk, vv in v.items():
-                        if isinstance(vv, paddle.Tensor):
-                            vv.name = name_mapping["master_weights"][kk]
-                else:
-                    if k in name_mapping and isinstance(v, paddle.Tensor):
-                        v.name = name_mapping[k]
-            logger.info(f"====save {path}===")
-            paddle.save(obj, path, protocol)
-            # dump savd_siganl
-            with open(saved_signal_path, mode="w+") as f:
-                f.write("1")
-                f.flush()
-                os.fsync(f.fileno())
-
-            # except Exception as e:
-            # logger.warning(f"Error occurred during saving: {e}")
-            logger.info("===completed_event set")
-            completed_event.set()
+        self.pool = self.context.Pool(1)
+        self.result = None
 
     def run(self, optimizer_state_dict, path, saved_signal_path, protocol=4):
-        logger.info("===completed_event wait")
-        self.completed_event.wait()
-        self.completed_event.clear()
+        if self.result:
+            print(self.result.get())
 
         self.cpu_optimizer_state_dict.clear()
         name_mapping = {"master_weights": {}}
@@ -85,18 +63,9 @@ class AsyncSaver:
                 self.cpu_optimizer_state_dict[k] = v.pin_memory()
                 name_mapping[k] = v.name
             paddle.device.synchronize()
-
-        self._queue.put((self.cpu_optimizer_state_dict, name_mapping, path, saved_signal_path, protocol))
-
-        if self._save_process is None:
-            self._save_process = self.context.Process(
-                target=self._save_optimizer,
-                args=(
-                    self._queue,
-                    self.trigger_event,
-                    self.completed_event,
-                ),
-            )
-            self._save_process.start()
-        logger.info("===trigger_event set")
-        self.trigger_event.set()
+        logger.info("====pool.apply_async start===")
+        self.result = self.pool.apply_async(
+            _save_optimizer, args=(self.cpu_optimizer_state_dict, name_mapping, path, saved_signal_path, protocol)
+        )
+        print(f"===={self.result}==")
+        logger.info("====pool.apply_async end===")
