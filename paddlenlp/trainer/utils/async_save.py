ac60b239d793a3d598284a43bec7b027edf05efc
ForFishes <2282912238@qq.com>
2024-08-05 10:33:58 +0800

    add async saver

--- paddlenlp/trainer/utils/async_save.py
+++ paddlenlp/trainer/utils/async_save.py
@@ -24,39 +24,50 @@ class AsyncSaver:
     def __init__(self):
         self.context = multiprocessing.get_context("spawn")
         self.cpu_optimizer_state_dict = {}
+        self._queue = self.context.Queue()
         self.trigger_event = self.context.Event()
         self.completed_event = self.context.Event()
         self.completed_event.set()
         self._save_process = None
 
-    def _save_optimizer(self, obj, name_mapping, path, saved_signal_path, protocol, trigger_event, completed_event):
+    def _save_optimizer(
+        self,
+        queue,
+        # obj,
+        trigger_event,
+        completed_event,
+    ):
         while True:
+            logger.info("===trigger_event wait")
             trigger_event.wait()
             trigger_event.clear()
-            try:
-                if isinstance(obj, dict):
-                    for k, v in obj.items():
-                        if k == "master_weights" and isinstance(v, dict):
-                            for kk, vv in v.items():
-                                if isinstance(vv, paddle.Tensor):
-                                    vv.name = name_mapping["master_weights"][kk]
-                        else:
-                            if k in name_mapping and isinstance(v, paddle.Tensor):
-                                v.name = name_mapping[k]
+            # try:
+            obj, name_mapping, path, saved_signal_path, protocol = queue.get()
+            # obj = queue.get_nowait()
+            # if isinstance(obj, dict):
+            for k, v in obj.items():
+                if k == "master_weights" and isinstance(v, dict):
+                    for kk, vv in v.items():
+                        if isinstance(vv, paddle.Tensor):
+                            vv.name = name_mapping["master_weights"][kk]
+                else:
+                    if k in name_mapping and isinstance(v, paddle.Tensor):
+                        v.name = name_mapping[k]
+            logger.info(f"====save {path}===")
+            paddle.save(obj, path, protocol)
+            # dump savd_siganl
+            with open(saved_signal_path, mode="w+") as f:
+                f.write("1")
+                f.flush()
+                os.fsync(f.fileno())
 
-                paddle.save(obj, path, protocol)
-                # dump savd_siganl
-                with open(saved_signal_path, mode="w+") as f:
-                    f.write("1")
-                    f.flush()
-                    os.fsync(f.fileno())
-
-            except Exception as e:
-                logger.warning(f"Error occurred during saving: {e}")
-
+            # except Exception as e:
+            # logger.warning(f"Error occurred during saving: {e}")
+            logger.info("===completed_event set")
             completed_event.set()
 
     def run(self, optimizer_state_dict, path, saved_signal_path, protocol=4):
+        logger.info("===completed_event wait")
         self.completed_event.wait()
         self.completed_event.clear()
 
@@ -75,18 +86,17 @@ class AsyncSaver:
                 name_mapping[k] = v.name
             paddle.device.synchronize()
 
+        self._queue.put((self.cpu_optimizer_state_dict, name_mapping, path, saved_signal_path, protocol))
+
         if self._save_process is None:
             self._save_process = self.context.Process(
                 target=self._save_optimizer,
                 args=(
-                    self.cpu_optimizer_state_dict,
-                    name_mapping,
-                    path,
-                    saved_signal_path,
-                    protocol,
+                    self._queue,
                     self.trigger_event,
                     self.completed_event,
                 ),
             )
             self._save_process.start()
+        logger.info("===trigger_event set")
         self.trigger_event.set()
