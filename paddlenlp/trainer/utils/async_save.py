11e17f9c7d34880052e45f66f0e103eefdf15495
ForFishes <2282912238@qq.com>
2024-08-05 20:09:21 +0800

    add async save

--- paddlenlp/trainer/utils/async_save.py
+++ paddlenlp/trainer/utils/async_save.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import atexit
 import copy
 import multiprocessing
 import os
@@ -29,9 +31,8 @@ def _save_optimizer(obj, name_mapping, path, saved_signal_path, protocol):
         else:
             if k in name_mapping and isinstance(v, paddle.Tensor):
                 v.name = name_mapping[k]
-    logger.info(f"====save {path}===")
     paddle.save(obj, path, protocol)
-    # dump savd_siganl
+    # dump saved_signal
     with open(saved_signal_path, mode="w+") as f:
         f.write("1")
         f.flush()
@@ -44,28 +45,57 @@ class AsyncSaver:
         self.cpu_optimizer_state_dict = {}
         self.pool = self.context.Pool(1)
         self.result = None
+        self.name_mapping = None
+
+        atexit.register(self.shutdown)
 
     def run(self, optimizer_state_dict, path, saved_signal_path, protocol=4):
-        if self.result:
-            print(self.result.get())
+        self._wait_for_previous_result()
 
+        self._reset_state(path, saved_signal_path, protocol)
+        self._process_optimizer_state_dict(optimizer_state_dict)
+
+        self.result = self.pool.apply_async(
+            _save_optimizer,
+            args=(self.cpu_optimizer_state_dict, self.name_mapping, self.path, self.saved_signal_path, self.protocol),
+        )
+
+    def _wait_for_previous_result(self):
+        if self.result is not None:
+            max_retries = 5
+            for retries in range(max_retries):
+                try:
+                    self.result.get()
+                    break
+                except Exception as e:
+                    if retries == max_retries - 1:
+                        raise RuntimeError(f"Failed after {max_retries} retries during async save.")
+                    logger.warning(f"An error occurred during async save: {e}. Retrying...")
+
+    def _reset_state(self, path, saved_signal_path, protocol):
         self.cpu_optimizer_state_dict.clear()
-        name_mapping = {"master_weights": {}}
+        self.name_mapping = {"master_weights": {}}
+        self.path = path
+        self.saved_signal_path = saved_signal_path
+        self.protocol = protocol
+
+    def _process_optimizer_state_dict(self, optimizer_state_dict):
         for k, v in optimizer_state_dict.items():
             if k == "master_weights":
                 self.cpu_optimizer_state_dict[k] = {}
                 for kk, vv in v.items():
                     self.cpu_optimizer_state_dict[k][kk] = vv.pin_memory()
-                    name_mapping[k][kk] = vv.name
+                    self.name_mapping[k][kk] = vv.name
             elif k == "LR_Scheduler":
                 self.cpu_optimizer_state_dict[k] = copy.deepcopy(v)
             else:
                 self.cpu_optimizer_state_dict[k] = v.pin_memory()
-                name_mapping[k] = v.name
+                self.name_mapping[k] = v.name
             paddle.device.synchronize()
-        logger.info("====pool.apply_async start===")
-        self.result = self.pool.apply_async(
-            _save_optimizer, args=(self.cpu_optimizer_state_dict, name_mapping, path, saved_signal_path, protocol)
-        )
-        print(f"===={self.result}==")
-        logger.info("====pool.apply_async end===")
+
+    def shutdown(self):
+        self.pool.close()
+        self.pool.join()
+
+    def __del__(self):
+        self.shutdown()
