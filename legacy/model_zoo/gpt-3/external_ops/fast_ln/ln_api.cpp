79e8da02f66b145a89c21d82171d985c51f8197b
Wennie396 <wn295448@buaa.edu.cn>
2024-08-08 15:12:22 +0800

    del all added codes

--- legacy/model_zoo/gpt-3/external_ops/fast_ln/ln_api.cpp
+++ legacy/model_zoo/gpt-3/external_ops/fast_ln/ln_api.cpp
@@ -529,13 +529,7 @@ std::vector<std::vector<int64_t>> RMSLnBwdInferShape(
   return {x_shape, scale_shape};
 }
 
-std::vector<paddle::DataType> LnBwdInferDtype(paddle::DataType x_dtype,
-                                              paddle::DataType scale_dtype,
-                                              paddle::DataType mean_dtype,
-                                              paddle::DataType invvar_dtype,
-                                              paddle::DataType dy_dtype) {
-  return {x_dtype, scale_dtype, scale_dtype};
-}
+// fast_ln define SetInferDtypeFn
 
 PD_BUILD_OP(fast_ln)
     .Inputs({"x", "scale", "bias"})
