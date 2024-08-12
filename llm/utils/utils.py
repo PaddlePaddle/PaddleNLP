30fc639eba3083018f29867e34e8151603d1d128
zhaogf01 <148510471+zhaogf01@users.noreply.github.com>
2024-08-12 16:25:07 +0800

    [LLM]Update yuan model  (#8786)
    * add yuan model
    
    * add yuan model settings
    
    * fix conflict
    
    * add readme
    
    * update format
    
    * update readme
    
    * update readme
    
    * update for lint
    
    * update for lint
    
    * update for lint
    
    * update for lint
    
    * add fp16
    
    * update utils
    
    * fix bug
    
    * format
    
    * correct pre-commit
    
    * correct fp16
    
    * delete rearrange
    
    * add pre_train
    
    * auto convert from torch
    
    * support sft lora
    
    * update structure
    
    * fix yuantokenizer pad
    
    * update scripts
    
    * update readme
    
    * update readme
    
    * add pad_token_id
    
    * format
    
    * format
    
    * update readme
    
    * update for review
    
    * update for review
    
    * fix bug
    
    * update to CRLF
    
    * format
    
    * support param convert
    
    * update sft config
    
    * fix modeling
    
    * fix qk fuse&spilt and fix fa
    
    * format
    
    * format
    
    * format
    
    * fix fa dtype
    
    ---------
    
    Co-authored-by: drownfish19 <drownfish19@gmail.com>

--- llm/utils/utils.py
+++ llm/utils/utils.py
@@ -220,7 +220,17 @@ def get_lora_target_modules(model):
             ".*up_proj.*",
             ".*down_proj.*",
         ]
-    else:
+    elif model.base_model_prefix == "yuan":
+        target_modules = [
+            ".*q_proj.*",
+            ".*k_proj.*",
+            ".*v_proj.*",
+            ".*o_proj.*",
+            ".*gate_proj.*",
+            ".*up_proj.*",
+            ".*down_proj.*",
+        ]
+    else:    
         raise ValueError(f"Unknown base_model_prefix: {model.base_model_prefix}.")
     return target_modules
 
