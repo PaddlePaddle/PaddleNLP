a3ae87617e9963855a6922875b3305f0fd1bde15
lixc <1979818737@qq.com>
2024-08-27 17:14:41 +0000

    remove useless file

--- llm/predict.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-python predict/predictor.py \
-    --model_name_or_path Qwen/Qwen2-1.5B-Instruct \
-    --data_file ./data/humaneval.json  \
-    --dtype bfloat16 \
-    --output_file qwen2-1.5b.json
