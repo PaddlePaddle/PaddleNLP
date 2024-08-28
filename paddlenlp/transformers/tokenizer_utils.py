d6f2f38bb9519aafe18f03490ea6ea74411ca0e3
drownfish19 <drownfish19@gmail.com>
2024-08-28 06:24:31 +0000

    fix _decode

--- paddlenlp/transformers/tokenizer_utils.py
+++ paddlenlp/transformers/tokenizer_utils.py
@@ -1842,9 +1842,7 @@ class PretrainedTokenizer(ChatTemplateMixin, PretrainedTokenizerBase):
             token_ids = token_ids.tolist()
         self._decode_use_source_tokenizer = kwargs.pop("use_source_tokenizer", False)
         filtered_tokens = self.convert_ids_to_tokens(token_ids, skip_special_tokens=skip_special_tokens)
-        legacy_added_tokens = set(self.added_tokens_encoder.keys()) - set(self.all_special_tokens) | {
-            token for token in self.additional_special_tokens if self.convert_tokens_to_ids(token) >= self.vocab_size
-        }
+
         # To avoid mixing byte-level and unicode for byte-level BPT
         # we need to build string separately for added tokens and byte-level tokens
         # cf. https://github.com/huggingface/transformers/issues/1133
@@ -1853,7 +1851,7 @@ class PretrainedTokenizer(ChatTemplateMixin, PretrainedTokenizerBase):
         for token in filtered_tokens:
             if skip_special_tokens and token in self.all_special_ids:
                 continue
-            if token in legacy_added_tokens:
+            if token in self.added_tokens_encoder:
                 if current_sub_text:
                     sub_texts.append(self.convert_tokens_to_string(current_sub_text))
                     current_sub_text = []
