c28caf70b0a792f48f2098704a32a298384c913f
gaoziyuan <88373061+gzy19990617@users.noreply.github.com>
2024-08-29 20:58:38 +0800

    [Inference] Add cutlass gemm dequant op (#8909)
    * change gpu name
    
    * add cutlass gemm_dequant op
    
    * add cutlass gemm_dequant op
    
    * fix format
    
    * fix fused_transformer_layers
    
    * fix layer
    
    * fix layer
    
    * fix format
    
    * fix format
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review

--- csrc/setup_cuda.py
+++ csrc/setup_cuda.py
@@ -13,9 +13,29 @@
 # limitations under the License.
 
 import os
+import subprocess
 
 import paddle
 from paddle.utils.cpp_extension import CUDAExtension, setup
+
+
+def clone_git_repo(version, repo_url, destination_path):
+    try:
+        subprocess.run(["git", "clone", "-b", version, "--single-branch", repo_url, destination_path], check=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Git clone {repo_url} operation failed with the following error: {e}")
+        print("Please check your network connection or access rights to the repository.")
+        print(
+            "If the problem persists, please refer to the README file for instructions on how to manually download and install the necessary components."
+        )
+        return False
+
+
+def get_sm_version():
+    prop = paddle.device.cuda.get_device_properties()
+    cc = prop.major * 10 + prop.minor
+    return cc
 
 
 def strtobool(v):
@@ -49,52 +69,66 @@ def get_gencode_flags():
 
 
 gencode_flags = get_gencode_flags()
-library_path = os.environ.get('LD_LIBRARY_PATH', '/usr/local/cuda/lib64')
+library_path = os.environ.get("LD_LIBRARY_PATH", "/usr/local/cuda/lib64")
+
+
+sources = [
+    "./gpu/save_with_output.cc",
+    "./gpu/set_value_by_flags.cu",
+    "./gpu/token_penalty_multi_scores.cu",
+    "./gpu/token_penalty_multi_scores_v2.cu",
+    "./gpu/stop_generation_multi_ends.cu",
+    "./gpu/fused_get_rope.cu",
+    "./gpu/get_padding_offset.cu",
+    "./gpu/qkv_transpose_split.cu",
+    "./gpu/rebuild_padding.cu",
+    "./gpu/transpose_removing_padding.cu",
+    "./gpu/write_cache_kv.cu",
+    "./gpu/encode_rotary_qk.cu",
+    "./gpu/get_padding_offset_v2.cu",
+    "./gpu/rebuild_padding_v2.cu",
+    "./gpu/set_value_by_flags_v2.cu",
+    "./gpu/stop_generation_multi_ends_v2.cu",
+    "./gpu/update_inputs.cu",
+    "./gpu/get_output.cc",
+    "./gpu/save_with_output_msg.cc",
+    "./gpu/write_int8_cache_kv.cu",
+    "./gpu/step.cu",
+    "./gpu/quant_int8.cu",
+    "./gpu/dequant_int8.cu",
+    "./gpu/flash_attn_bwd.cc",
+    "./gpu/tune_cublaslt_gemm.cu",
+]
+
+cutlass_dir = "gpu/cutlass_kernels/cutlass"
+nvcc_compile_args = gencode_flags
+
+if not os.path.exists(cutlass_dir) or not os.listdir(cutlass_dir):
+    if not os.path.exists(cutlass_dir):
+        os.makedirs(cutlass_dir)
+    clone_git_repo("v3.5.0", "https://github.com/NVIDIA/cutlass.git", cutlass_dir)
+
+nvcc_compile_args += [
+    "-O3",
+    "-U__CUDA_NO_HALF_OPERATORS__",
+    "-U__CUDA_NO_HALF_CONVERSIONS__",
+    "-U__CUDA_NO_BFLOAT16_OPERATORS__",
+    "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+    "-U__CUDA_NO_BFLOAT162_OPERATORS__",
+    "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
+    "-Igpu/cutlass_kernels",
+    "-Igpu/cutlass_kernels/cutlass/include",
+]
+cc = get_sm_version()
+if cc >= 80:
+    sources += ["gpu/int8_gemm_with_cutlass/gemm_dequant.cu"]
 
 setup(
     name="paddlenlp_ops",
     ext_modules=CUDAExtension(
-        sources=[
-            "./generation/save_with_output.cc",
-            "./generation/set_value_by_flags.cu",
-            "./generation/token_penalty_multi_scores.cu",
-            "./generation/token_penalty_multi_scores_v2.cu",
-            "./generation/stop_generation_multi_ends.cu",
-            "./generation/fused_get_rope.cu",
-            "./generation/get_padding_offset.cu",
-            "./generation/qkv_transpose_split.cu",
-            "./generation/rebuild_padding.cu",
-            "./generation/transpose_removing_padding.cu",
-            "./generation/write_cache_kv.cu",
-            "./generation/encode_rotary_qk.cu",
-            "./generation/get_padding_offset_v2.cu",
-            "./generation/rebuild_padding_v2.cu",
-            "./generation/set_value_by_flags_v2.cu",
-            "./generation/stop_generation_multi_ends_v2.cu",
-            "./generation/update_inputs.cu",
-            "./generation/get_output.cc",
-            "./generation/save_with_output_msg.cc",
-            "./generation/write_int8_cache_kv.cu",
-            "./generation/step.cu",
-            "./generation/quant_int8.cu",
-            "./generation/dequant_int8.cu",
-            "./generation/flash_attn_bwd.cc",
-            "./generation/tune_cublaslt_gemm.cu",
-        ],
-        extra_compile_args={
-            "cxx": ["-O3"],
-            "nvcc": [
-                "-O3",
-                "-U__CUDA_NO_HALF_OPERATORS__",
-                "-U__CUDA_NO_HALF_CONVERSIONS__",
-                "-U__CUDA_NO_BFLOAT16_OPERATORS__",
-                "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
-                "-U__CUDA_NO_BFLOAT162_OPERATORS__",
-                "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
-            ]
-            + gencode_flags,
-        },
-        libraries=['cublasLt'],
+        sources=sources,
+        extra_compile_args={"cxx": ["-O3"], "nvcc": nvcc_compile_args},
+        libraries=["cublasLt"],
         library_dirs=[library_path],
     ),
 )
