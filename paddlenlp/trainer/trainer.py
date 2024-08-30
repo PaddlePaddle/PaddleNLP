7b14e64416a52f27a3e33157f13ee70da0267810
zhengzhonghui <zhengzhonghui@baidu.com>
2024-08-30 17:37:45 +0800

    debug CI, do not merge

--- paddlenlp/trainer/trainer.py
+++ paddlenlp/trainer/trainer.py
@@ -1309,6 +1309,7 @@ class Trainer:
         if timer_info or paddle_timer_info:
             logger.info(f"[Profile global_step: {self.state.global_step}] {timer_info} {paddle_timer_info}")
 
+    # debug dataloader
     def _get_item_from_loss(self, loss):
         assert isinstance(loss, paddle.Tensor) and loss._is_initialized()
         loss_value = loss.item()
