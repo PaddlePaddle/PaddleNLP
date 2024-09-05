7177f43829392ff429e27040101bc229705ba962
changwenbin <changwenbin@baidu.com>
2024-09-05 13:07:07 +0000

    fix jit_inference function

--- paddlenlp/transformers/utils.py
+++ paddlenlp/transformers/utils.py
@@ -170,7 +170,7 @@ def adapt_stale_fwd_patch(self, name, value):
 
         # NOTE(changwenbin & zhoukangkang):
         # When use model = paddle.incubate.jit.inference(model), it reportes errors, we fix it here.
-        # Because paddle.incubate.jit.is_inference_mode is currently only in paddle develop.
+        # is_inference_mode API is only avaliable in PaddlePaddle develop，so we add a try except.
         try:
             from paddle.incubate.jit import is_inference_mode
 
