ccd6bf5c4d484b17f6b408914e69999c39e0c58d
lugimzzz <63761690+lugimzzz@users.noreply.github.com>
2024-09-11 14:23:37 +0800

    Revert "cherry pick dpotrainer (#9117)"
    This reverts commit 333c29e61642055b9da97a82fde19809755e5021.

--- paddlenlp/peft/lora/lora_model.py
+++ paddlenlp/peft/lora/lora_model.py
@@ -70,7 +70,7 @@ from .lora_layers import (
     RowSequenceParallelLoRALinear,
 )
 
-AVAILABLE_LAYERS = [
+AVALIABLE_LAYERS = [
     ColumnParallelLoRALinear,
     ColumnSequenceParallelLoRALinear,
     LoRAConv2D,
@@ -90,7 +90,7 @@ try:
         RowParallelQuantizationLoRALinear,
     )
 
-    AVAILABLE_LAYERS += [
+    AVALIABLE_LAYERS += [
         ColumnParallelQuantizationLoRALinear,
         QuantizationLoRALinear,
         RowParallelQuantizationLoRALinear,
@@ -815,20 +815,20 @@ class LoRAModel(nn.Layer):
 
     def disable_lora(self):
         for _, layer in self.model.named_sublayers():
-            if any(isinstance(layer, lora_layer) for lora_layer in AVAILABLE_LAYERS):
+            if any(isinstance(layer, lora_layer) for lora_layer in AVALIABLE_LAYERS):
                 layer.disable_lora = True
 
     def enable_lora(self):
         for _, layer in self.model.named_sublayers():
-            if any(isinstance(layer, lora_layer) for lora_layer in AVAILABLE_LAYERS):
+            if any(isinstance(layer, lora_layer) for lora_layer in AVALIABLE_LAYERS):
                 layer.disable_lora = False
 
     def merge(self):
         for _, layer in self.model.named_sublayers():
-            if any(isinstance(layer, lora_layer) for lora_layer in AVAILABLE_LAYERS):
+            if any(isinstance(layer, lora_layer) for lora_layer in AVALIABLE_LAYERS):
                 layer.merge()
 
     def unmerge(self):
         for _, layer in self.model.named_sublayers():
-            if any(isinstance(layer, lora_layer) for lora_layer in AVAILABLE_LAYERS):
+            if any(isinstance(layer, lora_layer) for lora_layer in AVALIABLE_LAYERS):
                 layer.unmerge()
