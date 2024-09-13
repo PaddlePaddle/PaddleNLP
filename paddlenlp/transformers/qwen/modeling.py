5c1779c8bdba78f7bc81d00e0701b06684438581
Zhong Hui <zhonghui.net@gmail.com>
2024-09-12 23:53:07 -0700

    [Feature] Add hardware flops for pretraining (#9069)
    * fix hardware tflops.
    
    * Support mfu for pretraining.

--- paddlenlp/transformers/qwen/modeling.py
+++ paddlenlp/transformers/qwen/modeling.py
@@ -49,6 +49,7 @@ from ...utils.converter import StateDictNameMapping, init_name_mappings
 from .. import linear_utils
 from ..linear_utils import Linear
 from ..model_outputs import ModelOutput
+from ..utils import caculate_llm_flops
 from .configuration import QWenConfig
 
 try:
@@ -689,6 +690,39 @@ class QWenModel(QWenPretrainedModel):
             ]
         )
         self.ln_f = QWenRMSNorm(config)
+
+    def get_model_flops(self, batch_size=1, seq_length=None, **kwargs):
+        if seq_length is None:
+            if hasattr(self.config, "seq_length"):
+                seq_length = self.config.seq_length
+            else:
+                seq_length = 2048
+
+        return caculate_llm_flops(
+            hidden_size=self.config.hidden_size,
+            intermediate_size=self.config.intermediate_size,
+            layer_num=self.config.num_hidden_layers,
+            vocab_size=self.config.vocab_size,
+            seq_length=seq_length,
+            recompute=False,
+        )
+
+    def get_hardware_flops(self, batch_size=1, seq_length=None, recompute=False, **kwargs):
+        if seq_length is None:
+            if hasattr(self.config, "seq_length"):
+                seq_length = self.config.seq_length
+            else:
+                seq_length = 2048
+
+        return caculate_llm_flops(
+            hidden_size=self.config.hidden_size,
+            intermediate_size=self.config.intermediate_size,
+            layer_num=self.config.num_hidden_layers,
+            vocab_size=self.config.vocab_size,
+            seq_length=seq_length,
+            recompute=recompute,
+            recompute_granularity=self.config.recompute_granularity,
+        )
 
     def get_input_embeddings(self):
         return self.wte
