20aa16333fc7723143027ccf6238d05fc584693f
liusz <liusz@smail.nju.edu.cn>
2024-09-15 16:12:46 +0800

    update remove_assign_out_pass, now it only trigger with cf.yield

--- csrc/gpu/pass/remove_assign_out_pass.cc
+++ csrc/gpu/pass/remove_assign_out_pass.cc
@@ -13,6 +13,14 @@ public:
     const auto &assign_out_ = pat.Op("pd_op.assign_out_");
     assign_out_({&pat.Tensor("assign_in"), &pat.Tensor("assign_out")}, {&pat.Tensor("out")});
 
+    pat.AddConstraint([](const paddle::drr::MatchContext &match_ctx) {
+      auto &out = match_ctx.Tensor("out");
+      if (out.use_count() == 1 && out.use_begin()->owner()->name() == "cf.yield") {
+        return true;
+      }
+      return false;
+    });
+
     paddle::drr::ResultPattern res = pat.ResultPattern();
     res.Tensor("out").Assign(res.Tensor("assign_in"));
   }
