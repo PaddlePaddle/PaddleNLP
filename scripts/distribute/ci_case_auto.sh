7adfbe31d6f04b9df981083dd2999a6aaca8df95
andsonder <changlu@keter.top>
2024-10-06 19:48:45 +0800

    add new test

--- scripts/distribute/ci_case_auto.sh
+++ scripts/distribute/ci_case_auto.sh
@@ -58,8 +58,8 @@ function llama_case_list_auto() {
     llama_align_dygraph_dy2st_pir_auto_bs2_bf16_DP2-MP2-PP1-SP
     llama_align_dygraph_dy2st_pir_auto_bs2_bf16_DP2-MP2-PP2-SP
     llama_align_dygraph_dy2st_pir_auto_grad_merge_bs2_fp32_DP1-MP1-PP1
-    llama_align_dy2st_fthenb_and_pp_auto_bs2_fp32_DP1-MP1-PP4
-    llama_align_dygraph_dy2st_pir_auto_vpp_bs2_bf16_DP1-MP1-PP4
+    llama_align_dy2st_fthenb_and_vpp_auto_bs2_fp32_DP1-MP1-PP4
+    llama_align_dygraph_dy2st_pir_auto_pp_bs2_bf16_DP1-MP1-PP4
 }
 
 function llm_gpt_case_list_auto() {
