73f1a20c3ad78159ba99229dd8292aeef6d850f3
Zhong Hui <zhonghui.net@gmail.com>
2024-10-08 01:54:23 -0700

    Revert "Revert "fix speed ppt eval hang (#9218)" (#9229)" (#9230)
    This reverts commit 81de41ae0b9855ef0c6f251da2d2a7bb1d2b43de.

--- paddlenlp/trainer/trainer.py
+++ paddlenlp/trainer/trainer.py
@@ -1623,7 +1623,7 @@ class Trainer:
                 eval_dataset,
                 batch_sampler=eval_sampler,
                 collate_fn=self.data_collator,
-                num_workers=self.args.dataloader_num_workers,
+                num_workers=0,
                 **additional_configs,
             )
 
@@ -1670,7 +1670,7 @@ class Trainer:
                 test_dataset,
                 batch_size=self.args.per_device_eval_batch_size * self.world_size,
                 collate_fn=self.data_collator,
-                num_workers=self.args.dataloader_num_workers,
+                num_workers=0,
                 **additional_config,
             )
         else:
