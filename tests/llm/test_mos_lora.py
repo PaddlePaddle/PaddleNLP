fa1764a81d47d2898d4021d6cd0c8eca9fd0f10b
ning <1907964014@qq.com>
2024-10-29 10:20:36 +0800

    add tests

--- tests/llm/test_mos_lora.py
+++ tests/llm/test_mos_lora.py
@@ -36,7 +36,7 @@ from .testing_utils import LLMTest
         ["baichuan"],
     ],
 )
-class LoraTest(LLMTest, unittest.TestCase):
+class MosLoraTest(LLMTest, unittest.TestCase):
     config_path: str = "./tests/fixtures/llm/mos_lora.yaml"
     model_dir: str = None
 
@@ -50,7 +50,7 @@ class LoraTest(LLMTest, unittest.TestCase):
         LLMTest.tearDown(self)
         sys.path.remove(self.model_codes_dir)
 
-    def test_lora(self):
+    def test_mos_lora(self):
         self.disable_static()
         paddle.set_default_dtype("float32")
 
@@ -77,37 +77,7 @@ class LoraTest(LLMTest, unittest.TestCase):
             merge()
 
         # TODO(wj-Mcat): disable chatglm2 test temporarily
-        if self.model_dir not in ["qwen", "baichuan", "chatglm2"]:
-            self.run_predictor({"inference_model": True})
-
-        self.run_predictor({"inference_model": False})
-
-    def test_rslora_plus(self):
-        self.disable_static()
-        paddle.set_default_dtype("float32")
-
-        lora_config = load_test_config(self.config_path, "rslora_plus", self.model_dir)
-        lora_config["output_dir"] = self.output_dir
-        lora_config["dataset_name_or_path"] = self.data_dir
-
-        with argv_context_guard(lora_config):
-            from run_finetune import main
-
-            main()
-
-        # merge weights
-        merge_lora_weights_config = {
-            "lora_path": lora_config["output_dir"],
-            "model_name_or_path": lora_config["model_name_or_path"],
-            "output_path": lora_config["output_dir"],
-        }
-        with argv_context_guard(merge_lora_weights_config):
-            from tools.merge_lora_params import merge
-
-            merge()
-
-        # TODO(wj-Mcat): disable chatglm2 test temporarily
-        if self.model_dir not in ["qwen", "baichuan", "chatglm2"]:
+        if self.model_dir not in ["qwen", "baichuan", "chatglm2", "llama"]:
             self.run_predictor({"inference_model": True})
 
         self.run_predictor({"inference_model": False})
