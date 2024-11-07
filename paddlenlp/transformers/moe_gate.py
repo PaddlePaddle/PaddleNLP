83bdeddaff6a7d9e05c6b4986f074c1430ee1aa3
DrownFish19 <DrownFish19@gmail.com>
2024-11-07 10:22:49 +0000

    update token_priority method

--- paddlenlp/transformers/moe_gate.py
+++ paddlenlp/transformers/moe_gate.py
@@ -176,6 +176,56 @@ class PretrainedMoEGate(nn.Layer, MoEGateMixin):
         self.top_k = kwargs.pop("top_k", 2)
         self.norm_topk_prob = kwargs.pop("norm_topk_prob", False)
 
+    def _priority(self, topk_idx: paddle.Tensor, capacity: int) -> paddle.Tensor:
+        """_summary_
+            The priority is the cumulative sum of the expert indices.
+
+            This method is used in hunyuan model
+        Args:
+            topk_idx (paddle.Tensor): [batch_size * seq_len, topk]
+
+        Returns:
+            paddle.Tensor: cumsum locations
+        """
+        # Make num_selected_experts the leading axis to ensure that top-1 choices
+        # have priority over top-2 choices, which have priority over top-3 choices,
+        # etc.
+        expert_index = paddle.transpose(topk_idx, [1, 0])  # [topk, B*S]
+        # Shape: [num_selected_experts * tokens_per_group]
+        expert_index = expert_index.reshape([-1])
+
+        # Create mask out of indices.
+        # Shape: [tokens_per_group * num_selected_experts, num_experts].
+        expert_mask = F.one_hot(expert_index, self.num_experts).cast(paddle.int32)
+
+        # Experts have a fixed capacity that we cannot exceed. A token's priority
+        # within the expert's buffer is given by the masked, cumulative capacity of
+        # its target expert.
+        # Shape: [tokens_per_group * num_selected_experts, num_experts].
+        token_priority = paddle.cumsum(expert_mask, axis=0) * expert_mask - 1
+        # Shape: [num_selected_experts, tokens_per_group, num_experts].
+        token_priority = token_priority.reshape((self.top_k, -1, self.num_experts))
+        # Shape: [tokens_per_group, num_selected_experts, num_experts].
+        token_priority = paddle.transpose(token_priority, [1, 0, 2])
+        # For each token, across all selected experts, select the only non-negative
+        # (unmasked) priority. Now, for group G routing to expert E, token T has
+        # non-negative priority (i.e. token_priority[G,T,E] >= 0) if and only if E
+        # is its targeted expert.
+        # Shape: [tokens_per_group, num_experts].
+        token_priority = paddle.max(token_priority, axis=1)
+
+        # Token T can only be routed to expert E if its priority is positive and
+        # less than the expert capacity. One-hot matrix will ignore indices outside
+        # the range [0, expert_capacity).
+        # Shape: [tokens_per_group, num_experts, expert_capacity].
+        valid_mask = paddle.logical_and(token_priority >= 0, token_priority < capacity)
+        token_priority = paddle.masked_fill(token_priority, ~valid_mask, 0)
+        dispatch_mask = F.one_hot(token_priority, capacity)
+        valid_mask = valid_mask.unsqueeze(-1).expand(valid_mask.shape + [capacity])
+        dispatch_mask = paddle.masked_fill(dispatch_mask, ~valid_mask, 0)
+
+        return dispatch_mask
+
     def topk_naive(self, scores: paddle.Tensor, k: int) -> Tuple[paddle.Tensor, paddle.Tensor]:
         """_summary_
 
@@ -405,23 +455,19 @@ class PretrainedMoEGate(nn.Layer, MoEGateMixin):
             if self.drop_policy == "probs":
                 topk_masked_gates = paddle.zeros_like(gates).put_along_axis(top_idx, top_gate, axis=1)
                 capacity_probs, capacity_indices = paddle.topk(topk_masked_gates, k=capacity, axis=0, sorted=False)
-                capacity_mask = paddle.zeros_like(gates).put_along_axis(capacity_indices, paddle.to_tensor(1.0), axis=0)  # fmt:skip
-                mask = mask * capacity_mask
-                locations = paddle.cumsum(mask, axis=0) - 1
+                token_priority = self._priority(capacity_indices, capacity)
 
             elif self.drop_policy == "position":
-                locations = paddle.cumsum(mask, axis=0) - 1
-                mask *= (locations < capacity).cast(paddle.int64)
+                token_priority = self._priority(top_idx, capacity)
             else:
                 raise ValueError(f"Invalid drop_policy: {self.drop_policy}")
         else:
             # Do not drop tokens - set capacity according to current expert assignments
-            locations = paddle.cumsum(mask, axis=0) - 1
-
-            new_capacity = paddle.max(exp_counts)
+            local_capacity = paddle.max(exp_counts)
             if self.group is not None:
-                dist.all_reduce(new_capacity, op=dist.ReduceOp.MAX, group=self.group)
-            capacity = int(new_capacity)
+                dist.all_reduce(local_capacity, op=dist.ReduceOp.MAX, group=self.group)
+            capacity = int(local_capacity)
+            token_priority = self._priority(top_idx, capacity)
 
         # normalize gates
         gates_masked = gates * mask
@@ -430,9 +476,7 @@ class PretrainedMoEGate(nn.Layer, MoEGateMixin):
         if self.norm_topk_prob:
             gates_masked = gates_masked / denom_s
 
-        # dispatch_mask
-        locations_sc = self._one_hot_to_float(locations * mask, num_classes=capacity)
-        combine_weights = paddle.einsum("se,sec->sec", gates_masked, locations_sc)
+        combine_weights = paddle.einsum("se,sec->sec", gates_masked, token_priority)
         dispatch_mask = combine_weights.cast(paddle.bool)
 
         return capacity, combine_weights, dispatch_mask, exp_counts, l_aux, l_zloss
