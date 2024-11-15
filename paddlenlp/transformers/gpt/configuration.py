b06d1d4cd043168e0dba473d346f22d1c2be0841
blacksheep-Aristotle <zhangweilong01@baidu.com>
2024-11-15 16:03:14 +0800

    [AutoParallel]: gpt model_auto support sp,fast_ln,fused_dropout_add,fused_linear

--- paddlenlp/transformers/gpt/configuration.py
+++ paddlenlp/transformers/gpt/configuration.py
@@ -301,3 +301,4 @@ class GPTConfig(PretrainedConfig):
 
         self.use_fast_layer_norm = use_fast_layer_norm
         self.fused_softmax_with_triangular = fused_softmax_with_triangular
+        self.register_unsavable_keys(["use_fast_layer_norm", "fuse_attention_ffn", "fuse_attention_qkv"])
