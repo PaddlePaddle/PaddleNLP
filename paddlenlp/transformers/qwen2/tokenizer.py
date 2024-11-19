13e95bd253d8b57926716828c0ea512439ba2109
DrownFish19 <DrownFish19@gmail.com>
2024-11-19 02:04:58 +0000

    compatible with python 3.10

--- paddlenlp/transformers/qwen2/tokenizer.py
+++ paddlenlp/transformers/qwen2/tokenizer.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tokenization classes for Qwen2."""
+from __future__ import annotations
 
 import json
 import os
