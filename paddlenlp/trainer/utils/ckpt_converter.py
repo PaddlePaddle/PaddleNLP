7302949f112faf8f17ff70fedefe5183e7775587
YuangLiu <liuyuang@baidu.com>
2024-11-21 16:25:39 +0800

    bug fix:

--- paddlenlp/trainer/utils/ckpt_converter.py
+++ paddlenlp/trainer/utils/ckpt_converter.py
@@ -555,7 +555,7 @@ class CheckpointConverter:
             for k, v in state_dict.items():
                 memory_size += v.numel() * v.element_size()
 
-        memory_size = memory_size.numpy() / 2**20
+        memory_size = memory_size / 2**20
         logger.debug(
             f"The current rank has finished loading the checkpoint file and has allocated {memory_size} MB of GPU memory."
         )
