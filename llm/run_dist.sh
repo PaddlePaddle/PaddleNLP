3e7cbbc33e4f9701614556694dd93cc3ff3c0705
Xiaokang Shang <xiaokang.shang@mthreads.com>
2024-11-26 16:58:25 +0000

    set 128 pp16

--- llm/run_dist.sh
+++ llm/run_dist.sh
@@ -42,7 +42,7 @@ local_ip=`ifconfig eth0 | grep 'inet ' | awk '{print $2}'`
 
 PYTHONPATH=../ ${python} -m paddle.distributed.launch \
 	--master "${master_ip}:8678" \
-	--nnodes 8 \
+	--nnodes 16 \
 	--log_dir "/home/dist/baidu_test/log/${local_ip}" \
         --gpus 0,1,2,3,4,5,6,7 \
 	run_pretrain.py \
