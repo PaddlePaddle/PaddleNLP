ec77d014629e60e2375dc3c8dc24909c9fd18106
YuangLiu <liuyuang@baidu.com>
2024-11-27 13:07:30 +0800

    fix sharding

--- paddlenlp/transformers/model_utils.py
+++ paddlenlp/transformers/model_utils.py
@@ -2885,13 +2885,19 @@ class PretrainedModel(Layer, GenerationMixin, ConversionMixin):
                 if ShardingOption.FULL_SHARD in sharding:
                     level = 3
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> fix sharding
             final_config["dp_config"] = {
                 "sharding_level": level,
                 "sharding_mesh_dim": auto_dist_degree.get("sharding_mesh_dim", None),
             }
+<<<<<<< HEAD
 =======
             final_config["dp_config"] = {"level": level}
 >>>>>>> New version of auto config
+=======
+>>>>>>> fix sharding
 
         return final_config
 
