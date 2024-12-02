8cb0a1d27234c080cc4c65aad88341fe7259c193
luoyuedong <530122286@qq.com>
2024-12-02 19:18:23 +0800

    增加resume-ckpt支持

--- paddlenlp/trainer/argparser.py
+++ paddlenlp/trainer/argparser.py
@@ -317,15 +317,18 @@ class PdArgumentParser(ArgumentParser):
                         logger.info("resume training process from scratch (step 0)")
                         return None
                     else:
-                        logger.info(f"init_step == 0 and user has defined resume_from_checkpoint: {user_defined_resume_from_checkpoint}")
+                        logger.info(
+                            f"init_step == 0 and user has defined resume_from_checkpoint: {user_defined_resume_from_checkpoint}"
+                        )
                         return user_defined_resume_from_checkpoint
                 else:
                     # pdc_init_step > 0
                     logger.info(f"resume training process by pdc longjob with resume step: {pdc_init_step}")
                     if user_defined_resume_from_checkpoint is not None:
-                        logger.warning(f"pdc_init_step:{pdc_init_step} and user_defined_resume_from_checkpoint:{user_defined_resume_from_checkpoint} exist together, use pdc_init_step:{pdc_init_step}")
+                        logger.warning(
+                            f"pdc_init_step:{pdc_init_step} and user_defined_resume_from_checkpoint:{user_defined_resume_from_checkpoint} exist together, use pdc_init_step:{pdc_init_step}"
+                        )
                     return os.path.join(args.get("output_dir", None), f"checkpoint-{pdc_init_step}")
-
 
         args["resume_from_checkpoint"] = get_resume_checkpoint_path(args)
         args_for_json = to_regular_dict(args)
