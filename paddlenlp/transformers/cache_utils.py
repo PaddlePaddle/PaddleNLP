9106b35b86e3a6bd695ec4689ba4c3a3bd302f76
lvdongyi <lvdongyi1222@gmail.com>
2024-12-02 14:21:36 +0800

    update

--- paddlenlp/transformers/cache_utils.py
+++ paddlenlp/transformers/cache_utils.py
@@ -16,6 +16,8 @@ from typing import Any, Dict, List, Optional, Tuple
 
 import paddle
 
+from paddlenlp.utils.log import logger
+
 
 class Cache:
     """
@@ -50,21 +52,190 @@ class Cache:
 
     def get_seq_length(self, layer_idx: Optional[int] = 0) -> int:
         """Returns the sequence length of the cached states. A layer index can be optionally passed."""
+        # TODO: deprecate this function in favor of `cache_position`
         raise NotImplementedError("Make sure to implement `get_seq_length` in a subclass.")
 
+    # # Deprecate in favor of max-cache-shape because we want to be specifc by what we mean with "max_length"
+    # # Prev some cache objects didn't have "max_length" (SlidingWindowCache or SinkCache) because the cache object technically handles
+    # # infinite amount of tokens. In the codebase what we really need to check is the max capacity of certain cache instances, so
+    # # we change naming to be more explicit
+    # def get_max_length(self) -> Optional[int]:
+    #     logger.warning_once(
+    #         "`get_max_cache()` is deprecated for all Cache classes. Use `get_max_cache_shape()` instead. "
+    #         "Calling `get_max_cache()` will raise error from v4.48"
+    #     )
+    #     return self.get_max_cache_shape()
 
+    def get_max_cache_shape(self) -> Optional[int]:
+        """Returns the maximum sequence length (i.e. max capacity) of the cache object"""
+        raise NotImplementedError("Make sure to implement `get_max_cache_shape` in a subclass.")
+
+    def get_usable_length(self, new_seq_length: int, layer_idx: Optional[int] = 0) -> int:
+        """Given the sequence length of the new inputs, returns the usable length of the cache."""
+        # Cache without size limit -> all cache is usable
+        # Cache with size limit -> if the length cache plus the length of the new inputs is larger the maximum cache
+        #   length, we will need to evict part of the cache (and thus not all cache is usable)
+        max_length = self.get_max_cache_shape()
+        previous_seq_length = self.get_seq_length(layer_idx)
+        if max_length is not None and previous_seq_length + new_seq_length > max_length:
+            return max_length - new_seq_length
+        return previous_seq_length
+
+    def reorder_cache(self, beam_idx: paddle.Tensor):
+        """Reorders the cache for beam search, given the selected beam indices."""
+        for layer_idx in range(len(self.key_cache)):
+            if self.key_cache[layer_idx] != []:
+                device = self.key_cache[layer_idx].device
+                self.key_cache[layer_idx] = self.key_cache[layer_idx].index_select(0, beam_idx.to(device))
+            if self.value_cache[layer_idx] != []:
+                device = self.value_cache[layer_idx].device
+                self.value_cache[layer_idx] = self.value_cache[layer_idx].index_select(0, beam_idx.to(device))
+
+    @property
+    def seen_tokens(self):
+        logger.warning_once(
+            "The `seen_tokens` attribute is deprecated and will be removed in v4.41. Use the `cache_position` "
+            "model input instead."
+        )
+        if hasattr(self, "_seen_tokens"):
+            return self._seen_tokens
+        else:
+            return None
+
+
+# class DynamicCache(Cache):
+#     """
+#     A cache that grows dynamically as more tokens are generated. This is the default for generative models.
+
+#     It stores the Key and Value states as a list of tensors, one for each layer. The expected shape for each tensor is
+#     `[batch_size, num_heads, seq_len, head_dim]`.
+#     """
+
+#     def __init__(self) -> None:
+#         self.key_cache: List[paddle.Tensor] = []
+#         self.value_cache: List[paddle.Tensor] = []
+#         self.seen_tokens = 0  # Used in `generate` to keep tally of how many tokens the cache has seen
+
+#     def __getitem__(self, layer_idx: int) -> List[Tuple[paddle.Tensor]]:
+#         """
+#         Support for backwards-compatible `past_key_value` indexing, e.g. `past_key_value[0][0].shape[2]` to get the
+#         sequence length.
+#         """
+#         if layer_idx < len(self):
+#             return (self.key_cache[layer_idx], self.value_cache[layer_idx])
+#         else:
+#             raise KeyError(f"Cache only has {len(self)} layers, attempted to access layer with index {layer_idx}")
+
+#     def __iter__(self):
+#         """
+#         Support for backwards-compatible `past_key_value` iteration, e.g. `for x in past_key_value:` to iterate over
+#         keys and values
+#         """
+#         for layer_idx in range(len(self)):
+#             yield (self.key_cache[layer_idx], self.value_cache[layer_idx])
+
+#     def __len__(self):
+#         """
+#         Support for backwards-compatible `past_key_value` length, e.g. `len(past_key_value)`. This value corresponds
+#         to the number of layers in the model.
+#         """
+#         return len(self.key_cache)
+
+#     def update(
+#         self,
+#         key_states: paddle.Tensor,
+#         value_states: paddle.Tensor,
+#         layer_idx: int,
+#         cache_kwargs: Optional[Dict[str, Any]] = None,
+#     ) -> Tuple[paddle.Tensor, paddle.Tensor]:
+#         """
+#         Updates the cache with the new `key_states` and `value_states` for the layer `layer_idx`.
+
+#         Parameters:
+#             key_states (`paddle.Tensor`):
+#                 The new key states to cache.
+#             value_states (`paddle.Tensor`):
+#                 The new value states to cache.
+#             layer_idx (`int`):
+#                 The index of the layer to cache the states for.
+#             cache_kwargs (`Dict[str, Any]`, `optional`):
+#                 Additional arguments for the cache subclass. No additional arguments are used in `DynamicCache`.
+
+#         Return:
+#             A tuple containing the updated key and value states.
+#         """
+#         # Update the number of seen tokens
+#         if layer_idx == 0:
+#             self.seen_tokens += key_states.shape[-3]
+
+#         # Update the cache
+#         if len(self.key_cache) <= layer_idx:
+#             self.key_cache.append(key_states)
+#             self.value_cache.append(value_states)
+#         else:
+#             self.key_cache[layer_idx] = paddle.concat([self.key_cache[layer_idx], key_states], axis=-3)
+#             self.value_cache[layer_idx] = paddle.concat([self.value_cache[layer_idx], value_states], axis=-3)
+
+#         return self.key_cache[layer_idx], self.value_cache[layer_idx]
+
+#     def get_seq_length(self, layer_idx: Optional[int] = 0) -> int:
+#         """Returns the sequence length of the cached states. A layer index can be optionally passed."""
+#         if len(self.key_cache) <= layer_idx:
+#             return 0
+#         return self.key_cache[layer_idx].shape[-3]
+
+#     def reorder_cache(self, beam_idx: paddle.Tensor):
+#         """Reorders the cache for beam search, given the selected beam indices."""
+#         for layer_idx in range(len(self.key_cache)):
+#             self.key_cache[layer_idx] = self.key_cache[layer_idx].index_select(beam_idx)
+#             self.value_cache[layer_idx] = self.value_cache[layer_idx].index_select(beam_idx)
+
+#     def to_legacy_cache(self) -> Tuple[Tuple[paddle.Tensor], Tuple[paddle.Tensor]]:
+#         """Converts the `DynamicCache` instance into the its equivalent in the legacy cache format."""
+#         legacy_cache = ()
+#         for layer_idx in range(len(self)):
+#             legacy_cache += ((self.key_cache[layer_idx], self.value_cache[layer_idx]),)
+#         return legacy_cache
+
+#     @classmethod
+#     def from_legacy_cache(cls, past_key_values: Optional[Tuple[Tuple[paddle.Tensor]]] = None) -> "DynamicCache":
+#         """Converts a cache in the legacy cache format into an equivalent `DynamicCache`."""
+#         cache = cls()
+#         if past_key_values is not None:
+#             for layer_idx in range(len(past_key_values)):
+#                 key_states, value_states = past_key_values[layer_idx]
+#                 cache.update(key_states, value_states, layer_idx)
+#         return cache
 class DynamicCache(Cache):
     """
     A cache that grows dynamically as more tokens are generated. This is the default for generative models.
 
     It stores the Key and Value states as a list of tensors, one for each layer. The expected shape for each tensor is
     `[batch_size, num_heads, seq_len, head_dim]`.
+
+    Example:
+
+        ```python
+        >>> from transformers import AutoTokenizer, AutoModelForCausalLM, DynamicCache
+
+        >>> model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
+        >>> tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
+
+        >>> inputs = tokenizer(text="My name is Qwen2", return_tensors="pt")
+
+        >>> # Prepare a cache class and pass it to model's forward
+        >>> past_key_values = DynamicCache()
+        >>> outputs = model(**inputs, past_key_values=past_key_values, use_cache=True)
+        >>> outputs.past_key_values # access cache filled with key/values from generation
+        DynamicCache()
+        ```
     """
 
     def __init__(self) -> None:
+        super().__init__()
+        self._seen_tokens = 0  # Used in `generate` to keep tally of how many tokens the cache has seen
         self.key_cache: List[paddle.Tensor] = []
         self.value_cache: List[paddle.Tensor] = []
-        self.seen_tokens = 0  # Used in `generate` to keep tally of how many tokens the cache has seen
 
     def __getitem__(self, layer_idx: int) -> List[Tuple[paddle.Tensor]]:
         """
@@ -116,32 +287,46 @@ class DynamicCache(Cache):
         """
         # Update the number of seen tokens
         if layer_idx == 0:
-            self.seen_tokens += key_states.shape[-3]
+            self._seen_tokens += key_states.shape[-3]
 
         # Update the cache
-        if len(self.key_cache) <= layer_idx:
-            self.key_cache.append(key_states)
-            self.value_cache.append(value_states)
-        else:
-            self.key_cache[layer_idx] = paddle.concat([self.key_cache[layer_idx], key_states], axis=-3)
-            self.value_cache[layer_idx] = paddle.concat([self.value_cache[layer_idx], value_states], axis=-3)
+        if key_states is not None:
+            if len(self.key_cache) <= layer_idx:
+                # There may be skipped layers, fill them with empty lists
+                for _ in range(len(self.key_cache), layer_idx):
+                    self.key_cache.append([])
+                    self.value_cache.append([])
+                self.key_cache.append(key_states)
+                self.value_cache.append(value_states)
+            elif (
+                len(self.key_cache[layer_idx]) == 0
+            ):  # fills previously skipped layers; checking for tensor causes errors
+                self.key_cache[layer_idx] = key_states
+                self.value_cache[layer_idx] = value_states
+            else:
+                self.key_cache[layer_idx] = paddle.concat([self.key_cache[layer_idx], key_states], axis=-3)
+                self.value_cache[layer_idx] = paddle.concat([self.value_cache[layer_idx], value_states], axis=-3)
 
         return self.key_cache[layer_idx], self.value_cache[layer_idx]
 
     def get_seq_length(self, layer_idx: Optional[int] = 0) -> int:
         """Returns the sequence length of the cached states. A layer index can be optionally passed."""
-        if len(self.key_cache) <= layer_idx:
-            return 0
-        return self.key_cache[layer_idx].shape[-3]
+        # TODO: deprecate this function in favor of `cache_position`
+        is_empty_layer = (
+            len(self.key_cache) == 0  # no cache in any layer
+            or len(self.key_cache) <= layer_idx  # skipped `layer_idx` and hasn't run a layer with cache after it
+            or len(self.key_cache[layer_idx]) == 0  # the layer has no cache
+        )
+        layer_seq_length = self.key_cache[layer_idx].shape[-3] if not is_empty_layer else 0
+        return layer_seq_length
 
-    def reorder_cache(self, beam_idx: paddle.Tensor):
-        """Reorders the cache for beam search, given the selected beam indices."""
-        for layer_idx in range(len(self.key_cache)):
-            self.key_cache[layer_idx] = self.key_cache[layer_idx].index_select(beam_idx)
-            self.value_cache[layer_idx] = self.value_cache[layer_idx].index_select(beam_idx)
+    def get_max_cache_shape(self) -> Optional[int]:
+        """Returns the maximum sequence length of the cache object. DynamicCache does not have a maximum length."""
+        return None
 
     def to_legacy_cache(self) -> Tuple[Tuple[paddle.Tensor], Tuple[paddle.Tensor]]:
-        """Converts the `DynamicCache` instance into the its equivalent in the legacy cache format."""
+        """Converts the `DynamicCache` instance into the its equivalent in the legacy cache format. Used for
+        backward compatibility."""
         legacy_cache = ()
         for layer_idx in range(len(self)):
             legacy_cache += ((self.key_cache[layer_idx], self.value_cache[layer_idx]),)
@@ -149,10 +334,65 @@ class DynamicCache(Cache):
 
     @classmethod
     def from_legacy_cache(cls, past_key_values: Optional[Tuple[Tuple[paddle.Tensor]]] = None) -> "DynamicCache":
-        """Converts a cache in the legacy cache format into an equivalent `DynamicCache`."""
+        """Converts a cache in the legacy cache format into an equivalent `DynamicCache`. Used for
+        backward compatibility."""
         cache = cls()
         if past_key_values is not None:
             for layer_idx in range(len(past_key_values)):
                 key_states, value_states = past_key_values[layer_idx]
                 cache.update(key_states, value_states, layer_idx)
         return cache
+
+    def crop(self, max_length: int):
+        """Crop the past key values up to a new `max_length` in terms of tokens. `max_length` can also be
+        negative to remove `max_length` tokens. This is used in assisted decoding and contrastive search."""
+        # In case it is negative
+        if max_length < 0:
+            max_length = self.get_seq_length() - abs(max_length)
+
+        if self.get_seq_length() <= max_length:
+            return
+
+        self._seen_tokens = max_length
+        for idx in range(len(self.key_cache)):
+            if self.key_cache[idx] != []:
+                self.key_cache[idx] = self.key_cache[idx][..., :max_length, :]
+                self.value_cache[idx] = self.value_cache[idx][..., :max_length, :]
+
+    def batch_split(self, full_batch_size: int, split_size: int) -> List["DynamicCache"]:
+        """Split the current instance into a list of `DynamicCache` by the batch size. This will be used by
+        `_split_model_inputs()` in `generation.utils`"""
+        out = []
+        for i in range(0, full_batch_size, split_size):
+            current_split = DynamicCache()
+            current_split._seen_tokens = self._seen_tokens
+            current_split.key_cache = [tensor[i : i + split_size] for tensor in self.key_cache]
+            current_split.value_cache = [tensor[i : i + split_size] for tensor in self.value_cache]
+            out.append(current_split)
+        return out
+
+    @classmethod
+    def from_batch_splits(cls, splits: List["DynamicCache"]) -> "DynamicCache":
+        """This is the opposite of the above `batch_split()` method. This will be used by `stack_model_outputs` in
+        `generation.utils`"""
+        cache = cls()
+        for idx in range(len(splits[0])):
+            key_cache = [current.key_cache[idx] for current in splits if current.key_cache[idx] != []]
+            value_cache = [current.value_cache[idx] for current in splits if current.value_cache[idx] != []]
+            if key_cache != []:
+                layer_keys = paddle.concat(key_cache, axis=0)
+                layer_values = paddle.concat(value_cache, axis=0)
+                cache.update(layer_keys, layer_values, idx)
+        return cache
+
+    def batch_repeat_interleave(self, repeats: int):
+        """Repeat the cache `repeats` times in the batch dimension. Used in contrastive search."""
+        for layer_idx in range(len(self)):
+            self.key_cache[layer_idx] = self.key_cache[layer_idx].repeat_interleave(repeats, dim=0)
+            self.value_cache[layer_idx] = self.value_cache[layer_idx].repeat_interleave(repeats, dim=0)
+
+    def batch_select_indices(self, indices: paddle.Tensor):
+        """Only keep the `indices` in the batch dimension of the cache. Used in contrastive search."""
+        for layer_idx in range(len(self)):
+            self.key_cache[layer_idx] = self.key_cache[layer_idx][indices, ...]
+            self.value_cache[layer_idx] = self.value_cache[layer_idx][indices, ...]
