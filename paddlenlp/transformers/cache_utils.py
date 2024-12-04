84d07eb4ac1503447624578f93bbfb68a08f59ab
lvdongyi <lvdongyi1222@gmail.com>
2024-12-04 12:41:28 +0800

    update

--- paddlenlp/transformers/cache_utils.py
+++ paddlenlp/transformers/cache_utils.py
@@ -55,17 +55,6 @@ class Cache:
         # TODO: deprecate this function in favor of `cache_position`
         raise NotImplementedError("Make sure to implement `get_seq_length` in a subclass.")
 
-    # # Deprecate in favor of max-cache-shape because we want to be specifc by what we mean with "max_length"
-    # # Prev some cache objects didn't have "max_length" (SlidingWindowCache or SinkCache) because the cache object technically handles
-    # # infinite amount of tokens. In the codebase what we really need to check is the max capacity of certain cache instances, so
-    # # we change naming to be more explicit
-    # def get_max_length(self) -> Optional[int]:
-    #     logger.warning_once(
-    #         "`get_max_cache()` is deprecated for all Cache classes. Use `get_max_cache_shape()` instead. "
-    #         "Calling `get_max_cache()` will raise error from v4.48"
-    #     )
-    #     return self.get_max_cache_shape()
-
     def get_max_cache_shape(self) -> Optional[int]:
         """Returns the maximum sequence length (i.e. max capacity) of the cache object"""
         raise NotImplementedError("Make sure to implement `get_max_cache_shape` in a subclass.")
@@ -103,109 +92,6 @@ class Cache:
             return None
 
 
-# class DynamicCache(Cache):
-#     """
-#     A cache that grows dynamically as more tokens are generated. This is the default for generative models.
-
-#     It stores the Key and Value states as a list of tensors, one for each layer. The expected shape for each tensor is
-#     `[batch_size, num_heads, seq_len, head_dim]`.
-#     """
-
-#     def __init__(self) -> None:
-#         self.key_cache: List[paddle.Tensor] = []
-#         self.value_cache: List[paddle.Tensor] = []
-#         self.seen_tokens = 0  # Used in `generate` to keep tally of how many tokens the cache has seen
-
-#     def __getitem__(self, layer_idx: int) -> List[Tuple[paddle.Tensor]]:
-#         """
-#         Support for backwards-compatible `past_key_value` indexing, e.g. `past_key_value[0][0].shape[2]` to get the
-#         sequence length.
-#         """
-#         if layer_idx < len(self):
-#             return (self.key_cache[layer_idx], self.value_cache[layer_idx])
-#         else:
-#             raise KeyError(f"Cache only has {len(self)} layers, attempted to access layer with index {layer_idx}")
-
-#     def __iter__(self):
-#         """
-#         Support for backwards-compatible `past_key_value` iteration, e.g. `for x in past_key_value:` to iterate over
-#         keys and values
-#         """
-#         for layer_idx in range(len(self)):
-#             yield (self.key_cache[layer_idx], self.value_cache[layer_idx])
-
-#     def __len__(self):
-#         """
-#         Support for backwards-compatible `past_key_value` length, e.g. `len(past_key_value)`. This value corresponds
-#         to the number of layers in the model.
-#         """
-#         return len(self.key_cache)
-
-#     def update(
-#         self,
-#         key_states: paddle.Tensor,
-#         value_states: paddle.Tensor,
-#         layer_idx: int,
-#         cache_kwargs: Optional[Dict[str, Any]] = None,
-#     ) -> Tuple[paddle.Tensor, paddle.Tensor]:
-#         """
-#         Updates the cache with the new `key_states` and `value_states` for the layer `layer_idx`.
-
-#         Parameters:
-#             key_states (`paddle.Tensor`):
-#                 The new key states to cache.
-#             value_states (`paddle.Tensor`):
-#                 The new value states to cache.
-#             layer_idx (`int`):
-#                 The index of the layer to cache the states for.
-#             cache_kwargs (`Dict[str, Any]`, `optional`):
-#                 Additional arguments for the cache subclass. No additional arguments are used in `DynamicCache`.
-
-#         Return:
-#             A tuple containing the updated key and value states.
-#         """
-#         # Update the number of seen tokens
-#         if layer_idx == 0:
-#             self.seen_tokens += key_states.shape[-3]
-
-#         # Update the cache
-#         if len(self.key_cache) <= layer_idx:
-#             self.key_cache.append(key_states)
-#             self.value_cache.append(value_states)
-#         else:
-#             self.key_cache[layer_idx] = paddle.concat([self.key_cache[layer_idx], key_states], axis=-3)
-#             self.value_cache[layer_idx] = paddle.concat([self.value_cache[layer_idx], value_states], axis=-3)
-
-#         return self.key_cache[layer_idx], self.value_cache[layer_idx]
-
-#     def get_seq_length(self, layer_idx: Optional[int] = 0) -> int:
-#         """Returns the sequence length of the cached states. A layer index can be optionally passed."""
-#         if len(self.key_cache) <= layer_idx:
-#             return 0
-#         return self.key_cache[layer_idx].shape[-3]
-
-#     def reorder_cache(self, beam_idx: paddle.Tensor):
-#         """Reorders the cache for beam search, given the selected beam indices."""
-#         for layer_idx in range(len(self.key_cache)):
-#             self.key_cache[layer_idx] = self.key_cache[layer_idx].index_select(beam_idx)
-#             self.value_cache[layer_idx] = self.value_cache[layer_idx].index_select(beam_idx)
-
-#     def to_legacy_cache(self) -> Tuple[Tuple[paddle.Tensor], Tuple[paddle.Tensor]]:
-#         """Converts the `DynamicCache` instance into the its equivalent in the legacy cache format."""
-#         legacy_cache = ()
-#         for layer_idx in range(len(self)):
-#             legacy_cache += ((self.key_cache[layer_idx], self.value_cache[layer_idx]),)
-#         return legacy_cache
-
-#     @classmethod
-#     def from_legacy_cache(cls, past_key_values: Optional[Tuple[Tuple[paddle.Tensor]]] = None) -> "DynamicCache":
-#         """Converts a cache in the legacy cache format into an equivalent `DynamicCache`."""
-#         cache = cls()
-#         if past_key_values is not None:
-#             for layer_idx in range(len(past_key_values)):
-#                 key_states, value_states = past_key_values[layer_idx]
-#                 cache.update(key_states, value_states, layer_idx)
-#         return cache
 class DynamicCache(Cache):
     """
     A cache that grows dynamically as more tokens are generated. This is the default for generative models.
