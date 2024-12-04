4e5688e5d1b1b9109d3718113add7c0d3782c308
huxinye <2392038429@qq.com>
2024-12-04 16:49:47 +0800

    参数上传修改

--- llm/tools/run_model_weight_merge.py
+++ llm/tools/run_model_weight_merge.py
@@ -11,22 +11,66 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import argparse
+
 from paddlenlp.mergekit import MergeConfig, MergeModel
 
-merge_config = MergeConfig(
-    merge_type="della_linear",
-    linear_ratio=0.5,
-    n_process=2,
-    dtype="bfloat16",
-    device="cpu",
-    merge_preifx="model",
-)
-mergekit = MergeModel(merge_config)
-model_path0 = "/root/paddlejob/workspace/env_run/output/huxinye/model/cnn1"
-model_path1 = "/root/paddlejob/workspace/env_run/output/huxinye/model/cnn2"
-base_path = "/root/paddlejob/workspace/env_run/output/huxinye/model/cnn_base"
-output_path = "/root/paddlejob/workspace/env_run/output/huxinye/model/cnn_merge"
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name_or_path0", default=None, required=True, type=str, help="The directory of model.")
+    parser.add_argument("--model_name_or_path1", default=None, required=True, type=str, help="The directory of model.")
+    parser.add_argument(
+        "--model_name_or_path_base", default=None, required=True, type=str, help="The directory of model."
+    )
+    parser.add_argument("--output_path", default=None, type=str, required=True, help="The directory of saved model ")
+
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="gpu",
+        choices=["gpu", "npu", "cpu"],
+        help="Device for selecting for merging lora weights, currently only supports gpu/npu/cpu.",
+    )
+    parser.add_argument("--merge_type", default="linear", type=str, help="The type of merge strategy.")
+    parser.add_argument(
+        "--dot_threshold",
+        default=0.99,
+        type=float,
+        help="Threshold for considering the two vectors as colinear.(Used in slerp)",
+    )
+    parser.add_argument("--scaling", default=False, type=bool, help="Whether to scale the weights.")
+    parser.add_argument("--normalize", default=True, type=bool, help="Whether to normalize the weights.")
+    parser.add_argument("--drop_rate", default=0.7, type=float, help="Drop rate for the merge.")
+    parser.add_argument("--della_rate", default=0.2, type=float, help="Della rate for the merge.")
+    parser.add_argument(
+        "--tensor_type", default="np", type=str, help="Tensor type to use for the merge. Choose np or pd"
+    )
+    parser.add_argument("--n_process", default=1, type=int, help="Number of processes to use for the merge.")
+    parser.add_argument("--dtype", default="bfloat16", type=str, help="Data type to use for the merge.")
+    parser.add_argument("--linear_ratio", default=0.5, type=float, help="Linear merge ratio.")
+    parser.add_argument("--merge_preifx", default="model", type=str, help="Prefix name: model or master_weights")
+    return parser.parse_args()
 
 
-mergekit.merge_model(model_path0, model_path1, output_path, base_path)
-merge_config.save_pretrained(output_path)
+def merge_model():
+    args = parse_arguments()
+    merge_config = MergeConfig(
+        merge_type=args.merge_type,
+        linear_ratio=args.linear_ratio,
+        n_process=args.n_process,
+        dtype=args.dtype,
+        device=args.device,
+        merge_preifx=args.merge_preifx,
+    )
+    mergekit = MergeModel(merge_config)
+    model_path0 = args.model_name_or_path0
+    model_path1 = args.model_name_or_path1
+    base_path = args.model_name_or_path_base
+    output_path = args.output_path
+    mergekit.merge_model(model_path0, model_path1, output_path, base_path)
+    merge_config.save_pretrained(output_path)
+
+
+if __name__ == "__main__":
+    merge_model()
