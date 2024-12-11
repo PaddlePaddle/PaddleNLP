ce6cf36af5977c969c12256f12b1d17d56142447
yuanlehome <yuanlehome@163.com>
2024-12-11 17:02:51 +0800

    fix:

--- llm/predict/predictor.py
+++ llm/predict/predictor.py
@@ -185,7 +185,7 @@ class BasePredictor:
         self.model_config = AutoConfig.from_pretrained(config.model_name_or_path)
         self.config: PredictorArgument = config
         if tokenizer is None:
-            tokenizer = AutoTokenizer.from_pretrained(config.model_name_or_path, use_fast=True, padding_side="left")
+            tokenizer = AutoTokenizer.from_pretrained(config.model_name_or_path, padding_side="left")
 
         self.tokenizer = tokenizer
 
@@ -1293,10 +1293,7 @@ def create_predictor(
     predictor_args: PredictorArgument,
     model_args: ModelArgument,
 ):
-    tokenizer = AutoTokenizer.from_pretrained(
-        predictor_args.model_name_or_path,
-        use_fast=True,
-    )
+    tokenizer = AutoTokenizer.from_pretrained(predictor_args.model_name_or_path)
     # init chat_template for tokenizer
     llm_utils.init_chat_template(tokenizer, predictor_args.model_name_or_path, predictor_args.chat_template)
 
