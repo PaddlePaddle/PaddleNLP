d28aeae26bd66cad365d258024a6514b741c487e
liujie44 <liujie44@baidu.com>
2024-12-13 10:39:50 +0800

    update

--- scripts/distribute/ci_case_auto.sh
+++ scripts/distribute/ci_case_auto.sh
@@ -643,7 +643,7 @@ function llama_dy2st_auto_bs4_bf16_DP1-MP1-PP4-SD2-VPP3_split_bw() {
         echo "result: loss=$loss ips=$ips mem=$mem"
         loss_base=7.57775269 # record new data
         ips_base=5825.427
-        mem_base=-1
+        mem_base=25.562287092208862
         check_result $FUNCNAME ${loss_base} ${loss} ${ips_base} ${ips} ${mem_base} ${mem}
         echo "=========== $FUNCNAME run  end ==========="  
     fi
@@ -732,7 +732,9 @@ function llama_align_dygraph_dy2st_pir_auto_bs2_bf16_DP2-MP2-PP1-SP() {
         echo "result: to_static=$to_static loss=$loss ips=$ips mem=$mem"
         loss_base=9.16783295
         loss_md5_base=8ea72495fba4e1b9ba004b4431e27218
-        if [ $IS_A100 -ne 0 ];then
+        if [ $IS_A100 -ne 0 ] && [ $to_static -eq 0 ];then
+            loss_base=9.37966919
+        elif [ $IS_A100 -ne 0 ] && [ $to_static -eq 1 ];then
             loss_base=9.38012543
         fi
         ips_base=-1
@@ -922,7 +924,9 @@ function llama_align_dygraph_dy2st_pir_auto_bs2_bf16_DP2-MP2-PP2-SP() {
         echo "result: to_static=$to_static loss=$loss loss_md5=$loss_md5 ips=$ips mem=$mem"
         loss_base=9.25199432
         loss_md5_base=83531e98ee11cd271db175150ab254bb
-        if [ $IS_A100 -ne 0 ];then
+        if [ $IS_A100 -ne 0 ] && [ $to_static -eq 0 ];then
+            loss_base=9.44203949
+        elif [ $IS_A100 -ne 0 ] && [ $to_static -eq 1 ];then
             loss_base=9.44225311
         fi
         ips_base=-1
@@ -2788,6 +2792,7 @@ function before_hook_for_gpt() {
     else
         echo -e "\033[31m ---- Skip install requirements for GPT auto cases  \033[0m"
     fi
+    unset http_proxy && unset https_proxy
     if [[ ! $FLAGS_download_data =~ "gpt" ]];then
         echo -e "\033[31m ---- Download GPT data  \033[0m"
         rm -rf data
@@ -2831,6 +2836,7 @@ function before_hook_for_llama() {
     export no_proxy=bcebos.com
     python -m pip install -r $root_path/requirements.txt
     python -m pip install -r $root_path/requirements-dev.txt
+    unset http_proxy && unset https_proxy
     if [[ ! $FLAGS_download_data =~ "llama" ]];then
         echo -e "\033[31m ---- Download LLaMA data  \033[0m"
         rm -rf data
