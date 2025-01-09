d7be6f107ce533e82756d7b18e81cfb991a844ca
greycooker <526929599@qq.com>
2025-01-09 14:27:55 +0000

    fix loraga merge

--- paddlenlp/peft/lora/lora_model.py
+++ paddlenlp/peft/lora/lora_model.py
@@ -351,7 +351,7 @@ class LoRAModel(nn.Layer):
                         # Reinit base model
                         offset = init_loraA.cuda() @ init_loraB.cuda()
                         ori_weight = model_state_dict[base_name]
-                        model_state_dict[base_name].set_value(ori_weight - self.lora_config.scaling * offset)
+                        model_state_dict[base_name].set_value(ori_weight + self.lora_config.scaling * offset)
         del model_state_dict
         gc.collect()
         self.model.set_state_dict(state_dict)
@@ -684,7 +684,7 @@ class LoRAModel(nn.Layer):
                     if "lora_A" in name:
                         trainable_state_dict[name] = paddle.concat([weight, self.loraga_init_dict[name]], axis=1)
                     else:
-                        trainable_state_dict[name] = paddle.concat([weight, self.loraga_init_dict[name]], axis=0)
+                        trainable_state_dict[name] = paddle.concat([weight, -self.loraga_init_dict[name]], axis=0)
                 else:
                     trainable_state_dict[name] = weight
         return trainable_state_dict
