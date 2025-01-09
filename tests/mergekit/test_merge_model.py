7b80472a2cce1634e890a0222dae8b91ccf948f4
Mangodada <2392038429@qq.com>
2025-01-09 11:04:17 +0800

    gpu test

--- tests/mergekit/test_merge_model.py
+++ tests/mergekit/test_merge_model.py
@@ -20,6 +20,7 @@ from parameterized import parameterized
 
 from paddlenlp.mergekit import MergeConfig, MergeModel
 from paddlenlp.transformers import AutoModel
+from tests.testing_utils import require_gpu
 
 
 class TestMergeModel(unittest.TestCase):
@@ -77,6 +78,16 @@ class TestMergeModel(unittest.TestCase):
             )
             mergekit = MergeModel(merge_config)
             mergekit.merge_model()
+
+    @parameterized.expand([("slerp",), ("della",), ("dare_linear",), ("ties",)])
+    @require_gpu(2)
+    def test_merge_model_gpu(self, merge_method):
+        with TemporaryDirectory() as tempdir:
+            model = AutoModel.from_pretrained("__internal_testing__/tiny-random-bert", dtype="bfloat16")
+            pd_path = os.path.join(tempdir, "pd_model")
+            model.save_pretrained(pd_path)
+            safe_path = os.path.join(tempdir, "safe_model")
+            model.save_pretrained(safe_path, safe_serialization="safetensors")
 
             # test safetensor only with pd and gpu
             merge_config = MergeConfig(
