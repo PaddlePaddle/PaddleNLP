1e5433dcff2371cba2f596d6ff6f3f0b26968af1
chenxuyi <chenxuyi@baidu.com>
2025-02-14 19:29:18 +0800

    polish FC memory

--- paddlenlp/trainer/trainer.py
+++ paddlenlp/trainer/trainer.py
@@ -87,6 +87,8 @@ try:
     from ..quantization.quantization_linear import QuantizationLinear
 except:
     QuantizationLinear = None
+from paddlenlp.transformers.utils import device_guard
+
 from ..transformers.context_parallel_utils import split_inputs_sequence_dim_load_balance
 from ..transformers.model_utils import (
     PretrainedModel,
@@ -696,7 +698,8 @@ class Trainer:
         """
         assert isinstance(self.model, PretrainedModel), "model should be a PretrainedModel when using flash"
         logger.info("Create flash checkpoint manager...")
-        if isinstance(unwrapped_model, PipelineLayer):
+        if isinstance(self.model, PipelineLayer):
+            logger.info("FC using pp hook")
             pipeline_hooks_capacity = (
                 unwrapped_model.forward_pipeline_parallel_hook_capacity
                 + unwrapped_model.backward_pipeline_parallel_hook_capacity
@@ -717,6 +720,7 @@ class Trainer:
                     location=i, hook=self.flash_checkpoint_manager.flash_checkpoint_pipeline_hook
                 )
         else:
+            logger.info("FC using paddlenlp hook")
             pipeline_hooks_capacity = self.args.gradient_accumulation_steps
             self.flash_checkpoint_manager = FlashCheckpointManager(
                 worker_num=self.args.flash_workers_num,
@@ -732,8 +736,12 @@ class Trainer:
         if resume_from_checkpoint is not None:
             path = _add_variant(PADDLE_OPTIMIZER_NAME, self.args.optimizer_name_suffix)
             path = os.path.join(resume_from_checkpoint, path).replace("optimizer", "ema")
-            logger.info(f"FC EMA load from {path}")
-            self.flash_checkpoint_manager.set_ema_state_dict(path)
+            if os.path.exists(path):
+                logger.info(f"FC EMA load from {path}")
+                self.flash_checkpoint_manager.set_ema_state_dict(path)
+            else:
+                logger.info(f"FC EMA state dict not found, in: {path}")
+
         logger.info("Create flash checkpoint manager done.")
 
     def train(
@@ -1770,14 +1778,29 @@ class Trainer:
         if "gpu" not in paddle.device.get_device():
             logger.warning("offload/reload optimizer's states is only supported on GPU devices.")
             return
-
+        if self.optimizer.fusion_storage is not None:
+            # logger.info(f'fuse-opt: {self.optimizer.fusion_storage.buffer if self.optimizer.fusion_storage is not None else None}')
+            self.optimizer.fusion_storage.buffer = getattr(self.optimizer.fusion_storage.buffer, action)()
+            self.optimizer.fusion_storage.buffer_ipc_meta = None  # offload fusion_storage 之后就不用 IPCmeta 了
+            fusion_storage = self.optimizer.fusion_storage
+            if action == "pin_memory":
+                with device_guard("cpu"):
+                    logger.info(f"remap: {action}")
+                    fusion_storage.mapping_tensor()
+                    logger.info(f"remap: {action}, done")
+            elif action == "cuda":
+                logger.info(f"remap: {action}")
+                fusion_storage.mapping_tensor()
+                logger.info(f"remap: {action}, done")
+            else:
+                raise NotImplementedError(action)
+            return
         attributes = [
             ("_accumulators", "_moment1_acc_str"),
             ("_accumulators", "_moment2_acc_str"),
             ("_master_weights",),
             ("_accumulators_holder",),
         ]
-
         for attr in attributes:
             if all(hasattr(self.optimizer, a) for a in attr):
                 target_attr = getattr(self.optimizer, attr[0])
