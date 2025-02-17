175d7352a2706cd1224312a17aad19a1f023bc09
DrownFish19 <DrownFish19@gmail.com>
2025-02-17 15:33:20 +0800

    update deafult config

--- paddlenlp/transformers/deepseek_v2/configuration.py
+++ paddlenlp/transformers/deepseek_v2/configuration.py
@@ -137,7 +137,7 @@ class DeepseekV2Config(PretrainedConfig):
         intermediate_size=11008,
         moe_intermediate_size=1407,
         num_hidden_layers=30,
-        num_nextn_predict_layers=1,
+        num_nextn_predict_layers=0,
         num_nextn_predict_lambda=1.0,
         num_attention_heads=32,
         num_key_value_heads=32,
