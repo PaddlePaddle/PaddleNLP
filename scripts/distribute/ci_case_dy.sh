6e5ba658db172e08136b6359cab5b184101c3170
liujie44 <liujie44@baidu.com>
2025-02-18 16:10:55 +0800

    dist set +e

--- scripts/distribute/ci_case_dy.sh
+++ scripts/distribute/ci_case_dy.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set +e
 
 export log_path=/workspace/case_logs
 export root_path=/workspace/PaddleNLP
