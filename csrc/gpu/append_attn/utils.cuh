bbd0051f2b0e535f61e49614c376dc9100bd169e
yuanlehome <yuanlehome@163.com>
2025-02-18 19:43:49 +0800

    add pd_throw

--- csrc/gpu/append_attn/utils.cuh
+++ csrc/gpu/append_attn/utils.cuh
@@ -368,6 +368,9 @@ __forceinline__ __host__ __device__ void vec_cast<nv_bfloat16, float>(
   } else if (group_size == 8) {                              \
     constexpr size_t GROUP_SIZE = 8;                         \
     __VA_ARGS__                                              \
+  } else if (group_size == 12) {                             \
+    constexpr size_t GROUP_SIZE = 12;                        \
+    __VA_ARGS__                                              \
   } else if (group_size == 16) {                             \
     constexpr size_t GROUP_SIZE = 16;                        \
     __VA_ARGS__                                              \
