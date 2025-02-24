833be8a63c6f9751fb62776dc9578da325bf40ea
DrownFish19 <DrownFish19@gmail.com>
2025-02-24 10:52:55 +0800

    fix

--- paddlenlp/taskflow/task.py
+++ paddlenlp/taskflow/task.py
@@ -20,7 +20,6 @@ from abc import abstractmethod
 from multiprocessing import cpu_count
 
 import paddle
-from paddle.base.framework import use_pir_api
 from paddle.dataset.common import md5file
 
 from ..utils.env import (
@@ -398,10 +397,7 @@ class Task(metaclass=abc.ABCMeta):
             self._static_model_file = self._static_fp16_model_file
             self._static_params_file = self._static_fp16_params_file
         if self._predictor_type == "paddle-inference":
-            if use_pir_api():
-                self._config = paddle.inference.Config(self._static_json_file, self._static_params_file)
-            else:
-                self._config = paddle.inference.Config(self._static_model_file, self._static_params_file)
+            self._config = paddle.inference.Config(self._static_model_file, self._static_params_file)
             self._prepare_static_mode()
         else:
             self._prepare_onnx_mode()
