3eabbff507a66b707c0ca7ce15238bbeb1d5e217
vivienfanghua <vivienfanghua@163.com>
2025-03-03 17:25:04 +0800

    update

--- llm/benchmark/mmlu/run_mmlu.sh
+++ llm/benchmark/mmlu/run_mmlu.sh
@@ -1,5 +1,5 @@
 
-python predict/flash_server.py --model_name_or_path $MODEL_NAME --port 8010 --flash_port 8011
+python predict/flask_server.py --model_name_or_path $MODEL_NAME --port 8010 --flash_port 8011
 
 if [ ! -f data.tar ]; then
     wget https://people.eecs.berkeley.edu/~hendrycks/data.tar
