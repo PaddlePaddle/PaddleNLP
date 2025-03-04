22894d7ee0e7ce14b7d0dca66ba0c7ac84f7c101
l1cacheDell <d31409163@163.com>
2025-03-04 21:15:36 +0800

    modify license info

--- paddlenlp/experimental/transformers/sageattention.py
+++ paddlenlp/experimental/transformers/sageattention.py
@@ -1,7 +1,4 @@
-# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
-#
-# This file is inspired by the SageAttention project (https://github.com/thu-ml/SageAttention),
-# but has been re-implemented by PaddlePaddle.
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +11,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# This file is adapted from:
+#     https://github.com/thu-ml/SageAttention/blob/main/sageattention/core.py.
+# The original license is kept as-is:
+#
+# Copyright (c) 2024 by SageAttention team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import warnings
 from typing import Any, Optional
 
