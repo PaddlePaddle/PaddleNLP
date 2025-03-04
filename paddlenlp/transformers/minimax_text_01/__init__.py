4f18248118c5523c0fcc5c181e770cef50410b29
aooxin <aosiweixin@outlook.com>
2025-03-04 19:27:48 +0800

    add copyright.

--- paddlenlp/transformers/minimax_text_01/__init__.py
+++ paddlenlp/transformers/minimax_text_01/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+# Copyright 2025 MiniMax AI. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
