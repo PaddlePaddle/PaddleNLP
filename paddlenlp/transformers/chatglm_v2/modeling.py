9184ab9581f4d12e0c2fa35346ddc5951486518a
wangxueyang <xueyang.wang@aminer.cn>
2025-03-04 14:48:04 +0800

    Update glm

--- paddlenlp/transformers/chatglm_v2/modeling.py
+++ paddlenlp/transformers/chatglm_v2/modeling.py
@@ -105,7 +105,7 @@ def parallel_matmul(x: Tensor, y: Tensor, tensor_parallel_output):
 
 
 class RotaryEmbedding(nn.Layer):
-    def __init__(self, dim, original_impl=False):
+    def __init__(self, dim, rope_ratio=1, original_impl=False):
         super().__init__()
         self.default_dtype = paddle.get_default_dtype()
         inv_freq = 1.0 / (10000 ** (paddle.arange(0, dim, 2, dtype=self.default_dtype) / dim))
@@ -119,6 +119,7 @@ class RotaryEmbedding(nn.Layer):
         transformers/rope/__init__.py. MIT License:
         https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/license.
         """
+        base = base * self.rope_ratio
         # $\Theta = {\theta_i = 10000^{\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
         theta = 1.0 / (base ** (paddle.arange(0, n_elem, 2, dtype="float32") / n_elem))
 
@@ -1027,7 +1028,7 @@ class ChatGLMv2Model(ChatGLMv2PretrainedModel):
                 **config.long_sequence_init_args,
             )
         else:
-            self.rotary_pos_emb = RotaryEmbedding(rotary_dim // 2)
+            self.rotary_pos_emb = RotaryEmbedding(rotary_dim // 2,rope_ratio=config.rope_ratio, original_impl=config.original_rope)
         self.encoder = GLMTransformer(config)
         self.output_layer = Chatglmv2LMHead(config)
         self.apply(self.init_weights)
