de3ec29669aeceacdba7175251e6580db1b67c5b
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2025-03-04 13:22:35 +0000

    [LLM] 修改tag 版本

--- llm/server/server/scripts/start_server.sh
+++ llm/server/server/scripts/start_server.sh
@@ -25,10 +25,10 @@ export ENC_DEC_BLOCK_NUM=${ENC_DEC_BLOCK_NUM:-"4"}
 export MAX_PREFILL_BATCH=${MAX_PREFILL_BATCH:-"4"}
 export STOP_THRESHOLD=${STOP_THRESHOLD:-"0"}
 
-export tag="3.0.0.b"
+export tag=${tag:"3.0.0.b4"}
 export model_name=$1
 export MODEL_DIR=${MODEL_DIR:-"/models"}
-if [ ! "$model_name" == "" ]]; then
+if [ ! "$model_name" == "" ]; then
     export MODEL_DIR=${MODEL_DIR}/${model_name}
     mkdir -p $MODEL_DIR
 fi
