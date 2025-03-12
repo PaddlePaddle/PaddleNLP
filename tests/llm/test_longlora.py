504156ad4ac0a5fa73d78b5eb599ae7cf6f22500
micelvrice <songjx@bupt.edu.cn>
2025-03-12 08:37:33 +0800

    update tests

--- tests/llm/test_longlora.py
+++ tests/llm/test_longlora.py
@@ -64,7 +64,6 @@ class LoraTest(LLMTest, unittest.TestCase):
 
             main()
         replace_llama_attn(use_ssa=False)
-
         # merge weights
         merge_lora_weights_config = {
             "lora_path": lora_config["output_dir"],
