82dd714a800d4d10b52218b40f078bb8f5acb4ed
risemeup1 <515586620@qq.com>
2025-03-14 02:58:16 +0000

    fix

--- ops/csrc/fp8/deep_gemm/jit/runtime.py
+++ ops/csrc/fp8/deep_gemm/jit/runtime.py
@@ -20,6 +20,9 @@ import ctypes
 import os
 from typing import Optional
 
+import paddle
+from paddle import Tensor
+
 
 class Runtime:
     def __init__(self, path: str) -> None:
@@ -44,21 +47,49 @@ class Runtime:
         if self.lib is None:
             self.lib = ctypes.CDLL(os.path.join(self.path, "kernel.so"))
 
-        cargs = [
-            ctypes.c_void_p(args[0].data_ptr()),
-            ctypes.c_void_p(args[1].data_ptr()),
-            ctypes.c_void_p(args[2].data_ptr()),
-            ctypes.c_void_p(args[3].data_ptr()),
-            ctypes.c_void_p(args[4].data_ptr()),
-            ctypes.c_int(args[5]),
-            ctypes.c_void_p(args[6].cuda_stream),
-            ctypes.c_int(args[7]),
-            ctypes.c_int(args[8]),
-        ]
-
+        if len(args) == 9:
+            cargs = [
+                ctypes.c_void_p(args[0].data_ptr()),
+                ctypes.c_void_p(args[1].data_ptr()),
+                ctypes.c_void_p(args[2].data_ptr()),
+                ctypes.c_void_p(args[3].data_ptr()),
+                ctypes.c_void_p(args[4].data_ptr()),
+                ctypes.c_int(args[5]),
+                ctypes.c_void_p(args[6].cuda_stream),
+                ctypes.c_int(args[7]),
+                ctypes.c_int(args[8]),
+            ]
+        elif len(args) == 10:
+            cargs = [
+                ctypes.c_void_p(args[0].data_ptr()),
+                ctypes.c_void_p(args[1].data_ptr()),
+                ctypes.c_void_p(args[2].data_ptr()),
+                ctypes.c_void_p(args[3].data_ptr()),
+                ctypes.c_void_p(args[4].data_ptr()),
+                ctypes.c_void_p(args[5].data_ptr()),
+                ctypes.c_int(args[6]),
+                ctypes.c_void_p(args[7].cuda_stream),
+                ctypes.c_int(args[8]),
+                ctypes.c_int(args[9]),
+            ]
+        elif len(args) == 11:
+            cargs = [
+                ctypes.c_void_p(args[0].data_ptr()),
+                ctypes.c_void_p(args[1].data_ptr()),
+                ctypes.c_void_p(args[2].data_ptr()),
+                ctypes.c_void_p(args[3].data_ptr()),
+                ctypes.c_void_p(args[4].data_ptr()),
+                ctypes.c_void_p(args[5].data_ptr()),
+                ctypes.c_int(args[6]),
+                ctypes.c_int(args[7]),
+                ctypes.c_void_p(args[8].cuda_stream),
+                ctypes.c_int(args[9]),
+                ctypes.c_int(args[10]),
+            ]
+        else:
+            raise ValueError("Invalid number of arguments")
         return_code = ctypes.c_int(0)
         self.lib.launch(*cargs, ctypes.byref(return_code))
-        return return_code.value
 
 
 class RuntimeCache:
