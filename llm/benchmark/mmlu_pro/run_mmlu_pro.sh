a022ca08a45e9b85e36fd9af126f5591ac4248cf
root <root@gajl-flwvvc0i.bcc-gajl.baidu.com>
2025-03-14 07:32:01 +0000

    tmp

--- llm/benchmark/mmlu_pro/run_mmlu_pro.sh
+++ llm/benchmark/mmlu_pro/run_mmlu_pro.sh
@@ -1,1 +1,6 @@
-python3 evaluate_from_api.py --backend trtllm --ip $IP --port $PORT --output_dir ./eval_trtlllm
+# export http_proxy=http://agent.baidu.com:8891
+# export https_proxy=http://agent.baidu.com:8891
+# export no_proxy=localhost,bj.bcebos.com,su.bcebos.com,pypi.tuna.tsinghua.edu.cn,paddle-ci.gz.bcebos.com 
+IP=127.0.0.1
+PORT=9965
+python3 evaluate_from_api.py --backend paddle --ip $IP --port $PORT --output_dir ./eval_r1 --model_name deepseek-chat
