27087586c51301455d80e36a7c760c8822baf5b2
umiswing <umiswing@foxmail.com>
2025-03-16 11:34:35 +0800

    revert gpu sum in add loss for mtp

--- paddlenlp/transformers/deepseek_v2/modeling.py
+++ paddlenlp/transformers/deepseek_v2/modeling.py
@@ -2378,7 +2378,7 @@ class DeepseekV2PretrainingCriterion(nn.Layer):
                 masked_lm_labels_cur_depth = masked_lm_labels_ori[:, (depth + 1) : (depth + 1 + seq_length)]
                 res_cur_depth = compute_loss(prediction_scores_cur_depth, masked_lm_labels_cur_depth)
                 mtp_loss_res.append(res_cur_depth)
-            loss = add_loss(loss, self.config.num_nextn_predict_lambda * paddle.sum(paddle.concat(mtp_loss_res)) / len(mtp_loss_res))  # fmt: skip
+            loss = add_loss(loss, self.config.num_nextn_predict_lambda * sum([x for x in mtp_loss_res]) / len(mtp_loss_res))  # fmt: skip
 
         else:
             loss = compute_loss(prediction_scores, masked_lm_labels)
