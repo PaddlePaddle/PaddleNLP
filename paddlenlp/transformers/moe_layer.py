b1d48c8cbc30ea34060f178e65223398c68ade30
zhangbo9674 <zhangbo54@baidu.com>
2025-03-18 09:08:24 +0000

    close fuse_moe

--- paddlenlp/transformers/moe_layer.py
+++ paddlenlp/transformers/moe_layer.py
@@ -500,19 +500,19 @@ class MoEFlexTokenLayer(nn.Layer):
         # reshaped_input = hidden_states.reshape([-1, d_model])
         probs, routing_map, l_aux, l_zloss = self.router(hidden_states)
 
-        if DSV3_USE_FP8_GEMM:
-            output = FusionMoe.apply(hidden_states, probs, routing_map, self)
-        else:
-            (
-                dispatched_input,
-                token_permuted_indices,
-                prob_permuted_indices,
-                dispatched_probs,
-            ) = self.token_dispatcher.token_permutation(hidden_states, probs, routing_map)
-            expert_output = self.expert_forward(dispatched_input)
-            output, _ = self.token_dispatcher.token_unpermutation(
-                expert_output, token_permuted_indices, prob_permuted_indices, dispatched_probs, None
-            )
+        # if DSV3_USE_FP8_GEMM:
+        #     output = FusionMoe.apply(hidden_states, probs, routing_map, self)
+        # else:
+        (
+            dispatched_input,
+            token_permuted_indices,
+            prob_permuted_indices,
+            dispatched_probs,
+        ) = self.token_dispatcher.token_permutation(hidden_states, probs, routing_map)
+        expert_output = self.expert_forward(dispatched_input)
+        output, _ = self.token_dispatcher.token_unpermutation(
+            expert_output, token_permuted_indices, prob_permuted_indices, dispatched_probs, None
+        )
 
         return output, l_aux, l_zloss
 
