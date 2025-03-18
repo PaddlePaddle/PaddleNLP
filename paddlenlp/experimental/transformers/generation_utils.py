fb5e81e69a976be517b8ecf2365f557b9dac4983
chang-wenbin <changwenbin@baidu.com>
2025-03-18 21:53:55 +0800

    fix mix model save_output

--- paddlenlp/experimental/transformers/generation_utils.py
+++ paddlenlp/experimental/transformers/generation_utils.py
@@ -756,13 +756,14 @@ class GenerationBlockInferenceModel(GenerationMixin):
                     model_kwargs["next_tokens"],
                 )
 
-            from paddlenlp_ops import save_output
+            if self.config["vision_config"] is None:
+                from paddlenlp_ops import save_output
 
-            save_output(
-                next_tokens,
-                model_kwargs["not_need_stop"],
-                self.config.tensor_parallel_rank,
-            )
+                save_output(
+                    next_tokens,
+                    model_kwargs["not_need_stop"],
+                    self.config.tensor_parallel_rank,
+                )
             return next_tokens
 
         # encoder
