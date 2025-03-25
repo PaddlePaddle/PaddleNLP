867a4eb0bf4ba25880bfc1e553f54a63afafef2a
DrownFish19 <DrownFish19@gmail.com>
2025-03-25 10:58:38 +0800

    update readme

--- llm/application/distill/prepare_data.py
+++ llm/application/distill/prepare_data.py
@@ -28,6 +28,7 @@ dataset = load_dataset("PaddlePaddle/GSM8K_distilled_zh")
 dataset["train"].to_json("data/gsm8k_distilled_zh/GSM8K_distilled_zh-train.json", force_ascii=False)
 dataset["test"].to_json("data/gsm8k_distilled_zh/GSM8K_distilled_zh-test.json", force_ascii=False)
 
+
 # make data for sft
 def process_data_zh(example):
     src = example.get("question_zh", "")
@@ -36,12 +37,14 @@ def process_data_zh(example):
     tgt = reasoning_content + content
     return {"src": src, "tgt": tgt}
 
+
 def process_data_en(example):
     src = example.get("question", "")
     content = example.get("deepseek_r1_response", "")
     reasoning_content = example.get("deepseek_r1_reasoning", "")
     tgt = reasoning_content + content
     return {"src": src, "tgt": tgt}
+
 
 # construct Chinese sft dataset
 paddlenlp_datatset = deepcopy(dataset)
