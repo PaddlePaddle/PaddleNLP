aec0e1952fe6c74a0506ff44e4f95007cb4151f7
co63oc <co63oc@users.noreply.github.com>
2025-03-26 15:26:44 +0800

    Fix

--- tests/transformers/llama/test_modeling.py
+++ tests/transformers/llama/test_modeling.py
@@ -271,7 +271,7 @@ class LlamaModelTester:
 
     def create_and_check_gqa_model(self, config, input_ids, input_mask, *args):
         model = LlamaForCausalLM(config)
-        config.num_key_value_heads = 8 # gqa
+        config.num_key_value_heads = 8  # gqa
         config.use_fused_rope = True
         model.eval()
 
