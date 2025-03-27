20da7ae667cd15cb65959b53a44d1354bc3ec842
Pan Zhaowu <panzhaowu@baidu.com>
2025-03-27 17:41:27 +0800

    update

--- slm/model_zoo/gpt-3/external_ops/token_dispatcher_utils/tokens_unzip_and_zip.cu
+++ slm/model_zoo/gpt-3/external_ops/token_dispatcher_utils/tokens_unzip_and_zip.cu
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "utils.h"
+
 template <typename X_T,
           typename routemap_T,
           typename probs_T,
