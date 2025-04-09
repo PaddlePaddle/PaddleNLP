a98deed8f5e3752423cc1c0f169b63f4b3201323
DrownFish19 <DrownFish19@gmail.com>
2025-04-09 10:48:09 +0800

    lint

--- paddlenlp/transformers/llama/fusion_ops.py
+++ paddlenlp/transformers/llama/fusion_ops.py
@@ -306,9 +306,9 @@ def fusion_flash_attention(
             concat_axis=2,
         )
         # attn_output shape: [bs, seqlen/sep, num_head, head_dim]
-        assert config.sep_parallel_degree > 1 and q_len % config.sep_parallel_degree == 0, (
-            f"q_len:{q_len}, config.sep_parallel_degree:{config.sep_parallel_degree}"
-        )
+        assert (
+            config.sep_parallel_degree > 1 and q_len % config.sep_parallel_degree == 0
+        ), f"q_len:{q_len}, config.sep_parallel_degree:{config.sep_parallel_degree}"
         q_len = q_len // config.sep_parallel_degree
         num_heads = num_heads * config.sep_parallel_degree
 
