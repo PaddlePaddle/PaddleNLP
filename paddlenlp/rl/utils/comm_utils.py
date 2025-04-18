0b7e6f124bbda3f2d3c53bc76062f076bb6d87a0
yujun <573009727@qq.com>
2025-04-18 22:45:05 +0800

    use verl balance algo

--- paddlenlp/rl/utils/comm_utils.py
+++ paddlenlp/rl/utils/comm_utils.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import random
 import sys
 from collections import defaultdict
 from enum import Enum, auto
@@ -30,7 +29,146 @@ from .reshard_utils import init_reshard_mappings, init_rollout_env, reshard_to_r
 
 global_dev_id = 0 if paddle.get_device() == "cpu" else int(paddle.get_device().split(":")[1])
 
-rng = random.Random(42)
+import heapq
+from typing import List, Tuple
+
+
+def karmarkar_karp(seqlen_list: List[int], k_partitions: int, equal_size: bool):
+    # see: https://en.wikipedia.org/wiki/Largest_differencing_method
+    class Set:
+        def __init__(self) -> None:
+            self.sum = 0
+            self.items = []
+
+        def add(self, idx: int, val: int):
+            self.items.append((idx, val))
+            self.sum += val
+
+        def merge(self, other):
+            for idx, val in other.items:
+                self.items.append((idx, val))
+                self.sum += val
+
+        def __lt__(self, other):
+            if self.sum != other.sum:
+                return self.sum < other.sum
+            if len(self.items) != len(other.items):
+                return len(self.items) < len(other.items)
+            return self.items < other.items
+
+    class State:
+        def __init__(self, items: List[Tuple[int, int]], k: int) -> None:
+            self.k = k
+            # sets should always be decreasing order
+            self.sets = [Set() for _ in range(k)]
+            assert len(items) in [1, k], f"{len(items)} not in [1, {k}]"
+            for i, (idx, seqlen) in enumerate(items):
+                self.sets[i].add(idx=idx, val=seqlen)
+            self.sets = sorted(self.sets, reverse=True)
+
+        def get_partitions(self):
+            partitions = []
+            for i in range(len(self.sets)):
+                cur_partition = []
+                for idx, _ in self.sets[i].items:
+                    cur_partition.append(idx)
+                partitions.append(cur_partition)
+            return partitions
+
+        def merge(self, other):
+            for i in range(self.k):
+                self.sets[i].merge(other.sets[self.k - 1 - i])
+            self.sets = sorted(self.sets, reverse=True)
+
+        @property
+        def spread(self) -> int:
+            return self.sets[0].sum - self.sets[-1].sum
+
+        def __lt__(self, other):
+            # least heap, let the state with largest spread to be popped first,
+            # if the spread is the same, let the state who has the largest set
+            # to be popped first.
+            if self.spread != other.spread:
+                return self.spread > other.spread
+            return self.sets[0] > other.sets[0]
+
+        def __repr__(self) -> str:
+            repr_str = "["
+            for i in range(self.k):
+                if i > 0:
+                    repr_str += ","
+                repr_str += "{"
+                for j, (_, seqlen) in enumerate(self.sets[i].items):
+                    if j > 0:
+                        repr_str += ","
+                    repr_str += str(seqlen)
+                repr_str += "}"
+            repr_str += "]"
+            return repr_str
+
+    sorted_seqlen_list = sorted([(seqlen, i) for i, seqlen in enumerate(seqlen_list)])
+    states_pq = []
+    if equal_size:
+        assert len(seqlen_list) % k_partitions == 0, f"{len(seqlen_list)} % {k_partitions} != 0"
+        for offset in range(0, len(sorted_seqlen_list), k_partitions):
+            items = []
+            for i in range(k_partitions):
+                seqlen, idx = sorted_seqlen_list[offset + i]
+                items.append((idx, seqlen))
+            heapq.heappush(states_pq, State(items=items, k=k_partitions))
+    else:
+        for seqlen, idx in sorted_seqlen_list:
+            heapq.heappush(states_pq, State(items=[(idx, seqlen)], k=k_partitions))
+
+    while len(states_pq) > 1:
+        state0 = heapq.heappop(states_pq)
+        state1 = heapq.heappop(states_pq)
+        # merge states
+        state0.merge(state1)
+        heapq.heappush(states_pq, state0)
+
+    final_state = states_pq[0]
+    partitions = final_state.get_partitions()
+    if equal_size:
+        for i, partition in enumerate(partitions):
+            assert len(partition) * k_partitions == len(
+                seqlen_list
+            ), f"{len(partition)} * {k_partitions} != {len(seqlen_list)}"
+    return partitions
+
+
+def get_seqlen_balanced_partitions(seqlen_list: List[int], k_partitions: int, equal_size: bool):
+    """get order of seq lengths to make partitions balanced, this is
+        used in balacing sum of seqlength across dp ranks and microbatches
+    Parameters:
+        seqlen_list (List[int]):
+            seq lengths of each items
+        k_partitions (int):
+            resulting number of partitions
+        equal_size (bool):
+            if True, number of items in each partitions must be equal.
+            if False, only consider balancing the sum, each partition can have
+            variable number of items
+    Returns:
+        partitions (List[List[int]]):
+            return k_partitions list containing the index of items.
+    """
+    assert len(seqlen_list) >= k_partitions, f"number of items:[{len(seqlen_list)}] < k_partitions:[{k_partitions}]"
+
+    def _check_and_sort_partitions(partitions):
+        assert len(partitions) == k_partitions, f"{len(partitions)} != {k_partitions}"
+        seen_idx = set()
+        sorted_partitions = [None] * k_partitions
+        for i, partition in enumerate(partitions):
+            assert len(partition) > 0, f"the {i}-th partition is empty"
+            for idx in partition:
+                seen_idx.add(idx)
+            sorted_partitions[i] = sorted(partition)
+        assert seen_idx == set(range(len(seqlen_list)))
+        return sorted_partitions
+
+    partitions = karmarkar_karp(seqlen_list=seqlen_list, k_partitions=k_partitions, equal_size=equal_size)
+    return _check_and_sort_partitions(partitions)
 
 
 class ActorStages(Enum):
@@ -783,7 +921,7 @@ def split_batch_by_rank(
     sharding_rank,
     dp_degree,
     sharding_degree,
-    num_return_sequences,
+    num_return_sequences=None,  # do not use this
     balance_batch_across_dp_group=False,
 ):
     """
@@ -811,36 +949,15 @@ def split_batch_by_rank(
             end = start + chunk_size
             total_batch[key] = total_batch[key][start:end]
     else:
-        num_prompt = total_batch["input_ids"].shape[0] // num_return_sequences
-        num_prompt_per_rank = num_prompt // dataset_world_size
-
         # Compute total valid tokens per prompt
-        valid_tokens = total_batch["prompt_len_without_pad"] + total_batch["response_len_without_pad"]
-        valid_tokens = paddle.to_tensor(
-            [valid_tokens[i * num_return_sequences : (i + 1) * num_return_sequences].sum() for i in range(num_prompt)]
-        )
-
-        # Sort prompts by valid token count
-        sorted_indices = paddle.argsort(valid_tokens)
-        grouped_shuffled_indices = []
-        for i in range(dataset_world_size):
-            start = i * num_prompt_per_rank
-            end = (i + 1) * num_prompt_per_rank
-            group = sorted_indices[start:end].tolist()
-            rng.shuffle(group)
-            grouped_shuffled_indices.extend(group)
-
-        shuffled_indices = paddle.to_tensor(grouped_shuffled_indices, dtype="int32")
-
-        selected_queries = [shuffled_indices[i * dataset_world_size + global_rank] for i in range(num_prompt_per_rank)]
-
-        selected_indices = []
-        for query_index in selected_queries:
-            base = int(query_index) * num_return_sequences
-            selected_indices.extend(range(base, base + num_return_sequences))
-
+        valid_tokens_list = (total_batch["prompt_len_without_pad"] + total_batch["response_len_without_pad"]).tolist()
+        balanced_index = get_seqlen_balanced_partitions(
+            valid_tokens_list,
+            k_partitions=dataset_world_size,
+            equal_size=True,
+        )[global_rank]
         for key in total_batch.keys():
-            total_batch[key] = total_batch[key][selected_indices]
+            total_batch[key] = total_batch[key][balanced_index]
 
     return total_batch
 
