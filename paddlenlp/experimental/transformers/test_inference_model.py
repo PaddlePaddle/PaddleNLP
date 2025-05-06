7e66614a335d66a931ded52bc50e9d40637befc3
gaoziyuan <m13689897706@163.com>
2025-05-06 23:31:39 +0800

    add test

--- paddlenlp/experimental/transformers/test_inference_model.py
+++ paddlenlp/experimental/transformers/test_inference_model.py
@@ -16,17 +16,18 @@ predictor_args.quant_type = "weight_only_int8"
 inference_model = InferenceModel(predictor_args, model_args, load_model_from_ipc=True, cold_start=True)
 
 model = inference_model.get_model()
+print(model.get_name_mappings_to_training())
 
 # 获取inference model 的 key\shape\type
 inference_model.get_model_static_info()
 
 # 获取qwen2 训练和推理的key映射关系
-infer_to_train = inference_model.get_name_mappings_to_training()
+infer_to_train = model.get_name_mappings_to_training()
 
 print(infer_to_train)
 
-keys_all = model.state_dict().keys()
-for k, v in infer_to_train.items():
-    if k not in keys_all:
-        print("missing key is :", k)
-        print("missing v is :", v)
+# keys_all = model.state_dict().keys()
+# for k, v in infer_to_train.items():
+#     if k not in keys_all:
+#         print("missing key is :", k)
+#         print("missing v is :", v)
