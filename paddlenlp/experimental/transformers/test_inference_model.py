390ce039af81fca9d97b203695091486336ce6e6
gaoziyuan <m13689897706@163.com>
2025-05-06 21:48:05 +0800

    add test

--- paddlenlp/experimental/transformers/test_inference_model.py
+++ paddlenlp/experimental/transformers/test_inference_model.py
@@ -9,24 +9,21 @@ predictor_args = PredictorArgument()
 model_args = ModelArgument()
 
 predictor_args.model_name_or_path = "/root/paddlejob/workspace/env_run/output/gaoziyuan/paddlenllp_model/models/Qwen/Qwen2-7B"
-predictor_args.max_length = 2048
-predictor_args.dtype = "bfloat16"
-predictor_args.total_max_length = 4096
-predictor_args.inference_model = True
-predictor_args.mode = "dynamic"
-predictor_args.block_attn = True
 
-nranks = 1
-rank = 0
+# 如果需要
+predictor_args.quant_type = "weight_only_int8"
 
-inference_model = InferenceModel(predictor_args, model_args, nranks, rank, load_model_from_ipc=True)
+inference_model = InferenceModel(predictor_args, model_args, load_model_from_ipc=True, cold_start=True)
+
 model = inference_model.get_model()
 
 # 获取inference model 的 key\shape\type
 inference_model.get_model_static_info()
 
 # 获取qwen2 训练和推理的key映射关系
-train_to_infer, infer_to_train = inference_model.get_qwen2_train_infer_keys_map()
+infer_to_train = inference_model.get_name_mappings_to_training()
+
+print(infer_to_train)
 
 keys_all = model.state_dict().keys()
 for k, v in infer_to_train.items():
