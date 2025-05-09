1dadfd1d5e6ed0f27709b3e9f4913a906a33dfa4
blacksheep-Aristotle <zhangweilong01@baidu.com>
2025-05-09 14:19:46 +0800

    fix rng_state checkpoint error when tp8

--- paddlenlp/trainer/trainer_utils.py
+++ paddlenlp/trainer/trainer_utils.py
@@ -132,6 +132,7 @@ def _get_distributed_seeds(seed: int = 1234, topo: Topology = None):
         sharding_rank, _ = 0, 1
     # in mpu/random.py , local_seed = seed + 1 + mp_rank * pp_size + pp_rank , so here add a offset to avoid same with local_seed
     seed_offset = seed + 2 + mp_size * pp_size + pp_size
+
     global_seed = (
         seed_offset
         + sep_rank * (mp_size)
