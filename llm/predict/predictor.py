1ca3feaabdfa1bdafc2fb6fb8d1767dd8b6da3a3
ckl117 <ckl117@163.com>
2025-05-11 22:31:15 +0800

    fix input_text dy_insert

--- llm/predict/predictor.py
+++ llm/predict/predictor.py
@@ -1311,7 +1311,7 @@ class DygraphBlockInferencePredictor(BlockInferencePredictorMixin):
         self.model_inputs["stop_flags"][pos] = False
         self.model_inputs["result_id"][pos][0] = task_id
         self.model_inputs["step_idx"][pos, 0] = 1
-        self.model_inputs["pre_ids"][pos][0] = self.input_ids[query_id][-1]
+        self.model_inputs["pre_ids"][pos][0] = np.array(self.input_ids[query_id][-1])
         self.model_inputs["pre_ids"][pos][1:] = -1
         self.model_inputs["not_need_stop"][0] = True
 
