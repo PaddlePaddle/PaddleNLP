1c1afe09a5f738ad12ffc73ea983e93d69c070d7
l1cacheDell <d31409163@163.com>
2025-05-12 05:49:14 +0000

    boost compiling

--- csrc/setup_cuda.py
+++ csrc/setup_cuda.py
@@ -138,6 +138,7 @@ sources += find_end_files("./gpu/speculate_decoding_kernels", ".cu")
 
 nvcc_compile_args = gencode_flags
 update_git_submodule()
+os.environ.pop('PADDLE_CUDA_ARCH_LIST', None)
 nvcc_compile_args += [
     "-O3",
     "-DNDEBUG",
@@ -192,11 +193,8 @@ if cc == 89 and cuda_version >= 12.4:
     ]
 
 if cc >= 80 and nvcc_version >= Version("12.4"):
-    os.environ.pop('PADDLE_CUDA_ARCH_LIST', None)
     nvcc_compile_args += [
-        "-std=c++17",
         "--use_fast_math",
-        "--threads=8",
         "-D_GLIBCXX_USE_CXX11_ABI=1",
     ]
     sources += ["./gpu/sage_attn_kernels/sageattn_fused.cu"]
@@ -235,7 +233,7 @@ setup(
     ext_modules=CUDAExtension(
         sources=sources,
         extra_compile_args={
-            "cxx": ["-O3", "-fopenmp", "-lgomp", "-std=c++17", "-DENABLE_BF16"],
+            "cxx": ["-O3", "-fopenmp", "-lgomp", "-std=c++17", "-DENABLE_BF16", "--threads=8"],
             "nvcc": nvcc_compile_args,
         },
         libraries=["cublasLt"],
