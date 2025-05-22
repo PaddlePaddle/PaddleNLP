b997921166a3754daea2f0053dfa07c108c02b82
Zhaowu Pan <panzhaowu@baidu.com>
2025-05-22 10:46:03 +0800

    Update setup_fp8.py
    fix arch

--- slm/model_zoo/gpt-3/external_ops/setup_fp8.py
+++ slm/model_zoo/gpt-3/external_ops/setup_fp8.py
@@ -60,7 +60,7 @@ def setup_fused_quant_ops():
                     "-lineinfo",
                     "-DCUTLASS_DEBUG_TRACE_LEVEL=0",
                     "-maxrregcount=50",
-                    "-gencode=arch=compute_80,code=sm_80",
+                    "-gencode=arch=compute_90a,code=sm_90a",
                     "-DNDEBUG",
                 ]
             },
