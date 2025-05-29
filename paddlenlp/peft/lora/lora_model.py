5d94e77b0892282fdcc91b4be9ffe4f661797fc5
She Yifei <bupt.1415926@bupt.edu.cn>
2025-05-29 13:50:24 +0800

    'nola提交'

--- paddlenlp/peft/lora/lora_model.py
+++ paddlenlp/peft/lora/lora_model.py
@@ -482,13 +482,14 @@ class LoRAModel(nn.Layer):
                 rslora=lora_config.rslora,
                 lora_plus_scale=lora_config.lora_plus_scale,
                 pissa=lora_config.pissa,
+                nola=lora_config.nola,
+                nola_basis_num=lora_config.nola_basis_num,
                 bias_attr=False if module.bias is None else None,
                 use_quick_lora=lora_config.use_quick_lora,
                 lora_use_mixer=lora_config.lora_use_mixer,
                 use_mora=lora_config.use_mora,
                 mp_moe=getattr(module.weight, "mp_moe", False),
                 is_distributed=getattr(module.weight, "is_distributed", False),
-                lorapro=lora_config.lorapro,
             )
         elif isinstance(module, nn.Conv2D):
             lora_module = LoRAConv2D(
@@ -520,6 +521,8 @@ class LoRAModel(nn.Layer):
                 rslora=lora_config.rslora,
                 lora_plus_scale=lora_config.lora_plus_scale,
                 pissa=lora_config.pissa,
+                nola=lora_config.nola,
+                nola_basis_num=lora_config.nola_basis_num,
                 lora_A_weight_attr=paddle.ParamAttr(
                     initializer=nn.initializer.KaimingUniform(negative_slope=math.sqrt(5), nonlinearity="leaky_relu")
                 ),
@@ -546,6 +549,8 @@ class LoRAModel(nn.Layer):
                 rslora=lora_config.rslora,
                 lora_plus_scale=lora_config.lora_plus_scale,
                 pissa=lora_config.pissa,
+                nola=lora_config.nola,
+                nola_basis_num=lora_config.nola_basis_num,
                 use_quick_lora=lora_config.use_quick_lora,
             )
             # Lora column parallel will spilt lora A matrix
--- paddlenlp/trl/model_config.py
+++ paddlenlp/trl/model_config.py
@@ -86,18 +86,8 @@ class ModelConfig:
     use_mora: bool = field(
         default=False, metadata={"help": "Whether to use MoRA: https://arxiv.org/pdf/2405.12130.pdf"}
     )
-    lorapro: bool = field(
-        default=False, metadata={"help": "Whether to use LoRA-Pro: https://arxiv.org/pdf/2407.18242"}
-    )
-    lorapro_x_mode: str = field(
-        default="zero",
-        metadata={"help": "X mode for AdamWLoRAPro optimizer (zero, sylvester, symmetry)."},
-    )
-    lorapro_scaling_factor: float = field(
-        default=2.0,
-        metadata={"help": "Scaling factor for AdamWLoRAPro optimizer."},
-    )
-
+    nola: bool = field(default=False, metadata={"help": "Whether to use Nola: https://arxiv.org/pdf/2310.02556"})
+    nola_basis_num: int = field(default=1, metadata={"help": "When use nola, the number of basis"})
     # vera related parameters
     vera: bool = field(default=False, metadata={"help": "Whether to use vera technique"})
     vera_rank: int = field(default=8, metadata={"help": "Vera attention dimension"})
@@ -107,7 +97,7 @@ class ModelConfig:
     lokr_path: str = field(
         default=None, metadata={"help": "Initialize lokr state dict and apply customized lokr config"}
     )
-    lokr_dim: int = field(default=8, metadata={"help": "Lora dimension in LoKr dimension for adapter matrix"})
+    lokr_dim: int = field(default=8, metadata={"help": "Lora dimention in LoKr dimension for adapter matrix"})
 
     # prefix tuning related parameters
     prefix_tuning: bool = field(default=False, metadata={"help": "Whether to use Prefix technique"})
