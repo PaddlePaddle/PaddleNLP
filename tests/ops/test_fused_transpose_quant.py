c17b58d76ee59868796717279a0c7d7353b2fafa
Liang Shuhao <liangshuhao@baidu.com>
2025-06-10 06:23:17 +0000

    Add forward unittest for fused_transpose_split_quant

---
+++ tests/ops/test_fused_transpose_quant.py
@@ -0,0 +1,65 @@
+import FusedQuantOps as FQO
+
+import paddle
+import numpy as np
+
+
+def fused_transpose_split_quant_ref(x, tokens_per_expert, pow_2_scales):
+    shape = x.shape
+    x = x.reshape([shape[0]//128, 128, shape[1]])
+    amax = x.astype('float32').abs().max(axis=1)
+
+    scale = paddle.where(amax == 0, 1.0, 448.0 / amax)
+    if pow_2_scales:
+        _, exp = paddle.frexp(scale)
+        scale = paddle.ldexp(paddle.to_tensor([1.0]), exp - 1)
+
+    out = x * scale.unsqueeze(1)
+    out = out.reshape(shape).astype('float8_e4m3fn')
+    out = out.transpose([1, 0]).split(tokens_per_expert, axis=1)
+
+    scale = paddle.reciprocal(scale)
+    scale = scale.split([t // 128 for t in tokens_per_expert], axis=0)
+    return out, scale
+
+
+def test_fused_transpose_split_quant(tokens_per_expert, seq_len, pow_2_scales):
+    print(tokens_per_expert, seq_len, pow_2_scales)
+
+    x = paddle.randn([sum(tokens_per_expert), seq_len], dtype='bfloat16')
+    x = paddle.clip(x, min=-50, max=50)
+
+    out, scale = [], []
+    for tokens in tokens_per_expert:
+        out.append(paddle.empty([seq_len, tokens], dtype='float8_e4m3fn'))
+        scale.append(paddle.empty([tokens//128, seq_len], dtype='float32'))
+    FQO.fused_transpose_split_quant(x, out, scale, pow_2_scales)
+
+    out_ref, scale_ref = fused_transpose_split_quant_ref(x, tokens_per_expert, pow_2_scales)
+
+    for t, t_ref in zip(out, out_ref):
+        np.testing.assert_allclose(t.astype('float32'), t_ref.astype('float32'))
+
+    for t, t_ref in zip(scale, scale_ref):
+        np.testing.assert_allclose(t, t_ref)
+
+
+def run():
+    test_fused_transpose_split_quant([0, 0], 1024, False)
+    test_fused_transpose_split_quant([128, 2*128], 0, True)
+    test_fused_transpose_split_quant([128], 1, False)
+    test_fused_transpose_split_quant([0, 128, 0, 2*128], 127, True)
+    test_fused_transpose_split_quant([3*128, 4*128, 5*128], 233, False)
+    test_fused_transpose_split_quant(
+        [24*128, 128, 50*128, 16*128], 2162, True
+    )
+    test_fused_transpose_split_quant(
+        [7*128, 29*128, 3*128, 128*128, 13*128], 4000, False
+    )
+    test_fused_transpose_split_quant(
+        [18*128, 5*128, 24*128, 128, 6*128, 0, 27*128, 7*128], 7168, True
+    )
+
+
+if __name__ == '__main__':
+    run()
