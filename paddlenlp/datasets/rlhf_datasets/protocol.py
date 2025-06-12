1f8e5259ef9d81223108cea8e502272559fc4a86
WanpengXu <xwp@cumt.edu.cn>
2025-06-12 17:08:18 +0800

    Update DataProto_ver250612, add copyright

--- paddlenlp/datasets/rlhf_datasets/protocol.py
+++ paddlenlp/datasets/rlhf_datasets/protocol.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
