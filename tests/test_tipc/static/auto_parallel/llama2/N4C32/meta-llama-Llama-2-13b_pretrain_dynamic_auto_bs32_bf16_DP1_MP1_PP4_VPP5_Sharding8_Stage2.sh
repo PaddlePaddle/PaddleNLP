4321ed13ba3b9f323f00c9b4509461c3fd727a56
xuexixi <xuexixi@baidu.com>
2025-06-19 16:42:25 +0800

    update new interface

--- tests/test_tipc/static/auto_parallel/llama2/N4C32/meta-llama-Llama-2-13b_pretrain_dynamic_auto_bs32_bf16_DP1_MP1_PP4_VPP5_Sharding8_Stage2.sh
+++ tests/test_tipc/static/auto_parallel/llama2/N4C32/meta-llama-Llama-2-13b_pretrain_dynamic_auto_bs32_bf16_DP1_MP1_PP4_VPP5_Sharding8_Stage2.sh
@@ -20,9 +20,12 @@ param+="nnodes=4 "
 param+="model_type=llama2_13b "
 param+='dynamic_auto=_dynamic_auto '
 
+# This optimization currently only runs in the dynamic automatic parallelism of Llama7B.
 export FLAGS_fuse_reducescatter_in_opt=1
-export FLAGS_enable_sharding_overlap=1
+
+# Enable tensor fusion and sharding overlap optimization
 export FLAGS_enable_tensor_fusion=1
+export FLAGS_enable_sharding_overlap=1
 
 cd ./tests
 bash ./test_tipc/static/auto_parallel/llama2/benchmark_common/prepare.sh
