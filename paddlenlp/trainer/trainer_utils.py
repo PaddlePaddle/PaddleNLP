0e33abbe4c77e8ee79e615664584d4ad8f319954
ZhenxingLi <lizhenxing02@baidu.com>
2025-06-23 14:18:00 +0800

    Update trainer_utils.py

--- paddlenlp/trainer/trainer_utils.py
+++ paddlenlp/trainer/trainer_utils.py
@@ -43,6 +43,7 @@ from paddle.optimizer.lr import LambdaDecay
 from paddlenlp.ops import Topology
 
 from ..trainer.argparser import strtobool
+from ..transformers import get_gpt_pp_schedule
 from ..transformers.tokenizer_utils_base import BatchEncoding
 from ..utils.env import PREFIX_CHECKPOINT_DIR, _re_checkpoint  # noqa for compatibility
 from ..utils.fault_tolerance import PDC_DOWNLOAD_ERROR
@@ -1253,3 +1254,13 @@ def download_recovery_ckpt_from_pdc(recovery_checkpoint_path, timeout):
         raise RuntimeError(
             f"{PDC_DOWNLOAD_ERROR}; Error occurred when trying to download checkpoint from PDC, recovery_checkpoint_path: {recovery_checkpoint_path}, timeout: {timeout}; error details: {PDCErrorMessageMap[result]}"
         )
+
+def check_auto_parallel_pipeline_support(model_type=None):
+    support_types = ["gpt_pp"]
+    return model_type in support_types
+
+
+def get_pp_schedule(model, model_type, n_microbatches, loss_fn, mode, pp_degree, group):
+    assert check_auto_parallel_pipeline_support(model_type)
+    if model_type == "gpt_pp":
+        return get_gpt_pp_schedule(model, n_microbatches, loss_fn, mode, pp_degree, group)
