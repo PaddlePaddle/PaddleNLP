35eda7fe15f61f49078a6f9afe89ac6dc515cea3
liujie44 <liujie44@baidu.com>
2025-06-26 11:39:39 +0800

    update

--- scripts/distribute/ci_case_auto.sh
+++ scripts/distribute/ci_case_auto.sh
@@ -306,7 +306,7 @@ function llama_dygraph_auto_bs4_bf16_SD2() {
                 elif [ "$flag" = "FLAGS_fuse_allreduce_in_opt" ]; then
                     loss_base=9.23502579
                 elif [ "$flag" = "FLAGS_fuse_reducescatter_in_opt" ]; then
-                    loss_base=9.23504791
+                    loss_base=9.23504105
                 elif [ "$flag" = "FLAGS_enable_tensor_fusion FLAGS_enable_sharding_overlap" ]; then
                     if [ $acc_step -eq 1 ]; then
                         loss_base=9.23504791
