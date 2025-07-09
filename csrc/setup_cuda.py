2a7c3a845ebc2666386082225069b609aa959a62
zhink <771809832@qq.com>
2025-07-09 11:30:48 +0800

    fix

--- csrc/setup_cuda.py
+++ csrc/setup_cuda.py
@@ -226,6 +226,8 @@ if cc >= 90 and cuda_version >= 12.0:
         "gpu/fp8_gemm_with_cutlass/fp8_fp8_half_block_gemm.cu",
         "gpu/fp8_gemm_with_cutlass/fp8_fp8_half_gemm_ptr_scale.cu",
     ]
+    sources += find_end_files("./gpu/mla_attn", ".cu")
+
     sources += [
         "gpu/sparse/sparse_compressor.cu",
         "gpu/sparse/sparse_mm.cu",
