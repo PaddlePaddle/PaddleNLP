7659e80ca5a1855d3f08e57b64c76d6bd075d309
yuzh0816 <2235943809@qq.com>
2025-07-10 13:30:55 +0800

    Fix #10826

--- paddlenlp/trainer/trainer.py
+++ paddlenlp/trainer/trainer.py
@@ -3459,7 +3459,8 @@ class Trainer:
         # Metrics!
         if self.compute_metrics is not None and all_preds is not None and all_labels is not None:
             # all_labels maybe is a tuple when prediction_steps output label_mask
-            batch_labels = all_labels[0] if isinstance(all_labels, (list, tuple)) else all_labels
+            # batch_labels = all_labels[0] if isinstance(all_labels, (list, tuple)) else all_labels
+            batch_labels = all_labels
             metrics = self.compute_metrics(EvalPrediction(predictions=all_preds, label_ids=batch_labels))
         else:
             metrics = {}
