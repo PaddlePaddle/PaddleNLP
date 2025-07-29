f2664bc30bf0007e00966d98dcd9dc4c28ca10f3
zhaody <95118789+dddd-d@users.noreply.github.com>
2025-07-29 15:06:29 +0800

    Update config_utils.py

--- paddlenlp/rl/utils/config_utils.py
+++ paddlenlp/rl/utils/config_utils.py
@@ -346,9 +346,7 @@ class TrainingArguments(TrainingArguments):
         self._post_init_parallel_degree()
 
         if self.global_mini_batch_size < 0:
-            self.global_mini_batch_size = (
-                self.global_batch_size * self.rollout_n * self.update_iters
-            ) // self.dataset_world_size
+            self.global_mini_batch_size = self.global_batch_size // self.dataset_world_size
 
         if (
             self.global_batch_size % self.dataset_world_size != 0
