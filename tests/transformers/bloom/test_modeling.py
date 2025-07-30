9955bf3c4da62a4dae8aa80e52160458714ab215
liujie44 <liujie44@baidu.com>
2025-07-30 11:27:57 +0800

    fix CI for Paddle#72862

--- tests/transformers/bloom/test_modeling.py
+++ tests/transformers/bloom/test_modeling.py
@@ -505,6 +505,10 @@ class BloomModelTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCase)
 
             with paddle.no_grad():
                 embeds_output = model(**inputs)
+            if isinstance(ids_output, tuple):
+                ids_output = ids_output[0]
+            if isinstance(embeds_output, tuple):
+                embeds_output = embeds_output[0]
 
             self.assertTrue(paddle.allclose(ids_output, embeds_output, rtol=1e-4, atol=1e-4))
 
