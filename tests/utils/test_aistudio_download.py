40fc08503574c1b45308c950b03769ce1bb6f0e2
liujie44 <liujie44@baidu.com>
2025-08-20 16:13:15 +0800

    skip aistudio down for connection

--- tests/utils/test_aistudio_download.py
+++ tests/utils/test_aistudio_download.py
@@ -13,12 +13,14 @@
 # limitations under the License.
 
 import unittest
+from ..testing_utils import skip_for_none_ce_case
 
 from paddlenlp.transformers.aistudio_utils import aistudio_download
 from paddlenlp.utils.download import resolve_file_path
 
 
 class TestAistudioDownload(unittest.TestCase):
+    @skip_for_none_ce_case
     def test_aistudio_download(self):
         # 设置测试数据
         repo_id = "PaddleNLP/DeepSeek-R1-Distill-Qwen-1.5B"
@@ -40,6 +42,7 @@ class TestAistudioDownload(unittest.TestCase):
         self.assertEqual(result, f"{local_dir}/{filename}")
 
     def test_aistudio_download_transformer(self):
+    @skip_for_none_ce_case
         # 设置测试数据
         repo_id = "PaddleNLP/DeepSeek-R1-Distill-Qwen-1.5B"
         filename = "model.safetensors"
