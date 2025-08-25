c73db15c3fbed185f1432ce994689a6ae215e191
xingmingyyj <zxm_3791@163.com>
2025-08-25 16:40:04 +0800

    re-impl init_optimizer

--- paddlenlp/trainer/trainer_utils.py
+++ paddlenlp/trainer/trainer_utils.py
@@ -28,7 +28,6 @@ import os
 import random
 import threading
 import time
-from collections import defaultdict
 from contextlib import contextmanager
 from enum import Enum
 from pathlib import Path
@@ -1396,31 +1395,38 @@ def init_optimizer(optimizer):
         def init_param_optimizer_states(param_iter):
             master_weights = {}
             state_dict = {}
-            for static_name, shape in param_iter:
-                master_weights[static_name] = paddle.zeros(shape, dtype="float32")
-                for moment in ("moment1_0", "moment2_0"):
-                    key = f"{static_name}_fp32_master_0_{moment}"
+            moments = ("moment1_0", "moment2_0")
+            betas = ("beta1_pow_acc_0", "beta2_pow_acc_0")
+            for static_name, shape, no_need_master_weights in param_iter:
+                if not no_need_master_weights:
+                    master_weights[static_name] = paddle.zeros(shape, dtype="float32")
+                    prefix = f"{static_name}_fp32_master_0_"
+                else:
+                    prefix = f"{static_name}_"
+
+                for moment in moments:
+                    key = f"{prefix}{moment}"
                     state_dict[key] = paddle.zeros(shape, dtype="float32")
-                for beta in ("beta1_pow_acc_0", "beta2_pow_acc_0"):
-                    key = f"{static_name}_fp32_master_0_{beta}"
+                for beta in betas:
+                    key = f"{prefix}{beta}"
                     state_dict[key] = paddle.zeros((1,), dtype="float32")
             return master_weights, state_dict
 
         def buffer_params():
             for buffer in optimizer._comm_buffer_list:
                 for param_name, grad_view in buffer._sharding_param_grad_view.items():
-                    numel = grad_view._param.numel().item()
                     param_begin = grad_view._param_begin
                     param_end = grad_view._param_end
-                    index = grad_view._index
-                    padding_begin = index + numel
-                    shape = (min(padding_begin, param_end) - param_begin,)
+                    shape = (param_end - param_begin,)
+                    no_need_master_weights = grad_view._param.dtype == paddle.float32
+
                     if shape[0] > 0:
-                        yield param_name, shape
+                        yield param_name, shape, no_need_master_weights
 
         master_weights, state_dict = init_param_optimizer_states(buffer_params())
         state_dict["master_weights"] = master_weights
         state_dict["LR_Scheduler"] = {"last_epoch": 1, "last_lr": 5e-06}
+
         optimizer.set_state_dict(state_dict)
         return
     optimizer._create_accumulators(
