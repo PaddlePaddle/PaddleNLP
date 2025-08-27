c7e65b92c8940d8247e55aa99d88005e695c31a9
gongenlei <gongenlei@baidu.com>
2025-08-27 16:08:14 +0800

    add more args to Qwen2ForCausalLM

--- paddlenlp/transformers/qwen2/modeling.py
+++ paddlenlp/transformers/qwen2/modeling.py
@@ -1609,6 +1609,7 @@ class Qwen2ForCausalLM(Qwen2PretrainedModel):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
         attn_mask_startend_row_indices=None,
+        **kwargs,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         r"""
         Args:
