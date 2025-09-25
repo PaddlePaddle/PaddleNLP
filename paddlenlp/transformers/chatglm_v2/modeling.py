a842392ab8aa0b265e12c9919ccc459fd23d9980
makubes <2416013822@qq.com>
2025-09-25 22:12:18 +0800

    fixbug: chatglm v2 remove useless memcpy

--- paddlenlp/transformers/chatglm_v2/modeling.py
+++ paddlenlp/transformers/chatglm_v2/modeling.py
@@ -414,6 +414,15 @@ class SelfAttention(nn.Layer):
             value_layer.shape[:2] + [self.num_attention_heads_per_partition, self.hidden_size_per_attention_head]
         )
 
+        B, S, G, D = key_layer.shape
+        key_layer = key_layer.unsqueeze(-2)
+        key_layer = key_layer.expand(B, S, G, multiplier, D)
+        key_layer = key_layer.reshape(B, S, self.num_attention_heads_per_partition, self.hidden_size_per_attention_head)
+        value_layer = value_layer.unsqueeze(-2)
+        value_layer = value_layer.expand(B, S, G, multiplier, D)
+        value_layer = value_layer.reshape(B, S, self.num_attention_heads_per_partition, self.hidden_size_per_attention_head)
+
+
         # ==================================
         # core attention computation
         # ==================================
