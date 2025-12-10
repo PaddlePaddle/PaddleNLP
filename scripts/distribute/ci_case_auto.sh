9d9784d1b201d892e7045fcf7b0337f9217e9f1c
baoqiwen <baoqiwen@baidu.com>
2025-12-10 21:46:26 +0800

    for test.

--- scripts/distribute/ci_case_auto.sh
+++ scripts/distribute/ci_case_auto.sh
@@ -14,6 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+
+################
+# just for test
+################
+
 set -e
 
 export log_path=/workspace/case_logs
